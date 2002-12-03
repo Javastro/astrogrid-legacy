9fc4a4ea517fdaf967eccae9266e4de3b5d43680
Martin Hill <Martin.Hill@astrogrid.org>
2002-12-03 17:27:09 +0000

    Bug fixes

--- astrogrid/tools/src/java/org/astrogrid/io/OutputStreamSplitter.java
+++ astrogrid/tools/src/java/org/astrogrid/io/OutputStreamSplitter.java
@@ -1,17 +1,10 @@
 /**
-
  * OutputStreamSplitter.java
-
  *
-
  * Allows you to 'plug in' to a stream and route the bytes being written to
-
  * other streams
-
  *
-
  * @author M Hill
-
  */
 
 
@@ -19,74 +12,58 @@
 package org.astrogrid.tools.io;
 
 
-
 import java.io.OutputStream;
 
 
-
 public class OutputStreamSplitter extends OutputStream
-
 {
 
    private OutputStream out1 = null;
-
    private OutputStream out2 = null;
 
-
+   public OutputStreamSplitter()
+   {
+   }
 
    public OutputStreamSplitter(OutputStream out)
-
    {
-
       out1 = out;
-
    }
-
-
 
    public OutputStreamSplitter(OutputStream out, OutputStream splitout)
-
    {
-
       out1 = out;
-
       out2 = splitout;
-
    }
 
-
-
-   public void addStream(OutputStream out)
-
+   public void addStream(OutputStream newOut)
    {
-
-      out1 = out;
-
-      out2 = new OutputStreamSplitter(out2, out);
-
+      if (out1 == null)
+      {
+         out1 = newOut;
+      }
+      else if (out2 == null)
+      {
+         out2 = newOut;
+      }
+      else
+      {
+         out2 = new OutputStreamSplitter(out2, newOut);
+      }
    }
-
 
 
    public void write(int b) throws java.io.IOException
-
    {
-
-      out1.write(b);
-
-      if (out2 != null)
-
+      if (out1 != null)
       {
-
-         out2.write(b);
-
+         out1.write(b);
       }
-
+      if (out2 != null)
+      {
+         out2.write(b);
+      }
    }
-
-
-
-
 
 }
 
