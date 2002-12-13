aa0586bc4ce5f450edf0b7a26a5fff739a75e915
Martin Hill <Martin.Hill@astrogrid.org>
2002-12-13 16:40:32 +0000

    Fixed strange double spacing in source

--- astrogrid/tools/src/java/org/astrogrid/votable/VotOutputStream.java
+++ astrogrid/tools/src/java/org/astrogrid/votable/VotOutputStream.java
@@ -1,92 +1,47 @@
 /*
-
-   VotOutputStream.java
-
-
+   $Id: VotOutputStream.java,v 1.2 2002/12/13 16:40:32 mch Exp $
 
    Date       Author      Changes
-
    $date$     M Hill      Created
 
-
-
    (c) Copyright...
-
 */
-
-
 
 package org.astrogrid.tools.votable;
 
-
-
 import java.io.*;
 
-
-
 /**
-
  * A class for outputting vot tags to a stream.  Allows only the correct
-
  * root tag.
-
  *
-
  * @version %I%
-
  * @author M Hill
-
  */
 
-
-
 import org.astrogrid.tools.ascii.AsciiOutputStream;
-
 import org.astrogrid.tools.xml.XmlOutputStream;
 
-
-
 public class VotOutputStream
-
 {
-
    XmlOutputStream out = null;
-
    //VotTag votTag = null;
 
-
-
    public VotOutputStream(OutputStream targetOut) throws IOException
-
    {
-
       super();
-
       this.out = new XmlOutputStream(targetOut);
-
    }
-
-
 
    public VotTag newTag() throws IOException
-
    {
-
       return new VotTag(out);
-
    }
-
-
 
    public void close() throws IOException
-
    {
-
       out.close();
-
    }
-
 }
-
 
 
