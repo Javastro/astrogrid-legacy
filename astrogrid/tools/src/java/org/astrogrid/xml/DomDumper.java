16f3a9b4a6f9fa3672b6f534198632115b3d5bb0
Kona Andrews <Kona.Andrews@astrogrid.org>
2002-12-18 12:35:28 +0000

    Changed dumpNode() to use Axis XMLUtils tooling for pretty-printing XML.

--- astrogrid/tools/src/java/org/astrogrid/xml/DomDumper.java
+++ astrogrid/tools/src/java/org/astrogrid/xml/DomDumper.java
@@ -10,12 +10,14 @@
 package org.astrogrid.tools.xml;
 
 import java.io.*;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.*;
 import javax.xml.transform.stream.*;
+import org.apache.axis.utils.XMLUtils;
 
 /**
  * Used to dump a DOM to file.  Mostly for debugging, etc.
@@ -26,6 +28,27 @@ import javax.xml.transform.stream.*;
 
 public class DomDumper
 {
+	/* Axis-based version */
+   public static void dumpNode(Node aNode, OutputStream out) throws IOException
+   {
+		if (aNode.getNodeType() == Node.DOCUMENT_NODE)
+		{
+			XMLUtils.DocumentToStream((Document)aNode, out);
+		}
+		else if (aNode.getNodeType() == Node.ELEMENT_NODE)
+		{
+			XMLUtils.ElementToStream((Element)aNode, out);
+		}
+		else 
+		{
+			throw new IOException(
+				"Error - expected Document or Element node to dump.");
+		}
+	}
+
+
+	/*
+	// ORIGINAL VERSION (REQUIRES JAVA 1.4)
    public static void dumpNode(Node aNode, OutputStream out) throws IOException
    {
       try
@@ -43,6 +66,6 @@ public class DomDumper
          throw new IOException(""+e);
       }
    }
-   
+	*/
 }
 
