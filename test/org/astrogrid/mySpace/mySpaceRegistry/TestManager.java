0bc9c0724e9db67fb2872d42fcba199990701abf
Keith Noddle <ktn@roe.ac.uk>
2003-05-05 10:56:51 +0000

    First JUnit code stubs

---
+++ test/org/astrogrid/mySpace/mySpaceRegistry/TestManager.java
@@ -0,0 +1,30 @@
+package test.org.astrogrid.mySpace.mySpaceRegistry;
+
+import junit.framework.*;
+import org.astrogrid.mySpace.mySpaceRegistry.Manager;
+
+/** JUnit TestCase. 
+ * @testfamily JUnit
+ * @testkind testcase
+ * @testsetup Default TestCase
+ * @testedclass org.astrogrid.mySpace.mySpaceRegistry.Manager*/
+public class TestManager extends TestCase {
+    /** Constructs a test case with the given name. */
+    public TestManager(String name) {
+        super(name);
+    }
+
+    /** Sets up the fixture, for example, open a network connection. This method is called before a test is executed. */
+    protected void setUp() {
+        // Write your code here
+    }
+
+    /** Tears down the fixture, for example, close a network connection. This method is called after a test is executed. */
+    protected void tearDown() {
+        // Write your code here
+    }
+
+    public void testProcessRequest() {
+        fail("not implemented");
+    }
+}
---
+++ test/org/astrogrid/mySpace/mySpaceRegistry/TestUserAccount.java
@@ -0,0 +1,26 @@
+package test.org.astrogrid.mySpace.mySpaceRegistry;
+
+import junit.framework.*;
+import org.astrogrid.mySpace.mySpaceRegistry.UserAccount;
+
+/** JUnit TestCase. 
+ * @testfamily JUnit
+ * @testkind testcase
+ * @testsetup Default TestCase
+ * @testedclass org.astrogrid.mySpace.mySpaceRegistry.UserAccount*/
+public class TestUserAccount extends TestCase {
+    /** Constructs a test case with the given name. */
+    public TestUserAccount(String name) {
+        super(name);
+    }
+
+    /** Sets up the fixture, for example, open a network connection. This method is called before a test is executed. */
+    protected void setUp() {
+        // Write your code here
+    }
+
+    /** Tears down the fixture, for example, close a network connection. This method is called after a test is executed. */
+    protected void tearDown() {
+        // Write your code here
+    }
+}
