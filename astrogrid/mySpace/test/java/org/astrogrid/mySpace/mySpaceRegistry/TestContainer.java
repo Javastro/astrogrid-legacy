526f2687b46a7a598329a16953cb910d79d64010
Catherine Qin <Catherine.Qin@astrogrid.org>
2003-05-29 17:01:17 +0000

    junit test cases

---
+++ astrogrid/mySpace/test/java/org/astrogrid/mySpace/mySpaceRegistry/TestContainer.java
@@ -0,0 +1,26 @@
+package test.org.astrogrid.mySpace.mySpaceRegistry;
+
+import junit.framework.*;
+import org.astrogrid.mySpace.mySpaceRegistry.Container;
+
+/** JUnit TestCase. 
+ * @testfamily JUnit
+ * @testkind testcase
+ * @testsetup Empty TestCase
+ * @testedclass org.astrogrid.mySpace.mySpaceRegistry.Container*/
+public class TestContainer extends TestCase {
+    /** Constructs a test case with the given name. */
+    public TestContainer(String name) {
+        super(name);
+    }
+
+    /** Sets up the fixture, for example, open a network connection. This method is called before a test is executed. */
+    protected void setUp() {
+        // Write your code here
+    }
+
+    /** Tears down the fixture, for example, close a network connection. This method is called after a test is executed. */
+    protected void tearDown() {
+        // Write your code here
+    }
+}
---
+++ astrogrid/mySpace/test/java/org/astrogrid/mySpace/mySpaceRegistry/TestRegistryManager.java
@@ -0,0 +1,26 @@
+package test.org.astrogrid.mySpace.mySpaceRegistry;
+
+import junit.framework.*;
+import org.astrogrid.mySpace.mySpaceRegistry.RegistryManager;
+
+/** JUnit TestCase. 
+ * @testfamily JUnit
+ * @testkind testcase
+ * @testsetup Empty TestCase
+ * @testedclass org.astrogrid.mySpace.mySpaceRegistry.RegistryManager*/
+public class TestRegistryManager extends TestCase {
+    /** Constructs a test case with the given name. */
+    public TestRegistryManager(String name) {
+        super(name);
+    }
+
+    /** Sets up the fixture, for example, open a network connection. This method is called before a test is executed. */
+    protected void setUp() {
+        // Write your code here
+    }
+
+    /** Tears down the fixture, for example, close a network connection. This method is called after a test is executed. */
+    protected void tearDown() {
+        // Write your code here
+    }
+}
