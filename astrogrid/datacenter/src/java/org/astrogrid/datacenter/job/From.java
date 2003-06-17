b72684709326e7abf2100874b1ae66a9311abe04
Phil Nicolson <Phil.Nicolson@astrogrid.org>
2003-06-17 07:25:19 +0000

    *** empty log message ***

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/job/From.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/job/From.java
@@ -21,7 +21,7 @@ import org.w3c.dom.* ;
  * </pre></blockquote>
  * <p>
  *
- * @author  Jeff Lusted
+ * @author  Jeff Lusted/Phil Nicolson
  * @version 1.0 27-May-2003
  * @see     org.astrogrid.datacenter.Query
  * @since   AstroGrid 1.2
