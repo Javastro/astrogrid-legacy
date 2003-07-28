f0fb6f28a0caa0dd4bd78a694f9620fb0f6ca7b8
Kevin Benson <kmb@mssl.ucl.ac.uk>
2003-07-28 08:06:04 +0000

    Got rid of references to AstPortalQuery for now since it is not used in the query.

--- astrogrid/portalB/src/java/org/astrogrid/portal/session/AstPortalSession.java
+++ astrogrid/portalB/src/java/org/astrogrid/portal/session/AstPortalSession.java
@@ -1,12 +1,15 @@
 /*
  *
  * <cvs:source>$Source: /Users/pharriso/Work/ag/repo/git/astrogrid-mirror/astrogrid/portalB/src/java/org/astrogrid/portal/session/Attic/AstPortalSession.java,v $</cvs:source>
- * <cvs:date>$Author: dave $</cvs:date>
- * <cvs:author>$Date: 2003/06/26 14:15:10 $</cvs:author>
- * <cvs:version>$Revision: 1.4 $</cvs:version>
+ * <cvs:date>$Author: KevinBenson $</cvs:date>
+ * <cvs:author>$Date: 2003/07/28 08:06:04 $</cvs:author>
+ * <cvs:version>$Revision: 1.5 $</cvs:version>
  *
  * <cvs:log>
  * $Log: AstPortalSession.java,v $
+ * Revision 1.5  2003/07/28 08:06:04  KevinBenson
+ * Got rid of references to AstPortalQuery for now since it is not used in the query.
+ *
  * Revision 1.4  2003/06/26 14:15:10  dave
  * Added explorer pages and actions to Cocoon
  *
@@ -32,7 +35,6 @@ import org.astrogrid.portal.base.AstPortalBase  ;
 import org.astrogrid.portal.base.AstPortalIdent ;
 
 import org.astrogrid.portal.user.AstPortalUser ;
-import org.astrogrid.portal.query.AstPortalQuery ;
 import org.astrogrid.portal.explorer.AstPortalView ;
 
 import org.astrogrid.portal.cocoon.explorer.ExplorerView ;
@@ -85,47 +87,6 @@ public class AstPortalSession
 	public AstPortalUser getUser()
 		{
 		return this.user ;
-		}
-
-	/**
-	 * Our list of queries for this session.
-	 *
-	 */
-	protected Map queries = new Hashtable() ;
-	
-	/**
-	 * Access to our list of current queries.
-	 *
-	 */
-	public Iterator getCurrentQueries()
-		{
-		return this.queries.values().iterator() ;
-		}
-
-	/**
-	 * Access to a specific query.
-	 *
-	 */
-	public AstPortalQuery getQuery(String ident)
-		{
-		if (null != ident)
-			{
-			return (AstPortalQuery) this.queries.get(ident) ;
-			}
-		else {
-			return null ;
-			}
-		}
-
-	/**
-	 * Create a new query.
-	 *
-	 */
-	public AstPortalQuery createQuery()
-		{
-		AstPortalQuery query = new AstPortalQuery() ;
-		this.queries.put(query.getIdent(), query) ;
-		return query ;
 		}
 
 //
