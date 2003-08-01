0822106663a2a66d9262a7403d931c5cec17bb71
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-08-01 09:03:56 +0000

    *** empty log message ***

---
+++ astrogrid/templateservice/src/java/org/astrogrid/templateservice/TemplateException.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)TemplateException.java   1.0
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+package org.astrogrid.templateservice ;
+
+import org.astrogrid.i18n.AstroGridMessage;
+
+
+public class TemplateException extends org.astrogrid.AstroGridException {
+	
+	public TemplateException( AstroGridMessage message ) { 
+		super( message ) ;
+	}
+
+	public TemplateException( AstroGridMessage message, Exception exception ) {
+		super( message, exception ) ;
+	}
+    
+}
