29922d2ee4db107390dc1cb5b3f0b318b5f2190d
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-08-04 09:46:51 +0000

    *** empty log message ***

---
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/delegate/datasetAgent/DatasetAgentDelegate.java
@@ -0,0 +1,56 @@
+package org.astrogrid.datacenter.delegate.datasetAgent;
+
+import java.net.URL ;
+import java.net.MalformedURLException ;
+import java.rmi.RemoteException ;
+import org.astrogrid.datacenter.delegate.DatacenterDelegateException ;
+
+/**
+ * The <code>DatasetAgentDelegate</code> class. 
+ *
+ * @author  Jeff Lusted
+ * @version 1.0 02-Aug-2003
+ * @since   AstroGrid 1.2
+ */
+public class DatasetAgentDelegate {
+    
+    private String 
+        targetEndPoint = null ;
+    private int
+        timeout = 60000 ;
+        
+    public DatasetAgentDelegate( String targetEndPoint ) {
+      this.targetEndPoint = targetEndPoint;
+    }
+    
+    public DatasetAgentDelegate( String targetEndPoint, int timeout ) {
+      this.targetEndPoint = targetEndPoint ;
+      this.timeout = timeout ;
+    }
+    
+    public void runQuery(String req) throws DatacenterDelegateException {
+        
+        DatasetAgentSoapBindingStub 
+            binding = null ;
+            
+        try {
+            binding = (DatasetAgentSoapBindingStub)
+                          new DatasetAgentServiceLocator().getDatasetAgent( new URL( targetEndPoint ) );                        
+            binding.setTimeout( timeout ) ;    
+            binding.runQuery(req);
+        }
+        catch( MalformedURLException mex ) {
+            throw new DatacenterDelegateException( mex ) ;
+        }
+        catch( RemoteException rex) {
+            throw new DatacenterDelegateException( rex ) ;            
+        }
+        catch( javax.xml.rpc.ServiceException sex ) {
+            throw new DatacenterDelegateException( sex ) ;    
+        }
+              
+        return ;
+  
+    } // end of runQuery()
+
+} // end of class DatasetAgentDelegate
---
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/delegate/datasetAgent/DatasetAgentService.java
@@ -0,0 +1,16 @@
+/**
+ * DatasetAgentService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package org.astrogrid.datacenter.delegate.datasetAgent;
+
+public interface DatasetAgentService extends javax.xml.rpc.Service {
+    public java.lang.String getDatasetAgentAddress();
+
+    public org.astrogrid.datacenter.delegate.datasetAgent.DatasetAgent getDatasetAgent() throws javax.xml.rpc.ServiceException;
+
+    public org.astrogrid.datacenter.delegate.datasetAgent.DatasetAgent getDatasetAgent(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
