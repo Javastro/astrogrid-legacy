e0fddfe83d1ce3c8bd97f0c03f0eac757cc7a232
Elizabeth Auden <Elizabeth.Auden@astrogrid.org>
2003-08-05 16:42:54 +0000

    Added by Elizabeth Auden, 5/08/03
    Added RegistryAdminService stubs to /devel

---
+++ astrogrid/registry/stubs/java/org/astrogrid/registry/stubs/RegistryAdminService.java
@@ -0,0 +1,12 @@
+/**
+ * RegistryAdminService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis WSDL2Java emitter.
+ */
+
+package org.astrogrid.registry.stubs;
+
+public interface RegistryAdminService extends java.rmi.Remote {
+    public java.lang.String adminQuery(java.lang.String in0) throws java.rmi.RemoteException;
+}
