cdcb2845a2bff6b04b5e4607ee9bfcff36f2dc83
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2003-08-21 12:23:00 +0000

    split Configuration into an interface and default implemeentation -- allows us to unit test without mucking about with on-disk config files.

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/config/Configuration.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/config/Configuration.java
@@ -1,46 +1,20 @@
-/* $Id: Configuration.java,v 1.1 2003/08/20 14:42:59 nw Exp $
- * Created on 19-Aug-2003
+/*$Id: Configuration.java,v 1.2 2003/08/21 12:23:00 nw Exp $
+ * Created on 20-Aug-2003
+ *
  * Copyright (C) AstroGrid. All rights reserved.
  *
  * This software is published under the terms of the AstroGrid 
  * Software License version 1.2, a copy of which has been included 
- * with this distribution in the LICENSE.txt file. 
- */
-package org.astrogrid.datacenter.config;
-import org.astrogrid.AstroGridException;
-/**
- * System configuration object.
- * <p>
- * A wrapper around a very ugly static configuration system. {@link DTC} which in turn builds upon the 
- * static {@link org.astrogrid.common.Configurator} class. Statics are generall a bad idea - they make a system less modular
- * by introducing hidden dependancies; and break locality of data -  any class may access the contents of a static class.
- * <p>
- *  The aim of this class is to hide it all - make sure only accessible though this configuration object.  
- * The mess we're hiding can then be cleaned up later.
- 
- * @author Noel Winstanley nw@jb.man.ac.uk 20-Aug-2003
+ * with this distribution in the LICENSE.txt file.  
  *
+**/
+package org.astrogrid.datacenter.config;
+
+/** Inteface to a system configuration object
+ * @author Noel Winstanley nw@jb.man.ac.uk 20-Aug-2003
+ * @see ConfigurationDefaultImpl
  */
-public class Configuration {
-
-	public Configuration() {
-		wrapped = DTC.getInstance();
-
-	}
-	
-	private final DTC wrapped;
-
-	/**
-	 * Verify the configuration properties have loaded correctly
-     * (and load these properties in the process)
-     * <p>FUTURE - rename to something more descriptive (e.g. load)
-     * @throws AstroGridException if the configuration cannot be loaded
-	 */
-	public void checkPropertiesLoaded() throws AstroGridException{
-		wrapped.checkPropertiesLoaded();
-		
-	}
-
+public interface Configuration {
     /**
     * Lookup a  property in the component's configuration.
     * 
@@ -49,18 +23,13 @@ public class Configuration {
     * @return the String value of the property, or the empty string if null
     * 
     * @see org.jconfig.jConfig */
-	public String getProperty(String key, String category) {
-		return wrapped.getProperty(key,category);
-	}
-	
-    /** generate a string representation of this object
-     * @return details of the filename this configuration was loaded from.
-     */
-	public String toString() {
-		return wrapped.getConfigFileName();
-	
-	}
+    public abstract String getProperty(String key, String category);
 }
-/*
- * Log
- */
+
+/* 
+$Log: Configuration.java,v $
+Revision 1.2  2003/08/21 12:23:00  nw
+split Configuration into an interface and default implemeentation
+-- allows us to unit test without mucking about with on-disk config files.
+ 
+*/
---
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/config/ConfigurationDefaultImpl.java
@@ -0,0 +1,66 @@
+/* $Id: ConfigurationDefaultImpl.java,v 1.1 2003/08/21 12:23:00 nw Exp $
+ * Created on 19-Aug-2003
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file. 
+ */
+package org.astrogrid.datacenter.config;
+import org.astrogrid.AstroGridException;
+/**
+ * Default implementation of a System configuration object.
+ * <p>
+ * A wrapper around a very ugly static configuration system. {@link DTC} which in turn builds upon the 
+ * static {@link org.astrogrid.common.Configurator} class. Statics are generall a bad idea - they make a system less modular
+ * by introducing hidden dependancies; and break locality of data -  any class may access the contents of a static class.
+ * <p>
+ *  The aim of this class is to hide it all - make sure only accessible though this configuration object.  
+ * The mess we're hiding can then be cleaned up later.
+ 
+ * @author Noel Winstanley nw@jb.man.ac.uk 20-Aug-2003
+ *
+ */
+public class ConfigurationDefaultImpl implements Configuration {
+
+	public ConfigurationDefaultImpl() {
+		wrapped = DTC.getInstance();
+
+	}
+	
+	private final DTC wrapped;
+
+	/**
+	 * Verify the configuration properties have loaded correctly
+     * (and load these properties in the process)
+     * <p>FUTURE - rename to something more descriptive (e.g. load)
+     * @throws AstroGridException if the configuration cannot be loaded
+	 */
+	public void checkPropertiesLoaded() throws AstroGridException{
+		wrapped.checkPropertiesLoaded();
+		
+	}
+
+    /**
+    * Lookup a  property in the component's configuration.
+    * 
+    * @param key - the property key within category
+    * @param category - the category within the configuration
+    * @return the String value of the property, or the empty string if null
+    * 
+    * @see org.jconfig.jConfig */
+	public String getProperty(String key, String category) {
+		return wrapped.getProperty(key,category);
+	}
+	
+    /** generate a string representation of this object
+     * @return details of the filename this configuration was loaded from.
+     */
+	public String toString() {
+		return wrapped.getConfigFileName();
+	
+	}
+}
+/*
+ * Log
+ */
