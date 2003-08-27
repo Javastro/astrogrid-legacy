cf0e2a278fab66f5f38a689be7081e058c4fd1c5
Martin Hill <Martin.Hill@astrogrid.org>
2003-08-27 17:34:29 +0000

    Comment etc changes after reading Maven report :-)

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/delegate/DatacenterStatusListener.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/delegate/DatacenterStatusListener.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DatacenterStatusListener.java,v 1.2 2003/08/25 22:52:11 mch Exp $
+ * $Id: DatacenterStatusListener.java,v 1.3 2003/08/27 17:34:29 mch Exp $
  *
  * (C) Copyright AstroGrid...
  */
@@ -8,7 +8,8 @@ package org.astrogrid.datacenter.delegate;
 
 
 /**
- * Classes that implement this interface, and register with the DatacenterDelegate,
+ * Classes that implement this interface, and register with the
+ * DatacenterDelegate,
  * will be informed when the status of the job changes, rather than having to
  * poll the service.
  *
@@ -19,16 +20,28 @@ package org.astrogrid.datacenter.delegate;
 
 public interface DatacenterStatusListener
 {
-   public final static String STARTING = "Starting";
-   public final static String RUNNING = "Running";
-   public final static String FINISHED = "Finished";
-   public final static String UNKNOWN = "Unknown";
+   /** Service instance is starting, ie initialising... */
+   public static final String STARTING = "Starting";
+   /** Service instance is running, ie query is being processed... */
+   public static final String RUNNING_QUERY = "Querying";
+   /** Service is processing results, eg converting to VOTable */
+   public static final String RUNNING_RESULTS = "Results";
+   /** Service instance has finished/closed/no longer available */
+   public static final String FINISHED = "Finished";
+   /** Service instance is unknown, or cannot provide state */
+   public static final String UNKNOWN = "Unknown";
 
+   /** Called by the datacenter delegate when it has been notified of a
+    * status change
+    */
    public void datacenterStatusChanged(String newStatus);
 }
 
 /*
 $Log: DatacenterStatusListener.java,v $
+Revision 1.3  2003/08/27 17:34:29  mch
+Comment etc changes after reading Maven report :-)
+
 Revision 1.2  2003/08/25 22:52:11  mch
 Combined code from DatasetAgentDelegate with DatacenterDelegate
 
