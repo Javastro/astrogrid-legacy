1bcd5b2183711e799e7eb2a5be994beca76a3b5b
Martin Hill <Martin.Hill@astrogrid.org>
2003-08-27 17:35:23 +0000

    Better reporting and comments (after reading Maven reports :-)

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/queriers/mysql/MySqlQuerier.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/queriers/mysql/MySqlQuerier.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MySqlQuerier.java,v 1.2 2003/08/27 11:21:46 mch Exp $
+ * $Id: MySqlQuerier.java,v 1.3 2003/08/27 17:35:23 mch Exp $
  *
  * (C) Copyright Astrogrid...
  */
@@ -11,6 +11,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.astrogrid.datacenter.queriers.DatabaseAccessException;
 import org.astrogrid.datacenter.queriers.DatabaseQuerier;
 import org.astrogrid.datacenter.queriers.QueryResults;
 import org.astrogrid.datacenter.queriers.sql.SqlResults;
@@ -24,61 +25,75 @@ import org.astrogrid.datacenter.query.Query;
 
 public class MySqlQuerier implements DatabaseQuerier
 {
-   Connection jdbcConnection = null;
+   /** the standard sql jdbc connection, opened when the instance is created */
+   private Connection jdbcConnection = null;
 
    /**
     * Constructor for test purposes really - assumes there is a mySQL
     * database server running on the localhost
     */
-   public MySqlQuerier() throws SQLException
+   public MySqlQuerier() throws DatabaseAccessException
    {
       this("jdbc:mysql://localhost/Catalogue");
    }
 
    /**
     * Constructor takes the address of the server in the form
-    * jdbc:subprotocol:subname
+    * jdbc:subprotocol:subname, eg:
+    * jdbc:mysql://localhost/Catalogue
     */
-   public MySqlQuerier(String url) throws SQLException
+   public MySqlQuerier(String url) throws DatabaseAccessException
    {
       try
       {
-         org.gjt.mm.mysql.Driver.class.newInstance();
+         //declaring like this ensures the compiler checks that the driver is available
+         //org.gjt.mm.mysql.Driver.class.newInstance();
 
          //or... which is not checked at compiletime but can be configured at runtime
-         //Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+         Class.forName("org.gjt.mm.mysql.Driver").newInstance();
+         
+         jdbcConnection = DriverManager.getConnection(url);
       }
       catch (IllegalAccessException e)
       {
-         //rethrow as SQLException - not quite right but easier to catch
-         throw new SQLException("JDBC Driver error: "+e.toString());
+         throw new DatabaseAccessException(e,"JDBC Driver error: " + e.toString());
       }
       catch (InstantiationException e)
       {
-         //rethrow as SQLException - not quite right but easier to catch
-         throw new SQLException("JDBC Driver error: "+e.toString());
+         throw new DatabaseAccessException(e, "JDBC Driver error: " + e.toString());
       }
-      //catch (ClassNotFoundException e) {}
+      catch (ClassNotFoundException e)
+      {
+         throw new DatabaseAccessException(e, "JDBC Driver error: " + e.toString());
+      }
+      catch (SQLException se)
+      {
+         throw new DatabaseAccessException(se,"Could not connect to MySQL server: " + se.toString());
+      }
 
-      jdbcConnection = DriverManager.getConnection(url);
    }
 
-   public QueryResults queryDatabase(Query query)
+   /**
+    * Synchronous call to the mysql database, submitting the given query
+    * in sql form and returning the results as an SqlResults wrapper around
+    * the SQL ResultSet.
+    */
+   public QueryResults queryDatabase(Query query) throws DatabaseAccessException
    {
+      String sql = null;
       try
       {
          Statement statement = jdbcConnection.createStatement();
-         statement.execute(query.toSQLString());
-
+         sql = query.toSQLString(); //store this so we can use it in case of exceptions
+         statement.execute(sql);
          ResultSet results = statement.getResultSet();
 
          return new SqlResults(results);
       }
       catch (SQLException e)
       {
-         e.printStackTrace(System.out);
+         throw new DatabaseAccessException(e, "Could not query database using '" + sql + "'");
       }
-      return null;
    }
 
 
