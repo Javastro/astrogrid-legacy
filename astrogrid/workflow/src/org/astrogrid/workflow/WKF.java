ff7cc47e87908b48e401826b5a1e9d4fbe16537e
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-08-27 09:22:47 +0000

    *** empty log message ***

---
+++ astrogrid/workflow/src/org/astrogrid/workflow/WKF.java
@@ -0,0 +1,94 @@
+/*
+ * @(#)WKF.java   1.0
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */
+package org.astrogrid.workflow;
+
+import org.astrogrid.Configurator ;
+
+/**
+ * The <code>WKF</code> class represents 
+ *
+ * @author  Jeff Lusted
+ * @version 1.0 08-Jul-2003
+ * @see     
+ * @see     
+ * @since   AstroGrid 1.2
+ */
+public class WKF extends org.astrogrid.Configurator {
+    
+    private static final String 
+    /** Three letter acronym for this subsystem within the overall AstroGrid system... 
+     *  "WKF" stands for Workflow  */  
+        SUBSYSTEM_ACRONYM = "WKF" ; 
+            
+    private static final String 
+    /** Configuration file for this component. */  
+        CONFIG_FILENAME = "ASTROGRID_workflowconfig.xml" ;
+	    
+	public static final String 
+	/** Workflow category within the component's configuration */  
+		WORKFLOW_CATEGORY = "WORKFLOW" ,	
+	/** Key within component's configuration identifying the template used for the top workflow. */  
+        WORKFLOW_XML_TEMPLATE = "TEMPLATE.WORKFLOW_XML" ;
+        
+    public static final String 
+    /** Sequence category within the component's configuration */  
+        SEQUENCE_CATEGORY = "SEQUENCE" ,    
+    /** Key within component's configuration identifying the template used for the sequence element. */  
+        SEQUENCE_XML_TEMPLATE = "TEMPLATE.SEQUENCE_XML" ;
+         
+    public static final String 
+    /** Flow category within the component's configuration */  
+        FLOW_CATEGORY = "FLOW" ,    
+    /** Key within component's configuration identifying the template used for the flow element. */  
+        FLOW_XML_TEMPLATE = "TEMPLATE.FLOW_XML" ;         
+    
+    public static final String 
+    /** MySpace category within the component's configuration */  
+        MYSPACE_CATEGORY = "MYSPACE" ,    
+    /** Key within component's configuration identifying the MySpace URL. */  
+        MYSPACE_URL = "URL" ;       
+    
+    
+    private static WKF
+        singletonWKF = new WKF() ;
+        
+        
+    private WKF(){
+        super() ;
+    }
+    
+    
+    public static WKF getInstance() {
+        return singletonWKF ;
+    }
+        
+    
+    
+    /**
+      *  
+      * Static getter for properties from the component's configuration.
+      * <p>
+      * 
+      * @param key - the property key within category
+      * @param category - the category within the configuration
+      * @return the String value of the property, or the empty string if null
+      * 
+      * @see org.jconfig.jConfig
+      **/       
+    public static String getProperty( String key, String category ) {
+        return Configurator.getProperty( SUBSYSTEM_ACRONYM, key, category ) ;
+    }
+ 
+    protected String getConfigFileName() { return CONFIG_FILENAME ; }    
+    protected String getSubsystemAcronym() { return SUBSYSTEM_ACRONYM ; }
+    
+	 
+} // end of class WKF
