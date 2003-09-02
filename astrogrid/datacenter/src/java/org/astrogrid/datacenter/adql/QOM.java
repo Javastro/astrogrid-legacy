0d77aace9f540a2a2608378ca128f47d65b122e4
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2003-09-02 14:44:55 +0000

    addedd interface to visitor pattern

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/adql/QOM.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/adql/QOM.java
@@ -1,4 +1,4 @@
-/*$Id: QOM.java,v 1.1 2003/08/28 15:27:54 nw Exp $
+/*$Id: QOM.java,v 1.2 2003/09/02 14:44:55 nw Exp $
  * Created on 28-Aug-2003
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -15,9 +15,12 @@ import java.io.Writer;
 
 import org.exolab.castor.xml.CastorException;
 import org.xml.sax.ContentHandler;
-
+import org.exolab.castor.mapping.*;
 /** interface that describes commonality between all classes in the  Query Object Model.
- * At present this is just the marshalling / unmarshalling to XML routines. - 
+ * <p>
+ * Defines the marshalling / unmarshalling to XML routines. - 
+ *  * <p>
+ * Also defines a dynamic DynamicVisitor pattern for the QOM. See {@link DynamicVisitor} for details 
  * @author Noel Winstanley nw@jb.man.ac.uk 28-Aug-2003
  *
  */
@@ -46,12 +49,47 @@ public interface QOM {
      */
     public void validate() throws CastorException;
     
+    /**
+     * Entry point for a dynamic visitor to process the object tree in a top down fashion (i.e. visit the
+     * parent before it's children)
+     * @param v the visitor object
+     * @throws Exception if an error occurs during traversal
+     * @todo could split the exceptions into those that may be thrown in traversal, and user exceptions.
+     * @see DynamicVisitor
+     */
+    public void acceptTopDown(DynamicVisitor v) throws Exception;
     
+    /**Entry point for a dynamic visitor to process the object tree in a bottom-up manner (i.e. visit the child
+     * nodes before their parents).
+     * 
+     * @param v visitor object
+     * @throws Exception if an error occurs during traversal.
+     * @see DynamicVisitor
+     */
+    public void acceptBottomUp(DynamicVisitor v) throws Exception;
+
+/**
+ * 
+ * @param v
+ * @throws Exception
+ */
+    public void callVisitor(DynamicVisitor v) throws Exception;
+
+   
+    /** compute the children elements of this element 
+     * @return array of child QOM objects (all non-null child elements of this element)
+     * @todo - is it possible add an intermediate caching of results, so reflection only needs to be done one.
+     *  
+     */
+    public QOM[] getChildren() throws MappingException; 
 }
 
 
 /* 
 $Log: QOM.java,v $
+Revision 1.2  2003/09/02 14:44:55  nw
+addedd interface to visitor pattern
+
 Revision 1.1  2003/08/28 15:27:54  nw
 added ADQL object model.
  
