503d849493d752f9c901b15dfdfb7a069ad9e2e0
Martin Hill <Martin.Hill@astrogrid.org>
2003-09-08 16:43:50 +0000

    Removed toInputStream() from QueryResults as we don't really know what it's for yet

--- astrogrid/datacenter/test/java/org/astrogrid/datacenter/queriers/sql/SqlQuerierTest.java
+++ astrogrid/datacenter/test/java/org/astrogrid/datacenter/queriers/sql/SqlQuerierTest.java
@@ -1,4 +1,4 @@
-/*$Id: SqlQuerierTest.java,v 1.3 2003/09/07 18:58:58 mch Exp $
+/*$Id: SqlQuerierTest.java,v 1.4 2003/09/08 16:43:50 mch Exp $
  * Created on 04-Sep-2003
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -97,12 +97,13 @@ public class SqlQuerierTest extends TestCase {
         QueryResults results = querier.queryDatabase(queryElement);
         assertNotNull(results);
 
-        InputStream voStream = results.getInputStream();
-        assertNotNull(voStream);
-        Document resultDoc = XMLUtils.newDocument(voStream);
-        assertNotNull(resultDoc);
+        Document voElement = results.toVotable();
+//        InputStream voStream = results.getInputStream();
+//        assertNotNull(voStream);
+//        Document resultDoc = XMLUtils.newDocument(voStream);
+//        assertNotNull(resultDoc);
 
-        Element voElement = resultDoc.getDocumentElement();
+//        Element voElement = resultDoc.getDocumentElement();
         assertNotNull(voElement);
         assertEquals("VOTABLE",voElement.getLocalName());
         // could add extra checking to compare with expected results here..
@@ -113,6 +114,9 @@ public class SqlQuerierTest extends TestCase {
 
 /*
 $Log: SqlQuerierTest.java,v $
+Revision 1.4  2003/09/08 16:43:50  mch
+Removed toInputStream() from QueryResults as we don't really know what it's for yet
+
 Revision 1.3  2003/09/07 18:58:58  mch
 Updated tests for weekends changes to main code (mostly threaded queries, typesafe ServiceStatus)
 
