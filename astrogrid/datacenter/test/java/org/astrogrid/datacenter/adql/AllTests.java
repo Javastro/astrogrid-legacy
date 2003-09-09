26cb3c2f14b00a244555ac72f8e2fd49db686ad6
Martin Hill <Martin.Hill@astrogrid.org>
2003-09-09 17:57:14 +0000

    Fix wasn't running tests

--- astrogrid/datacenter/test/java/org/astrogrid/datacenter/adql/AllTests.java
+++ astrogrid/datacenter/test/java/org/astrogrid/datacenter/adql/AllTests.java
@@ -1,11 +1,11 @@
-/*$Id: AllTests.java,v 1.4 2003/09/03 14:45:59 nw Exp $
+/*$Id: AllTests.java,v 1.5 2003/09/09 17:57:14 mch Exp $
  * Created on 28-Aug-2003
  *
  * Copyright (C) AstroGrid. All rights reserved.
  *
- * This software is published under the terms of the AstroGrid 
- * Software License version 1.2, a copy of which has been included 
- * with this distribution in the LICENSE.txt file.  
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
  *
 **/
 package org.astrogrid.datacenter.adql;
@@ -20,7 +20,7 @@ import junit.framework.TestSuite;
 public class AllTests {
 
     public static void main(String[] args) {
-        junit.textui.TestRunner.run(AllTests.class);
+        junit.textui.TestRunner.run(suite());
     }
 
     public static Test suite() {
@@ -37,8 +37,11 @@ public class AllTests {
 }
 
 
-/* 
+/*
 $Log: AllTests.java,v $
+Revision 1.5  2003/09/09 17:57:14  mch
+Fix wasn't running tests
+
 Revision 1.4  2003/09/03 14:45:59  nw
 renamed test to match renamed class
 
@@ -50,5 +53,5 @@ added unit test that runs a set of sample ADQL documents through the object mode
 
 Revision 1.1  2003/08/28 15:26:44  nw
 unit tests for adql
- 
+
 */
