d772f6b4b616b7da5a1870b041b951dec31eb53f
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2003-09-11 09:30:10 +0000

    demonstrator for using castor to map javabeans to xml

---
+++ astrogrid/datacenter/test/int/java/org/astrogrid/messagebeans/InterestingBean.java
@@ -0,0 +1,96 @@
+/*$Id: InterestingBean.java,v 1.1 2003/09/11 09:30:10 nw Exp $
+ * Created on 11-Sep-2003
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.messagebeans;
+import java.net.URL;
+import java.util.*;
+/**
+ * @author Noel Winstanley nw@jb.man.ac.uk 11-Sep-2003
+ *
+ */
+public class InterestingBean {
+
+    private Date aDate;
+    private int[] someInts;
+    protected CommunityBean communityBean ;
+    private List list;
+
+    public void setADate(Date aDate) {
+        this.aDate = aDate;
+    }
+
+    public Date getADate() {
+        return aDate;
+    }
+
+    public void setSomeInts(int[] someInts) {
+        this.someInts = someInts;
+    }
+
+    public int[] getSomeInts() {
+        return someInts;
+    }
+    /**
+     * @return
+     */
+    public CommunityBean getCommunityBean() {
+        return communityBean;
+    }
+
+    /**
+     * @param bean
+     */
+    public void setCommunityBean(CommunityBean bean) {
+        communityBean = bean;
+    }
+
+    public void setList(List foobleList) {
+        this.list = foobleList;
+    }
+
+
+    public List getList() {
+        return list;
+    }
+    
+    public Object getList(int i) {
+        return list.get(i);
+    }
+    
+    public void setList(int i, Object o) {
+        if (list == null) {
+            list = new ArrayList();
+        }
+        if (list.size() == i) {
+            list.add(o);
+        }
+        list.set(i,o);
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(Object obj) {
+        InterestingBean other = (InterestingBean)obj;
+        return (this.aDate != null ? this.aDate.equals(other.aDate) : other.aDate == null) 
+            && (this.communityBean != null ? this.communityBean.equals(other.communityBean) : other.communityBean == null)
+            && Arrays.equals(this.someInts,other.someInts)
+            && (this.list != null ? this.list.equals(other.list) : other.list == null);
+    }
+
+}
+
+
+/* 
+$Log: InterestingBean.java,v $
+Revision 1.1  2003/09/11 09:30:10  nw
+demonstrator for using castor to map javabeans to xml
+ 
+*/
