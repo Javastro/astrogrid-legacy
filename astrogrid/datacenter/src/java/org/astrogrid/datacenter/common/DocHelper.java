b03673ff1607b0476f62529449324d89430eff2c
Martin Hill <Martin.Hill@astrogrid.org>
2003-09-15 17:21:49 +0000

    Better exception throwing

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/common/DocHelper.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/common/DocHelper.java
@@ -1,5 +1,5 @@
 /*
- * $Id: DocHelper.java,v 1.3 2003/09/15 15:39:34 mch Exp $
+ * $Id: DocHelper.java,v 1.4 2003/09/15 17:21:49 mch Exp $
  *
  * (C) Copyright Astrogrid...
  */
@@ -7,10 +7,13 @@
 package org.astrogrid.datacenter.common;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import javax.xml.parsers.ParserConfigurationException;
 import org.apache.axis.utils.XMLUtils;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import sun.security.krb5.internal.crypto.e;
 
 /**
  * Common tasks for the document helpers
@@ -31,13 +34,15 @@ public class DocHelper
         InputStream is = new ByteArrayInputStream(xmlSnippet.getBytes());
          return XMLUtils.newDocument(is);
       }
-      catch (java.io.IOException e)
+      catch (IOException e)
       {
-         throw new SAXException(e);
+         //really should not happen in bytearray constructor!
+         throw new RuntimeException(e);
       }
-      catch (javax.xml.parsers.ParserConfigurationException e)
+      catch (ParserConfigurationException e)
       {
-         throw new SAXException(e);
+         //really should not happen - setup problem
+         throw new RuntimeException(e);
       }
    }
 }
