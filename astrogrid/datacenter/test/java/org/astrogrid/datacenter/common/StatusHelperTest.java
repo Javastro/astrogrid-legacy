71d1d4aba5d5103282a53f8ed9417834286337d4
Martin Hill <Martin.Hill@astrogrid.org>
2003-09-15 11:43:42 +0000

    StatusHelper.makeStatus now takes typesafe status

--- astrogrid/datacenter/test/java/org/astrogrid/datacenter/common/StatusHelperTest.java
+++ astrogrid/datacenter/test/java/org/astrogrid/datacenter/common/StatusHelperTest.java
@@ -12,47 +12,47 @@ import org.xml.sax.SAXException;
  */
 
 public class StatusHelperTest extends TestCase {
-	//declare reusable objects to be used across multiple tests
-	public StatusHelperTest(String name) {
-		super(name);
-	}
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(StatusHelperTest.class);
-	}
-	public static Test suite() {
-		return new TestSuite(StatusHelperTest.class);
-	}
-	protected void setUp() {
-	//define reusable objects to be used across multiple tests
-	}
-	protected void tearDown() {
-	//clean up after testing (if necessary)
-	}
-	public void testMakeStatusTag()  throws Exception{
+   //declare reusable objects to be used across multiple tests
+   public StatusHelperTest(String name) {
+      super(name);
+   }
+   public static void main(String[] args) {
+      junit.textui.TestRunner.run(StatusHelperTest.class);
+   }
+   public static Test suite() {
+      return new TestSuite(StatusHelperTest.class);
+   }
+   protected void setUp() {
+   //define reusable objects to be used across multiple tests
+   }
+   protected void tearDown() {
+   //clean up after testing (if necessary)
+   }
+   public void testMakeStatusTag()  throws Exception{
 
-		String result = StatusHelper.makeStatusTag("foo","bar");
+      String result = StatusHelper.makeStatusTag("foo", ServiceStatus.STARTING);
         Document doc = DocHelper.wrap(result); // just test its correct XML.
         assertNotNull(doc);
-        
 
-	}
-	public void testMakeJobNotificationTag() throws Exception {
 
-	   String result  = StatusHelper.makeJobNotificationTag("foo","bar");
+   }
+   public void testMakeJobNotificationTag() throws Exception {
+
+      String result  = StatusHelper.makeJobNotificationTag("foo","bar");
         Document doc = DocHelper.wrap(result);
         assertNotNull(doc);
-	}
-    
-    /** @todo - as far as I can see this test is correct - the implementation of GetServiceStatus needs fixing */
-	public void testGetServiceStatus() throws Exception {
+   }
 
-		String src = StatusHelper.makeStatusTag("foo","working");
+    /** @todo - as far as I can see this test is correct - the implementation of GetServiceStatus needs fixing */
+   public void testGetServiceStatus() throws Exception {
+
+      String src = StatusHelper.makeStatusTag("foo",ServiceStatus.RUNNING_QUERY);
         System.out.println(src);
         Document doc = DocHelper.wrap("<bar>"+src+"</bar>");
-        assertNotNull(doc);        
-		String result = StatusHelper.getServiceStatus("foo", doc.getDocumentElement());
+        assertNotNull(doc);
+      String result = StatusHelper.getServiceStatus("foo", doc.getDocumentElement());
         assertNotNull(result);
         assertEquals("working",result);
 
-	}
+   }
 }
