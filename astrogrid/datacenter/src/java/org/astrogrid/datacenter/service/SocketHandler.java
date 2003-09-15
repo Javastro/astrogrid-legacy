6ac3a50c77ff943baec0baf59d7a799516fd45ec
Martin Hill <Martin.Hill@astrogrid.org>
2003-09-15 11:15:01 +0000

    Fixes to make it work... document reading/writing problems

--- astrogrid/datacenter/src/java/org/astrogrid/datacenter/service/SocketHandler.java
+++ astrogrid/datacenter/src/java/org/astrogrid/datacenter/service/SocketHandler.java
@@ -1,26 +1,28 @@
 /*
- * $Id: SocketHandler.java,v 1.6 2003/09/11 17:41:33 mch Exp $
+ * $Id: SocketHandler.java,v 1.7 2003/09/15 11:15:01 mch Exp $
  *
  * (C) Copyright AstroGrid...
  */
 
 package org.astrogrid.datacenter.service;
 
-import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
-import javax.xml.parsers.ParserConfigurationException;
+import java.net.SocketException;
 import org.apache.axis.utils.XMLUtils;
 import org.astrogrid.datacenter.common.ResponseHelper;
 import org.astrogrid.datacenter.common.ServiceStatus;
+import org.astrogrid.datacenter.common.StatusHelper;
 import org.astrogrid.datacenter.delegate.SocketDelegate;
 import org.astrogrid.datacenter.queriers.DatabaseQuerier;
 import org.astrogrid.datacenter.query.QueryException;
 import org.astrogrid.datacenter.service.ServiceListener;
+import org.astrogrid.datacenter.io.SocketXmlInputStream;
+import org.astrogrid.datacenter.io.SocketXmlOutputStream;
+import org.astrogrid.datacenter.io.TraceInputStream;
 import org.astrogrid.log.Log;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 /**
@@ -32,15 +34,22 @@ import org.xml.sax.SAXException;
 public class SocketHandler extends ServiceServer implements Runnable, ServiceListener
 {
    private Socket socket = null;
-   private DataOutputStream out = null;
+   private SocketXmlOutputStream out = null;
+   private SocketXmlInputStream in = null;
 
    public SocketHandler(Socket givenSocket) throws IOException
    {
       this.socket = givenSocket;
-      out = new DataOutputStream(socket.getOutputStream());
+      out = new SocketXmlOutputStream(socket.getOutputStream());
+
+      TraceInputStream tin = new TraceInputStream(socket.getInputStream());
+//      tin.setState(true);
+      tin.copy2File(new File("incomingMsgs.log"));
+      in = new SocketXmlInputStream(tin);
+
 
       //create a thread running this isntance and start it
-      new Thread(this).start();
+      new Thread(this, "SocketHandler").start();
    }
 
    /** called when the status changes - writes out the new status to the
@@ -50,7 +59,7 @@ public class SocketHandler extends ServiceServer implements Runnable, ServiceLis
    {
       try
       {
-         out.writeChars(""+querier.getStatus());
+         out.writeAsDoc(StatusHelper.makeStatusTag(querier.getHandle(), querier.getStatus()));
       }
       catch (IOException e)
       {
@@ -71,81 +80,89 @@ public class SocketHandler extends ServiceServer implements Runnable, ServiceLis
     */
    public void run()
    {
-      try
-      {
-         InputStream in = socket.getInputStream();
-         while (socket.isConnected())
+         //InputStream in = socket.getInputStream();
+         boolean fatal = false;
+
+         while (socket.isConnected() && !fatal)
          {
             try
             {
-               Log.trace("SocketHandler: Reading incoming document");
+               Log.trace("SocketHandler: Reading/Waiting on incoming document");
                //read incoming document
-               Element docRequest = readIncomingDocument();
-               Log.trace("SocketHandler: incoming document root tag="+docRequest.getNodeName());
+               Document docRequest = in.readDoc();
 
                //what kind of operation is it?
                if (docRequest.getElementsByTagName(SocketDelegate.REQ_REG_METADATA_TAG).getLength() > 0)
                {
-                  //request registry metadata
-                  XMLUtils.ElementToStream(getVOResource(), out);
+                  //requested registry metadata
+                  Log.trace("SocketHandler: Writing registry metadata");
+
+                  out.writeAsDoc(getVOResource());
                }
                else
                {
                   //assume a blocking query
-                  DatabaseQuerier querier = DatabaseQuerier.doQueryGetResults(docRequest);
+                  DatabaseQuerier querier = DatabaseQuerier.doQueryGetResults(docRequest.getDocumentElement());
 
                   querier.setStatus(ServiceStatus.RUNNING_RESULTS);
 
                   Document response = ResponseHelper.makeResultsResponse(querier, querier.getResults().toVotable().getDocumentElement());
-   
-                  XMLUtils.DocumentToStream(response, out);
+
+                  out.writeAsDoc(response.getDocumentElement());
                }
+            }
+            catch (SocketException e)
+            {
+               Log.logWarning(this, "(assuming fatal & terminating handler)", e);
+               fatal = true;
             }
             catch (IOException e)
             {
-               e.printStackTrace(System.out);
+               Log.logError("", e);
             }
             catch (SAXException e)
             {
-               //need to tell user
-               out.writeChars("Oh no it's all gone horribly wrong "+e);
-               e.printStackTrace(System.out);
+               String s = "XML Error with message: (unknown)";
+               Log.logError(s, e);
+               writeError(s,e);
             }
             catch (QueryException e)
             {
                //need to tell user
-               out.writeChars("Oh no it's all gone horribly wrong "+e);
-               e.printStackTrace(System.out);
+               String s = "Given Query is invalid";
+               Log.logError(s, e);
+               writeError(s,e);
             }
             catch (Throwable e)
             {
-               out.writeChars("Oh no it's all gone horribly wrong "+e);
-               e.printStackTrace(System.out);
+               String s = "Oh no it's all gone horribly wrong";
+               Log.logError(s, e);
+               writeError(s,e);
             }
          }
-      }
-      catch (IOException e)
-      {
-         e.printStackTrace(System.out);
-      }
    }
 
-   /**
-    * So we can plug in trace
-    */
-   private Element readIncomingDocument() throws IOException, SAXException, ParserConfigurationException
-   {
-      InputStream in = socket.getInputStream();
-      
-      Element element = XMLUtils.newDocument(in).getDocumentElement();
 
-      return element;
+   /**
+    * Attempts to let client know of error
+    * @todo wrap up in proper XML
+    */
+   private void writeError(String s, Throwable th)
+   {
+      try
+      {
+         out.writeAsDoc("<"+ResponseHelper.ERROR_TAG+">"+s+" ["+th+"]"+"</"+ResponseHelper.ERROR_TAG+">");
+      }
+      catch (IOException ioe) {}
    }
 
 }
 
 /*
 $Log: SocketHandler.java,v $
+Revision 1.7  2003/09/15 11:15:01  mch
+Fixes to make it work... document reading/writing problems
+
 Revision 1.6  2003/09/11 17:41:33  mch
 Fixes to socket-based client/server
 
