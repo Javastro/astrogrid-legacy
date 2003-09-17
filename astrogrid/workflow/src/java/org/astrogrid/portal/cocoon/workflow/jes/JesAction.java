ee6668db7b3264adcd65c718d9b8df639976e98f
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-09-17 14:03:19 +0000

    *** empty log message ***

---
+++ astrogrid/workflow/src/java/org/astrogrid/portal/cocoon/workflow/jes/JesAction.java
@@ -0,0 +1,321 @@
+/*
+ * @(#)JesAction.java   1.0
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.3, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */
+package org.astrogrid.portal.cocoon.workflow.jes ;
+
+import org.apache.log4j.Logger ;
+
+import org.astrogrid.i18n.*;
+
+// import org.astrogrid.workflow.*;
+// import org.astrogrid.workflow.design.activity.*;
+
+import org.astrogrid.AstroGridException ;
+
+import org.astrogrid.portal.workflow.*;
+import org.astrogrid.portal.workflow.jes.*;
+
+
+import java.io.File;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+import org.xml.sax.EntityResolver;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.avalon.framework.parameters.ParameterException ;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.environment.ObjectModelHelper;
+
+/**
+ * A Cocoon action to handle our workflow design commands.
+ *
+ */
+public class JesAction extends AbstractAction {
+    
+    /** Compile-time switch used to turn tracing on/off. 
+      * Set this to false to eliminate all trace statements within the byte code.*/         
+    private static final boolean 
+        TRACE_ENABLED = true ;
+        
+    private static Logger 
+        logger = Logger.getLogger( JesAction.class ) ; 
+        
+    private static final String
+        ASTROGRIDERROR_SOMEMESSAGE = "AGWKFE00050" ; // none so far 
+
+
+	/**
+	 * Cocoon param for the user param in the session.
+	 *
+	 */
+	public static final String USER_PARAM_NAME = "user-param" ;
+
+	/**
+	 * Cocoon param for the user param in the session.
+	 *
+	 */
+	public static final String COMMUNITY_PARAM_TAG = "community" ;
+
+	/**
+	 * Http request param for the action.
+	 *
+	 */
+	public static final String ACTION_PARAM_TAG = "action" ;
+
+	/**
+	 * Http request param to confirm an action.
+	 *
+	 */
+	public static final String CONFIRM_PARAM_TAG = "confirm" ;
+    
+    public static final String
+        HTTP_WORKFLOW_TAG = "workflow-tag" ;
+        
+    public static final String
+        JOB_LIST_PARAMETER = "job-list" ;
+    
+    public static final String 
+        ACTION_READ_JOB_LIST = "read-job-list" ;
+
+    /**
+    * Our action method.
+    *
+    */
+    public Map act ( Redirector redirector
+                   , SourceResolver resolver
+                   , Map objectModel
+                   , String source
+                   , Parameters params ) {                  
+        if( TRACE_ENABLED ) trace( "JesAction.act() entry" ) ;  
+        
+        JesActionImpl
+            myAction = null ;
+        Map
+            retMap = null ;
+        
+        try { 
+            debug( "About to check properties loaded") ;
+            // Load the workflow config file and messages...
+            WKF.getInstance().checkPropertiesLoaded() ;
+            debug( "Properties loaded OK") ;
+            
+            myAction = new JesActionImpl( redirector
+                                        , resolver
+                                        , objectModel
+                                        , source
+                                        , params ) ;
+                                           
+            retMap = myAction.act() ;    
+                                          
+        }
+        catch ( AstroGridException agex ) {
+            debug( agex.toString() ) ;
+        }
+        finally {
+            if( TRACE_ENABLED ) trace( "JesAction.act() exit" ) ;  
+        }
+                                        
+        return retMap ; 
+ 
+    } // end of act() 
+
+
+    private class JesActionImpl {
+        
+        private Redirector
+            redirector ;
+        private SourceResolver
+            resolver ;
+        private Map
+            objectModel,
+            results ;
+        private String
+            source ;
+        private Parameters
+            params ;
+        private Request
+            request ;
+        private Session
+            session ;
+        private String
+            userid,
+            community ;
+        private String
+            action ;
+        private boolean
+            bConfirm ;
+		private String
+			template ;            
+        
+        public JesActionImpl( Redirector redirector
+                            , SourceResolver resolver
+                            , Map objectModel
+                            , String source
+                            , Parameters params ) {                                   
+            if( TRACE_ENABLED ) trace( "JesActionImpl() entry" ) ; 
+            
+            try {
+          
+                this.redirector = redirector ;
+                this.resolver = resolver ;
+                this.objectModel = objectModel ;
+                this.source = source ;
+                this.params = params ; 
+            
+                this.results = new HashMap() ;
+            
+                // Get current request and session.
+                this.request = ObjectModelHelper.getRequest( objectModel ) ;
+                this.session = request.getSession() ;
+            
+                // Get user and community 
+                this.retrieveUserDetails() ;
+            
+                this.action = request.getParameter( ACTION_PARAM_TAG ) ;
+                this.bConfirm = new Boolean ( request.getParameter(CONFIRM_PARAM_TAG) ).booleanValue() ;
+                          
+            }
+            finally {
+                if( TRACE_ENABLED ) trace( "JesActionImpl() exit" ) ; 
+            }
+              
+        } // end of JesActionImpl()
+        
+        
+        public Map act() {
+            if( TRACE_ENABLED ) trace( "JesActionImpl.act() entry" ) ;      
+        
+            try {
+            	
+				debug( "action is: " + action ) ;
+                
+                this.consistencyCheck() ;
+                    
+                if( action == null ){
+                    debug( "action is null") ;  
+                }  
+                    
+                if( action.equals( ACTION_READ_JOB_LIST ) ) {
+                    this.readWorkflowList() ; 
+                }
+                else {
+                    debug( "unsupported action") ; 
+                }
+                
+            }
+            catch( ConsistencyException cex ) {
+                debug( "ConsistencyException occurred");
+            }
+            //JBL Note: these should only be here during testing...
+            catch( Exception ex) {
+                debug( "Exception: ex" ) ;
+                ex.printStackTrace() ;
+            }
+            //JBL Note: these should only be here during testing...
+            catch( Throwable th ) {
+                debug( "Throwable th" ) ;
+                th.printStackTrace() ;
+            }
+            finally {
+                if( TRACE_ENABLED ) trace( "JesActionImpl.act() exit" ) ;  
+            }
+    
+            return results ;
+            
+        } // end of JesActionImpl()
+        
+        
+        private void retrieveUserDetails() {
+            if( TRACE_ENABLED ) trace( "JesActionImpl.retrieveUserDetails() entry" ) ;   
+                     
+            String 
+                tag  = null ;
+
+            try {
+                tag = params.getParameter( USER_PARAM_NAME ) ;
+                this.userid = (String) session.getAttribute( tag ) ;
+				tag = params.getParameter( COMMUNITY_PARAM_TAG ) ;
+				this.community = (String) session.getAttribute( tag ) ;
+            }
+            catch( ParameterException pex ) {
+                ; // some logging here
+            }
+            finally {
+                if( TRACE_ENABLED ) trace( "JesActionImpl.retrieveUserDetails() exit" ) ;  
+            }
+                
+        } // end of retrieveUserDetails()
+        
+        
+        private void consistencyCheck() throws ConsistencyException {
+			if( TRACE_ENABLED ) trace( "consistencyCheck() entry" ) ;
+			debug( "userid: " + userid ) ;
+			debug( "community: " + community ) ;
+			debug( "name: "  ) ; 
+			debug( "description: "  ) ; 
+            
+            
+            
+            if( userid == null ) {
+                ; // redirection required 
+                throw new ConsistencyException() ;
+            }
+            else if( action == null ) {
+                
+                debug( "action is null" ) ;
+                // throw new ConsistencyException() ;
+     
+            }
+			if( TRACE_ENABLED ) trace( "consistencyCheck()) exit" ) ;
+        }
+        
+        
+        private void readWorkflowList() {
+            if( TRACE_ENABLED ) trace( "JesActionImpl.readWorkflowList() entry" ) ;
+              
+            try {
+                //NB: The filter argument is ignored at present (Sept 2003).
+                Iterator
+                    iterator =  Job.readJobList( userid, community, "*" ) ;
+                this.request.setAttribute( JOB_LIST_PARAMETER, iterator ) ;               
+            }
+            finally {
+                if( TRACE_ENABLED ) trace( "JesActionImpl.readWorkflowList() exit" ) ;
+            }
+                    
+        } // end of readWorkflowList()   
+           
+   
+    } // end of inner class JesActionImpl
+        
+    
+    private class ConsistencyException extends Exception {
+    }
+    
+    
+    private void trace( String traceString ) {
+        // logger.debug( traceString ) ;
+        System.out.println( traceString ) ;
+    }
+    
+    private void debug( String logString ){
+        // logger.debug( logString ) ;
+        System.out.println( logString ) ;
+    }
+             
+            
+} // end of class JesAction 
--- astrogrid/workflow/src/java/org/astrogrid/portal/workflow/jes/Job.java
+++ astrogrid/workflow/src/java/org/astrogrid/portal/workflow/jes/Job.java
@@ -21,6 +21,8 @@ import java.text.MessageFormat ;
 import java.io.InputStream ;
 import org.xml.sax.* ;
 import java.io.StringReader ;
+import java.util.Date ;
+import java.util.ArrayList ;
 
 import org.apache.log4j.Logger ;
 import org.apache.axis.utils.XMLUtils ;
@@ -139,18 +141,21 @@ public class Job {
      */
     public static ListIterator readJobList( String userid, String community, String filter ) {
         if( TRACE_ENABLED ) trace( "Job.readJobList() entry") ; 
-        
-        // JBL: For the moment we are ignoring filter.
-        
+                
         ListIterator
            iterator = null ;
-        java.util.Vector
-           vector = null ;
-        StringBuffer
-           argumentBuffer = new StringBuffer( 64 ) ;
-        
+        String
+            jesLocation = null,
+            request = null ,
+            response = null ;
+        JobControllerDelegate
+            jobController = null ;
+                   
         try {
-                          
+            jesLocation = WKF.getProperty( WKF.JES_URL, WKF.JES_CATEGORY ) ;
+            jobController = new JobControllerDelegate( jesLocation ) ;
+            response = jobController.readJobList( userid, community, filter ) ; 
+            iterator = Job.decodeListResponse( response ) ;               
         }
         catch ( Exception ex ) {
             ex.printStackTrace() ;
@@ -163,12 +168,104 @@ public class Job {
         
     } // end of readJobList()
     
+    
+    private static ListIterator decodeListResponse( String listXML ) {
+        if( TRACE_ENABLED ) trace( "Job.decodeListResponse() entry") ;
+        
+        ListIterator
+            iterator = null ;
+        
+        try {
+            
+            InputSource
+               source = new InputSource( new StringReader( listXML ) );
+                         
+            Document
+                document = XMLUtils.newDocument(source) ;   
+            
+            Element
+               element = document.getDocumentElement() ;                
+            
+            NodeList
+               nodeList = element.getChildNodes(),
+               jobsList = null ;              
+                            
+            for( int i=0 ; i < nodeList.getLength() ; i++ ) {           
+                if( nodeList.item(i).getNodeType() == Node.ELEMENT_NODE ) {
+                    
+                    element = (Element) nodeList.item(i) ;
+                
+                    if ( element.getTagName().equals( JobDD.JOBLIST_ELEMENT ) ) {
+                         jobsList = element.getChildNodes() ;  
+                         iterator = Job.formatList( jobsList ) ;
+                    }  
+                    else if( element.getTagName().equals( JobDD.MESSAGE_ELEMENT ) ) {
+                         debug( element.getFirstChild().getNodeValue().trim() ) ;
+                    }                 
+                    
+                } // end if
+                                
+            } // end for        
+        }
+        catch( Exception ex ){
+            ex.printStackTrace() ;
+        }
+        finally {
+            if( TRACE_ENABLED ) trace( "Job.decodeListResponse() exit") ;
+        }
+        return iterator ;
+         
+    } // end of decodeListResponse()
+   
+   
+    private static ListIterator formatList( NodeList nodeList ) {
+         if( TRACE_ENABLED ) trace( "Job.formatList() entry") ;
+        
+        ArrayList
+           jobList = new ArrayList() ;              
+        
+         try {
+            
+             Element
+                element = null ;
+       
+                            
+             for( int i=0 ; i < nodeList.getLength() ; i++ ) {           
+                 if( nodeList.item(i).getNodeType() == Node.ELEMENT_NODE ) {
+                    
+                     element = (Element) nodeList.item(i) ;
+                
+                     if ( element.getTagName().equals( JobDD.JOB_ELEMENT ) ) {
+                          jobList.add( new Job( element ) ) ;  
+                     }  
+ 
+                 } // end if
+                                
+             } // end for        
+         }
+         catch( Exception ex ){
+             ex.printStackTrace() ;
+         }
+         finally {
+             if( TRACE_ENABLED ) trace( "Job.formatList() exit") ;
+         }
+         
+         return jobList.listIterator() ;
+         
+     } // end of formatList()
+    
      
     private String
         name,
         description,
         userid,
-        community ;            
+        community,
+        jobid,
+        status ;   
+       
+    private Date
+       timestamp ;
+         
         
     /**
       * <p> 
@@ -184,7 +281,47 @@ public class Job {
     }
     
     
-public void setName(String name) {
+    private Job( Element element ) {
+        if( TRACE_ENABLED ) trace( "Job(Element) entry") ;
+        
+        try {
+            
+             name = element.getAttribute( JobDD.NAME_ATTR ) ;           
+            
+             NodeList
+                nodeList = element.getChildNodes() ;                           
+             for( int i=0 ; i < nodeList.getLength() ; i++ ) {           
+                 if( nodeList.item(i).getNodeType() == Node.ELEMENT_NODE ) {
+                    
+                     element = (Element) nodeList.item(i) ;
+                
+                     if ( element.getTagName().equals( JobDD.DESCRIPTION_ELEMENT ) ) {
+                         this.description = element.getFirstChild().getNodeValue().trim() ;  
+                     }  
+                     else if( element.getTagName().equals( JobDD.STATUS_ELEMENT ) ) {
+                         this.status = element.getFirstChild().getNodeValue().trim() ;
+                     }  
+                     else if( element.getTagName().equals( JobDD.TIME_ELEMENT ) ) {
+                         //JBL Is this OK? Look at jes.
+                         this.timestamp = new Date ( element.getFirstChild().getNodeValue().trim() ) ;
+                     }  
+                     else if ( element.getTagName().equals( JobDD.JOBID_ELEMENT ) ) {
+                         this.jobid = element.getFirstChild().getNodeValue().trim() ;   
+                     } 
+                                      
+                    
+                 } // end if
+                                
+             } // end for           
+        }
+        finally {
+            if( TRACE_ENABLED ) trace( "Job(Element) exit") ;
+        }
+        
+    } // end of Job( Element element )
+    
+     
+    public void setName(String name) {
 		this.name = name;
 	}
 
@@ -226,5 +363,29 @@ public void setName(String name) {
         System.out.println( logString ) ;
         // logger.debug( logString ) ;
     }
+
+	public void setJobid(String jobid) {
+		this.jobid = jobid;
+	}
+
+	public String getJobid() {
+		return jobid;
+	}
+
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
     
 } // end of class Job
