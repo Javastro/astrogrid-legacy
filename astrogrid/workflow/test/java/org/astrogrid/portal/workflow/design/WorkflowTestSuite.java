1108cbadf53cb1838af8d26c6693b2b410ce1be7
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-09-17 08:40:41 +0000

    *** empty log message ***

--- astrogrid/workflow/test/java/org/astrogrid/portal/workflow/design/WorkflowTestSuite.java
+++ astrogrid/workflow/test/java/org/astrogrid/portal/workflow/design/WorkflowTestSuite.java
@@ -11,7 +11,7 @@ import junit.framework.TestSuite;
 import org.apache.axis.utils.XMLUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.astrogrid.portal.workflow.design.*;
+// import org.astrogrid.portal.workflow.design.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -25,13 +25,8 @@ public class WorkflowTestSuite extends TestCase {
 		logger = Logger.getLogger( WorkflowTestSuite.class ) ;
 		
 	private static final String
-//	    log4jproperties = "/usr/local/astrogrid/eclipse/log4j.properties" ;
         log4jproperties = "/home/jl99/log4j.properties" ;		
         
-	public static String
-	    path ; // Path to directory where query XML docs are stored
-		
-	
 	/**
 	 * Sets up the test fixture.
 	 *
@@ -147,47 +142,6 @@ public class WorkflowTestSuite extends TestCase {
         }
         
     } // end of testCreateWorkflowFromTemplate_OneStepTemplate()
-    
-    
-    
-
-
-
-
-
-
-/*
-	public void testQueryToString_CONE() {
-		logger.info( "enter: QueryTestSuite.testQueryToString()" );	
-		
-		// This is the pseudo-SQL... "SELECT URA, UDEC, PMPROB FROM USNOB WHERE CONE(234.56, -12.34, 0.01)"
-        final String
-            sqlString = "SELECT  URA, UDEC, PMPROB FROM USNOB..USNOB  WHERE " +
-                        "( ((2 * ASIN(SQRT(POW(SIN(-12.34-UDEC)/2),2) + COS(UDEC) + POW(SIN(234.56-URA)/2),2)) < 0.01 )" ,
-            fileName = "query_CONE.xml" ;
-        Element
-            queryElement = this.setUpQueryElement( path + System.getProperty( "file.separator" ) + fileName ) ;
-		Query
-			query = null ;
-		String
-		    resultString = null ;
-			
-		try{
-			query = new Query( queryElement, factory ) ;
-			resultString = query.toSQLString() ;
-//			logger.info( "testQueryToString_CONE: " + resultString ) ;
-			assertTrue( resultString.equals( sqlString ) ) ;	
-		}
-		catch( Exception ex ) {
-			assertTrue( false ) ;
-		}
-		finally {
-			logger.info( "exit: QueryTestSuite.testQueryToString()" );	
-		}
-        
-	} // end of testQueryToString_CONE()
-	
-*/
 
 
 	/** 
@@ -208,19 +162,11 @@ public class WorkflowTestSuite extends TestCase {
     public static void main( String args[] )  {
 
 //		PropertyConfigurator.configure( log4jproperties ) ;
-				
-		if( args.length == 0 ) {
-			 System.out.println( "Usage: Path to XML query directory required" ) ;
-		}
-		else { 
+			
+	   logger.info("Entering WorkflowTestSuite application.");	
+	   junit.textui.TestRunner.run( suite() ) ;
+	   logger.info("Exit WorkflowTestSuite application.");
 		
-			 path = args[0] ;			
-
-			 logger.info("Entering WorkflowTestSuite application.");	
-			 junit.textui.TestRunner.run( suite() ) ;
-			 logger.info("Exit WorkflowTestSuite application.");
-		
-		} // end else
     }
     
-} // end of class QueryTestSuite
+} // end of class WorkflowTestSuite
