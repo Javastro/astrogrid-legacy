954e348326ee2acba51400a548810b2945143a4e
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2003-10-14 12:43:42 +0000

    moved from parent datacenter project.

---
+++ astrogrid/datacenter/common/src/java/org/astrogrid/datacenter/adql/QOM.java
@@ -0,0 +1,108 @@
+/*$Id: QOM.java,v 1.1 2003/10/14 12:43:42 nw Exp $
+ * Created on 28-Aug-2003
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.datacenter.adql;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.exolab.castor.xml.CastorException;
+import org.xml.sax.ContentHandler;
+/** interface that describes commonality between all classes in the  Query Object Model.
+ * <p>
+ * Defines the marshalling / unmarshalling to XML routines. - 
+ *  * <p>
+ * Also defines a dynamic DynamicVisitor pattern for the QOM. See {@link DynamicVisitor} for details 
+ * @author Noel Winstanley nw@jb.man.ac.uk 28-Aug-2003
+ *
+ */
+public interface QOM {
+    /**
+     * wrapper around {@link #validate}
+     * @return true if <tt>validate()</tt> succeeds, false if it throws a <tt>CastorException</tt>
+     * @see #validate
+     */
+    public boolean isValid() ;
+    /**
+     *  write xml representation out to a writer
+     * @param w the writer to output to
+     * @throws CastorException on error
+     */
+    public void marshal(Writer w) throws CastorException;
+    /**
+     * write xml representation out to a content handler
+     * @param c handler to write out to
+     * @throws CastorException on error
+     */
+    public void marshal(ContentHandler c) throws CastorException, IOException;
+    /** 
+     * verify this query object would generate an xml representation valid against the ADQL xml schema
+     * @throws CastorException if validation fails.
+     */
+    public void validate() throws CastorException;
+    
+    /**
+     * Entry point for a dynamic visitor to process the object tree in a top down fashion (i.e. visit the
+     * parent before it's children)
+     * @param v the visitor object
+     * @throws TraversalException is the traversal mechanism breaks
+     * @throws ProcessingException if the supplied <tt>visit()</tt> method throws an exception
+     * @see DynamicVisitor
+     */
+    public void acceptTopDown(DynamicVisitor v) throws TraversalException, ProcessingException;
+    
+    /**Entry point for a dynamic visitor to process the object tree in a bottom-up manner (i.e. visit the child
+     * nodes before their parents).
+     * 
+     * @param v visitor object
+    * @throws TraversalException is the traversal mechanism breaks
+     * @throws ProcessingException if the supplied <tt>visit()</tt> method throws an exception
+     * @see DynamicVisitor
+     */
+    public void acceptBottomUp(DynamicVisitor v) throws TraversalException, ProcessingException;
+
+/**
+ * 
+ * @param v
+ * @throws Exception
+ */
+    public void callVisitor(DynamicVisitor v) throws TraversalException, ProcessingException;
+
+   
+    /** compute the children elements of this element 
+     * @return array of child QOM objects (all non-null child elements of this element)
+     * @todo - is it possible add an intermediate caching of results, so reflection only needs to be done one.
+     *  
+     */
+    public QOM[] getChildren() throws TraversalException; 
+}
+
+
+/* 
+$Log: QOM.java,v $
+Revision 1.1  2003/10/14 12:43:42  nw
+moved from parent datacenter project.
+
+Revision 1.5  2003/09/26 14:27:44  nw
+documentation tweak
+
+Revision 1.4  2003/09/17 14:51:30  nw
+tidied imports - will stop maven build whinging
+
+Revision 1.3  2003/09/08 09:34:56  nw
+Improved exception handling
+
+Revision 1.2  2003/09/02 14:44:55  nw
+addedd interface to visitor pattern
+
+Revision 1.1  2003/08/28 15:27:54  nw
+added ADQL object model.
+ 
+*/
