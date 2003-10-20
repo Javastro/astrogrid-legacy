61e5c9d0218d187fe0e09fa867a013ddd009c609
John Taylor <John.Taylor@astrogrid.org>
2003-10-20 13:25:55 +0000

    Initial Commit.

---
+++ astrogrid/jes/test/java/org/astrogrid/jes/TrivialTest.java
@@ -0,0 +1,42 @@
+/*
+ * Created on 20/10/03 by John Taylor jdt@roe.ac.uk .
+ * 
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file. 
+ */
+package org.astrogrid.jes;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jdt
+ *
+ * Trivial test simply to force CVS to create the test directory structure so I 
+ * can run Maven.
+ */
+public class TrivialTest extends TestCase {
+
+  /**
+   * Constructor for TrivialTest.
+   * @param arg0
+   */
+  public TrivialTest(String arg0) {
+    super(arg0);
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(TrivialTest.class);
+  }
+  
+  /**
+   * Does nothing.
+   *
+   */
+  public void testTrivial() {
+  }
+  
+
+}
