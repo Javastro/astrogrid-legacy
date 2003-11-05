fa498ce082ba98ec25817db300a9584e629351c4
Guy Rixon <gtr@ast.cam.ac.uk>
2003-11-05 20:37:00 +0000

    The invokedViaAxis property in the WarehouseServiceImpl is set to false.

--- astrogrid/warehouse/src/java/org/astrogrid/warehouse/service/Server.java
+++ astrogrid/warehouse/src/java/org/astrogrid/warehouse/service/Server.java
@@ -5,7 +5,17 @@ import electric.server.http.HTTP;
 
 public class Server {
   public static void main (String[] args) throws Exception {
+    
+    // This is the service implementation. It behaves differently
+    // when inside an Axis container, so we reassure it that it's
+    // safe inside GLUE.
+    WarehouseServiceImpl w = new WarehouseServiceImpl();
+    w.invokedViaAxis = false;
+    
+    // This is the HTTP server.  We start it running (i.e.
+    // start up one or more daemon threads and then push
+    // the warehouse-service implementation into it.
     HTTP.startup ("http://localhost:8004/glue");
-    Registry.publish( "warehouse", new WarehouseServiceImpl() );
+    Registry.publish ("warehouse", w);
   }
 }
