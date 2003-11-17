27b0a155581a1ad32b27a590ca96f02cf23d3699
Martin Hill <Martin.Hill@astrogrid.org>
2003-11-17 12:53:07 +0000

    Moving common to snippet

---
+++ astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/snippet/QueryIdHelper.java
@@ -0,0 +1,92 @@
+/*
+ * $Id: QueryIdHelper.java,v 1.1 2003/11/17 12:53:07 mch Exp $
+ *
+ * (C) Copyright Astrogrid...
+ */
+
+package org.astrogrid.datacenter.snippet;
+
+import org.apache.axis.utils.XMLUtils;
+import org.astrogrid.log.Log;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ * A helper class for constructing and extracting service id tags and
+ * attributes
+ *
+ * @author M Hill
+ */
+
+public class QueryIdHelper
+{
+   /** The service id name when used as a tag tag name */
+   public static final String QUERY_ID_TAG = "QueryId";
+
+   /** The service id name when using as an attribute */
+   public static final String QUERY_ID_ATT = "queryid";
+
+   /** Makes a service tag with the given id as the tag value */
+   public static String makeQueryIdTag(String id)
+   {
+      Log.affirm((id != null) && (id.length() >0), "Id is empty");
+
+      String tagString = "<"+QUERY_ID_TAG+">"+id+"</"+QUERY_ID_TAG+">";
+
+      return tagString;
+      //return XMLUtils.newDocument(tagString).getDocumentElement();
+   }
+
+   /** Finds the service tag in the given dom and returns its tag value
+    * NWW: fixed for case when dom element is the one required */
+   public static String getQueryId(Element domContainingId)
+   {
+
+       
+      NodeList idNodes = domContainingId.getElementsByTagName(QUERY_ID_TAG);
+
+      if (idNodes.getLength() == 0)
+      {
+          if (QUERY_ID_TAG.equals(domContainingId.getLocalName())) {
+              return domContainingId.getFirstChild().getNodeValue(); 
+          }
+         //no service id tag found, look for attribute in top tag
+         String attribute = domContainingId.getAttribute(QueryIdHelper.QUERY_ID_ATT);
+
+         Log.affirm((attribute != null) && (attribute.length() != 0),
+                    "No query id tag or attribute found in "+XMLUtils.ElementToString(domContainingId));
+
+         return attribute;
+      }
+
+      Log.affirm(idNodes.getLength() == 1, "Should only be 1 service id tag in an element");
+
+      String nodeValue = idNodes.item(0).getNodeValue();
+
+      if (nodeValue == null) //look at first non-whitespace child
+      {
+         //bug trace
+         if (idNodes.item(0).getFirstChild() == null)
+         {
+            Log.trace(XMLUtils.ElementToString( (Element) idNodes.item(0)));
+         }
+
+
+         nodeValue = idNodes.item(0).getFirstChild().getNodeValue();
+      }
+
+      return nodeValue;
+   }
+
+   /** Makes a tag with the given name, and a service id attribute set to the
+    * given id
+    */
+   public static String makeTagWithQueryIdAttr(String tagName, String id)
+   {
+      return "<"+tagName+"  "+QUERY_ID_ATT+"='"+id+"'>";
+   }
+}
+
+
+
+
---
+++ astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/snippet/ResponseHelper.java
@@ -0,0 +1,166 @@
+/*
+ * $Id: ResponseHelper.java,v 1.1 2003/11/17 12:53:07 mch Exp $
+ *
+ * (C) Copyright Astrogrid...
+ */
+
+package org.astrogrid.datacenter.snippet;
+
+import java.net.URL;
+import org.apache.axis.utils.XMLUtils;
+import org.astrogrid.datacenter.queriers.DatabaseQuerier;
+import org.astrogrid.datacenter.query.*;
+import org.astrogrid.log.Log;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * A set of methods to help build documents that will contain results
+ * information.  Really a temporary class until we work out what the
+ * results documents will look like and create some generated code for them.
+ *
+ * @author M Hill
+ */
+
+public class ResponseHelper
+{
+   /** Root tag used when acknowledging a query request, that a query has started */
+   public static final String QUERY_STARTED_RESP_TAG = "QueryStarted";
+
+   /** Root tag used when acknowledging a query request, that a query has been made */
+   public static final String QUERY_CREATED_RESP_TAG = "QueryCreated";
+
+   /** Root tag used for documents containing the results */
+   public static final String DATACENTER_RESULTS_TAG = "DatacenterResults";
+
+   /** Tag used to describe an error */
+   public static final String ERROR_TAG = "Error";
+
+   /** Tag used to describe results */
+   public static final String RESULTS_TAG = "Results";
+   
+   /**
+    * Returns an element that indicates the query has started
+    *<p>
+    * If the query has already got an exception, throws this so that
+    * client gets it (don't like this, very general)
+    */
+   public static Document makeQueryStartedResponse(DatabaseQuerier querier) throws Throwable
+   {
+      if (querier.getStatus() == QueryStatus.ERROR)
+      {
+         throw querier.getError();
+      }
+
+      String doc =
+          "<"+QUERY_STARTED_RESP_TAG+">\n"
+         +"   "+QueryIdHelper.makeQueryIdTag(querier.getHandle())
+         +"   "+StatusHelper.makeStatusTag(querier.getStatus())
+         +"</"+QUERY_STARTED_RESP_TAG+">\n";
+
+      return DocHelper.wrap(doc);
+   }
+
+   /**
+    * Returns an element that indicates the query has started
+    *<p>
+    * If the query has already got an exception, throws this so that
+    * client gets it (don't like this, very general)
+    */
+   public static Document makeQueryCreatedResponse(DatabaseQuerier querier) throws Throwable
+   {
+      if (querier.getStatus() == QueryStatus.ERROR)
+      {
+         throw querier.getError();
+      }
+
+      String doc =
+          "<"+QUERY_CREATED_RESP_TAG+">\n"
+         +"   "+QueryIdHelper.makeQueryIdTag(querier.getHandle())
+         +"   "+StatusHelper.makeStatusTag(querier.getStatus())
+         +"</"+QUERY_CREATED_RESP_TAG+">\n";
+
+      return DocHelper.wrap(doc);
+   }
+
+   /**
+    * Returns a document that indicates the status of the query.  This is based
+    * on the StatusHelper.makeStatus, but wrapped up in a document
+    * <p>
+    * If the query has already got an exception, throws this so that
+    * client gets it (don't like this, very general)
+    */
+   public static Document makeStatusResponse(DatabaseQuerier querier) throws Throwable
+   {
+      if (querier.getStatus() == QueryStatus.ERROR)
+      {
+         throw querier.getError();
+      }
+
+      String doc =
+         StatusHelper.makeStatusTag(querier.getHandle(), querier.getStatus());
+
+      return DocHelper.wrap(doc);
+   }
+
+   /**
+    * Returns a document indicating that the service id given in the request
+    * is unknown
+    */
+   public static Document makeUnknownIdResponse(String id)
+   {
+         String doc =
+            "<"+ERROR_TAG+">\n"
+            +"   Unknown Service ID submitted: "+id
+            +"</"+ERROR_TAG+">\n";
+
+         return DocHelper.wrap(doc);
+   }
+
+   /**
+    * Makes a document up with the results incorporated - note that the results
+    * are given separately as a pre-worked out Element (results parameter) as
+    * there may be different formats...
+    * <p>
+    * If the query has already got an exception, throws this so that
+    * client gets it (don't like this, very general)
+    */
+   public static Document makeResultsResponse(DatabaseQuerier querier, Element results)
+   {
+      Log.affirm(results != null, "Results=null");
+
+      String doc =
+          QueryIdHelper.makeTagWithQueryIdAttr(DATACENTER_RESULTS_TAG, querier.getHandle())+"\n"
+         +"   <TIME>"+querier.getQueryTimeTaken()+"</TIME>\n"
+         +"   <"+RESULTS_TAG+" type='votable'>\n"
+         +XMLUtils.ElementToString(results)
+         +"   </"+RESULTS_TAG+">\n"
+         +"</"+DATACENTER_RESULTS_TAG+">\n";
+
+      return DocHelper.wrap(doc);
+
+   }
+
+   /**
+    * Makes a document up with the results URL incorporated
+    * <p>
+    * If the query has already got an exception, throws this so that
+    * client gets it (don't like this, very general)
+    */
+   public static Document makeResultsResponse(DatabaseQuerier querier, URL results)
+   {
+      Log.affirm(results != null, "Results=null");
+
+      String doc =
+          QueryIdHelper.makeTagWithQueryIdAttr(DATACENTER_RESULTS_TAG, querier.getHandle())+"\n"
+         +"   <TIME>"+querier.getQueryTimeTaken()+"</TIME>\n"
+         +"   <"+RESULTS_TAG+" type='url'>"+results+"</"+RESULTS_TAG+">\n"
+         +"</"+DATACENTER_RESULTS_TAG+">\n";
+
+      return DocHelper.wrap(doc);
+
+   }
+
+
+}
+
