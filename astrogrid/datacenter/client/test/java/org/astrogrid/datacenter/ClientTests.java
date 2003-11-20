0760f394dbe3d8c54b0f890a283108a979fc5bbb
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2003-11-20 15:43:31 +0000

    linked in missing tests

--- astrogrid/datacenter/client/test/java/org/astrogrid/datacenter/ClientTests.java
+++ astrogrid/datacenter/client/test/java/org/astrogrid/datacenter/ClientTests.java
@@ -1,4 +1,4 @@
-/*$Id: ClientTests.java,v 1.3 2003/11/19 18:48:47 nw Exp $
+/*$Id: ClientTests.java,v 1.4 2003/11/20 15:43:31 nw Exp $
  * Created on 21-Aug-2003
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -11,6 +11,8 @@
 package org.astrogrid.datacenter;
 
 import org.astrogrid.datacenter.axisdataserver.TransportTest;
+import org.astrogrid.datacenter.delegate.nvocone.AdqlNvoConeTest;
+import org.astrogrid.datacenter.delegate.nvocone.NvoConeTest;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -31,7 +33,7 @@ public class ClientTests {
 
 
     public static Test suite() {
-        TestSuite suite = new TestSuite("Test for org.astrogrid.datacenter");
+        TestSuite suite = new TestSuite("Test for datacenter client");
         //$JUnit-BEGIN$
         //suite.addTest(QueryTestSuite.suite());
         //$JUnit-END$ 
@@ -40,6 +42,8 @@ public class ClientTests {
         suite.addTest(org.astrogrid.datacenter.adql.AllTests.suite());
         suite.addTest(org.astrogrid.datacenter.snippet.AllTests.suite());
         suite.addTest(new TestSuite(TransportTest.class));
+        suite.addTest(AdqlNvoConeTest.suite());
+        suite.addTest(NvoConeTest.suite());
         return suite;
     }
 }
@@ -47,6 +51,9 @@ public class ClientTests {
 
 /*
 $Log: ClientTests.java,v $
+Revision 1.4  2003/11/20 15:43:31  nw
+linked in missing tests
+
 Revision 1.3  2003/11/19 18:48:47  nw
 fixed transport bug
 
