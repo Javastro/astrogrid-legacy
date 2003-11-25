46ab9005e0ac09ab481f981d1ef7f4bad5b55020
Martin Hill <Martin.Hill@astrogrid.org>
2003-11-25 15:04:21 +0000

    Extended to be more useful

--- astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/delegate/Certification.java
+++ astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/delegate/Certification.java
@@ -7,16 +7,40 @@ package org.astrogrid.datacenter.delegate;
 
 
 /**
- * Tenporary interface so that DatacenterDelegate has something to work with...
+ * Tenporary class so that DatacenterDelegate has something to work with...
+ * Expect to replace with something from community
  * @author M Hill
  */
 
-public interface Certification
+public class Certification
 {
+   public final static Certification ANONYMOUS = new Certification("Anon", "None");
+   
+   String userId = null;
+   String communityId = null;
+
+   public Certification(String givenUserId, String givenCommunityId)
+   {
+      this.userId = givenUserId;
+      this.communityId = givenCommunityId;
+   }
+   
+   public String getUserId() { return userId; }
+   public String getCommunityId() { return communityId; }
+   public String getCredentials() { return "UNKNOWN"; }
+   
+   
+   public boolean equals(Certification cert)
+   {
+      return (cert.getUserId().equals(this.userId) && (cert.getCommunityId().equals(this.communityId)));
+   }
 }
 
 /*
 $Log: Certification.java,v $
+Revision 1.2  2003/11/25 15:04:21  mch
+Extended to be more useful
+
 Revision 1.1  2003/11/14 00:36:40  mch
 Code restructure
 
