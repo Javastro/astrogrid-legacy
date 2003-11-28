aa3695454ed3fc2edf75427aff53948ec74b6cf1
Martin Hill <Martin.Hill@astrogrid.org>
2003-11-28 19:16:53 +0000

    Extended matching

--- astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/queriers/fits/FitsQuerier.java
+++ astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/queriers/fits/FitsQuerier.java
@@ -1,20 +1,28 @@
 /*
- * $Id: FitsQuerier.java,v 1.4 2003/11/28 18:22:18 mch Exp $
+ * $Id: FitsQuerier.java,v 1.5 2003/11/28 19:16:53 mch Exp $
  *
  * (C) Copyright Astrogrid...
  */
 
 package org.astrogrid.datacenter.queriers.fits;
 
-import java.awt.Rectangle;
+import java.awt.Polygon;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Vector;
 import org.apache.axis.utils.XMLUtils;
+import org.astrogrid.datacenter.adql.generated.Circle;
 import org.astrogrid.datacenter.queriers.DatabaseAccessException;
 import org.astrogrid.datacenter.queriers.QueryResults;
 import org.astrogrid.datacenter.queriers.spi.BaseQuerierSPI;
 import org.astrogrid.datacenter.queriers.spi.QuerierSPI;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import java.awt.geom.Area;
 
 /**
  *
@@ -25,21 +33,79 @@ import org.w3c.dom.Document;
 public class FitsQuerier extends BaseQuerierSPI implements QuerierSPI
 {
 
+   
    /**
     * locates all the fits files in this dataset that overlap the given
     * circular region
     */
    public URL[] findFitsInCone(double ra, double dec, double sr) throws IOException
    {
-//    Document index = loadIndex(asdfasdf);
+      String rawIndex = IndexGenerator.generateIndex(
+         new URL[] {
+               new URL("http://www.roe.ac.uk/~mch/r169411.fit"),
+               new URL("http://www.roe.ac.uk/~mch/r169097.fit"),
+               new URL("http://www.roe.ac.uk/~mch/r169101.fit")
+         }
+      );
+      Document index = loadIndex(rawIndex);
+
+      Ellipse2D cone = new Ellipse2D.Double(ra-sr, dec-sr, sr*2,sr*2);
+      Area matchingArea = new Area(cone);
+      
+      Vector intersectingFits = new Vector();
+      
       //first locate all the coverage nodes in the index
+      NodeList coverages = index.getElementsByTagName("Coverage");
+
+      for (int i=0;i<coverages.getLength();i++)
+      {
+         //extract coverage region
+         Area coverage = new Area(getCoverage( (Element) coverages.item(i)));
+         
+         //does it intersect with circle?
+         if (intersects(matchingArea, coverage))
+         {
+            //find parent tag describing whole file
+            Element parent = (Element) coverages.item(i).getParentNode();
+            Node fileNode = parent.getElementsByTagName("Filename").item(0);
+            
+            intersectingFits.add(fileNode.getNodeValue());
+         }
       
-      
-      Rectangle r = new Rectangle();
+      }
       
       return null;
    }
 
+   public Polygon getCoverage(Element dom)
+   {
+      Polygon region = new Polygon();
+
+      NodeList points = dom.getElementsByTagName("Point");
+         
+      for (int l=0;l<points.getLength();l++)
+      {
+         Node raNode = ((Element) points.item(l)).getElementsByTagName("RA").item(0);
+         Node decNode = ((Element) points.item(l)).getElementsByTagName("Dec").item(0);
+         
+         region.addPoint(Integer.parseInt(raNode.getNodeValue()),
+                         Integer.parseInt(decNode.getNodeValue()));
+      }
+      
+      return region;
+   }
+   
+   /**
+    * returns true if area 1 intersects with area 2
+    * At the moment checks if the rectangular *boundaries* of area 1 intersect
+    * with area 2.
+    * @todo do properly
+    */
+   public boolean intersects(Area a1, Area a2)
+   {
+      return a2.intersects(a1.getBounds2D());
+   }
+   
    /**
     * Loads the index from the given string.  Useful for testing...
     */
@@ -76,6 +142,9 @@ public String getPluginInfo() {
 
 /*
 $Log: FitsQuerier.java,v $
+Revision 1.5  2003/11/28 19:16:53  mch
+Extended matching
+
 Revision 1.4  2003/11/28 18:22:18  mch
 IndexGenerator now working
 
