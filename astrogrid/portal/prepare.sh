a8390b7bf7e30eae94312efb3382942f481edb5c
gps <gps>
2003-12-01 10:49:11 +0000

    *** empty log message ***

--- astrogrid/portal/prepare.sh
+++ astrogrid/portal/prepare.sh
@@ -3,10 +3,11 @@
 rm -fr build/webapp/web
 cp -r site/* build/webapp/
 rm -fr build/dest/*
+cp -r site/* build/dest/
 
 cp site/WEB-INF/menu/menu-schema.xsd build/dest/
-cp site/web/style/css/main.css build/dest/
-cp site/web/scripts/paneltab.js build/dest/
+# cp site/web/style/css/main.css build/dest/
+# cp site/web/scripts/paneltab.js build/dest/
 
 mkdir build/webapp/WEB-INF/lib
 cp -ru $COCOON_HOME/build/webapp/WEB-INF/lib/* build/webapp/WEB-INF/lib
