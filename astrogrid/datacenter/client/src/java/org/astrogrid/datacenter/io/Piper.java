057eb9238b080a2ae2a9d8c1e694f2ec0f53b56b
Martin Hill <Martin.Hill@astrogrid.org>
2003-12-02 17:55:47 +0000

    Added readers, buffered

--- astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/io/Piper.java
+++ astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/io/Piper.java
@@ -7,15 +7,14 @@ package org.astrogrid.datacenter.io;
  * @author M Hill
  */
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class Piper
 {
    /**
     * Helper method for reading all the bytes from the given input stream
-    * and sending them to the given output stream
+    * and sending them to the given output stream.  Remember that it is much
+    * more efficient to use Buffered streams. @see bufferedPipe.
     */
    public static void pipe(InputStream in, OutputStream out) throws IOException
    {
@@ -27,5 +26,39 @@ public class Piper
          read = in.read(block);
       }
    }
+   
+   /** Convenience routine to wrap given streams in Buffered*putStreams and
+    * pipe
+    */
+   public static void bufferedPipe(InputStream in, OutputStream out) throws IOException
+   {
+      pipe(new BufferedInputStream(in), new BufferedOutputStream(out));
+   }
+
+   
+   /**
+    * Helper method for reading all the bytes from the given reader
+    * and sending them to the given writer.  Remember that it is much
+    * more efficient to use Buffered streams. @see bufferedPipe.
+    */
+   public static void pipe(Reader in, Writer out) throws IOException
+   {
+      char[] block = new char[1000];
+      int read = in.read(block);
+      while (read > -1)
+      {
+         out.write(block,0, read);
+         read = in.read(block);
+      }
+   }
+
+   /**
+    * Convenience routine to wrap given readers/writers in Buffereds
+    */
+   public static void bufferedPipe(Reader in, Writer out) throws IOException
+   {
+      pipe(new BufferedReader(in), new BufferedWriter(out));
+   }
+   
 }
 
