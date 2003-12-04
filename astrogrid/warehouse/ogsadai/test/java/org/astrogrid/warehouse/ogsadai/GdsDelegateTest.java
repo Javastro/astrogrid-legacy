143423d9eb25a1d7b4075109e4e2d4bf5b6d1649
Elizabeth Auden <Elizabeth.Auden@astrogrid.org>
2003-12-04 19:01:14 +0000

    Second draft of GdsDelegateTest: this one compiles!  Hurrah!
    04/12/03 Elizabeth Auden

--- astrogrid/warehouse/ogsadai/test/java/org/astrogrid/warehouse/ogsadai/GdsDelegateTest.java
+++ astrogrid/warehouse/ogsadai/test/java/org/astrogrid/warehouse/ogsadai/GdsDelegateTest.java
@@ -1,13 +1,12 @@
 package org.astrogrid.warehouse.ogsadai;
 
-import java.util.GregorianCalendar;
 import junit.framework.TestCase;
-import org.gridforum.ogsi.GridService;
 
 /**
  * Tests {@link GdsDelegate}.
  * Live tests are performed against a grid service.
  */
+
 public class GdsDelegateTest extends TestCase {
 	public GdsDelegateTest() {}
 	
@@ -18,42 +17,47 @@ public class GdsDelegateTest extends TestCase {
 	private String testRegistryUrl1
 		= "http://astrogrid.ast.cam.ac.uk:4040/gdw/services/ogsadai/DAIServiceGroupRegistry";
 	private String testRegistryUrl2
-		= "http:/					/hydra.star.le.ac.uk:8082/gdw/services/ogsadai/DAIServiceGroupRegistry";	
+		= "http://hydra.star.le.ac.uk:8082/gdw/services/ogsadai/DAIServiceGroupRegistry";	
 	private String query1 = "SELECT * FROM first WHERE POS_EQ_DEC &gt; 59.9";	
 	private String query2 = "SELECT * FROM first WHERE POS_EQ_RA &lt; 0.5";
 	private int timeoutValue = 15;		
 
-	public void testGetFactoryUrlFromRegistry(){
+	public void testGetFactoryUrlFromRegistry() throws Exception{
 		GdsDelegate gds = new GdsDelegate();
+		String factoryPort = null;
 		
-		// Make sure that DAI service locator and port type are null.
-		assertNull(gds.gdsrLocator);
-		assertNull(gds.gdsrPort);
-		assertNull(gds.portTypes);
-		assertNull(gds.query);
-		assertNull(gds.result);
-				
+		// Test method with valid testRegistryUrl:
+		
 		// Get factory URL from registry
-		gds.getFactoryUrlFromRegistry(testRegistryUrl1, timeoutValue);
+		try {
+			GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl1, timeoutValue);
+			assertNotNull(gds.factoryPort);
+		}
+		catch (Exception e){}
+		
+		// Test method with invalid testRegistryUrl:
+		
+		String bogusRegistryUrl = "bogus";
+		// Get factory URL from registry
+		try {
+			GdsDelegate.getFactoryUrlFromRegistry(bogusRegistryUrl, timeoutValue);
+			fail("Factory Url passed back from bogus registry");
+		}
+		catch (Exception e){}	
 
-		// Make sure that DAI service locator and port type are not null.
-		assertNotNull(gds.gdsrLocator);
-		assertNotNull(gds.gdsrPort);
-		assertNotNull(gds.portTypes);
-		assertNotNull(gds.query);
-		assertNotNull(gds.result);
+		// Test method with invalid timeoutValue:
+		
+		int bogusTimeoutValue = -1;
+		// Get factory URL from registry
+		try {
+			GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl1, bogusTimeoutValue);
+			fail("Factory Url passed back with timeoutValue set to -1");
+		}
+		catch (Exception e){}
 						
-		// Test that no "can't find registry" exception is thrown.
-		// Test that registry is found.
-		// Test that "no locators" exception is not thrown.
-		// Test that "no handles" exception is not thrown.
-		// Test that haveRegistryUrl is true.
-		// Test that "no factories registered" exception is not thrown.
-		// Test that "could not find factory url" exception is not thrown.
-		// Test factoryURL string
 	}
 	
-	public void testConnect(){
+	public void testConnect() throws Exception {
 
 		GdsDelegate gds = new GdsDelegate();
 				
@@ -61,17 +65,19 @@ public class GdsDelegateTest extends TestCase {
 		// port, service instance, and application port are null.
 		assertFalse(gds.connected);
 		assertNull(gds.factoryPort);
-		assertNull(gds.serviceInstance);
 		assertNull(gds.applicationPort);
 		
 		// Connect the service.
-		gds.connect();
+		try {
+			gds.connect();
+		} catch (Exception e) {
+			fail("Service did not connect: " + e);
+		}
 				
 		// Make sure that service is now connected and factory
 		// port, service instance, and application port are not null.		
 		assertTrue(gds.connected);
 		assertNotNull(gds.factoryPort);
-		assertNotNull(gds.serviceInstance);
 		assertNotNull(gds.applicationPort);
 				
 	}
@@ -81,21 +87,5 @@ public class GdsDelegateTest extends TestCase {
 		// Test "could not execute query" exception is not thrown.
 		// Test result.	
 	} 
-	
-
-
-  /**
-   * Creates a fresh delegate for a unit test and sets up that delegate
-   * according to the test configuration listed above.
-   */
-  /*
-  private GridServiceDelegate getConfiguredDelegate() throws Exception {
-    GridServiceDelegate d = new GridServiceDelegate();
-    d.setSimulating(this.simulating);
-    d.setThrowsExceptions(true);
-    d.setFactoryHandle(this.factoryGsh);
-    return d;
-  }
-  */
   
 }
