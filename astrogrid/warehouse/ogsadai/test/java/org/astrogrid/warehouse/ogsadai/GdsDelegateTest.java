2b92d83e60ccc4892b6616c5b534f140e81a2d34
Guy Rixon <gtr@ast.cam.ac.uk>
2003-12-05 15:35:22 +0000

    testGetFactoryUrlFromRegistry was corrected: it now calls gds.setFactoryHandle() instead of assuming that the handle is set automatically by the SUT. The tests are altered to use either of the two registries if the first-tried is down.

--- astrogrid/warehouse/ogsadai/test/java/org/astrogrid/warehouse/ogsadai/GdsDelegateTest.java
+++ astrogrid/warehouse/ogsadai/test/java/org/astrogrid/warehouse/ogsadai/GdsDelegateTest.java
@@ -5,6 +5,9 @@ import junit.framework.TestCase;
 /**
  * Tests {@link GdsDelegate}.
  * Live tests are performed against a grid service.
+ *
+ * @author E. Auden
+ * @author G. Rixon
  */
 
 public class GdsDelegateTest extends TestCase {
@@ -20,20 +23,44 @@ public class GdsDelegateTest extends TestCase {
 		= "http://hydra.star.le.ac.uk:8082/gdw/services/ogsadai/DAIServiceGroupRegistry";	
 	private String query1 = "SELECT * FROM first WHERE POS_EQ_DEC &gt; 59.9";	
 	private String query2 = "SELECT * FROM first WHERE POS_EQ_RA &lt; 0.5";
-	private int timeoutValue = 15;		
-
-	public void testGetFactoryUrlFromRegistry() throws Exception{
+	private int timeoutValue = 15;
+        
+        
+        /**
+         * Attempts to get one GDSF GSH from either of the two known registries.
+         * Both registries are polled. If both reply, the GSH from the first-named
+         * registry is used.  If neither reply, null is returned.
+         *
+         * @return the GSH, or null if neither registry responded.
+         */
+        private String getFactoryGsh () {
+          String gsh  = null;
+          String gsh1 = null;
+          String gsh2 = null;
+          try {
+            System.out.println("Searching for a GDSF in registry " + testRegistryUrl1);
+            gsh1 = GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl1, timeoutValue);
+            System.out.println("Searching for a GDSF in registry " + testRegistryUrl2);
+            gsh2 = GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl2, timeoutValue);
+          }
+          catch (Exception e) {}
+          if (gsh1 != null) {
+            return gsh1;
+          }
+          else {
+            return gsh2;
+          }
+        }
+        
+         
+	public void testGetFactoryUrlFromRegistry() throws Exception {
 		GdsDelegate gds = new GdsDelegate();
 		String factoryPort = null;
 		
 		// Test method with valid testRegistryUrl:
-		
-		// Get factory URL from registry
-		try {
-			GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl1, timeoutValue);
-			assertNotNull(gds.factoryPort);
-		}
-		catch (Exception e){}
+		String gsh = this.getFactoryGsh();
+                assertNotNull(gsh);
+                System.out.println("Got GDSF GSH: " + gsh);
 		
 		// Test method with invalid testRegistryUrl:
 		
@@ -50,7 +77,7 @@ public class GdsDelegateTest extends TestCase {
 		int bogusTimeoutValue = -1;
 		// Get factory URL from registry
 		try {
-			GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl1, bogusTimeoutValue);
+			GdsDelegate.getFactoryUrlFromRegistry(testRegistryUrl2, bogusTimeoutValue);
 			fail("Factory Url passed back with timeoutValue set to -1");
 		}
 		catch (Exception e){}
@@ -60,7 +87,11 @@ public class GdsDelegateTest extends TestCase {
 	public void testConnect() throws Exception {
 
 		GdsDelegate gds = new GdsDelegate();
-				
+                String gsh = this.getFactoryGsh();
+                assertNotNull(gsh);
+                gds.setFactoryHandle(gsh);
+		System.out.println("Got GDSF GSH: " + gsh);
+                		
 		// Make sure that service is initially unconnected and factory
 		// port, service instance, and application port are null.
 		assertFalse(gds.connected);
