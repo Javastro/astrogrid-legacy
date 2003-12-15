9f9e81e072b46dfd16b5fa5fb839357c28f0e693
Martin Hill <Martin.Hill@astrogrid.org>
2003-12-15 14:32:00 +0000

    Added doc, changed from JobMonitor to general

--- astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/webnotify/WebNotifier.java
+++ astrogrid/datacenter/client/src/java/org/astrogrid/datacenter/webnotify/WebNotifier.java
@@ -6,11 +6,6 @@
 package org.astrogrid.datacenter.webnotify;
 
 
-/**
- *
- * @author M Hill
- */
-
 import java.net.URL;
 
 import javax.xml.rpc.ParameterMode;
@@ -25,37 +20,45 @@ import org.astrogrid.datacenter.snippet.DocHelper;
 import org.astrogrid.datacenter.snippet.StatusHelper;
 import org.w3c.dom.Document;
 
+/**
+ * A class that can be used to notify standard astrogrid services of status
+ * changes.
+ *
+ * @author M Hill
+ */
+
+
 public class WebNotifier
 {
    protected URL endPoint = null;
-   
-   public static String METHOD = "datacenterStatusChanged"; //method called on listener service
-   
+
+   //public static String METHOD = "datacenterStatusChanged"; //method called on listener service
+
    public WebNotifier(URL givenEndPoint)
    {
       this.endPoint = givenEndPoint;
    }
-   
+
    public void tellServer(String queryId, QueryStatus status) throws ServiceException
    {
       Document statusDoc = DocHelper.wrap(makeNotificationTag(queryId, status));
-      
+
       Object[] parms = new Object[]
       {
          XMLUtils.DocumentToString(statusDoc.getOwnerDocument())
       };
-      
+
       Call call = (Call) new Service().createCall() ;
-      
+
       call.setTargetEndpointAddress( endPoint ) ;
-      call.setOperationName( "monitorJob" ) ;  // Set method to invoke
-      call.addParameter("monitorJobXML", XMLType.XSD_STRING,ParameterMode.IN);
+      call.setOperationName( "notify" ) ;  // Set method to invoke
+      call.addParameter("status", XMLType.XSD_STRING,ParameterMode.IN);
       call.setReturnType(XMLType.XSD_STRING);
-      
+
       call.invokeOneWay( parms ) ;
-      
+
    }
-   
+
    /**
     * Returns a string XML element with the notification document
     */
@@ -63,11 +66,14 @@ public class WebNotifier
    {
       return StatusHelper.makeStatusTag(queryId, status);
    }
-   
+
 }
 
 /*
  $Log: WebNotifier.java,v $
+ Revision 1.5  2003/12/15 14:32:00  mch
+ Added doc, changed from JobMonitor to general
+
  Revision 1.4  2003/11/26 16:31:46  nw
  altered transport to accept any query format.
  moved back to axis from castor
