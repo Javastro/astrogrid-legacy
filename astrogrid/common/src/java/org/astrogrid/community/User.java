52a541309a93c3f8d33909908f2fbdfd542e2e32
Martin Hill <Martin.Hill@astrogrid.org>
2003-12-16 13:35:06 +0000

    Added anonymous class constant for 'default' user

--- astrogrid/common/src/java/org/astrogrid/community/User.java
+++ astrogrid/common/src/java/org/astrogrid/community/User.java
@@ -1,15 +1,15 @@
 /*
- * $Id: User.java,v 1.2 2003/12/07 00:33:05 pah Exp $
- * 
+ * $Id: User.java,v 1.3 2003/12/16 13:35:06 mch Exp $
+ *
  * Created on 27-Nov-2003 by Paul Harrison (pah@jb.man.ac.uk)
  *
  * Copyright 2003 AstroGrid. All rights reserved.
  *
- * This software is published under the terms of the AstroGrid 
- * Software License version 1.2, a copy of which has been included 
- * with this distribution in the LICENSE.txt file.  
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
  *
- */ 
+ */
 
 package org.astrogrid.community;
 
@@ -24,6 +24,8 @@ public class User {
     String account;
     String group;
     String token;
+   
+    public final static User ANONYMOUS = new User("Anonymous","Anonymous","None");
     
     public User(){
     }
@@ -77,3 +79,9 @@ public class User {
    }
 
 }
+
+/* $Log: User.java,v $
+ * Revision 1.3  2003/12/16 13:35:06  mch
+ * Added anonymous class constant for 'default' user
+ * */
+
