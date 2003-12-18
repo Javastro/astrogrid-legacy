e01948a1d99cb2dc994800240d0c932de6ce8b0f
Phil Nicolson <Phil.Nicolson@astrogrid.org>
2003-12-18 15:35:15 +0000

    Removed un-used import statements as causing maven build to fail

--- astrogrid/portal/workflow/src/java/org/astrogrid/portal/cocoon/workflow/design/DesignAction.java
+++ astrogrid/portal/workflow/src/java/org/astrogrid/portal/cocoon/workflow/design/DesignAction.java
@@ -12,15 +12,15 @@ package org.astrogrid.portal.cocoon.workflow.design;
 
 import org.apache.log4j.Logger;
 
-import org.astrogrid.i18n.*;
+//import org.astrogrid.i18n.*;
 
 // import org.astrogrid.workflow.*;
 // import org.astrogrid.workflow.design.activity.*;
 
 import org.astrogrid.AstroGridException;
 
-import org.astrogrid.community.delegate.policy.PolicyServiceDelegate;
-import org.astrogrid.community.policy.data.PolicyPermission;
+//import org.astrogrid.community.delegate.policy.PolicyServiceDelegate;
+//import org.astrogrid.community.policy.data.PolicyPermission;
 import org.astrogrid.community.service.authentication.data.SecurityToken;
 
 // import org.astrogrid.mySpace.delegate.mySpaceManager.MySpaceManagerDelegate;
