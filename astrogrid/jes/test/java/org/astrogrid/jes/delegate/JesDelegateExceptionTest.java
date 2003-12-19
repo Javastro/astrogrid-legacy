608719c310315e0b96f745e2021af38c071ad072
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2003-12-19 11:29:42 +0000

    Removed test dependency on AstrogridMessage

--- astrogrid/jes/test/java/org/astrogrid/jes/delegate/JesDelegateExceptionTest.java
+++ astrogrid/jes/test/java/org/astrogrid/jes/delegate/JesDelegateExceptionTest.java
@@ -1,4 +1,4 @@
-/* $Id: JesDelegateExceptionTest.java,v 1.1 2003/10/21 17:37:34 jdt Exp $
+/* $Id: JesDelegateExceptionTest.java,v 1.2 2003/12/19 11:29:42 jl99 Exp $
  * Created on 21-Oct-2003 by John Taylor jdt@roe.ac.uk .
  * 
  * Copyright (C) AstroGrid. All rights reserved.
@@ -53,10 +53,10 @@ public class JesDelegateExceptionTest extends TestCase {
   public final void testConstructor() {
     JesDelegateException jesDelegateException =
       new JesDelegateException(exception);
-    assertEquals(
-      "Should get null since we didn't supply an AstroGridMessage",
-      jesDelegateException.getAstroGridMessage(),
-      null);
+//    assertEquals(
+//      "Should get null since we didn't supply an AstroGridMessage",
+//      jesDelegateException.getAstroGridMessage(),
+//      null);
     assertEquals(
       "Should get the same exception back it was created with",
       jesDelegateException.getCause(),
@@ -67,6 +67,9 @@ public class JesDelegateExceptionTest extends TestCase {
 
 /*
 *$Log: JesDelegateExceptionTest.java,v $
+*Revision 1.2  2003/12/19 11:29:42  jl99
+*Removed test dependency on AstrogridMessage
+*
 *Revision 1.1  2003/10/21 17:37:34  jdt
 *Some simple test classes to get the ball rolling....
 *
