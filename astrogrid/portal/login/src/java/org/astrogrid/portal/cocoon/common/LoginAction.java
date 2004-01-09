a31cf2b28d34593da349368dbf71115ffb6f8f8f
Dave Morris <dmr@roe.ac.uk>
2004-01-09 11:17:50 +0000

    Tidying up ... converted tabs to spaces

--- astrogrid/portal/login/src/java/org/astrogrid/portal/cocoon/common/LoginAction.java
+++ astrogrid/portal/login/src/java/org/astrogrid/portal/cocoon/common/LoginAction.java
@@ -1,11 +1,14 @@
 /*
  * <cvs:source>$Source: /Users/pharriso/Work/ag/repo/git/astrogrid-mirror/astrogrid/portal/login/src/java/org/astrogrid/portal/cocoon/common/LoginAction.java,v $</cvs:source>
  * <cvs:author>$Author: dave $</cvs:author>
- * <cvs:date>$Date: 2004/01/09 11:12:16 $</cvs:date>
- * <cvs:version>$Revision: 1.14 $</cvs:version>
+ * <cvs:date>$Date: 2004/01/09 11:17:50 $</cvs:date>
+ * <cvs:version>$Revision: 1.15 $</cvs:version>
  *
  * <cvs:log>
  *   $Log: LoginAction.java,v $
+ *   Revision 1.15  2004/01/09 11:17:50  dave
+ *   Tidying up ... converted tabs to spaces
+ *
  *   Revision 1.14  2004/01/09 11:12:16  dave
  *   Changes flag to healthy not pass - already used that.
  *
@@ -51,249 +54,249 @@ import java.util.HashMap;
  *
  */
 public class LoginAction extends AbstractAction
-	{
-	/**
-	 * Switch for our debug statements.
-	 *
-	 */
-	public static boolean DEBUG_FLAG = true;
+    {
+    /**
+     * Switch for our debug statements.
+     *
+     */
+    public static boolean DEBUG_FLAG = true;
 
-	public static final String ACTION_PARAM = "action" ;
-	public static final String LOGIN_ACTION = "login" ;
+    public static final String ACTION_PARAM = "action" ;
+    public static final String LOGIN_ACTION = "login" ;
 
-	public static final String NAME_PARAM = "name" ;
-	public static final String PASS_PARAM = "pass" ;
-	public static final String ACCOUNT_PARAM = "message" ;
-	public static final String MESSAGE_PARAM = "account" ;
+    public static final String NAME_PARAM = "name" ;
+    public static final String PASS_PARAM = "pass" ;
+    public static final String ACCOUNT_PARAM = "message" ;
+    public static final String MESSAGE_PARAM = "account" ;
 
-	public static final String POLICY_GROUP    = "guest" ;
-	public static final String POLICY_ACTION   = "read" ;
-	public static final String POLICY_RESOURCE = "portal.site" ;
+    public static final String POLICY_GROUP    = "guest" ;
+    public static final String POLICY_ACTION   = "read" ;
+    public static final String POLICY_RESOURCE = "portal.site" ;
 
-	public static final String EMPTY_NAME_MESSAGE = "Please enter a valid account name" ;
-	public static final String EMPTY_PASS_MESSAGE = "Please enter a valid password" ;
-	public static final String SYSTEM_ERROR_MESSAGE = "System error - login failed" ;
+    public static final String EMPTY_NAME_MESSAGE = "Please enter a valid account name" ;
+    public static final String EMPTY_PASS_MESSAGE = "Please enter a valid password" ;
+    public static final String SYSTEM_ERROR_MESSAGE = "System error - login failed" ;
 
-	public static final String LOGIN_FAIL_MESSAGE = "Login rejected" ;
-	public static final String LOGIN_PASS_MESSAGE = "Login accepted" ;
+    public static final String LOGIN_FAIL_MESSAGE = "Login rejected" ;
+    public static final String LOGIN_PASS_MESSAGE = "Login accepted" ;
 
-	public static final String POLICY_FAIL_MESSAGE = "Login rejected" ;
-	public static final String POLICY_PASS_MESSAGE = "Login accepted" ;
+    public static final String POLICY_FAIL_MESSAGE = "Login rejected" ;
+    public static final String POLICY_PASS_MESSAGE = "Login accepted" ;
 
-	/**
-	 * Our action method.
-	 *
-	 */
-	public Map act(
-		Redirector redirector,
-		SourceResolver resolver,
-		Map objectModel,
-		String source,
-		Parameters params)
-		{
-		//
-		// Initialise our community config.
-		init() ;
-		//
-		// Get our current request and session.
-		Request request = ObjectModelHelper.getRequest(objectModel);
-		Session session = request.getSession();
-		//
-		// Get the action param from our request.
-		String action = request.getParameter(ACTION_PARAM);
-		if (DEBUG_FLAG) System.out.println("LoginAction:act()") ;
-		if (DEBUG_FLAG) System.out.println("  Action : " + action) ;
-		//
-		// Setup our error message.
-		String message = null ;
-		//
-		// Setup our security token.
-		SecurityToken token = null ;
-		//
-		// Setup our authentication
-		boolean authorized = false ;
-		//
-		// Setup our results map.
-		Map results = null ;
-		//
-		// Setup our error flag.
-		boolean healthy = true ;
+    /**
+     * Our action method.
+     *
+     */
+    public Map act(
+        Redirector redirector,
+        SourceResolver resolver,
+        Map objectModel,
+        String source,
+        Parameters params)
+        {
+        //
+        // Initialise our community config.
+        init() ;
+        //
+        // Get our current request and session.
+        Request request = ObjectModelHelper.getRequest(objectModel);
+        Session session = request.getSession();
+        //
+        // Get the action param from our request.
+        String action = request.getParameter(ACTION_PARAM);
+        if (DEBUG_FLAG) System.out.println("LoginAction:act()") ;
+        if (DEBUG_FLAG) System.out.println("  Action : " + action) ;
+        //
+        // Setup our error message.
+        String message = null ;
+        //
+        // Setup our security token.
+        SecurityToken token = null ;
+        //
+        // Setup our authentication
+        boolean authorized = false ;
+        //
+        // Setup our results map.
+        Map results = null ;
+        //
+        // Setup our error flag.
+        boolean healthy = true ;
 
-		//
-		// If the request action is login.
-		if (LOGIN_ACTION.equals(action))
-			{
-			//
-			// Clear the current account info.
-			session.setAttribute("user", null) ;
-			session.setAttribute("community_account", null) ;
-			//
-			// Get the user name and password from our request.
-			String name = request.getParameter(NAME_PARAM);
-			String pass = request.getParameter(PASS_PARAM);
-			if (DEBUG_FLAG) System.out.println("LoginAction:login()") ;
-			if (DEBUG_FLAG) System.out.println("  Name : " + name) ;
-			if (DEBUG_FLAG) System.out.println("  Pass : " + pass) ;
+        //
+        // If the request action is login.
+        if (LOGIN_ACTION.equals(action))
+            {
+            //
+            // Clear the current account info.
+            session.setAttribute("user", null) ;
+            session.setAttribute("community_account", null) ;
+            //
+            // Get the user name and password from our request.
+            String name = request.getParameter(NAME_PARAM);
+            String pass = request.getParameter(PASS_PARAM);
+            if (DEBUG_FLAG) System.out.println("LoginAction:login()") ;
+            if (DEBUG_FLAG) System.out.println("  Name : " + name) ;
+            if (DEBUG_FLAG) System.out.println("  Pass : " + pass) ;
 
-			if (healthy)
-				{
-				//
-				// Check for a null or blank name.
-				if(null == name)
-					{
-					message = EMPTY_NAME_MESSAGE ;
-					}
-				else {
-					name = name.trim() ;
-					if(name.length() <= 0)
-						{
-						healthy = false ;
-						message = EMPTY_NAME_MESSAGE ;
-						}
-					}
-				}
+            if (healthy)
+                {
+                //
+                // Check for a null or blank name.
+                if(null == name)
+                    {
+                    message = EMPTY_NAME_MESSAGE ;
+                    }
+                else {
+                    name = name.trim() ;
+                    if(name.length() <= 0)
+                        {
+                        healthy = false ;
+                        message = EMPTY_NAME_MESSAGE ;
+                        }
+                    }
+                }
 
-			if (healthy)
-				{
-				//
-				// Check for a null or blank password.
-				if(null == pass)
-					{
-					healthy = false ;
-					message = EMPTY_PASS_MESSAGE ;
-					}
-				else {
-					pass = pass.trim() ;
-					if(pass.length() <= 0)
-						{
-						healthy = false ;
-						message = EMPTY_PASS_MESSAGE ;
-						}
-					}
-				}
+            if (healthy)
+                {
+                //
+                // Check for a null or blank password.
+                if(null == pass)
+                    {
+                    healthy = false ;
+                    message = EMPTY_PASS_MESSAGE ;
+                    }
+                else {
+                    pass = pass.trim() ;
+                    if(pass.length() <= 0)
+                        {
+                        healthy = false ;
+                        message = EMPTY_PASS_MESSAGE ;
+                        }
+                    }
+                }
 
-			if (healthy)
-				{
-				//
-				// Try creating a Community delegate.
-				AuthenticationDelegate authenticator = new AuthenticationDelegate();
-				if (null == authenticator)
-					{
-					if (DEBUG_FLAG) System.out.println("FAIL : Null AuthenticationDelegate") ;
-					healthy = false ;
-					message = SYSTEM_ERROR_MESSAGE ;
-					}
-				//
-				// 
-				else {
-					if (DEBUG_FLAG) System.out.println("PASS : Got AuthenticationDelegate") ;
-					//
-					// Try logging in to the Community.
-					try {
-						token = authenticator.authenticateLogin(name, pass);
-						if (null == token)
-							{
-							if (DEBUG_FLAG) System.out.println("FAIL : Null token") ;
-							healthy = false ;
-							message = LOGIN_FAIL_MESSAGE ;
-							}
-						else {
-							if (DEBUG_FLAG) System.out.println("PASS : Got token") ;
-							if (DEBUG_FLAG) System.out.println("  Token   : " + token) ;
-							if (DEBUG_FLAG) System.out.println("  Token   : " + token.getToken()) ;
-							if (DEBUG_FLAG) System.out.println("  Used    : " + token.getUsed()) ;
-							if (DEBUG_FLAG) System.out.println("  Target  : " + token.getTarget()) ;
-							if (DEBUG_FLAG) System.out.println("  Account : " + token.getAccount()) ;
-							if (DEBUG_FLAG) System.out.println("  Start   : " + token.getStartDate()) ;
-							if (DEBUG_FLAG) System.out.println("  End     : " + token.getExpirationDate()) ;
-							message = LOGIN_PASS_MESSAGE ;
-							}
-						}
-					catch(Exception ouch)
-						{
-						healthy = false ;
-						message = SYSTEM_ERROR_MESSAGE ;
-						ouch.printStackTrace();
-						}
-					}
-				}
+            if (healthy)
+                {
+                //
+                // Try creating a Community delegate.
+                AuthenticationDelegate authenticator = new AuthenticationDelegate();
+                if (null == authenticator)
+                    {
+                    if (DEBUG_FLAG) System.out.println("FAIL : Null AuthenticationDelegate") ;
+                    healthy = false ;
+                    message = SYSTEM_ERROR_MESSAGE ;
+                    }
+                //
+                // 
+                else {
+                    if (DEBUG_FLAG) System.out.println("PASS : Got AuthenticationDelegate") ;
+                    //
+                    // Try logging in to the Community.
+                    try {
+                        token = authenticator.authenticateLogin(name, pass);
+                        if (null == token)
+                            {
+                            if (DEBUG_FLAG) System.out.println("FAIL : Null token") ;
+                            healthy = false ;
+                            message = LOGIN_FAIL_MESSAGE ;
+                            }
+                        else {
+                            if (DEBUG_FLAG) System.out.println("PASS : Got token") ;
+                            if (DEBUG_FLAG) System.out.println("  Token   : " + token) ;
+                            if (DEBUG_FLAG) System.out.println("  Token   : " + token.getToken()) ;
+                            if (DEBUG_FLAG) System.out.println("  Used    : " + token.getUsed()) ;
+                            if (DEBUG_FLAG) System.out.println("  Target  : " + token.getTarget()) ;
+                            if (DEBUG_FLAG) System.out.println("  Account : " + token.getAccount()) ;
+                            if (DEBUG_FLAG) System.out.println("  Start   : " + token.getStartDate()) ;
+                            if (DEBUG_FLAG) System.out.println("  End     : " + token.getExpirationDate()) ;
+                            message = LOGIN_PASS_MESSAGE ;
+                            }
+                        }
+                    catch(Exception ouch)
+                        {
+                        healthy = false ;
+                        message = SYSTEM_ERROR_MESSAGE ;
+                        ouch.printStackTrace();
+                        }
+                    }
+                }
 
-			if (healthy)
-				{
-				//
-				// Try creating a policy delegate.
-				PolicyServiceDelegate delegate = new PolicyServiceDelegate();
-				if (null == delegate)
-					{
-					if (DEBUG_FLAG) System.out.println("FAIL : Null PolicyServiceDelegate") ;
-					healthy = false ;
-					message = SYSTEM_ERROR_MESSAGE ;
-					}
-				//
-				// 
-				else {
-					if (DEBUG_FLAG) System.out.println("PASS : Got PolicyServiceDelegate") ;
-					//
-					// Check if the account has permission to see the site.
-					try {
-						authorized = delegate.checkPermissions(name, POLICY_GROUP, POLICY_RESOURCE, POLICY_ACTION);
-						if (authorized)
-							{
-							if (DEBUG_FLAG) System.out.println("PASS : Permission check ok") ;
-							message = POLICY_PASS_MESSAGE ;
-							}
-						else {
-							if (DEBUG_FLAG) System.out.println("FAIL : Permission check failed") ;
-							healthy = false ;
-							message = POLICY_FAIL_MESSAGE ;
-							}
-						}
-					catch(Exception ouch)
-						{
-						healthy = false ;
-						message = SYSTEM_ERROR_MESSAGE ;
-						ouch.printStackTrace();
-						}
-					}
-				}
+            if (healthy)
+                {
+                //
+                // Try creating a policy delegate.
+                PolicyServiceDelegate delegate = new PolicyServiceDelegate();
+                if (null == delegate)
+                    {
+                    if (DEBUG_FLAG) System.out.println("FAIL : Null PolicyServiceDelegate") ;
+                    healthy = false ;
+                    message = SYSTEM_ERROR_MESSAGE ;
+                    }
+                //
+                // 
+                else {
+                    if (DEBUG_FLAG) System.out.println("PASS : Got PolicyServiceDelegate") ;
+                    //
+                    // Check if the account has permission to see the site.
+                    try {
+                        authorized = delegate.checkPermissions(name, POLICY_GROUP, POLICY_RESOURCE, POLICY_ACTION);
+                        if (authorized)
+                            {
+                            if (DEBUG_FLAG) System.out.println("PASS : Permission check ok") ;
+                            message = POLICY_PASS_MESSAGE ;
+                            }
+                        else {
+                            if (DEBUG_FLAG) System.out.println("FAIL : Permission check failed") ;
+                            healthy = false ;
+                            message = POLICY_FAIL_MESSAGE ;
+                            }
+                        }
+                    catch(Exception ouch)
+                        {
+                        healthy = false ;
+                        message = SYSTEM_ERROR_MESSAGE ;
+                        ouch.printStackTrace();
+                        }
+                    }
+                }
 
-			//
-			// If we pass the tests.
-			if ((healthy) && (null != token) && (authorized))
-				{
-				//
-				// Set the current account info in our session.
-				session.setAttribute("user", name) ;
-				session.setAttribute("community_account", token.getAccount()) ;
-				//
-				// Set the current account info in our request.
-				request.setAttribute(ACCOUNT_PARAM, token.getAccount());
-				//
-				// Add our results.
-				results = new HashMap() ;
-				results.put("account", token.getAccount());
-				}
+            //
+            // If we pass the tests.
+            if ((healthy) && (null != token) && (authorized))
+                {
+                //
+                // Set the current account info in our session.
+                session.setAttribute("user", name) ;
+                session.setAttribute("community_account", token.getAccount()) ;
+                //
+                // Set the current account info in our request.
+                request.setAttribute(ACCOUNT_PARAM, token.getAccount());
+                //
+                // Add our results.
+                results = new HashMap() ;
+                results.put("account", token.getAccount());
+                }
 
-			//
-			// Add our display message.
-			request.setAttribute(MESSAGE_PARAM, message);
+            //
+            // Add our display message.
+            request.setAttribute(MESSAGE_PARAM, message);
 
-			}
+            }
 
-		return results;
-		}
+        return results;
+        }
 
-	/**
-	 * Load our community config.
-	 *
-	 */
-	public void init()
-		{
-		if (DEBUG_FLAG) System.out.println("LoginAction:init()") ;
-		//
-		// Load our community config.
-		CommunityConfig.loadConfig();
-		}
+    /**
+     * Load our community config.
+     *
+     */
+    public void init()
+        {
+        if (DEBUG_FLAG) System.out.println("LoginAction:init()") ;
+        //
+        // Load our community config.
+        CommunityConfig.loadConfig();
+        }
 
    /**
     * Our action method.
@@ -337,8 +340,8 @@ public class LoginAction extends AbstractAction
       
       String secure_url = null;
 
-	/*
-	 *
+    /*
+     *
 
       if(HTTPS_CONNECTION.equals(secureConn)) {
          
@@ -356,8 +359,8 @@ public class LoginAction extends AbstractAction
          }
       }
 
-	 *
-	 */
+     *
+     */
 
       String admin = CommunityConfig.getAdministrator();
       String adminEmail = CommunityConfig.getAdministratorEmail();
