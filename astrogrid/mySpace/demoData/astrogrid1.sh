94e23193dc93ead98cf049aedd237b53766e8820
John Taylor <John.Taylor@astrogrid.org>
2004-01-15 12:01:16 +0000

    made to agree with example servers file

--- astrogrid/mySpace/demoData/astrogrid1.sh
+++ astrogrid/mySpace/demoData/astrogrid1.sh
@@ -1,4 +1,4 @@
 #Quick script for packrat to setup the myspace database on astrogrid1
 #14.12.03 JDT
 # you should have astrogrid1.servers in your current directory (the base dir of the webapp)
-java -cp "WEB-INF/classes/:WEB-INF/lib/hsqldb-1.7.1.jar:WEB-INF/lib/log4j-1.2.4.jar" org.astrogrid.mySpace.mySpaceDemo.CreateMySpaceRegistry astrogrid1
+java -cp "WEB-INF/classes/:WEB-INF/lib/hsqldb-1.7.1.jar:WEB-INF/lib/log4j-1.2.4.jar" org.astrogrid.mySpace.mySpaceDemo.CreateMySpaceRegistry astrogrid-mySpace
