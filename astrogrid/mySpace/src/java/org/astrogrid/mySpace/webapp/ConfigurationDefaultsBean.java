b4bee1f14d995460d7ca35d5c531a77ac9c2ef04
John Taylor <John.Taylor@astrogrid.org>
2004-01-19 19:16:30 +0000

    formatting and checkstyle violations

--- astrogrid/mySpace/src/java/org/astrogrid/mySpace/webapp/ConfigurationDefaultsBean.java
+++ astrogrid/mySpace/src/java/org/astrogrid/mySpace/webapp/ConfigurationDefaultsBean.java
@@ -1,4 +1,4 @@
-/* $Id: ConfigurationDefaultsBean.java,v 1.2 2004/01/16 17:09:07 jdt Exp $
+/* $Id: ConfigurationDefaultsBean.java,v 1.3 2004/01/19 19:16:30 jdt Exp $
  * Created on 15-Jan-2004 by John Taylor jdt@roe.ac.uk .
  * 
  * Copyright (C) AstroGrid. All rights reserved.
@@ -36,7 +36,7 @@ public final class ConfigurationDefaultsBean {
    * MySpaceServers URL property
    */
   private String msmsUrl; //an accident waiting to happen
-  
+
   /**
    * getter
    * @return bean prop
@@ -54,61 +54,63 @@ public final class ConfigurationDefaultsBean {
   }
 
   /**
-   * @return
+   * @return property
    */
   public String getMsmsUrl() {
     return msmsUrl;
   }
-
   /**
-   * @return
+   * @return property
    */
   public String getMssUrl() {
     return mssUrl;
   }
 
   /**
-   * @return
-   */
+  * @return property
+  */
   public String getRegistryconf() {
     return registryconf;
   }
 
   /**
-   * @return
+   * @return property
    */
   public String getVersion() {
     return version;
   }
 
   /**
-   * @param string
+   * setter
+   * @param  string property
    */
-  public void setMsmsUrl(String string) {
+  public void setMsmsUrl(final String string) {
     msmsUrl = string;
   }
 
   /**
-   * @param string
+   * setter
+   * @param  string property
    */
-  public void setMssUrl(String string) {
+  public void setMssUrl(final String string) {
     mssUrl = string;
   }
 
   /**
-   * @param string
+   * setter
+   * @param  string property
    */
-  public void setRegistryconf(String string) {
+  public void setRegistryconf(final String string) {
     registryconf = string;
   }
-
   /**
-   * @param string
+   * setter
+   * @param  string property
    */
-  public void setVersion(String string) {
+  public void setVersion(final String string) {
     version = string;
   }
-  
+
   /**
    * Attempt to set the values from MMC
    *
@@ -116,23 +118,29 @@ public final class ConfigurationDefaultsBean {
   public ConfigurationDefaultsBean() {
     version = MMC.getProperty(MMC.GENERAL_VERSION_NUMBER, MMC.GENERAL_CATEGORY);
     registryconf = MMC.getProperty(MMC.REGISTRYCONF, MMC.CATLOG);
-    msmUrl = MMC.getProperty(MMC.mySpaceManagerLoc,MMC.CATLOG); 
-    mssUrl = MMC.getProperty(MMC.serverManagerLoc,MMC.CATLOG); 
-    msmsUrl = MMC.getProperty(MMC.MYSPACEMANAGERURLs,MMC.CATLOG); 
+    msmUrl = MMC.getProperty(MMC.mySpaceManagerLoc, MMC.CATLOG);
+    mssUrl = MMC.getProperty(MMC.serverManagerLoc, MMC.CATLOG);
+    msmsUrl = MMC.getProperty(MMC.MYSPACEMANAGERURLs, MMC.CATLOG);
   }
-  
+
+  /**
+   * Sets the configuration (MMC) with the values in this bean   *
+   */
   public void update() {
     MMC.setProperty(MMC.GENERAL_VERSION_NUMBER, MMC.GENERAL_CATEGORY, version);
     MMC.setProperty(MMC.REGISTRYCONF, MMC.CATLOG, registryconf);
-    MMC.setProperty(MMC.mySpaceManagerLoc,MMC.CATLOG, msmUrl); 
-    MMC.setProperty(MMC.serverManagerLoc,MMC.CATLOG, mssUrl); 
-    MMC.setProperty(MMC.MYSPACEMANAGERURLs,MMC.CATLOG, msmsUrl); 
+    MMC.setProperty(MMC.mySpaceManagerLoc, MMC.CATLOG, msmUrl);
+    MMC.setProperty(MMC.serverManagerLoc, MMC.CATLOG, mssUrl);
+    MMC.setProperty(MMC.MYSPACEMANAGERURLs, MMC.CATLOG, msmsUrl);
   }
 
 }
 
 /*
 *$Log: ConfigurationDefaultsBean.java,v $
+*Revision 1.3  2004/01/19 19:16:30  jdt
+*formatting and checkstyle violations
+*
 *Revision 1.2  2004/01/16 17:09:07  jdt
 *Changes to allow the changing and saving of properties
 *
