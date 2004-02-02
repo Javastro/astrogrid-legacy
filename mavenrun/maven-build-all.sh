a17e775cdb74387dcd8bc99d460bc1818d9b5504
John Taylor <John.Taylor@astrogrid.org>
2004-02-02 14:44:00 +0000

    Removed old projects, and add a new base project.

--- mavenrun/maven-build-all.sh
+++ mavenrun/maven-build-all.sh
@@ -13,6 +13,11 @@ echo "AstroGrid Build ($DATE)" >> $BUILD_DIR/$LOG_FILE 2>&1
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" >> $BUILD_DIR/$LOG_FILE 2>&1
 echo >> $BUILD_DIR/$LOG_FILE 2>&1
 
+#Check out the maven project first, since this may act as a base
+#from which other projects may inherit
+#[may need to give this its own buildscript if maven-build.sh isn't appropriate]
+$SCRIPTHOME/maven-build.sh maven >> $BUILD_DIR/$LOG_FILE 2>&1
+#Now the "real" projects.
 $SCRIPTHOME/maven-build.sh applications >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh common >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh community >> $BUILD_DIR/$LOG_FILE 2>&1
@@ -20,12 +25,8 @@ $SCRIPTHOME/maven-build.sh datacenter >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh jes >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh mySpace >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh portal >> $BUILD_DIR/$LOG_FILE 2>&1
-#$SCRIPTHOME/maven-build.sh portalB >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh registry >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh scripting >> $BUILD_DIR/$LOG_FILE 2>&1
-#$SCRIPTHOME/maven-build.sh templateservice >> $BUILD_DIR/$LOG_FILE 2>&1
-#$SCRIPTHOME/maven-build.sh tools >> $BUILD_DIR/$LOG_FILE 2>&1
-#$SCRIPTHOME/maven-build.sh ui >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh warehouse >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build.sh workflow >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-maven-site.sh >> $BUILD_DIR/$LOG_FILE 2>&1
