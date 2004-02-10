8bdff3b3e9b1fc03e7b38a5c94320f9bf0e30af5
Paul Harrison <paul.harrison@manchester.ac.uk>
2004-02-10 14:38:12 +0000

    add the necessary stubs

--- astrogrid/applications/src/java/org/astrogrid/applications/manager/WebServiceApplicationController.java
+++ astrogrid/applications/src/java/org/astrogrid/applications/manager/WebServiceApplicationController.java
@@ -1,5 +1,5 @@
 /*
- * $Id: WebServiceApplicationController.java,v 1.1 2004/02/10 14:30:20 pah Exp $
+ * $Id: WebServiceApplicationController.java,v 1.2 2004/02/10 14:38:12 pah Exp $
  *
  * Created on 09 February 2004 by Paul Harrison
  * Copyright 2003 AstroGrid. All rights reserved.
@@ -11,5 +11,32 @@
 
 package org.astrogrid.applications.manager;
 
+import org.astrogrid.applications.ParameterValues;
+import org.astrogrid.community.User;
+
 public class WebServiceApplicationController extends AbstractApplicationController {
+
+   /* (non-Javadoc)
+    * @see org.astrogrid.applications.manager.ApplicationController#initializeApplication(java.lang.String, java.lang.String, java.lang.String, org.astrogrid.community.User, org.astrogrid.applications.ParameterValues)
+    */
+   public String initializeApplication(String applicationID, String jobstepID, String jobMonitorURL, User user, ParameterValues parameters) {
+      // TODO Auto-generated method stub
+      throw new  UnsupportedOperationException("WebServiceApplicationController.initializeApplication() not implemented");
+   }
+
+   /* (non-Javadoc)
+    * @see org.astrogrid.applications.manager.ApplicationController#executeApplication(java.lang.String)
+    */
+   public boolean executeApplication(String executionId) {
+      // TODO Auto-generated method stub
+      throw new  UnsupportedOperationException("WebServiceApplicationController.executeApplication() not implemented");
+   }
+
+   /* (non-Javadoc)
+    * @see org.astrogrid.applications.manager.ApplicationController#queryApplicationExecutionStatus(java.lang.String)
+    */
+   public String queryApplicationExecutionStatus(String executionId) {
+      // TODO Auto-generated method stub
+      throw new  UnsupportedOperationException("WebServiceApplicationController.queryApplicationExecutionStatus() not implemented");
+   }
 }
