ec75b11348978a4c0659c1c327c0923931213589
Paul Harrison <paul.harrison@manchester.ac.uk>
2004-02-10 14:30:20 +0000

    skeleton to show where this is in the hierarchy

---
+++ astrogrid/applications/src/java/org/astrogrid/applications/manager/WebServiceApplicationController.java
@@ -0,0 +1,15 @@
+/*
+ * $Id: WebServiceApplicationController.java,v 1.1 2004/02/10 14:30:20 pah Exp $
+ *
+ * Created on 09 February 2004 by Paul Harrison
+ * Copyright 2003 AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ */
+
+package org.astrogrid.applications.manager;
+
+public class WebServiceApplicationController extends AbstractApplicationController {
+}
