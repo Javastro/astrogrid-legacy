d8d340b620a040b878e425d705ba08790cf93710
John Taylor <John.Taylor@astrogrid.org>
2004-02-11 15:06:07 +0000

    Modifications for new build procedure and release build prcedure.  The number of scripts is growing alarmingly - some refactoring required I think.

--- mavenrun/maven-build-all-release.sh
+++ mavenrun/maven-build-all-release.sh
@@ -17,17 +17,17 @@ echo >> $BUILD_DIR/$LOG_FILE 2>&1
 #from which other projects may inherit
 $SCRIPTHOME/cvs-checkout.sh maven-base >> $BUILD_DIR/$LOG_FILE 2>&1
 #Now the "real" projects.
-$SCRIPTHOME/maven-build.sh applications >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh common >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh community >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh datacenter >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh jes >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh mySpace >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh portal >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh registry >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh scripting >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh warehouse >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh workflow >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh applications >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh common >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh community >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh datacenter >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh jes >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh mySpace >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh portal >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh registry >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh scripting >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh warehouse >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-release.sh workflow >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-maven-site.sh >> $BUILD_DIR/$LOG_FILE 2>&1
 
 
---
+++ mavenrun/maven-build-new.sh
@@ -0,0 +1,55 @@
+OLDDIR=$PWD
+
+PROJECT_NAME=$1
+
+DATE=`date`
+
+echo
+echo "[ag-build] building $PROJECT_NAME"
+echo "[ag-build] build log: maven-build-$PROJECT_NAME.log"
+echo
+
+BUILD_HOME=/home/maven/build
+SCRIPTHOME=/home/maven/mavenrun
+PROJECT_HOME=$BUILD_HOME/astrogrid/$PROJECT_NAME
+LOG_FILE=$BUILD_HOME/maven-build-$PROJECT_NAME.log
+cd $BUILD_HOME
+echo "[ag-build-$PROJECT_NAME] remove old log"
+rm $LOG_FILE
+
+echo "[ag-build-$PROJECT_NAME] sourcing java"
+source $HOME/.bash-config/java >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] sourcing maven"
+source $HOME/.bash-config/maven >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] sourcing cvs"
+source $HOME/.bash-config/cvs >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] build home: $BUILD_HOME"
+cd $BUILD_HOME >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] remove old SNAPSHOTS"
+sh $SCRIPTHOME/maven-remove-jars.sh $PROJECT_NAME >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] removing $PROJECT_HOME"
+rm -fr $PROJECT_HOME >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] cvs checkout"
+cvs -d $CVSROOT co -A astrogrid/$PROJECT_NAME >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] project home: $PROJECT_HOME"
+cd $PROJECT_HOME >> $LOG_FILE 2>&1
+
+echo "[ag-build-$PROJECT_NAME] generate and deploy site; deploy SNAPSHOT"
+maven astrogrid-clean >> $LOG_FILE 2>&1 
+maven astrogrid-deploy-site >> $LOG_FILE 2>&1 
+maven astrogrid-deploy-snapshot >> $LOG_FILE 2>&1
+echo "[ag-build-$PROJECT_NAME] deploy build log"
+cp $LOG_FILE /var/www/www/maven/build/log
+
+echo "[ag-build-$PROJECT_NAME] back to start dir: $OLDDIR"
+cd $OLDDIR
+
+
+echo
