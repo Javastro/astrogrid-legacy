e317d3bfc03ceea54c5d927ac5fb0b57cda564c6
Martin Hill <Martin.Hill@astrogrid.org>
2004-02-14 13:11:53 +0000

    Local JAR updator

---
+++ astrogrid/lib/updateAstrogridJars.bat
@@ -0,0 +1,8 @@
+: Updates the library in /astrogrid/lib with the latest jars from maven
+
+pscp -2 mch@cvs.astrogrid.org:/var/www/www/maven/astrogrid-jes/jars/astrogrid-jes-SNAPSHOT.jar c:\astrogrid\lib\
+pscp -2 mch@cvs.astrogrid.org:/var/www/www/maven/astrogrid-applications/jars/astrogrid-applications-SNAPSHOT.jar c:\astrogrid\lib\
+pscp -2 mch@cvs.astrogrid.org:/var/www/www/maven/astrogrid-mySpace/jars/astrogrid-mySpace-SNAPSHOT.jar c:\astrogrid\lib\
+
+
+
