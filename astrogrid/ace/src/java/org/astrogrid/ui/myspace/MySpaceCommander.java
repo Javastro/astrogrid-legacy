6be7e09adb829aa8356492bafe5ae52b62e125a6
Martin Hill <Martin.Hill@astrogrid.org>
2004-02-17 11:56:12 +0000

    Added List command

--- astrogrid/ace/src/java/org/astrogrid/ui/myspace/MySpaceCommander.java
+++ astrogrid/ace/src/java/org/astrogrid/ui/myspace/MySpaceCommander.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MySpaceCommander.java,v 1.1 2004/02/17 03:47:04 mch Exp $
+ * $Id: MySpaceCommander.java,v 1.2 2004/02/17 11:56:12 mch Exp $
  *
  * Copyright 2003 AstroGrid. All rights reserved.
  *
@@ -24,6 +24,8 @@ import java.io.File;
 import java.io.IOException;
 import org.astrogrid.community.Account;
 import org.astrogrid.vospace.VospaceRL;
+import org.astrogrid.vospace.delegate.MySpaceEntry;
+import org.astrogrid.vospace.delegate.MySpaceFolder;
 import org.astrogrid.vospace.delegate.VoSpaceClient;
 import org.astrogrid.vospace.delegate.VoSpaceDelegateFactory;
 
@@ -45,14 +47,19 @@ public class MySpaceCommander
       System.out.println("VospaceRL is of the form: vospace://protocol.authority/endpointpath#myspace/path/filename");
       System.out.println("eg    vospace://http.grendel12.roe.ac.uk:8080/astrogrid-mySpace#test.astrogrid.org/avodemo/serv1/votable/6dfResults");
    }
+
+   public static VoSpaceClient contactServer(VospaceRL vorl) throws IOException {
+      System.out.println("Connecting to myspace...");
+      return VoSpaceDelegateFactory.createDelegate(Account.ANONYMOUS, vorl.getDelegateEndpoint().toString());
       
+   }
+   
    /**
     * Deletes the file at the given VoRL
     */
    public static void delete(VospaceRL vorl) throws IOException
    {
-      System.out.println("Connecting to myspace...");
-      VoSpaceClient delegate = VoSpaceDelegateFactory.createDelegate(Account.ANONYMOUS, vorl.getDelegateEndpoint().toString());
+      VoSpaceClient delegate = contactServer(vorl);
       
       System.out.println("...deleting...");
       delegate.delete(vorl.getDelegateFileRef());
@@ -63,11 +70,42 @@ public class MySpaceCommander
    /**
     * Lists the files at the given VoRL
     */
-   public static void list(VospaceRL server)
+   public static void list(VospaceRL server, String filter) throws IOException
    {
-      throw new UnsupportedOperationException("Not written list yet");
+      VoSpaceClient delegate = contactServer(server);
+      
+      System.out.println("Getting List (filter='"+filter+"')...");
+      File root = delegate.getEntries(null, filter);
+
+      //recursively display tree
+      System.out.println("Path, Type, Owner, Created Date, Expiry Date, Size, Permissions:");
+      printListEntry(root);
+      
    }
 
+   /** Called by list to recursively print directory/entry tree */
+   private static void printListEntry(File entry) {
+      System.out.print(entry.getPath());
+      
+      if (entry instanceof MySpaceEntry) {
+         //print more file details
+         MySpaceEntry mse = (MySpaceEntry) entry;
+         System.out.print( ",   "+mse.getType()+",  "+mse.getOwner()+", "+
+                             mse.getCreated()+", "+mse.getExpires()+", "+
+                             mse.getSize()+", "+mse.getPermissions());
+      }
+      System.out.println();
+      
+      if (entry instanceof MySpaceFolder) {
+         File[] files = ((MySpaceFolder) entry).listFiles();
+         
+         for (int i=0;i<files.length;i++) {
+            printListEntry(files[i]);
+         }
+      }
+   }
+      
+   
    /**
     * Gets the files from the given VoRL to the given File
     */
@@ -91,7 +129,9 @@ public class MySpaceCommander
          delete(new VospaceRL(args[1]));
       }
       else if (command.equals("list")) {
-         list(new VospaceRL(args[1]));
+         String filter = "*";
+         if (args.length>2) filter = args[2];
+         list(new VospaceRL(args[1]), filter);
       }
       else if (command.equals("get")) {
          if (args.length<3) {
@@ -111,6 +151,9 @@ public class MySpaceCommander
 
 /*
 $Log: MySpaceCommander.java,v $
+Revision 1.2  2004/02/17 11:56:12  mch
+Added List command
+
 Revision 1.1  2004/02/17 03:47:04  mch
 Naughtily large lump of various fixes for demo
 
