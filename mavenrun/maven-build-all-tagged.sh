d3001f41956f3f16dcef85ceb4fad57c5649fe86
John Taylor <John.Taylor@astrogrid.org>
2004-02-19 01:17:16 +0000

    corrected tag

--- mavenrun/maven-build-all-tagged.sh
+++ mavenrun/maven-build-all-tagged.sh
@@ -20,19 +20,19 @@ echo >> $BUILD_DIR/$LOG_FILE 2>&1
 
 #Check out the maven project first, since this may act as a base
 #from which other projects may inherit
-$SCRIPTHOME/maven-build-tagged.sh maven-base >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh maven-base $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 #Now the "real" projects.
-$SCRIPTHOME/maven-build-tagged.sh common TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh applications TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh community TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh datacenter TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh jes TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh mySpace TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh portal TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh registry TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh scripting TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh warehouse TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged.sh workflow TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh common $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh applications $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh community $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh datacenter $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh jes $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh mySpace $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh portal $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh registry $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh scripting $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh warehouse $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged.sh workflow $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 #this last one also needs attention
 $SCRIPTHOME/maven-build-maven-site.sh >> $BUILD_DIR/$LOG_FILE 2>&1
 
