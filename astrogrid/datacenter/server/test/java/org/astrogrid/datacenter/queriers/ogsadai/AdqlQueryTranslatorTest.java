a1a6b90cc9691afd7f27e9d0ae482f7317d32018
Elizabeth Auden <Elizabeth.Auden@astrogrid.org>
2004-02-25 00:22:10 +0000

    Made ogsadai directory and copied AdqlQueryTranslator from /sql/ to /ogsadai/.
    ElizabethAuden, 24 February 2004

---
+++ astrogrid/datacenter/server/test/java/org/astrogrid/datacenter/queriers/ogsadai/AdqlQueryTranslatorTest.java
@@ -0,0 +1,128 @@
+/*$Id: AdqlQueryTranslatorTest.java,v 1.1 2004/02/25 00:22:10 eca Exp $
+ * Created on 29-Aug-2003
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.datacenter.queriers.sql;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.astrogrid.datacenter.adql.generated.Select;
+import org.astrogrid.datacenter.queriers.QueryTranslator;
+/** test the query translator - maybe n the wrong package, but convenient to have it here for now.
+ * @author Noel Winstanley nw@jb.man.ac.uk 29-Aug-2003
+ * * @todo add wider range of tests.
+ */
+public class AdqlQueryTranslatorTest extends TestCase {
+
+    /**
+     * Constructor for MySQLVisitorTest.
+     * @param arg0
+     */
+    public AdqlQueryTranslatorTest(String arg0) {
+        super(arg0);
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(AdqlQueryTranslatorTest.class);
+    }
+    public void test1() throws Exception {
+        processFile("sample1.xml");
+    }
+
+    public void test2() throws Exception {
+        processFile("sample2.xml");
+    }
+
+    public void test3() throws Exception {
+        processFile("sample3.xml");
+    }
+    
+    public void test4() throws Exception {
+        processFile("sample4.xml");
+    }
+    
+    public void test5() throws Exception {
+        processFile("sample5.xml");
+    }
+    public void test6() throws Exception {
+        processFile("sample6.xml");
+    }
+    protected void processFile(String path) throws Exception {
+          InputStream is = this.getClass().getResourceAsStream(path);
+          assertNotNull(is);
+          Reader reader = new InputStreamReader(is);
+          Select query = Select.unmarshalSelect(reader);
+          assertNotNull(query);
+          assertTrue(query.isValid());
+
+          Properties results = new Properties();
+          InputStream propsStream  = this.getClass().getResourceAsStream("mysql-translations.properties");
+          results.load(propsStream);
+          assertFalse(results.isEmpty());
+            try {
+            QueryTranslator visitor = new AdqlQueryTranslator();
+            String sql = visitor.translate(query);
+            assertNotNull(sql);
+             System.out.println(sql);
+             String expectedSQL = results.getProperty(path);
+             assertNotNull(expectedSQL);
+             assertEquals(expectedSQL.trim(),sql.trim());
+          } catch (Exception e) {
+              e.printStackTrace();
+              fail("Caught Exception " + e.getMessage());
+          }
+ 
+        
+
+      }
+
+}
+
+
+/* 
+$Log: AdqlQueryTranslatorTest.java,v $
+Revision 1.1  2004/02/25 00:22:10  eca
+Made ogsadai directory and copied AdqlQueryTranslator from /sql/ to /ogsadai/.
+
+ElizabethAuden, 24 February 2004
+
+Revision 1.1  2003/11/28 16:10:30  nw
+finished plugin-rewrite.
+added tests to cover plugin system.
+cleaned up querier & queriermanager. tested
+
+Revision 1.3  2003/11/27 00:52:58  nw
+refactored to introduce plugin-back end and translator maps.
+interfaces in place. still broken code in places.
+
+Revision 1.2  2003/11/21 17:37:56  nw
+made a start tidying up the server.
+reduced the number of failing tests
+found commented out code
+
+Revision 1.1  2003/11/14 00:38:29  mch
+Code restructure
+
+Revision 1.1  2003/10/14 12:59:40  nw
+moved SqlQueryTranslator here from removed adql package
+
+Revision 1.2  2003/09/17 14:53:02  nw
+tidied imports
+
+Revision 1.1  2003/09/03 14:45:59  nw
+renamed test to match renamed class
+
+Revision 1.1  2003/09/02 14:41:15  nw
+added tests for ADQL parser
+ 
+*/
