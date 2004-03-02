8151e66a7ebc52678fdc6476bb53fe5ea9dc89a3
Pedro Contreras <Pedro.Contreras@astrogrid.org>
2004-03-02 04:38:00 +0000

    *** empty log message ***

---
+++ astrogrid/registry/experiment/castor/CommandLineMarshall.java
@@ -0,0 +1,33 @@
+package astrogrid.registry.experiment.castor;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2004</p>
+ * <p>Company: The Queen's University of Belfast</p>
+ * @author Pedro Contreras
+ * @version 1.0
+ */
+import java.io.*;
+
+public class CommandLineMarshall {
+  public CommandLineMarshall(String cmdline) {
+	try {
+	 String line;
+	 Process p = Runtime.getRuntime().exec(cmdline);
+	 BufferedReader input = new BufferedReader (new InputStreamReader(p.getInputStream()));
+	 while ((line = input.readLine()) != null) {
+	   System.out.println(line);
+	   }
+	 input.close();
+	 } 
+	catch (Exception err) {
+	 err.printStackTrace();
+	 }
+   }
+   
+  public static void main(String[] args) {
+	new commandLineMarshall("/astrogrid/astrogrid/registry/experiment/castor/script/runAllScript.bat");
+  }
+
+}
