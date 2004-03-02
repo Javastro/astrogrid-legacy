74f020eda3b836fc5e5a5e0859967568351c5977
John Taylor <John.Taylor@astrogrid.org>
2004-03-02 21:43:52 +0000

    *** empty log message ***

--- mavenrun/maven-build-all.sh
+++ mavenrun/maven-build-all.sh
@@ -18,26 +18,26 @@ echo >> $BUILD_DIR/$LOG_FILE 2>&1
 
 #Check out the maven project first, since this may act as a base
 #from which other projects may inherit
-$SCRIPTHOME/maven-build-new.sh maven-base >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh maven-base >> $BUILD_DIR/$LOG_FILE 2>&1
 #Now the "real" projects.
-$SCRIPTHOME/maven-build-new.sh common >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh workflow-objects >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh applications >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh community >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh datacenter >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh jes >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh mySpace >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh portal >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh registry >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh scripting >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build.sh warehouse >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh workflow >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-new.sh maven-site >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh common >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh workflow-objects >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh applications >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build.sh community >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh datacenter >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh jes >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh mySpace >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build.sh portal >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build.sh registry >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh scripting >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build.sh warehouse >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh workflow >> $BUILD_DIR/$LOG_FILE 2>&1
+#$SCRIPTHOME/maven-build-new.sh maven-site >> $BUILD_DIR/$LOG_FILE 2>&1
 
 echo "Moving docs to snapshot location" >> $BUILD_DIR/$LOG_FILE 2>&1
-cp -r $MAVEN_PUBLIC/build/* $SNAPSHOTDOCS
+cp -r $MAVEN_PUBLIC/build/* $SNAPSHOTDOCS >> $BUILD_DIR/$LOG_FILE 2>&1
 echo "Copying redirect pages over old location" >> $BUILD_DIR/$LOG_FILE 2>&1
-cp -r $DOCLOCATION/redirect/* $MAVEN_PUBLIC/build
+cp -r $DOCLOCATION/redirect/* $MAVEN_PUBLIC/build >> $BUILD_DIR/$LOG_FILE 2>&1
 
 echo >> $BUILD_DIR/$LOG_FILE 2>&1
 echo "AstroGrid Build ($DATE)" >> $BUILD_DIR/$LOG_FILE 2>&1
