c48a93ee6e4e3d812e5f26706d16a6fc45ec9241
Pedro Contreras <Pedro.Contreras@astrogrid.org>
2004-03-03 09:48:56 +0000

    *** empty log message ***

--- astrogrid/registry/experiment/castor/CommandLineMarshall.java
+++ astrogrid/registry/experiment/castor/CommandLineMarshall.java
@@ -27,7 +27,7 @@ public class CommandLineMarshall {
    }
    
   public static void main(String[] args) {
-  	//This line should be edited and point to the runAllScript.bat file address
+  	//This line should be edited and pointed to the runAllScript.bat file address
 	new CommandLineMarshall("D:\eclipse\workspace\astrogrid\registry\experiment\castor\script\runAllScript.bat");
   }
 
