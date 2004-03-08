384c48b2f438eb47bbfb8fc6d127df3d2b678dfa
gps <gps>
2004-03-08 12:37:06 +0000

    - taken out the hardwired VM

--- astrogrid/portal/datacenter/src/java/org/astrogrid/portal/datacenter/acting/MySpaceSaveAction.java
+++ astrogrid/portal/datacenter/src/java/org/astrogrid/portal/datacenter/acting/MySpaceSaveAction.java
@@ -62,9 +62,7 @@ public class MySpaceSaveAction extends AbstractAction {
     Session session = request.getSession(true);
     
     String endPoint = utils.getAnyParameter("myspace-end-point", "http://localhost:8080/myspace", params, request, session);
-    logger.error("JDT! myspace-end-point "+endPoint);
-    endPoint="http://vm05.astrogrid.org:8080/astrogrid-mySpace/services/MySpaceManager"; //FIXME TODO This is a temporary demo hack
-    logger.error("JDT! myspace-end-point hacked to "+endPoint);
+
     try {
       MySpaceClient delegate = MySpaceDelegateFactory.createDelegate(endPoint);
       logger.debug("[act] myspace-delegate-class: " + delegate.getClass().getName());
@@ -83,6 +81,7 @@ public class MySpaceSaveAction extends AbstractAction {
 			logger.debug("[act] mySpaceName: " + mySpaceName);
 
       String adqlDocument = utils.getAnyParameter("adql-query", params, request, session);
+      logger.debug("[act] adqlDocument: " + adqlDocument);
       
       boolean saved =
           delegate.saveDataHolding(
