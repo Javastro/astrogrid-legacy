fcea05df03d9d9b96065b0aa95845263e78d986e
Kevin Benson <kmb@mssl.ucl.ac.uk>
2004-03-10 11:56:58 +0000

    result of merge with 09-03 for kmb

--- astrogrid/registry/common/src/java/org/astrogrid/registry/RegistryException.java
+++ astrogrid/registry/common/src/java/org/astrogrid/registry/RegistryException.java
@@ -10,10 +10,12 @@ public class RegistryException extends Exception {
    public RegistryException(Throwable cause) {
       //Need to log here.
       super(cause);
+      cause.printStackTrace();      
    }
 
    public RegistryException(String msg, Throwable cause) {
       //Need to log here.
       super(msg, cause);
+      cause.printStackTrace();      
    }  
 }
