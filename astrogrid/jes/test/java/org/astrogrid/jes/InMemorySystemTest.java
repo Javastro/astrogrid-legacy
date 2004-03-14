c89357ebc0d8d6bbff3c1d27b045b4ea31a1f040
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-03-14 23:03:16 +0000

    fixed to fit in with naming change in picocontainer

--- astrogrid/jes/test/java/org/astrogrid/jes/InMemorySystemTest.java
+++ astrogrid/jes/test/java/org/astrogrid/jes/InMemorySystemTest.java
@@ -1,4 +1,4 @@
-/*$Id: InMemorySystemTest.java,v 1.9 2004/03/09 14:42:18 nw Exp $
+/*$Id: InMemorySystemTest.java,v 1.10 2004/03/14 23:03:16 nw Exp $
  * Created on 19-Feb-2004
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -32,7 +32,7 @@ import org.apache.axis.utils.XMLUtils;
 import org.exolab.castor.xml.Marshaller;
 import org.picocontainer.MutablePicoContainer;
 import org.picocontainer.defaults.CachingComponentAdapter;
-import org.picocontainer.defaults.ConstructorComponentAdapterFactory;
+import org.picocontainer.defaults.DefaultComponentAdapterFactory;
 import org.picocontainer.defaults.ImplementationHidingComponentAdapter;
 import org.w3c.dom.Document;
 
@@ -74,12 +74,12 @@ public class InMemorySystemTest extends AbstractTestWorkflowInputs {
                        
             // scheduler that notifies of completion by releasing a barrier
             pico.unregisterComponent(JobScheduler.class);
-            ConstructorComponentAdapterFactory fac = new ConstructorComponentAdapterFactory();
+            DefaultComponentAdapterFactory fac = new DefaultComponentAdapterFactory();
             pico.registerComponent(
                 new ImplementationHidingComponentAdapter(
                     new CachingComponentAdapter(
                         fac.createComponentAdapter(JobScheduler.class,ObservableJobScheduler.class,null)
-                        //new ConstructorComponentAdapter(JobScheduler.class,ObservableJobScheduler.class)
+                        //new DefaultComponentAdapterFactory(JobScheduler.class,ObservableJobScheduler.class)
                     )
                   )
                 );
@@ -169,6 +169,9 @@ public class InMemorySystemTest extends AbstractTestWorkflowInputs {
 
 /* 
 $Log: InMemorySystemTest.java,v $
+Revision 1.10  2004/03/14 23:03:16  nw
+fixed to fit in with naming change in picocontainer
+
 Revision 1.9  2004/03/09 14:42:18  nw
 updated to track version change in picocontainer
 
