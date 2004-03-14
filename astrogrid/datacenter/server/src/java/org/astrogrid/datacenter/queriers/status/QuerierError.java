ba7d89573a0777596ce8ea033fd8a3e1ac8fbbb8
Martin Hill <Martin.Hill@astrogrid.org>
2004-03-14 19:12:33 +0000

    Added stack trace to error details

--- astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/queriers/status/QuerierError.java
+++ astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/queriers/status/QuerierError.java
@@ -1,11 +1,13 @@
 /*
- * $Id: QuerierError.java,v 1.2 2004/03/14 00:39:55 mch Exp $
+ * $Id: QuerierError.java,v 1.3 2004/03/14 19:12:33 mch Exp $
  *
  * (C) Copyright Astrogrid...
  */
 
 package org.astrogrid.datacenter.queriers.status;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import org.astrogrid.datacenter.query.QueryState;
 
 public class QuerierError extends QuerierStatus implements QuerierClosed
@@ -16,6 +18,9 @@ public class QuerierError extends QuerierStatus implements QuerierClosed
    public QuerierError(String givenMessage, Throwable causeOfError) {
       this.cause = causeOfError;
       this.message = givenMessage;
+      StringWriter sw = new StringWriter();
+      cause.printStackTrace(new PrintWriter(sw));
+      this.details.add(sw.toString());
    }
    
    public QueryState getState() { return QueryState.ERROR; }
@@ -36,6 +41,9 @@ public class QuerierError extends QuerierStatus implements QuerierClosed
 
 /*
 $Log: QuerierError.java,v $
+Revision 1.3  2004/03/14 19:12:33  mch
+Added stack trace to error details
+
 Revision 1.2  2004/03/14 00:39:55  mch
 Added error trapping to DataServer and setting Querier error status
 
