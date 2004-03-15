adc1fb47732f53c090a91d6b15f76d785aa3a3ed
Martin Hill <Martin.Hill@astrogrid.org>
2004-03-15 17:26:53 +0000

    Added more robust (& lazy) registry & community delegate creation

--- astrogrid/mySpace/src/java/org/astrogrid/store/delegate/VoSpaceResolver.java
+++ astrogrid/mySpace/src/java/org/astrogrid/store/delegate/VoSpaceResolver.java
@@ -1,5 +1,5 @@
 /*
- * $Id: VoSpaceResolver.java,v 1.2 2004/03/14 03:32:14 mch Exp $
+ * $Id: VoSpaceResolver.java,v 1.3 2004/03/15 17:26:53 mch Exp $
  *
  * (C) Copyright Astrogrid...
  */
@@ -9,28 +9,34 @@ package org.astrogrid.store.delegate;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.astrogrid.community.User;
 import org.astrogrid.config.SimpleConfig;
 import org.astrogrid.registry.client.query.RegistryService;
 import org.astrogrid.store.Agsl;
 import org.astrogrid.store.Ivorn;
 import org.astrogrid.store.delegate.StoreDelegateFactory;
+import org.exolab.castor.xml.ValidationException;
 
 /**
- * A VoSpaceClient is used to access storepoints anywhere on the Virtual Observatory.
- * As such it is a 'higher' level delegate than the StoreClient implemenations
- * which only handle particular service types.
- * This class takes IVO ids and resolves these to StoreClients that can be used
- * to access the the store services or files.  As such it is really a factory,
- * making appropriate delegates as required, but it offers convenient methods
- * for, eg,  returning streams directly to IVO-identified files.
+ * A VoSpaceResolver is used to resolve actual locations from IVORNs to any
+ * storepoints anywhere on the Virtual Observatory.
+ *
+ * This class takes IVO ids and resolves these to Agsls (and can therefore
+ * resolve to streams) to access the the store services or files.
+ * As such it is really a factory, making appropriate delegates/etc as required
+ *
  * It makes use of the registry and commnunity to look up IVORNs
  *
  */
 
 public class VoSpaceResolver {
    
-   RegistryService registry = new RegistryService();
+   private static RegistryService registry = null;
+   //private static CommunityDelegate community = null;
+
+   private static Log log = LogFactory.getLog(VoSpaceResolver.class);
    
    /**
     * Given an IVO Resource Name, resolves the AGSL
@@ -42,29 +48,27 @@ public class VoSpaceResolver {
       //the fragment bit is not relevent to the service lookup - preserve it
       String fragment = ivorn.getFragment();
 
+      Agsl agsl = null;
+      
       //look up in config first
-      String agsl = SimpleConfig.getSingleton().getString(ivorn.toRegistryString(), null);
+      String s = SimpleConfig.getSingleton().getString(ivorn.toRegistryString(), null);
+      if (s != null) {
+         agsl = new Agsl(s);
+      }
 
-      /**/
       if (agsl == null) {
-         //look in registry
-         //agsl = registry.getEndPointByIdentifier(ivorn.toRegistryString());
+         agsl = registryResolve(ivorn);
       }
       
       if (agsl == null) {
-         //look in community
-         //ivorn = community.getMySpace(ivorn.toRegistryString());
-      
-         //agsl = registry.getEndPointByIdentifier(ivorn.toRegistryString());
-      
+         agsl = communityResolve(ivorn);
       }
-      /**/
 
       if (agsl == null) {
          throw new FileNotFoundException("Cannot resolve "+ivorn);
       }
 
-      return new Agsl(agsl);
+      return agsl;
    }
    
    /**
@@ -80,16 +84,83 @@ public class VoSpaceResolver {
     * Given an IVO Resource Name, resolves the Stream.  The user is the account
     * attempting to access the file.
     */
-   public InputStream resolveStream(User user, Ivorn ivorn) throws IOException {
+   public InputStream resolveInputStream(User user, Ivorn ivorn) throws IOException {
       return resolveAgsl(ivorn).openInputStream(user);
    }
    
+   /** Resolve using Registry
+    */
+   public Agsl registryResolve(Ivorn ivorn) throws IOException {
 
+      //lazy load registry delegate - also more robust in case it doesn't instantiate
+      if (registry == null) {
+         try {
+            makeRegistryDelegate();
+         }
+         catch (Exception e) {
+            log.error("Could not create Registry Delegate: ",e);
+         }
+      }
+      
+      if (registry != null) {
+         try {
+            //look in registry
+            return new Agsl(registry.getEndPointByIdentifier(ivorn.toRegistryString()));
+         }
+         catch (ValidationException e) {
+            throw new ResolverException("Registry failed resolving "+ivorn,e);
+         }
+      }
+      return null;
+   }
    
+   /**
+    * Lazy registry delegate load
+    */
+   public synchronized void makeRegistryDelegate() throws IOException {
+      if (registry == null) {
+         registry = new RegistryService();
+      }
+   }
+
+   /** Resolve using Community
+    */
+   public Agsl communityResolve(Ivorn ivorn) throws IOException {
+      /**
+
+       //lazy load delegate - also more robust in case it doesn't instantiate
+      if (community == null) {
+         try {
+            makeCommunityDelegate();
+         }
+         catch (Exception e) {
+            log.error("Could not create Community Delegate: ",e);
+         }
+      }
+      
+      if (community != null) {
+         return new Agsl(community.getEndPointByIdentifier(ivorn.toRegistryString()));
+      }
+       */
+      return null;
+   }
+   
+   /**
+    * Lazy community delegate load
+    *
+   public synchronized void makeCommunityDelegate() throws IOException {
+      if (community == null) {
+         community = new CommunityService();
+      }
+   }
+    /**/
 }
 
 /*
 $Log: VoSpaceResolver.java,v $
+Revision 1.3  2004/03/15 17:26:53  mch
+Added more robust (& lazy) registry & community delegate creation
+
 Revision 1.2  2004/03/14 03:32:14  mch
 Added openOutputStream
 
