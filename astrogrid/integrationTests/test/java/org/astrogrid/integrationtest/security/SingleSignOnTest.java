37e38db1f6d2f4fdb22b1e74d0c0b0b462d00940
Guy Rixon <gtr@ast.cam.ac.uk>
2004-03-25 17:26:58 +0000

    New tests for the single-sign-on feature in astrogrid.security. These exercise the link between the security facade, the registry and the community service.

---
+++ astrogrid/integrationTests/test/java/org/astrogrid/integrationtest/security/SingleSignOnTest.java
@@ -0,0 +1,92 @@
+package org.astrogrid.integrationtest.security;
+
+import junit.framework.TestCase;
+import org.astrogrid.security.ClientSecurityGuard;
+import org.astrogrid.security.NonceToken;
+import org.astrogrid.security.Password;
+
+
+/**
+ * JUnit tests for the integration of the security facade
+ * (org.astrogrid.security) with the Community services.
+ *
+ * @author Guy Rixon
+ */
+public class SingleSignOnTest extends TestCase {
+
+  /**
+   * Tests signing on to the grid and getting an initial
+   * security token.
+   */
+  public void testWithMockCredentials () throws Exception {
+
+    // These credentials will address a mock delegate.  They
+    // won't actually address a commmunity service.
+    String username = "ivo://org.astrogrid.mock/Fred";
+    String password = "secret";
+    String account = this.signOn(username, password);
+    assertNotNull("Account name", account);
+    assertEquals("Names match", username, account);
+  }
+
+
+  /**
+   * Tests signing on to the grid and getting an initial
+   * security token.
+   */
+  public void testWithValidCredentials () throws Exception {
+
+    // These credentials will address a mock delegate.  They
+    // won't actually address a commmunity service.
+    String username = "ivo://test.site/test/user";
+    String password = "secret";
+    String account = this.signOn(username, password);
+    assertNotNull("Account name", account);
+    assertEquals("Names match", username, account);
+  }
+
+
+  /**
+   * Tests signing on to the grid and getting an initial
+   * security token.
+   */
+  public void testWithInvalidCredentials () throws Exception {
+
+    // These credentials will address a real delegate but
+    // won't be accepted.
+    String username = "ivo://bogus.site/bogus/user";
+    String password = "secret";
+    try {
+      String account = this.signOn(username, password);
+      fail("Sign-on succeeded for invalid credentials.");
+    }
+    catch (Exception e) {
+      // Exception is expected; ignore it.
+    }
+  }
+
+
+  /**
+   * Exercises the sign-on function with given SSO credentials.
+   *
+   * @param username account name, in IVORN-string format
+   * @param password clear-text password known to Community
+   * @return the account name from the token sent back by the community
+   */
+  public String signOn (String username, String password) throws Exception {
+
+    // Record the SSO credentials.
+    ClientSecurityGuard sg = new ClientSecurityGuard();
+    sg.setUsername(username);
+    sg.setPassword(new Password(password, false));
+
+    // Use the SSO credentials to get a security token that can be
+    // sent to a service.
+    sg.signOn();
+
+    // Check that a token came back.
+    NonceToken n = sg.getNonceToken();
+    return n.getAccount();
+  }
+
+}
