449368d75eed52b1f5fad58f74da5237bcc66bdc
Guy Rixon <gtr@ast.cam.ac.uk>
2004-03-29 11:26:54 +0000

    whoAmI() now throws RemoteException.

--- astrogrid/security/sample/src/java/org/astrogrid/security/sample/SamplePortType.java
+++ astrogrid/security/sample/src/java/org/astrogrid/security/sample/SamplePortType.java
@@ -1,9 +1,10 @@
 package org.astrogrid.security.sample;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface SamplePortType extends Remote {
 
-  public String whoAmI ();
+  public String whoAmI () throws RemoteException;
 
 }
