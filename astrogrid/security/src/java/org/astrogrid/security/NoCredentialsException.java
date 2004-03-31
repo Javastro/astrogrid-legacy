d7a3285e8b1a3a1f24382ee492c30e5c11f48f5b
Guy Rixon <gtr@ast.cam.ac.uk>
2004-03-31 11:16:33 +0000

    New Exception.

---
+++ astrogrid/security/src/java/org/astrogrid/security/NoCredentialsException.java
@@ -0,0 +1,22 @@
+package org.astrogrid.security;
+
+/**
+ * Exception to be thrown when no credentials are found.
+ * This could be used in a handler looking for credentials
+ * in message, in code attempting authentication, or in
+ * code trying to send an authenticated message.  The
+ * message in the exception should make clear the context.
+ *
+ * @author Guy Rixon
+ */
+public class NoCredentialsException extends Exception {
+
+  public NoCredentialsException (String message) {
+    super(message);
+  }
+
+  public NoCredentialsException (String message, Throwable t) {
+    super(message, t);
+  }
+
+}
