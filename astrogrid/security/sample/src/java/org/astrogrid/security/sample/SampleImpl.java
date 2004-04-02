fb6f874133eae04e63c84633f6344803eb4b7646
Guy Rixon <gtr@ast.cam.ac.uk>
2004-04-02 16:30:47 +0000

    I added extra logging.

--- astrogrid/security/sample/src/java/org/astrogrid/security/sample/SampleImpl.java
+++ astrogrid/security/sample/src/java/org/astrogrid/security/sample/SampleImpl.java
@@ -1,5 +1,6 @@
 package org.astrogrid.security.sample;
 
+import javax.security.auth.Subject;
 import org.astrogrid.security.ServiceSecurityGuard;
 
 
@@ -7,6 +8,9 @@ public class SampleImpl implements SamplePortType {
 
   public String whoAmI () {
     ServiceSecurityGuard sg = ServiceSecurityGuard.getInstanceFromContext();
+    Subject s = sg.getGridSubject();
+    System.out.println(s.getPrincipals().size() + " Principals in gridSubject");
+    System.out.println(s.getPrivateCredentials().size() + " private credentials in gridSubject");
     if (sg.isAnonymous()) {
       return "anonymous";
     }
