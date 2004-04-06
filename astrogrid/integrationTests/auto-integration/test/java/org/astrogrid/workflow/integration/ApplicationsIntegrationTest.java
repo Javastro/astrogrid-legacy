aa7c7f80d2514ada3b64bbe576b999da50f61cec
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-04-06 10:55:08 +0000

    updated to cea connector

--- astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/workflow/integration/ApplicationsIntegrationTest.java
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/workflow/integration/ApplicationsIntegrationTest.java
@@ -1,4 +1,4 @@
-/*$Id: ApplicationsIntegrationTest.java,v 1.1 2004/03/16 17:48:34 nw Exp $
+/*$Id: ApplicationsIntegrationTest.java,v 1.2 2004/04/06 10:55:08 nw Exp $
  * Created on 12-Mar-2004
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -10,9 +10,9 @@
 **/
 package org.astrogrid.workflow.integration;
 
-import org.astrogrid.applications.delegate.ApplicationController;
+import org.astrogrid.applications.beans.v1.ApplicationList;
+import org.astrogrid.applications.delegate.CommonExecutionConnectorClient;
 import org.astrogrid.applications.delegate.DelegateFactory;
-import org.astrogrid.applications.delegate.beans.SimpleApplicationDescription;
 import org.astrogrid.scripting.Service;
 
 import org.exolab.castor.mapping.loader.DelegateFieldDescriptor;
@@ -40,30 +40,36 @@ public class ApplicationsIntegrationTest extends AbstractTestForIntegration {
         assertNotNull(apps);
         assertTrue(apps.size() > 0);
         serv = (Service)apps.get(0);
-        delegate = (ApplicationController)serv.createDelegate();
+        delegate = (CommonExecutionConnectorClient)serv.createDelegate();
     }
     protected Service serv;
-    protected ApplicationController delegate;
+    protected CommonExecutionConnectorClient delegate;
     
     public void testListApplications() throws Exception {
-        String[] results = delegate.listApplications();
+        ApplicationList results = delegate.listApplications();
         assertNotNull(results);
-        assertTrue(results.length > 0);
+        
+        assertTrue(results.getApplicationDefnCount() > 0);
         System.out.println("Applications for Server " + serv.getEndpoint());
-        for (int i = 0; i < results.length; i++) {
-            System.out.print(results[i]+ " ");
+        for (int i = 0; i < results.getApplicationDefnCount(); i++) {
+            System.out.print(results.getApplicationDefn(i)+ " ");
         } 
     }
     public void testApplicationDescription() throws Exception {
-        SimpleApplicationDescription description = delegate.getApplicationDescription(delegate.listApplications()[0]);
+        ApplicationList results = delegate.listApplications();
+        String name = results.getApplicationDefn(0).getName();
+        String description = delegate.getApplicationDescription(name);
         assertNotNull(description);
-        System.out.println(description.getXmlDescriptor());
+        System.out.println(description);
     }
 }
 
 
 /* 
 $Log: ApplicationsIntegrationTest.java,v $
+Revision 1.2  2004/04/06 10:55:08  nw
+updated to cea connector
+
 Revision 1.1  2004/03/16 17:48:34  nw
 first stab at an auto-integration project
  
