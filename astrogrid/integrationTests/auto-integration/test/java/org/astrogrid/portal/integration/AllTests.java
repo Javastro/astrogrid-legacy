f3a9c189082d5e22945bb3dd33450ca4611c5f5d
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-04-15 14:03:51 +0000

    draws things together

---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/portal/integration/AllTests.java
@@ -0,0 +1,42 @@
+/*$Id: AllTests.java,v 1.1 2004/04/15 14:03:51 nw Exp $
+ * Created on 15-Apr-2004
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.portal.integration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author Noel Winstanley nw@jb.man.ac.uk 15-Apr-2004
+ *
+ */
+public class AllTests {
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(AllTests.class);
+    }
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Portal Tests");
+        //$JUnit-BEGIN$
+        suite.addTest(new TestSuite(PortalLoginPageHttpUnitTest.class));
+        suite.addTest(new TestSuite(PortalLoginPageTest.class));
+        suite.addTest(new TestSuite(RegisterPageTest.class));
+        suite.addTest(new TestSuite(ReminderPageTest.class));
+        //$JUnit-END$
+        return suite;
+    }
+}
+
+
+/* 
+$Log: AllTests.java,v $
+Revision 1.1  2004/04/15 14:03:51  nw
+draws things together
+ 
+*/
