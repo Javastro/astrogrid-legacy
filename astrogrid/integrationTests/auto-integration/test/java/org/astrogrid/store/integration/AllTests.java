193713240165a9b5cfe939a2a0eda68104a75bc1
John Taylor <John.Taylor@astrogrid.org>
2004-04-15 14:53:09 +0000

    *** empty log message ***

---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/store/integration/AllTests.java
@@ -0,0 +1,39 @@
+/* $Id: AllTests.java,v 1.1 2004/04/15 14:53:09 jdt Exp $
+ * Created on Apr 15, 2004 by jdt@roe.ac.uk
+ * The auto-integration project
+ * Copyright (c) Astrigrid 2004.  All rights reserved. 
+ *
+ */
+package org.astrogrid.store.integration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ * 
+ * @author jdt
+ */
+public class AllTests {
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(AllTests.class);
+    }
+    public static Test suite() {
+        TestSuite suite =
+            new TestSuite("Test for org.astrogrid.store.integration");
+        //$JUnit-BEGIN$
+        suite.addTest(MySpaceTest.suite());
+        suite.addTest(MySpaceIt04DelegateTest.suite());
+        //$JUnit-END$
+        return suite;
+    }
+}
+
+
+/*
+ *  $Log: AllTests.java,v $
+ *  Revision 1.1  2004/04/15 14:53:09  jdt
+ *  *** empty log message ***
+ *
+ */
