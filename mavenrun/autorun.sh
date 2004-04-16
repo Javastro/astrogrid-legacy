4fc91abbd9494456b70770401e44068e12117434
Catherine Qin <Catherine.Qin@astrogrid.org>
2004-04-16 17:56:02 +0000

    first draft auto build script

---
+++ mavenrun/autorun.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+OLDDIR=$PWD
+
+CHECKOUTHOME=/data/cvsDownloads/itn05
+SCRIPTHOME=/home/integration/autobuilds
+BUILDHOME=/data/cvsDownloads/itn05/astrogrid/integrationTests/auto-integration
+
+export CVS_RSH=ssh
+export CVSROOT=:pserver:anoncvs@cvs.astrogrid.org:/devel
+
+#update from cvs first
+cd $CHECKOUTHOME
+cvs checkout -P astrogrid
+
+#run maven goals
+cd $BUILDHOME
+echo $BUILDHOME
+maven undeploy-all
+maven deploy-all
