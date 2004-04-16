a51aca1b9211b957d666d146e055a39d9d4aadf9
Martin Hill <Martin.Hill@astrogrid.org>
2004-04-16 15:41:03 +0000

    Added autotests

---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/datacenter/integration/IvoQuery2MySpaceTest.java
@@ -0,0 +1,117 @@
+/*$Id: IvoQuery2MySpaceTest.java,v 1.1 2004/04/16 15:41:03 mch Exp $
+ * Created on 22-Jan-2004
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ *
+**/
+package org.astrogrid.datacenter.integration;
+
+import java.net.URL;
+import junit.framework.TestCase;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.astrogrid.community.Account;
+import org.astrogrid.datacenter.delegate.DatacenterDelegateFactory;
+import org.astrogrid.datacenter.delegate.QuerySearcher;
+import org.astrogrid.datacenter.query.ConeQuery;
+import org.astrogrid.datacenter.query.QueryState;
+import org.astrogrid.store.Ivorn;
+import org.astrogrid.store.VoSpaceClient;
+import org.astrogrid.store.delegate.StoreFile;
+import org.astrogrid.store.delegate.VoSpaceResolver;
+import org.astrogrid.util.DomHelper;
+import org.w3c.dom.Document;
+
+/**
+ * Tests that queries are carried out OK and sent to myspace correctly, using
+ * IVORNs
+ *
+ */
+public class IvoQuery2MySpaceTest extends TestCase {
+
+   private static final Log log = LogFactory.getLog(Query2MySpaceTest.class);
+   
+   protected QuerySearcher delegate;
+   protected URL myspaceEndpoint;
+   private static final String resultsPath = "avodemo/autoIntegrationTest.results";
+
+   protected void setUp() throws Exception {
+      String datacenterEndpoint = "http://localhost:8080/astrogrid-pal-SNAPSHOT/services/AxisDataServer";
+      delegate = DatacenterDelegateFactory.makeQuerySearcher(
+         Account.ANONYMOUS,
+         datacenterEndpoint,
+         DatacenterDelegateFactory.ASTROGRID_WEB_SERVICE
+      );
+      assertNotNull("delegate was null",delegate);
+   }
+   
+   /** do a non-blocking query, where results are left in myspace.
+    * retreive from myspace, check they're what we expect
+    */
+   public void testSubmit() throws Exception {
+      
+      Ivorn resultsTarget = new Ivorn("ivo://org.astrogrid.localhost/myspace#"+resultsPath);
+      
+      String queryId = delegate.submitQuery(
+         new ConeQuery(30,30,6),
+         VoSpaceResolver.resolveAgsl(resultsTarget),
+         QuerySearcher.VOTABLE
+      );
+         
+      String stat = delegate.getStatus(queryId);
+
+      //wait until query finishes
+      do {
+         stat = delegate.getStatus(queryId);
+      } while (!stat.equals(QueryState.FINISHED) && (!stat.equals(QueryState.ERROR))); /* need some extra timout here too */
+ 
+      //see if results are in expected myspace location
+      VoSpaceClient store = new VoSpaceClient(Account.ANONYMOUS.toUser());
+      StoreFile file = store.getFile(resultsTarget);
+      
+      Document resultDoc = DomHelper.newDocument(store.getStream(resultsTarget));
+      assertNotNull("null result document",resultDoc);
+      
+   }
+   
+   public IvoQuery2MySpaceTest(String arg0) {
+      super(arg0);
+   }
+   public static void main(String[] args) {
+      junit.textui.TestRunner.run(Query2MySpaceTest.class);
+   }
+   
+   
+}
+
+
+/*
+$Log: IvoQuery2MySpaceTest.java,v $
+Revision 1.1  2004/04/16 15:41:03  mch
+Added autotests
+
+Revision 1.1  2004/03/22 17:23:06  mch
+moved from old package name
+
+Revision 1.5  2004/02/17 23:59:17  jdt
+commented out lines killing the build, and made to conform to
+
+coding stds
+
+Revision 1.4  2004/02/16 18:02:14  jdt
+removed a line that was breaking the build
+
+Revision 1.3  2004/01/23 09:08:09  nw
+added cone searcher test too
+
+Revision 1.2  2004/01/22 16:16:40  nw
+minor doc fix
+
+Revision 1.1  2004/01/22 16:14:59  nw
+added integration test for datacenter full searcher.
+ 
+*/
---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/datacenter/integration/Jes2QueryTest.java
@@ -0,0 +1,183 @@
+/*$Id: Jes2QueryTest.java,v 1.1 2004/04/16 15:41:03 mch Exp $
+ * Created on 12-Mar-2004
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
+ *
+**/
+package org.astrogrid.datacenter.integration;
+
+import junit.framework.TestCase;
+import org.astrogrid.applications.beans.v1.cea.castor.types.ExecutionPhase;
+import org.astrogrid.community.User;
+import org.astrogrid.community.beans.v1.Account;
+import org.astrogrid.community.beans.v1.Credentials;
+import org.astrogrid.community.beans.v1.Group;
+import org.astrogrid.jes.delegate.JobSummary;
+import org.astrogrid.portal.workflow.intf.ApplicationDescription;
+import org.astrogrid.portal.workflow.intf.ApplicationRegistry;
+import org.astrogrid.portal.workflow.intf.JobExecutionService;
+import org.astrogrid.portal.workflow.intf.WorkflowManager;
+import org.astrogrid.portal.workflow.intf.WorkflowManagerFactory;
+import org.astrogrid.scripting.Astrogrid;
+import org.astrogrid.store.Ivorn;
+import org.astrogrid.workflow.beans.v1.Step;
+import org.astrogrid.workflow.beans.v1.Tool;
+import org.astrogrid.workflow.beans.v1.Workflow;
+import org.astrogrid.workflow.beans.v1.execution.JobURN;
+
+/**
+ * Tests running a query on a datacenter using JES - thus also testing the
+ * datacenter's CEA interface
+ * @author MCH
+ * @author Noel Winstanley nw@jb.man.ac.uk 12-Mar-2004
+ *
+ */
+public class Jes2QueryTest extends TestCase {
+
+   protected User user;
+    protected Account acc;
+    protected Group group;
+    protected Credentials creds;
+    protected Workflow wf;
+    protected WorkflowManager manager;
+   protected Astrogrid ag;
+   
+   /*
+     * @see AbstractTestForIntegration#setUp()
+     */
+    protected void setUp() throws Exception {
+        super.setUp();
+      
+      User user = new User("avodemo", "test.astrogrid.org", "loony");
+      
+      acc = new Account();
+        acc.setName("avodemo");
+        acc.setCommunity("test.astrogrid.org");
+        assertNotNull(acc);
+    
+        group = new Group();
+        group.setName("inttest");
+        group.setCommunity("test.astrogrid.org");
+        assertNotNull(group);
+  
+    creds = new Credentials();
+        creds.setAccount(acc);
+        creds.setGroup(group);
+        creds.setSecurityToken("token");
+        assertNotNull(creds);
+
+      manager = new WorkflowManagerFactory().getManager();
+        wf = manager.getWorkflowBuilder().createWorkflow(creds,"test workflow","a description");
+    }
+    
+    public void testBuilder() throws Exception {
+        assertNotNull(wf);
+        assertTrue(wf.isValid());
+    }
+
+    /**
+    public void testStore() throws Exception{
+        WorkflowStore store = manager.getWorkflowStore();
+        assertNotNull(store);
+        store.saveWorkflow(acc, wf);
+        
+        // get it back again.
+        
+        Workflow wf1 = store.readWorkflow(acc,wf.getName());
+        assertNotNull(wf1);
+        assertEquals(wf1.getName(),wf.getName());
+        
+        // list it
+        String[] arr = store.listWorkflows(acc);
+        assertNotNull(arr);
+        assertTrue(arr.length > 0);
+        assertTrue(Arrays.asList(arr).contains(wf.getName()));
+        // delete it.
+        store.deleteWorkflow(acc,wf.getName());
+        // check its not there.
+        assertFalse(Arrays.asList(store.listWorkflows(acc)).contains(wf.getName()));
+    }
+    
+    public void testDatacenter() throws Exception {
+        ApplicationRegistry reg = manager.getToolRegistry();
+        String[] names = reg.listApplications();
+        assertNotNull(names);
+        assertTrue(names.length > 0);
+        
+        for (int i = 0; i < names.length; i++) {
+            ApplicationDescription descr = reg.getDescriptionFor(names[i]);
+            assertNotNull(descr);
+            assertEquals(descr.getName(),names[i]);
+        }
+    }
+     **/
+    
+    public void testSubmit() throws Exception {
+
+       Ivorn workflowIvorn = new Ivorn("ivo://org.astrogrid.localhost/myspace#Jes2QueryTest.workflow");
+ 
+       
+       ApplicationRegistry reg = manager.getToolRegistry();
+        // create a tool
+       ApplicationDescription descr = reg.getDescriptionFor(reg.listApplications()[0]);
+       assertNotNull(descr);
+       Tool tool = descr.createToolFromDefaultInterface();
+       assertNotNull(tool);
+       descr.validate(tool); // shouold be ready to go, with no further config.
+       // add a step to the workflow.
+       Step step = new Step();
+       step.setDescription("single step");
+       step.setName("test step");
+       step.setTool(tool);
+       wf.getSequence().addActivity(step);
+       assertTrue(wf.isValid());
+       // save it in the store,
+       manager.getWorkflowStore().saveWorkflow(user, workflowIvorn, wf);
+       // now submit it.
+
+       JobExecutionService jes = manager.getJobExecutionService();
+       JobURN urn = jes.submitWorkflow(wf);
+       assertNotNull(urn);
+       //check its in the list.
+       JobSummary summaries[] = jes.readJobList(acc);
+       assertNotNull(summaries);
+       assertTrue(summaries.length > 0);
+       boolean found = false;
+       for (int i = 0; i < summaries.length; i++) {
+          
+          if (summaries[i].getJobURN().getContent().equals(urn.getContent())) {
+               found=true;
+           }
+       }
+       assertTrue(found);
+           Thread.sleep(10000); // 10 seconds should be enough
+        // try retreiving the workflow.
+           Workflow w1 = jes.readJob(urn);
+           assertNotNull(w1);
+            assertEquals(w1.getName(),wf.getName());
+       
+       assertEquals(ExecutionPhase.COMPLETED_TYPE,w1.getJobExecutionRecord().getStatus().getType()); // i.e. its not in error
+       // dump it to myspace store - then we can look at it later.
+       w1.setName("execution results:" + urn.getContent());
+       manager.getWorkflowStore().saveWorkflow(user, workflowIvorn, w1);
+       
+       
+    }
+    
+}
+
+
+/*
+$Log: Jes2QueryTest.java,v $
+Revision 1.1  2004/04/16 15:41:03  mch
+Added autotests
+
+Revision 1.1  2004/04/15 17:27:57  mch
+JES single step test
+
+ 
+*/
