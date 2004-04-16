7c281caf7b652702492a03c0f6fd7a98672a0976
Martin Hill <Martin.Hill@astrogrid.org>
2004-04-16 15:17:41 +0000

    Removed it04Delegate test

--- astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/store/integration/AllTests.java
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/store/integration/AllTests.java
@@ -1,7 +1,7 @@
-/* $Id: AllTests.java,v 1.2 2004/04/15 23:11:20 nw Exp $
+/* $Id: AllTests.java,v 1.3 2004/04/16 15:17:41 mch Exp $
  * Created on Apr 15, 2004 by jdt@roe.ac.uk
  * The auto-integration project
- * Copyright (c) Astrigrid 2004.  All rights reserved. 
+ * Copyright (c) Astrigrid 2004.  All rights reserved.
  *
  */
 package org.astrogrid.store.integration;
@@ -10,7 +10,7 @@ import junit.framework.TestSuite;
 /**
  * To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Generation - Code and Comments
- * 
+ *
  * @author jdt
  */
 public class AllTests {
@@ -20,7 +20,6 @@ public class AllTests {
     public static Test suite() {
         TestSuite suite = new TestSuite("Store");
         //$JUnit-BEGIN$
-        suite.addTest(MySpaceIt04DelegateTest.suite());
         suite.addTest(MySpaceTest.suite());
         suite.addTest(VoSpaceTest.suite());
         //$JUnit-END$
@@ -29,6 +28,9 @@ public class AllTests {
 }
 /*
  *  $Log: AllTests.java,v $
+ *  Revision 1.3  2004/04/16 15:17:41  mch
+ *  Removed it04Delegate test
+ *
  *  Revision 1.2  2004/04/15 23:11:20  nw
  *  tweaks
  *
