e60b75e6cdabce601f21b42ac894096ffab3cde5
Catherine Qin <Catherine.Qin@astrogrid.org>
2004-04-17 09:03:47 +0000

    add to log

--- mavenrun/autorun.sh
+++ mavenrun/autorun.sh
@@ -4,17 +4,18 @@ OLDDIR=$PWD
 CHECKOUTHOME=/data/cvsDownloads/itn05
 SCRIPTHOME=/home/integration/autobuilds
 BUILDHOME=/data/cvsDownloads/itn05/astrogrid/integrationTests/auto-integration
+LOGFILE=/home/integration/autobuilds/auto.log
 
 export CVS_RSH=ssh
 export CVSROOT=:pserver:anoncvs@cvs.astrogrid.org:/devel
 
 #update from cvs first
 cd $CHECKOUTHOME
-cvs checkout -P astrogrid
+cvs update -P astrogrid
 
 #run maven goals
 cd $BUILDHOME
 echo $BUILDHOME
-maven undeploy-all
-maven deploy-all
-maven astrogrid-deploy-all
+maven undeploy-all >> $LOGFILE
+maven deploy-all >> $LOGFILE
+maven astrogrid-deploy-all >> $LOGFILE
