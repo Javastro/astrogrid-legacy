093bff762c39b85cc7ad2d76b9d601a6beb9e18c
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-04-20 14:45:37 +0000

    added in new test

--- astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/store/integration/AllTests.java
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/store/integration/AllTests.java
@@ -1,4 +1,4 @@
-/* $Id: AllTests.java,v 1.3 2004/04/16 15:17:41 mch Exp $
+/* $Id: AllTests.java,v 1.4 2004/04/20 14:45:37 nw Exp $
  * Created on Apr 15, 2004 by jdt@roe.ac.uk
  * The auto-integration project
  * Copyright (c) Astrigrid 2004.  All rights reserved.
@@ -22,12 +22,16 @@ public class AllTests {
         //$JUnit-BEGIN$
         suite.addTest(MySpaceTest.suite());
         suite.addTest(VoSpaceTest.suite());
+        suite.addTest(new TestSuite(DeployedManagerTest.class));
         //$JUnit-END$
         return suite;
     }
 }
 /*
  *  $Log: AllTests.java,v $
+ *  Revision 1.4  2004/04/20 14:45:37  nw
+ *  added in new test
+ *
  *  Revision 1.3  2004/04/16 15:17:41  mch
  *  Removed it04Delegate test
  *
