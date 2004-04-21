fe50bef6c46ef32f1eb860857c8f80d6048be492
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-04-21 10:06:21 +0000

    added backdoor servlet

---
+++ astrogrid/jes/src/java/org/astrogrid/jes/servlet/BackdoorServlet.java
@@ -0,0 +1,73 @@
+/*$Id: BackdoorServlet.java,v 1.1 2004/04/21 10:06:21 nw Exp $
+ * Created on 21-Apr-2004
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.jes.servlet;
+
+import org.astrogrid.jes.JesException;
+import org.astrogrid.jes.component.ComponentManager;
+import org.astrogrid.jes.component.ComponentManagerFactory;
+import org.astrogrid.jes.jobscheduler.Locator;
+import org.astrogrid.workflow.beans.v1.Step;
+import org.astrogrid.workflow.beans.v1.Tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Backdoor into some of the jes componets, for easy server-side testing.
+ * @author Noel Winstanley nw@jb.man.ac.uk 21-Apr-2004
+ *
+ */
+public class BackdoorServlet extends HttpServlet {
+    /** Construct a new BackdoorServlet
+     * 
+     */
+    public BackdoorServlet() {
+        super();
+    }
+    /**
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action.equalsIgnoreCase("locate")) {
+            String toolName = request.getParameter("name");
+            ComponentManager manager = ComponentManagerFactory.getInstance();
+            Locator locator = (Locator)manager.getContainer().getComponentInstance(Locator.class);
+            if (locator == null) {
+                throw new ServletException("locator object was null");
+            }
+            Step step = new Step();
+            Tool tool = new Tool();
+            tool.setName(toolName);
+            step.setTool(tool);
+            try {
+                String url = locator.locateTool(step);
+                PrintWriter writer = response.getWriter();
+                writer.println(url);
+            } catch (JesException e) {
+                throw new ServletException("Jes Barfed with exception",e);
+            }
+        } 
+    }
+
+}
+
+
+/* 
+$Log: BackdoorServlet.java,v $
+Revision 1.1  2004/04/21 10:06:21  nw
+added backdoor servlet
+ 
+*/
