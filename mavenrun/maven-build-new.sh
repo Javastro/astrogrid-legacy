1b43d76756297ee1ef244579701c3f1d5b98c5d0
John Taylor <John.Taylor@astrogrid.org>
2004-06-14 17:13:54 +0000

    removed clq as an admin since comma separated list seems to no longer work.

--- mavenrun/maven-build-new.sh
+++ mavenrun/maven-build-new.sh
@@ -10,7 +10,7 @@ PROJECT_HOME=$BUILD_HOME/astrogrid/$PROJECT_NAME
 DOC_HOME=/var/www/www/maven/docs
 ASTROGRID_VERSION=SNAPSHOT
 LOG_FILE=$BUILD_HOME/maven-build-$PROJECT_NAME.log
-ADMIN_EMAIL=clq2@star.le.ac.uk,jdt@roe.ac.uk
+ADMIN_EMAIL=jdt@roe.ac.uk
 
 echo
 echo "[ag-build] building $PROJECT_NAME $ASTROGRID_VERSION"
