f46d6993be62623b0ac8a56febc36be1feae37c8
John Taylor <John.Taylor@astrogrid.org>
2004-07-12 18:20:33 +0000

    new

---
+++ maven-plugins/regression-junit-plugin/test/java/pack/AnotherTest.java
@@ -0,0 +1,21 @@
+package pack;
+
+import junit.framework.TestCase;
+/*
+ * Created on Jul 12, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+/**
+ * @author jdt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class AnotherTest extends TestCase {
+	public void testMee() {
+		
+	}
+}
