f20d616b5f2b8f0db7f363e612583196dc3df342
John Taylor <John.Taylor@astrogrid.org>
2004-07-16 17:35:32 +0000

    New

---
+++ mavenTemplates/MultiMavenProject/common/src/java/org/astrogrid/foo/common/CommonClass.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jul 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.astrogrid.foo.common;
+
+/**
+ * @author jdt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CommonClass {
+	public String helloWorld() {
+		return "Hello";
+	}
+}
---
+++ mavenTemplates/MultiMavenProject/common/test/java/org/astrogrid/foo/common/CommonClassTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Jul 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.astrogrid.foo.common;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jdt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CommonClassTest extends TestCase {
+
+	public void testHelloWorld() {
+		String hello = new CommonClass().helloWorld();
+		assertEquals("Hello",hello);
+	}
+
+}
