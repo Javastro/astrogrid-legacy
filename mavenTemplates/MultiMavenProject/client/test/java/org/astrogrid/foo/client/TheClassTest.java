c2627614babdcbaf93adb7c8a05928ddd9bfde81
John Taylor <John.Taylor@astrogrid.org>
2004-07-17 11:00:48 +0000

    *** empty log message ***

---
+++ mavenTemplates/MultiMavenProject/client/test/java/org/astrogrid/foo/client/TheClassTest.java
@@ -0,0 +1,24 @@
+/*
+ * Created on Jul 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.astrogrid.foo.client;
+
+import junit.framework.TestCase;
+
+/**
+ * @author jdt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TheClassTest extends TestCase {
+
+	public void testHelloWorld() {
+		String hello = new TheClass().helloWorld();
+		assertEquals("Hello",hello);
+	}
+
+}
---
+++ mavenTemplates/MultiMavenProject/server/src/java/org/astrogrid/foo/server/TheClass.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Jul 16, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+package org.astrogrid.foo.server;
+import org.astrogrid.foo.common.CommonClass;
+
+/**
+ * @author jdt
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TheClass {
+	public String helloWorld() {
+		return new CommonClass().helloWorld();
+	}
+}
