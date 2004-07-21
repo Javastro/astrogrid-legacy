eaab031e32085431772a4559ab3c08c429b75d29
John Taylor <John.Taylor@astrogrid.org>
2004-07-21 09:39:30 +0000

    * A place holder just to create a link in the reports  * till we get some real tests

---
+++ astrogrid/integrationTests/auto-integration/org/astrogrid/applications/integration/http/DummyTest.java
@@ -0,0 +1,21 @@
+/*
+ * Created on Jul 21, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.astrogrid.applications.integration.http;
+
+import junit.framework.TestCase;
+
+/**
+ * A place holder just to create a link in the reports
+ * till we get some real tests
+ * @author jdt
+ *
+ */
+public class DummyTest extends TestCase {
+	public testDummy() {
+		
+	}
+}
