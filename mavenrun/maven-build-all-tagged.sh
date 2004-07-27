6d45be45c3aa5aeb486623d66bfa46779376bef2
John Taylor <John.Taylor@astrogrid.org>
2004-07-27 11:41:58 +0000

    changed build order and added the new Itn06 projects

--- mavenrun/maven-build-all-tagged.sh
+++ mavenrun/maven-build-all-tagged.sh
@@ -22,18 +22,19 @@ echo >> $BUILD_DIR/$LOG_FILE 2>&1
 
 $SCRIPTHOME/maven-build-tagged-new.sh common $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh workflow-objects $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh scripting $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh eXist $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh registry $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh community $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged-new.sh security $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged-new.sh ui $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh filestore $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh mySpace $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh security $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh applications $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged-new.sh datacenter $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh jes $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh warehouse $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh datacenter $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh workflow $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged-new.sh deployment $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
-$SCRIPTHOME/maven-build-tagged-new.sh scripting $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
+$SCRIPTHOME/maven-build-tagged-new.sh ui $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 $SCRIPTHOME/maven-build-tagged-new.sh portal $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
 #Index pages for this release
 $SCRIPTHOME/maven-build-maven-site-tagged.sh  $TAG_TO_BUILD >> $BUILD_DIR/$LOG_FILE 2>&1
