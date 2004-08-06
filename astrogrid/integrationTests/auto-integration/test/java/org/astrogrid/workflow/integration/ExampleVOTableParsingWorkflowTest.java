8f85c1ed579db42a7d6ebf02966bb69e8cc6eacd
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2004-08-06 10:28:23 +0000

    example workflow

---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/workflow/integration/ExampleVOTableParsingWorkflowTest.java
@@ -0,0 +1,100 @@
+/*$Id: ExampleVOTableParsingWorkflowTest.java,v 1.1 2004/08/06 10:28:23 nw Exp $
+ * Created on 06-Aug-2004
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+**/
+package org.astrogrid.workflow.integration;
+
+import org.astrogrid.portal.workflow.intf.ApplicationDescription;
+import org.astrogrid.portal.workflow.intf.WorkflowInterfaceException;
+import org.astrogrid.workflow.beans.v1.Script;
+import org.astrogrid.workflow.beans.v1.Step;
+import org.astrogrid.workflow.beans.v1.Tool;
+import org.astrogrid.workflow.beans.v1.Workflow;
+
+import org.apache.axis.utils.XMLUtils;
+import org.exolab.castor.xml.Marshaller;
+import org.w3c.dom.Document;
+
+/** Example workflow that acquires a votable of urls, parses it, and then passes list of urls to parameters of next step.
+ * @author Noel Winstanley nw@jb.man.ac.uk 06-Aug-2004
+ *
+ */
+public class ExampleVOTableParsingWorkflowTest extends AbstractTestForWorkflow {
+
+    /** Construct a new ExampleVOTableParsingWorkflowTest
+     * @param applications
+     * @param arg0
+     */
+    public ExampleVOTableParsingWorkflowTest(String arg0) {
+        super(new String[]{}, arg0); //@todo add in real names.
+    }
+
+    public static final String VOTABLE_SOURCE = "org.astrogrid.localhost/something"; //@todo
+    public static final String URL_LIST_SINK = "org.astrogrid.localhost/somethingelse";
+    
+    /**
+     * @see org.astrogrid.workflow.integration.AbstractTestForWorkflow#buildWorkflow()
+     */
+    protected void buildWorkflow() throws WorkflowInterfaceException {
+        wf.setName(this.getClass().getName());
+        // the app that generates the votable
+        ApplicationDescription descr = reg.getDescriptionFor(VOTABLE_SOURCE);
+        Tool sourceTool = descr.createToolFromDefaultInterface();
+        // setup parameters here. 
+        // set result to direct.
+        Step source = new Step();
+        source.setName("source-step");
+        source.setResultVar("source");
+        source.setTool(sourceTool);
+        wf.getSequence().addActivity(source);
+        
+        // now the script that mangles the results.
+        // assumes its just a single-column votable, with urls - pity votable isn't so easy to parse.
+        Script sc = new Script();
+        sc.setBody(
+                "votable = source.result; // access result of previous step\n" +
+                "parser = new XmlParser(); //create new parser \n" +
+                "nodes = parser.parseText(votable); //parse votable into node tree\n" +
+                "urls = nodes.depthFirst().findAll{it.name() == 'TD'}.collect{it.value()}.flatten(); // filter node tree on 'TD', project value\n" +
+                "print(urls); // show what we've found\n" +
+                "sinkStep = jes.getSteps().find {it.getName() == 'sink-step'}; // find next step in workflow\n" +
+               "inputs = sinkStep.getTool().getInput(); // get to set of input parameters" +
+                "urls.each { p = jes.newParameter(); p.setName('url'); p.setValue(it); inputs.addParameter(p);} // add a new parameter for each url\n"
+                
+        );
+        wf.getSequence().addActivity(sc);
+        
+        // now the app that consumes the list of parameters.
+        descr = reg.getDescriptionFor(URL_LIST_SINK);
+        Tool sinkTool = descr.createToolFromDefaultInterface();
+        // populate parameters here.
+        Step sink = new Step();
+        sink.setName("sink-step");
+        sink.setTool(sinkTool);
+        wf.getSequence().addActivity(sink);
+        
+    }
+    
+    public void checkExecutionResults(Workflow result) throws Exception {
+        super.checkExecutionResults(result);
+        // just print it out, prettyfied.
+        Document doc = XMLUtils.newDocument();
+        Marshaller.marshal(result,doc);
+        XMLUtils.PrettyDocumentToStream(doc,System.out);
+    }
+
+}
+
+
+/* 
+$Log: ExampleVOTableParsingWorkflowTest.java,v $
+Revision 1.1  2004/08/06 10:28:23  nw
+example workflow
+ 
+*/
