a8f87b05ca04653f0f4d2c8b588b8661cee12168
Martin Hill <Martin.Hill@astrogrid.org>
2004-09-08 11:22:56 +0000

    Extended to return raw CEA and other resource types using 'Resource' parameter

--- astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/servlet/GetMetadata.java
+++ astrogrid/datacenter/server/src/java/org/astrogrid/datacenter/servlet/GetMetadata.java
@@ -1,5 +1,5 @@
 /*
- * $Id: GetMetadata.java,v 1.2 2004/09/06 20:23:00 mch Exp $
+ * $Id: GetMetadata.java,v 1.3 2004/09/08 11:22:56 mch Exp $
  */
 
 package org.astrogrid.datacenter.servlet;
@@ -7,6 +7,7 @@ package org.astrogrid.datacenter.servlet;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.astrogrid.applications.component.CEAComponentManagerFactory;
 import org.astrogrid.datacenter.metadata.VoDescriptionServer;
 import org.astrogrid.util.DomHelper;
 
@@ -17,11 +18,22 @@ import org.astrogrid.util.DomHelper;
  */
 public class GetMetadata extends StdServlet {
    
+   
    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
       try {
          response.setContentType("text/xml");
-         response.getWriter().write(DomHelper.DocumentToString(VoDescriptionServer.getVoDescription()));
+
+         String resourceType = request.getParameter("Resource");
+         if (resourceType == null) {
+            response.getWriter().write(VoDescriptionServer.makeVoDescription());
+         }
+         else if (resourceType.toUpperCase().equals("CEA")) {
+            response.getWriter().write(CEAComponentManagerFactory.getInstance().getMetadataService().returnRegistryEntry());
+         }
+         else  {
+            response.getWriter().write(DomHelper.ElementToString(VoDescriptionServer.getResource(resourceType)));
+         }
       }
       catch (Throwable th) {
          doError(response, "Geting Metadata",th);
