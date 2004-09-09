ff3c349dedffc3d49cc87bcf62b48badbd52179c
Martin Hill <Martin.Hill@astrogrid.org>
2004-09-09 13:17:45 +0000

    Switched to use correct config

--- astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/datacenter/integration/StdKeys.java
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/datacenter/integration/StdKeys.java
@@ -10,13 +10,13 @@ import org.astrogrid.config.SimpleConfig;
 
 public interface StdKeys
 {
-   public final static String PAL_v041_ENDPOINT = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-SNAPSHOT/services/AxisDataServer";
-   public final static String PAL_v05_ENDPOINT = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-SNAPSHOT/services/AxisDataService05";
-   public final static String PAL_v05_FITS_ENDPOINT = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-fits-SNAPSHOT/services/AxisDataService05";
-   public final static String PAL_v05_SEC_ENDPOINT = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-sec-SNAPSHOT/services/AxisDataService05";
-   public final static String PAL_v05_VIZIER_ENDPOINT = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-cds-SNAPSHOT/services/AxisDataService05";
+   public final static String PAL_v041_ENDPOINT = SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-SNAPSHOT/services/AxisDataServer";
+   public final static String PAL_v05_ENDPOINT = SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-SNAPSHOT/services/AxisDataService05";
+   public final static String PAL_v05_FITS_ENDPOINT = SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-fits-SNAPSHOT/services/AxisDataService05";
+   public final static String PAL_v05_SEC_ENDPOINT = SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-sec-SNAPSHOT/services/AxisDataService05";
+   public final static String PAL_v05_VIZIER_ENDPOINT = SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-cds-SNAPSHOT/services/AxisDataService05";
 
-   public final static String PAL_QUERYSTATUS = "http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-pal-SNAPSHOT/queryStatus.jsp?";
-   public final static String MYSPACE = "myspace:http://localhost:"+SimpleConfig.getSingleton().getString("TomcatPort")+"/astrogrid-mySpace-SNAPSHOT/services/Manager";
+   public final static String PAL_QUERYSTATUS = "http://localhost:"+SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-pal-SNAPSHOT/queryStatus.jsp?";
+   public final static String MYSPACE = "myspace:http://localhost:"+SimpleConfig.getSingleton().getString("tomcat.root")+"/astrogrid-mySpace-SNAPSHOT/services/Manager";
 }
 
