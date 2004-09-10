bce4ebcfb3e5c638fb48e980d7b95c14fa4d213b
Paul Harrison <paul.harrison@manchester.ac.uk>
2004-09-10 19:16:40 +0000

    test that we can actually write to an output stream

--- astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/myspace/integration/VoSpaceIntegrationTest.java
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/myspace/integration/VoSpaceIntegrationTest.java
@@ -1,10 +1,13 @@
 /*
  * <cvs:source>$Source: /Users/pharriso/Work/ag/repo/git/astrogrid-mirror/astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/myspace/integration/Attic/VoSpaceIntegrationTest.java,v $</cvs:source>
- * <cvs:author>$Author: dave $</cvs:author>
- * <cvs:date>$Date: 2004/09/06 12:50:12 $</cvs:date>
- * <cvs:version>$Revision: 1.2 $</cvs:version>
+ * <cvs:author>$Author: pah $</cvs:author>
+ * <cvs:date>$Date: 2004/09/10 19:16:40 $</cvs:date>
+ * <cvs:version>$Revision: 1.3 $</cvs:version>
  * <cvs:log>
  *   $Log: VoSpaceIntegrationTest.java,v $
+ *   Revision 1.3  2004/09/10 19:16:40  pah
+ *   test that we can actually write to an output stream
+ *
  *   Revision 1.2  2004/09/06 12:50:12  dave
  *   Added VoSpace integration test.
  *
@@ -26,78 +29,65 @@
  * </cvs:log>
  *
  */
-package org.astrogrid.myspace.integration ;
+package org.astrogrid.myspace.integration;
+
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 import org.astrogrid.store.Ivorn;
 import org.astrogrid.store.VoSpaceClient;
 
+import org.astrogrid.applications.integration.commandline.CommandLineProviderServerInfo;
 import org.astrogrid.integration.AbstractTestForIntegration;
 
 /**
  * JUnit test for the VoSpace client interface to MySpace.
- *
+ *  
  */
-public class VoSpaceIntegrationTest
-	extends AbstractTestForIntegration
-	{
+public class VoSpaceIntegrationTest extends AbstractTestForIntegration {
 
-	/**
-	 * Reference to our target VoSpace.
-	 *
-	 */
-	protected VoSpaceClient vospace ;
+    /**
+     * Reference to our target VoSpace.
+     *  
+     */
+    protected VoSpaceClient vospace;
 
-	/**
-	 * Public constructor.
-	 * @param name
-	 *
-	 */
-	public VoSpaceIntegrationTest(String name)
-		{
-		super(name);
-		}
+    /**
+     * Public constructor.
+     * 
+     * @param name
+     *  
+     */
+    public VoSpaceIntegrationTest(String name) {
+        super(name);
+    }
 
-	/**
-	 * Set up our test.
-	 * Creates our target VoSpaceClient.
-	 *
-	 */
-	public void setUp()
-		throws Exception
-		{
-		super.setUp();
-System.out.println("") ;
-System.out.println("VoSpaceIntegrationTest.setUp()") ;
-System.out.println(" userIvorn    : " + userIvorn.toString()) ;
-System.out.println(" mySpaceIvorn : " + mySpaceIvorn.toString()) ;
+    /**
+     * Set up our test. Creates our target VoSpaceClient.
+     *  
+     */
+    public void setUp() throws Exception {
+        super.setUp();
 
-		vospace = new VoSpaceClient(user) ;
-		}
+        vospace = new VoSpaceClient(user);
+    }
 
-	/**
-	 * Test that we can create a VoSpaceClient.
-	 *
-	 */
-	public void testCreateClient()
-		throws Exception
-		{
-		assertNotNull(
-			vospace
-			) ;
-		}
+    /**
+     * Test that we can create a VoSpaceClient.
+     *  
+     */
+    public void testCreateClient() throws Exception {
+        assertNotNull(vospace);
+    }
 
-	/**
-	 * Test that we can create an output stream.
-	 *
-	 */
-	public void testCreateStream()
-		throws Exception
-		{
-		Ivorn target = createIVORN("/frog.txt") ;
-		assertNotNull(
-			vospace.putStream(
-				target
-				)
-			) ;
-		}
-	}
+    /**
+     * Test that we can write to an output stream.
+     *  
+     */
+    public void testWriteToStream() throws Exception {
+        Ivorn target = createIVORN("/frog.txt");
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(vospace.putStream(target)));
+        pw.println("content");
+        pw.close();     
+    }
+}
