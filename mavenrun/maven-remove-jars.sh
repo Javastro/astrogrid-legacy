8ba6685bc239f76b7f7f2c45e0cb052d355c8822
John Taylor <John.Taylor@astrogrid.org>
2004-09-13 10:49:04 +0000

    Included zips and poms in the list to be pruned.

--- mavenrun/maven-remove-jars.sh
+++ mavenrun/maven-remove-jars.sh
@@ -8,6 +8,6 @@ cd /var/www/www/maven/org.astrogrid/
 
 
 ## alternative - get rid of all the snapshot type files older than 5 days
-find . -regex ".*-200[0-9]+\.[0-9]+\.\(war\|jar\)\(\.md5\)?" -ctime +5 -exec rm '{}' ';' -print
+find . -regex ".*-200[0-9]+\.[0-9]+\.\(zip\|pom\|war\|jar\)\(\.md5\)?" -ctime +5 -exec rm '{}' ';' -print
 
 cd $OLDDIR
