5cf1ead76f59d01aeb8adaf020259ad94174288b
Martin Hill <Martin.Hill@astrogrid.org>
2004-09-29 13:55:27 +0000

    Updated ADQL versions

--- astrogrid/datacenter/implementations/sec/test/java/org/astrogrid/datacenter/sec/querier/EgsoQuerierTest.java
+++ astrogrid/datacenter/implementations/sec/test/java/org/astrogrid/datacenter/sec/querier/EgsoQuerierTest.java
@@ -1,4 +1,4 @@
-/*$Id: EgsoQuerierTest.java,v 1.6 2004/09/29 13:37:36 mch Exp $
+/*$Id: EgsoQuerierTest.java,v 1.7 2004/09/29 13:55:27 mch Exp $
  * Created on 01-Dec-2003
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -7,7 +7,7 @@
  * Software License version 1.2, a copy of which has been included
  * with this distribution in the LICENSE.txt file.
  *
-**/
+ **/
 package org.astrogrid.datacenter.sec.querier;
 
 import java.io.InputStream;
@@ -31,105 +31,92 @@ import org.w3c.dom.Document;
  *
  */
 public class EgsoQuerierTest extends ServerTestCase {
-
-    /**
-     * Constructor for VizierQuerierTest.
-     * @param arg0
-     */
-    public EgsoQuerierTest(String arg0) {
-        super(arg0);
-    }
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(EgsoQuerierTest.class);
-    }
-
-    /*
-     * @see TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        super.setUp();
-
-       //DummySqlPlugin.populateDb();
-       EgsoSqlPlugin.initConfig();
-        // should set configuration and workspace here -- however, not needed by this plugin implementaiton.
-    }
-    /*
-     * @see TestCase#tearDown()
-     */
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-    
-   public static Test suite() {
-         // Reflection is used here to add all the testXXX() methods to the suite.
-         return new TestSuite(EgsoQuerierTest.class);
-      }
-
+   
+   QuerierManager manager = new QuerierManager("SqlQuerierTest");
+   
+   public EgsoQuerierTest(String arg0) {
+      super(arg0);
+   }
+   
    public void testAdql1() throws Exception {
       askAdqlFromFile("sql-querier-test-5.xml");
    }
-   /*
-   public void testAdl2() throws Exception {
-      askAdqlFromFile("sql-querier-test-3.xml");
-   }
 
-   public void testAdl3() throws Exception {
-      askAdqlFromFile("sql-querier-test-3.xml");
+   public void testAdql2() throws Exception {
+    askAdqlFromFile("sql-querier-test-3.xml");
    }
-   */
+   
+   public void testAdql3() throws Exception {
+    askAdqlFromFile("sql-querier-test-3.xml");
+   }
+   
+   public void testMetaAll() throws Exception {
+    askAdqlFromFile("meta-all.xml");
+   }
+   
+   public void testNamedTarget() throws Exception {
+    askAdqlFromFile("named-target.xml");
+   }
+   
    /** Read ADQL input document, run query on dummy SQL plugin, and return VOTable document
     *
-    * @param queryFile resource file of query
+    * @param queryFile resource filename of query
     */
    protected void askAdqlFromFile(String queryFile) throws Exception {
-      fail("Need to update test to ADQL 0.7.4+");
-      /*
+      
       assertNotNull(queryFile);
       InputStream is = this.getClass().getResourceAsStream(queryFile);
       assertNotNull("Could not open query file :" + queryFile,is);
-      Select select = Select.unmarshalSelect(new InputStreamReader(is));
-      assertNotNull(select);
-
+      
       StringWriter sw = new StringWriter();
-      Querier q = Querier.makeQuerier(Account.ANONYMOUS, new AdqlQuery(select), new TargetIndicator(sw), ReturnTable.VOTABLE);
-
+      Querier q = Querier.makeQuerier(Account.ANONYMOUS, new AdqlQuery(is), new TargetIndicator(sw), ReturnTable.VOTABLE);
+      
       manager.askQuerier(q);
       
       Document results = DomHelper.newDocument(sw.toString());
       assertIsVotable(results);
-       */
    }
    
-   QuerierManager manager = new QuerierManager("SqlQuerierTest");
-
+   public static void main(String[] args) {
+      junit.textui.TestRunner.run(EgsoQuerierTest.class);
+   }
+   
+   
+   public static Test suite() {
+      // Reflection is used here to add all the testXXX() methods to the suite.
+      return new TestSuite(EgsoQuerierTest.class);
+   }
+   
 }
 
 
 /*
-$Log: EgsoQuerierTest.java,v $
-Revision 1.6  2004/09/29 13:37:36  mch
-Removed obsolete ADQL 0.5
+ $Log: EgsoQuerierTest.java,v $
+ Revision 1.7  2004/09/29 13:55:27  mch
+ Updated ADQL versions
 
-Revision 1.5  2004/09/02 12:44:35  mch
-Fixed FORMAT_VOTABLEs
+ Revision 1.6  2004/09/29 13:37:36  mch
+ Removed obsolete ADQL 0.5
 
-Revision 1.4  2004/08/27 14:32:35  KevinBenson
-class was placed at another package level, which caused a compile bug
+ Revision 1.5  2004/09/02 12:44:35  mch
+ Fixed FORMAT_VOTABLEs
 
-Revision 1.3  2004/08/19 17:50:22  mch
-Fix for TargetIndicator move
+ Revision 1.4  2004/08/27 14:32:35  KevinBenson
+ class was placed at another package level, which caused a compile bug
 
-Revision 1.2  2004/07/26 13:50:25  KevinBenson
-Small test case that goes out and queries sec based on an adql query
+ Revision 1.3  2004/08/19 17:50:22  mch
+ Fix for TargetIndicator move
 
-Revision 1.1  2004/07/07 09:17:40  KevinBenson
-New SEC/EGSO proxy to query there web service on the Solar Event Catalog
+ Revision 1.2  2004/07/26 13:50:25  KevinBenson
+ Small test case that goes out and queries sec based on an adql query
 
-Revision 1.2  2004/03/16 01:32:35  mch
-Fixed for cahnges to code to work with new plugins
+ Revision 1.1  2004/07/07 09:17:40  KevinBenson
+ New SEC/EGSO proxy to query there web service on the Solar Event Catalog
 
-Revision 1.1  2003/12/01 16:51:04  nw
-added tests for cds spi
+ Revision 1.2  2004/03/16 01:32:35  mch
+ Fixed for cahnges to code to work with new plugins
+
+ Revision 1.1  2003/12/01 16:51:04  nw
+ added tests for cds spi
  
-*/
+ */
