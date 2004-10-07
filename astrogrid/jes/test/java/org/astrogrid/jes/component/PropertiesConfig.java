b81177b590a0272c5b174e42b4f6e5af64a95632
Martin Hill <Martin.Hill@astrogrid.org>
2004-10-07 14:33:44 +0000

    Added new Config method

--- astrogrid/jes/test/java/org/astrogrid/jes/component/PropertiesConfig.java
+++ astrogrid/jes/test/java/org/astrogrid/jes/component/PropertiesConfig.java
@@ -1,11 +1,11 @@
-/*$Id: PropertiesConfig.java,v 1.3 2004/04/06 08:29:55 nw Exp $
+/*$Id: PropertiesConfig.java,v 1.4 2004/10/07 14:33:44 mch Exp $
  * Created on 27-Feb-2004
  *
  * Copyright (C) AstroGrid. All rights reserved.
  *
- * This software is published under the terms of the AstroGrid 
- * Software License version 1.2, a copy of which has been included 
- * with this distribution in the LICENSE.txt file.  
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
  *
 **/
 package org.astrogrid.jes.component;
@@ -25,7 +25,7 @@ import java.util.Set;
  */
 public class PropertiesConfig extends Config {
     /** Construct a new MapConfig
-     * 
+     *
      */
     public PropertiesConfig() {
         super();
@@ -48,6 +48,16 @@ public class PropertiesConfig extends Config {
     public void setProperty(String arg0, Object arg1) {
         props.put(arg0,arg1);
     }
+
+    /**
+     * @see org.astrogrid.config.Config#getProperties(java.lang.String).
+     * Unused in this implementation
+     */
+    public Object[] getProperties(String key) {
+        throw new UnsupportedOperationException("Didn't think this was used");
+    }
+
+    
     /**
      * @see org.astrogrid.config.Config#loadUrl(java.net.URL)
      */
@@ -68,8 +78,11 @@ public class PropertiesConfig extends Config {
 }
 
 
-/* 
+/*
 $Log: PropertiesConfig.java,v $
+Revision 1.4  2004/10/07 14:33:44  mch
+Added new Config method
+
 Revision 1.3  2004/04/06 08:29:55  nw
 bugfix
 
