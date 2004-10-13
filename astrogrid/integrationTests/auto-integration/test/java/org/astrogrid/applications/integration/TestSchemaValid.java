d4f3da6ab931310e95f39384281f11f6b6c52de0
Paul Harrison <paul.harrison@manchester.ac.uk>
2004-10-13 08:11:30 +0000

    a simple test that can be run from the commandline to test file validity

---
+++ astrogrid/integrationTests/auto-integration/test/java/org/astrogrid/applications/integration/TestSchemaValid.java
@@ -0,0 +1,48 @@
+/*
+ * $Id: TestSchemaValid.java,v 1.1 2004/10/13 08:11:30 pah Exp $
+ * 
+ * Created on 13-Oct-2004 by Paul Harrison (pah@jb.man.ac.uk)
+ * Copyright 2004 AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+
+package org.astrogrid.applications.integration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.astrogrid.test.AstrogridAssert;
+import org.astrogrid.test.schema.SchemaMap;
+
+import junit.framework.TestCase;
+
+/**
+ * Test an xml file for validity. This is a simple test that can be run manually to test for file validity.
+ * @author Paul Harrison (pah@jb.man.ac.uk) 13-Oct-2004
+ * @version $Name:  $
+ * @since iteration6
+ */
+public class TestSchemaValid extends TestCase {
+
+   private static File xmlFile;
+   public static void main(String[] args) {
+      xmlFile = new File(args[0]);
+      junit.textui.TestRunner.run(TestSchemaValid.class);
+   }
+   
+   public void testValid() throws FileNotFoundException
+   {
+      System.out.println(xmlFile.toString());
+      InputStream entry = new FileInputStream(xmlFile);
+      AstrogridAssert.assertSchemaValid(entry,"VODescription",SchemaMap.ALL);
+  
+   }
+   
+
+}
