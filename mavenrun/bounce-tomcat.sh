e0ffdd08eb76d4da8834a3a117aa564b924955e1
John Taylor <John.Taylor@astrogrid.org>
2004-12-07 12:43:46 +0000

    more generous wait time before killing processes

--- mavenrun/bounce-tomcat.sh
+++ mavenrun/bounce-tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: bounce-tomcat.sh,v 1.5 2004/12/05 23:35:23 jdt Exp $ 
+# $Id: bounce-tomcat.sh,v 1.6 2004/12/07 12:43:46 jdt Exp $ 
 ######################################################
 # Script to do a hard bounce of tomcat
 # Sometimes all processes fail to shutdown properly
@@ -10,7 +10,7 @@ echo Tomcat is installed at ${CATALINA_HOME?"Value of CATALINA_HOME must be set"
 
 $CATALINA_HOME/bin/shutdown.sh
 echo "Waiting for Tomcat to shutdown..."
-sleep 10
+sleep 30
 
 echo "Killing any hanging processes..."
 ps -elf | grep $CATALINA_HOME
