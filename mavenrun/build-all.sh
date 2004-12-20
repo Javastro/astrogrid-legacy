cd03be6e25b11d13fedabf435bffe17e8537b3f6
John Taylor <John.Taylor@astrogrid.org>
2004-12-20 12:02:26 +0000

    filemanager needs community common

--- mavenrun/build-all.sh
+++ mavenrun/build-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: build-all.sh,v 1.2 2004/12/20 11:07:27 jdt Exp $ 
+# $Id: build-all.sh,v 1.3 2004/12/20 12:02:26 jdt Exp $ 
 ##################################################
 #
 #  Build all the astrogrid components
@@ -18,8 +18,8 @@ build-component.sh workflow-objects $1
 build-component.sh eXist $1
 build-component.sh registry $1
 build-component.sh filestore $1
-build-component.sh filemanager $1
 build-component.sh community $1       #client
+build-component.sh filemanager $1
 build-component.sh slinger $1
 build-component.sh security $1
 build-component.sh mySpace $1
