ebcc3f9827dc69a0448de5ad6a7569613b834b2e
John Taylor <John.Taylor@astrogrid.org>
2004-12-21 14:00:14 +0000

    got the delays the wrong way round

--- mavenrun/bounce-tomcat.sh
+++ mavenrun/bounce-tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: bounce-tomcat.sh,v 1.11 2004/12/21 13:50:10 jdt Exp $ 
+# $Id: bounce-tomcat.sh,v 1.12 2004/12/21 14:00:14 jdt Exp $ 
 ######################################################
 # Script to do a hard bounce of tomcat
 # Sometimes all processes fail to shutdown properly
@@ -18,9 +18,10 @@ cd $BACK
 echo Shutting down tomcat
 $CATALINA_HOME/bin/shutdown.sh
 echo "Waiting for Tomcat to shutdown..."
-sleep 40
+sleep 90
+#90 seconds seems right at the moment...need to find out why it's taking so long
 
-#echo "Killing any hanging processes..."
+echo "Killing any hanging processes..."
 ps -elf | grep $CATALINA_HOME
 TOMCATPID=`ps -elf | grep $CATALINA_HOME | grep java | awk {'print $4'}`
 
@@ -36,6 +37,6 @@ rm -rf $CATALINA_HOME/logs/*
 echo "Starting up...."
 $CATALINA_HOME/bin/startup.sh
 echo "Waiting...."
-sleep 90
+sleep 40
 
-#90 seconds seems right at the moment...need to find out why it's taking so long
+
