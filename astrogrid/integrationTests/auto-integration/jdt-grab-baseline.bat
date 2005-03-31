460abc0c8ac61e6bb120be35c2e1809427231dcb
John Taylor <John.Taylor@astrogrid.org>
2005-03-31 21:20:52 +0000

    this grabs the baseline of twmbarlwm ready for me to stick in cvs

--- astrogrid/integrationTests/auto-integration/jdt-grab-baseline.bat
+++ astrogrid/integrationTests/auto-integration/jdt-grab-baseline.bat
@@ -1,2 +1,2 @@
-# A script to grab the latest test results from twmbarlwm - needs finishing
-pscpi integration@twmbarlwm.star.le.ac.uk:/data/
+rem A script to grab the latest test results from twmbarlwm - needs finishing
+pscpi integration@twmbarlwm.star.le.ac.uk:/data/cvsDownloads/testing/astrogrid/integrationTests/auto-integration/target/regression-baseline-summary.xml baselines/
