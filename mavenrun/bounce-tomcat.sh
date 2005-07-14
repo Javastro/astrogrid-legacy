2846074023c200045d8d14d3d1cf686f8617a9ae
John Taylor <John.Taylor@astrogrid.org>
2005-07-14 15:29:13 +0000

    removed the exist shutdown, and reduced the delay

--- mavenrun/bounce-tomcat.sh
+++ mavenrun/bounce-tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: bounce-tomcat.sh,v 1.12 2004/12/21 14:00:14 jdt Exp $ 
+# $Id: bounce-tomcat.sh,v 1.13 2005/07/14 15:29:13 jdt Exp $ 
 ######################################################
 # Script to do a hard bounce of tomcat
 # Sometimes all processes fail to shutdown properly
@@ -8,18 +8,12 @@
 # Some reminders
 echo Tomcat is installed at ${CATALINA_HOME?"Value of CATALINA_HOME must be set"}
 
-echo Trying to shutdown Exist:
-# Will need to customise this for different servers
-BACK=$PWD
-cd $CATALINA_HOME/webapps/exist/
-java -jar start.jar shutdown --uri=xmldb:exist://127.0.0.1:8888/exist/xmlrpc/db
-cd $BACK
 
 echo Shutting down tomcat
 $CATALINA_HOME/bin/shutdown.sh
 echo "Waiting for Tomcat to shutdown..."
-sleep 90
-#90 seconds seems right at the moment...need to find out why it's taking so long
+sleep 30
+#30 seconds seems right at the moment...
 
 echo "Killing any hanging processes..."
 ps -elf | grep $CATALINA_HOME
