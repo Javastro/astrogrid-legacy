e71278335e81a42729068ba40213973121e3c568
Catherine Qin <Catherine.Qin@astrogrid.org>
2005-09-01 16:08:46 +0000

    gtr_1230_2

--- astrogrid/applications/commandline/src/java/org/astrogrid/applications/commandline/CommandLineCEAComponentManager.java
+++ astrogrid/applications/commandline/src/java/org/astrogrid/applications/commandline/CommandLineCEAComponentManager.java
@@ -1,11 +1,11 @@
-/*$Id: CommandLineCEAComponentManager.java,v 1.9 2005/08/10 14:45:37 clq2 Exp $
+/*$Id: CommandLineCEAComponentManager.java,v 1.10 2005/09/01 16:08:46 clq2 Exp $
  * Created on 04-May-2004
  *
  * Copyright (C) AstroGrid. All rights reserved.
  *
- * This software is published under the terms of the AstroGrid 
- * Software License version 1.2, a copy of which has been included 
- * with this distribution in the LICENSE.txt file.  
+ * This software is published under the terms of the AstroGrid
+ * Software License version 1.2, a copy of which has been included
+ * with this distribution in the LICENSE.txt file.
  *
 **/
 package org.astrogrid.applications.commandline;
@@ -38,18 +38,18 @@ import java.net.URL;
 public class CommandLineCEAComponentManager extends EmptyCEAComponentManager  {
     /** configuration keys looked for. */
     /** key to look in config for location of configuration file (required) */
-    public static final String DESCRIPTION_URL ="cea.commandline.description.list.url";
+    public static final String DESCRIPTION_URL ="cea.local.apps.config";
     /** key to look in config for location of working dir (optional, defaults to /tmp) */
-    public static final String WORKING_DIR = "cea.commandline.workingdir.file";
-    
-    /** 
+    public static final String WORKING_DIR = "cea.job.temp.files";
+
+    /**
      * construct a component manager configured with a working cea server with the commandline application provider.
      */
     public CommandLineCEAComponentManager() {
         super();
-        final Config config =   SimpleConfig.getSingleton();        
- 
-        // base cea server also needs a provider for vodescription - registry entry builder does the job.        
+        final Config config =   SimpleConfig.getSingleton();
+
+        // base cea server also needs a provider for vodescription - registry entry builder does the job.
         EmptyCEAComponentManager.registerDefaultVOProvider(pico,config);
         //auto-registration with registry -- appropriate for this cea server.
         /* temporarily commented out - kevin suspects this causes problems in auto-integration
@@ -59,35 +59,38 @@ public class CommandLineCEAComponentManager extends EmptyCEAComponentManager  {
          * reinstated but the start method is disabled.
          */
         EmptyCEAComponentManager.registerDefaultRegistryUploader(pico);
-        
+
         // now need the other side - the cec manager itself.
-        registerDefaultServices(pico);        
+        registerDefaultServices(pico);
         EmptyCEAComponentManager.registerDefaultPersistence(pico,config);
-         
-       
+
+
         // now the provider
         registerCommandLineProvider(pico,config);
     }
     public void registerEnvironmentRetriever(final MutablePicoContainer pico)
     {
        logger.info("registering Commandline Environment Retriever");
-       pico.registerComponentImplementation(ApplicationEnvironmentRetriver.class, CommandLineExecutionEnvRetriever.class);
+       pico.registerComponentImplementation(ApplicationEnvironmentRetriver.class,
+                                            CommandLineExecutionEnvRetriever.class);
     }
-  
+
     /** register just the components for the commandline provider - none of the generic components */
     public static final void registerCommandLineProvider(MutablePicoContainer pico, final Config config) {
          log.info("registering commandline description loader");
        // stuff required for application descriptions
          pico.registerComponentImplementation(CommandLineDescriptionsLoader.class);
          // configuration for the loader
-         pico.registerComponentInstance(CommandLineDescriptionsLoader.DescriptionURL.class,new CommandLineDescriptionsLoader.DescriptionURL() {
+         pico.registerComponentInstance(CommandLineDescriptionsLoader.DescriptionURL.class,
+                                        new CommandLineDescriptionsLoader.DescriptionURL() {
              private final URL url = config.getUrl(DESCRIPTION_URL);
              public URL getURL() {
                      return url;
              }
          });
          log.info("registering the commandline application description factory");
-         // factory for appDescs - necessary to register parameter to this by hand - as latest version of pico is a bit funny about references to the container itself.
+         // factory for appDescs - necessary to register parameter to this by hand -
+         // as latest version of pico is a bit funny about references to the container itself.
          pico.registerComponentImplementation(CommandLineApplicationDescriptionFactory.class
                  ,CommandLineApplicationDescriptionFactory.class
                  , new Parameter[]{new ConstantParameter(pico)}
@@ -95,33 +98,44 @@ public class CommandLineCEAComponentManager extends EmptyCEAComponentManager  {
          // 'factory' for environments
          log.info("registering the commandline application environment factory");
          pico.registerComponent( // create a new instance each time.
-             new ConstructorInjectionComponentAdapter(CommandLineApplicationEnvironment.class,CommandLineApplicationEnvironment.class));
+             new ConstructorInjectionComponentAdapter(CommandLineApplicationEnvironment.class,
+                                                      CommandLineApplicationEnvironment.class));
          // configuration for environments
-         pico.registerComponentInstance(CommandLineApplicationEnvironment.WorkingDir.class,new CommandLineApplicationEnvironment.WorkingDir() {
+         pico.registerComponentInstance(CommandLineApplicationEnvironment.WorkingDir.class,new CommandLineApplicationEnvironment.
+         WorkingDir() {
              private final File file = new File(config.getString(WORKING_DIR,System.getProperty("java.io.tmpdir")));
              public File getDir() {
                  return file;
              }
-         }); 
-          
+         });
+
          // again, create a new one at each call. again, need to pass in pico parameter separately.
          pico.registerComponent(
              new ConstructorInjectionComponentAdapter(CommandLineApplicationDescription.class
                      ,CommandLineApplicationDescription.class
                      , new Parameter[]{new ComponentParameter(ApplicationDescriptionEnvironment.class)
                              ,new ConstantParameter(pico)
-                     }                                      
+                     }
              ));
-         
+
          pico.registerComponentImplementation(CommandLineCECControl.class);
     }
- 
-    
+
+
  }
 
 
-/* 
+/*
 $Log: CommandLineCEAComponentManager.java,v $
+Revision 1.10  2005/09/01 16:08:46  clq2
+gtr_1230_2
+
+Revision 1.9.2.2  2005/08/17 15:02:05  gtr
+The wrapping of very long lines is improved.
+
+Revision 1.9.2.1  2005/08/17 11:31:50  gtr
+The configuration keys were shortened.
+
 Revision 1.9  2005/08/10 14:45:37  clq2
 cea_pah_1317
 
@@ -174,5 +188,5 @@ checked in early, broken version - but it builds and tests (fail)
 
 Revision 1.1.2.1  2004/05/21 12:00:22  nw
 merged in latest changes from HEAD. start of refactoring effort
- 
+
 */
