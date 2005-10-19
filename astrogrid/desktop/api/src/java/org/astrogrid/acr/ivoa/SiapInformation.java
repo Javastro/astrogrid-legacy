e3c21f5db71127720f629dd44463f479a1fe039f
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2005-10-19 18:07:06 +0000

    changed inheritance hierarchy - now is a kind of application

--- astrogrid/desktop/api/src/java/org/astrogrid/acr/ivoa/SiapInformation.java
+++ astrogrid/desktop/api/src/java/org/astrogrid/acr/ivoa/SiapInformation.java
@@ -1,4 +1,4 @@
-/*$Id: SiapInformation.java,v 1.1 2005/10/18 16:51:41 nw Exp $
+/*$Id: SiapInformation.java,v 1.2 2005/10/19 18:07:06 nw Exp $
  * Created on 18-Oct-2005
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -10,10 +10,16 @@
 **/
 package org.astrogrid.acr.ivoa;
 
+import org.astrogrid.acr.astrogrid.ApplicationInformation;
+import org.astrogrid.acr.astrogrid.InterfaceBean;
+import org.astrogrid.acr.astrogrid.ParameterBean;
+import org.astrogrid.acr.astrogrid.ParameterReferenceBean;
 import org.astrogrid.acr.astrogrid.ResourceInformation;
 
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**Information bean that describes a registered siap service
  *  * <p>
@@ -24,7 +30,7 @@ import java.net.URL;
  * @author Noel Winstanley nw@jb.man.ac.uk 18-Oct-2005
  *
  */
-public class SiapInformation extends ResourceInformation {
+public class SiapInformation extends ApplicationInformation {
 
     /** Construct a new SiapInformation
      * @param ivorn
@@ -36,7 +42,7 @@ public class SiapInformation extends ResourceInformation {
             String imageServiceType, float maxQueryRegionSizeRa, float maxQueryRegionSizeDec,
             float maxImageExtentRa, float maxImageExtentDec, int maxImageSizeRa, int maxImageSizeDec,
             int maxFileSize, int maxRecords) {
-        super(ivorn, title, description, url);
+        super(ivorn, title, description, parameters,ifaces,url);
         this.imageServiceType = imageServiceType;
         this.maxQueryRegionSizeRa = maxQueryRegionSizeRa;
         this.maxQueryRegionSizeDec = maxQueryRegionSizeDec;
@@ -85,11 +91,90 @@ public class SiapInformation extends ResourceInformation {
     public int getMaxRecords() {
         return this.maxRecords;
     }
+    
+    private static Map parameters;
+    private static InterfaceBean[] ifaces;
+    static { // build static info for a siap applicaiton - doesn't mention optional parameters.
+        // missing paramers are ucd, default value, units, type, subtype,options
+        ParameterBean pos = new ParameterBean("POS","Position",
+                "The position of the region of interest, expressed as the right-ascension and declination of the field center, in decimal degrees" +
+                " using the ICRS coordinate system. A comma should delimit the two values; embedded whitespace is not permitted." +
+                " Example: '12.821,-33.4",null,null,"decimal degrees","float,float",null,null);
+        ParameterBean sz = new ParameterBean("SIZE","Size","The angular size of the region",null,null,"decimal degrees","float",null,null);
+        ParameterBean format = new ParameterBean("FORMAT","Format","desired format(fs) of images",null,null,"comma-delimited list of: mime type|ALL|GRAPHIC|METADATA","String",null,null);
+        ParameterBean intersect = new ParameterBean("INTERSECT","Intersect","indicates how matched images should intersec the  region of interest",
+                null,"OVERLAPS",null,"String",null,new String[]{"COVERS","ENCLOSED","CENTER","OVERLAPS"});
+        
+        ParameterBean naxis = new ParameterBean("NAXIS","Naxis","Size of the output image in pixels",null,null,null,"int,int",null,null);
+        ParameterBean cframe = new ParameterBean("CFRAME","CFrame","Coordinate system reference frame",null,"ICRS",null,"String",null,new String[]{"ICRS","FK5","FK4","ECL","GAL","SGAL"});
+        ParameterBean equinox = new ParameterBean("EQUINOX","Equinox","Epoch of the mean equator and equinox for the specified CFrame",null,null,null,"String",null,null);
+        ParameterBean crpix = new ParameterBean("CRPIX","CRPix","Coordinates of the reference pixel, expressed in the pixel coordinates of the output image",null,null,null,"int,int",null,null);
+        ParameterBean crval = new ParameterBean("CRVAL","CRVal","World coordinates relative to CFrame at the reference point",null,null,null,"coordinates",null,null);
+        ParameterBean cdelt = new ParameterBean("CDELT","CDelt","The scale of the output image",null,null,"decimal degrees per pixel","int,int",null,null);
+        ParameterBean rotang = new ParameterBean("ROTANG","Rotation angle", "Rotation angle of the image in defgrees relative to CFrame",null,"0",null,"angle",null,null);
+        ParameterBean proj = new ParameterBean("PROJ","Projection","Celestial projection of the output image",null,"TAN",null,"String",null,null);
+        ParameterBean verb = new ParameterBean("VERB","Verbose","Desired level of information to be returned",null,",1",null,"int",null,new String[]{"0","1","2","3"});
+        
+        ParameterBean result = new ParameterBean("result","Result","Votable of results",null,null,null,null,null,null);
+        
+        parameters = new HashMap();
+        parameters.put(pos.getName(),pos);
+        parameters.put(sz.getName(),sz);
+        parameters.put(result.getName(),result);
+        parameters.put(format.getName(),format);
+        parameters.put(intersect.getName(),intersect);
+        parameters.put(naxis.getName(),naxis);
+        parameters.put(cframe.getName(),cframe);
+        parameters.put(equinox.getName(),equinox);
+        parameters.put(crpix.getName(),crpix);
+        parameters.put(crval.getName(),crval);
+        parameters.put(cdelt.getName(),cdelt);
+        parameters.put(rotang.getName(),rotang);
+        parameters.put(proj.getName(),proj);
+        parameters.put(verb.getName(),verb);
+        
+        ifaces = new InterfaceBean[] {
+                new InterfaceBean(
+                        "Minimal"
+                        , new ParameterReferenceBean[] {
+                                new ParameterReferenceBean(pos.getName(),1,1)
+                                , new ParameterReferenceBean(sz.getName(),1,1)
+                        }
+                        , new ParameterReferenceBean[] {
+                                new ParameterReferenceBean(result.getName(),1,1)
+                        }
+                )                
+               ,new InterfaceBean(
+                       "Full"
+                       , new ParameterReferenceBean[] {
+                               new ParameterReferenceBean(pos.getName(),1,1)
+                               , new ParameterReferenceBean(sz.getName(),1,1)
+                               , new ParameterReferenceBean(intersect.getName(),1,0)
+                               ,new ParameterReferenceBean(format.getName(),1,0)
+                             ,new ParameterReferenceBean(naxis.getName(),1,0)
+                             ,new ParameterReferenceBean(cframe.getName(),1,0)
+                             ,new ParameterReferenceBean(equinox.getName(),1,0)
+                             ,new ParameterReferenceBean(crpix.getName(),1,0)
+                             ,new ParameterReferenceBean(crval.getName(),1,0)
+                             ,new ParameterReferenceBean(cdelt.getName(),1,0)
+                             ,new ParameterReferenceBean(rotang.getName(),1,0)
+                             ,new ParameterReferenceBean(proj.getName(),1,0) 
+                             ,new ParameterReferenceBean(verb.getName(),1,0)
+                       }
+                       , new ParameterReferenceBean[] {
+                               new ParameterReferenceBean(result.getName(),1,1)
+                       }
+               )
+        };
+    }        
 }
 
 
 /* 
 $Log: SiapInformation.java,v $
+Revision 1.2  2005/10/19 18:07:06  nw
+changed inheritance hierarchy - now is a kind of application
+
 Revision 1.1  2005/10/18 16:51:41  nw
 added information beans for siap and cone registry entries.
  
--- astrogrid/desktop/api/src/java/org/astrogrid/acr/nvo/ConeInformation.java
+++ astrogrid/desktop/api/src/java/org/astrogrid/acr/nvo/ConeInformation.java
@@ -1,4 +1,4 @@
-/*$Id: ConeInformation.java,v 1.1 2005/10/18 16:51:41 nw Exp $
+/*$Id: ConeInformation.java,v 1.2 2005/10/19 18:07:06 nw Exp $
  * Created on 18-Oct-2005
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -11,10 +11,15 @@
 package org.astrogrid.acr.nvo;
 
 import org.astrogrid.acr.astrogrid.AbstractInformation;
+import org.astrogrid.acr.astrogrid.ApplicationInformation;
+import org.astrogrid.acr.astrogrid.InterfaceBean;
+import org.astrogrid.acr.astrogrid.ParameterBean;
+import org.astrogrid.acr.astrogrid.ParameterReferenceBean;
 import org.astrogrid.acr.astrogrid.ResourceInformation;
-
 import java.net.URI;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /** Information bean that describes a registered cone server
  * <p>
@@ -24,7 +29,7 @@ import java.net.URL;
  * @author Noel Winstanley nw@jb.man.ac.uk 18-Oct-2005
  *
  */
-public class ConeInformation extends ResourceInformation {
+public class ConeInformation extends ApplicationInformation {
 
     /** Construct a new ConeInformation
      * @param ivorn
@@ -33,7 +38,7 @@ public class ConeInformation extends ResourceInformation {
      * @param url
      */
     public ConeInformation(URI ivorn, String title, String description, URL url,float maxSR,int maxRecords, boolean verbosity) {
-        super(ivorn, title, description, url);
+        super(ivorn, title, description,parameters,ifaces,url);
         this.maxSR = maxSR;
         this.maxRecords = maxRecords;
         this.verbosity = verbosity;
@@ -56,11 +61,43 @@ public class ConeInformation extends ResourceInformation {
     public boolean isVerbosity() {
         return this.verbosity;
     }
+    
+    private static Map parameters;
+    private static InterfaceBean[] ifaces;
+    static { // build static info for a cone applicaiton.
+        // missing paramers are ucd, default value, units, type, subtype,options
+        ParameterBean ra = new ParameterBean("ra","Right Ascension","right ascension of the position",null,null,null,"float",null,null); // @todo improve the metadata
+        ParameterBean dec = new ParameterBean("dec","Declination","declination of the position",null,null,null,"float",null,null);
+        ParameterBean sz = new ParameterBean("sz","Size","size of search radius",null,"1.0",null,"float",null,null);
+        ParameterBean result = new ParameterBean("result","Result","Votable of results",null,null,null,"votable",null,null);
+        parameters = new HashMap();
+        parameters.put(ra.getName(),ra);
+        parameters.put(dec.getName(),dec);
+        parameters.put(sz.getName(),sz);
+        parameters.put(result.getName(),result);
+        
+        ifaces = new InterfaceBean[] {
+              new InterfaceBean(
+                       "Cone"
+                       , new ParameterReferenceBean[] {
+                               new ParameterReferenceBean(ra.getName(),1,1)
+                               , new ParameterReferenceBean(dec.getName(),1,1)
+                               , new ParameterReferenceBean(sz.getName(),1,1)
+                       }
+                       , new ParameterReferenceBean[] {
+                               new ParameterReferenceBean(result.getName(),1,1)
+                       }
+               )
+        };
+    }    
 }
 
 
 /* 
 $Log: ConeInformation.java,v $
+Revision 1.2  2005/10/19 18:07:06  nw
+changed inheritance hierarchy - now is a kind of application
+
 Revision 1.1  2005/10/18 16:51:41  nw
 added information beans for siap and cone registry entries.
  
