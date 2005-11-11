b7c1fcb671b617a870525724f1ae0b27585b4c22
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2005-11-11 10:08:52 +0000

    added new interface

---
+++ astrogrid/desktop/api/src/java/org/astrogrid/acr/ui/Lookout.java
@@ -0,0 +1,32 @@
+/*$Id: Lookout.java,v 1.1 2005/11/11 10:08:52 nw Exp $
+ * Created on 27-Oct-2005
+ *
+ * Copyright (C) AstroGrid. All rights reserved.
+ *
+ * This software is published under the terms of the AstroGrid 
+ * Software License version 1.2, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ **/
+package org.astrogrid.acr.ui;
+
+
+/**
+ * @author Noel Winstanley nw@jb.man.ac.uk 27-Oct-2005
+ *
+ */
+public interface Lookout  {
+    public void show();
+    public void hide();
+    public void refresh();
+}
+
+/* 
+ $Log: Lookout.java,v $
+ Revision 1.1  2005/11/11 10:08:52  nw
+ added new interface
+
+ Revision 1.1  2005/11/01 09:19:46  nw
+ messsaging for applicaitons.
+ 
+ */
