bce6c96d0ed93256114d8332f974d6a711be3870
John Taylor <John.Taylor@astrogrid.org>
2005-11-24 18:35:47 +0000

    robustified

--- astrogrid/desktop/impl/src/python-scripts/vorm_minus_r_star.py
+++ astrogrid/desktop/impl/src/python-scripts/vorm_minus_r_star.py
@@ -21,16 +21,19 @@ def rmminusrstar(bringoutyerdead):
         
     else: 
         print "Strange - what's a " + folderInfo['name'] + "?"
+    try:
+        acr.astrogrid.myspace.delete(bringoutyerdead)
+    except Exception,e:
+        print "whoops "+str(e)
 
-    acr.astrogrid.myspace.delete(bringoutyerdead)
-
-print "I hope you really meant to do that..."
-home = acr.astrogrid.myspace.getHome()
-print "HomeSpace: " + home
-# home should be a directory !
-contents = acr.astrogrid.myspace.listIvorns(home)
-for f in contents:
-    rmminusrstar(f)
+if __name__ == "__main__":
+    print "I hope you really meant to do that..."
+    home = acr.astrogrid.myspace.getHome()
+    print "HomeSpace: " + home
+    # home should be a directory !
+    contents = acr.astrogrid.myspace.listIvorns(home)
+    for f in contents:
+        rmminusrstar(f)
 
 
 
