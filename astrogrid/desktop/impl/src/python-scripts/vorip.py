2b9aaaa5bc8caacb3ccd1d63864f74c8610fadc7
John Taylor <John.Taylor@astrogrid.org>
2005-11-24 18:43:58 +0000

    Another example python script

---
+++ astrogrid/desktop/impl/src/python-scripts/vorip.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# Produce a tree-view of the user's homespace
+""" 
+Backup your entire myspace to the current directory
+Needs astrogrid.py
+   Author: jdt@roe.ac.uk
+"""
+from astrogrid import acr
+import os
+import urllib
+
+def copyFolder(folder, indentLvl=""):
+    # should check that it is a folder
+    folderInfo = acr.astrogrid.myspace.getNodeInformation(folder)
+    if not folderInfo['folder']:
+        raise RuntimeError, "This function should be called with a folder"
+    
+    indentLvl=indentLvl+"/"+folderInfo['name']
+    os.mkdir(indentLvl)
+    print indentLvl
+    contents = acr.astrogrid.myspace.listIvorns(folder)
+
+
+    for f in contents:
+        fileInfo = acr.astrogrid.myspace.getNodeInformation(f)
+        if fileInfo['file']:
+            print "Copying " + f + " to " + indentLvl
+            try:
+                contentsURL = acr.astrogrid.myspace.getReadContentURL(f)
+                #externalFile = urllib2.urlopen(contentsURL)
+                localFileName = indentLvl+"/"+fileInfo['name']
+                #localFile = open(localFileName,'w')
+                
+                print urllib.urlretrieve(contentsURL, localFileName);
+                
+                #localFile.write(externalFile.read());
+                #localFile.close()
+            except Exception, e:
+                print "Whoops " + str(e)
+                
+        else:
+            copyFolder(f, indentLvl)
+
+
+def copyRoot():
+    home = acr.astrogrid.myspace.getHome()
+    print "HomeSpace: " + home
+    # home should be a directory !
+    copyFolder(home,".")
+
+if __name__ == "__main__":
+    copyRoot()
+
+
+    
+    
