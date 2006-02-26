7179d976d39d827ae33f3b139ff9799a3c60ea9e
Norman Gray <norman.gray@glasgow.ac.uk>
2006-02-26 18:00:57 +0000

    Add rdf-ingest-from-stream to read RDF from a stream Add MIME<->language mappings

--- experiments/norman/quaestor/src/main/scm/quaestor/jena.scm
+++ experiments/norman/quaestor/src/main/scm/quaestor/jena.scm
@@ -5,7 +5,10 @@
 
 (module jena
  (new-empty-model
-  rdf-ingest-from-stream)
+  rdf-ingest-from-stream
+  rdf-language-ok?
+  mime-type->rdf-language
+  rdf-language->mime-type)
 
  ;; Return a new empty model
  (define (new-empty-model)
@@ -34,14 +37,78 @@
                (java-null <java.lang.string>)))
      model))
 
+ ;; Given a language LANG, which may be a Jstring, scheme string or #f,
+ ;; return true if it is one of the allowed RDF languages, "RDF/XML[-ABBREV]",
+ ;; "N-TRIPLE" and "N3".  Returns #f if it is not a legal language.
+ (define (rdf-language-ok? lang)
+   (or (not lang)
+       (member (as-scheme-string lang)
+               '("RDF/XML" "RDF/XML-ABBREV" "N-TRIPLE" "N3"))))
+
+ ;; The set of mappings from MIME types to RDF languages.
+ ;; Used in both directions.
+ (define mime-lang-mappings
+   '(("application/n3"      . "N3")
+     ;; http://infomesh.net/2002/notation3/#mimetype
+     ("text/rdf+n3"         . "N3")
+     ;; http://www.w3.org/DesignIssues/Notation3.html
+     ("application/rdf+xml" . "RDF/XML")
+     ;; http://www.w3.org/TR/rdf-syntax-grammar/#section-MIME-Type
+     ;; Generic RDF MIME type: http://www.ietf.org/rfc/rfc3870.txt
+     ("text/plain"          . "N-TRIPLE")
+     ;; http://www.dajobe.org/2001/06/ntriples/
+     ))
+
+ ;; Given a MIME type, return an RDF language, as one of the strings accepted
+ ;; by RDF-LANGUAGE-OK?.  If the MIME type isn't recognised, return #f.
+ (define (mime-type->rdf-language s)
+   (let ((p (assoc s mime-lang-mappings)))
+     (and p (cdr p))))
+
+ ;; Map RDF language to MIME type.  This is the inverse of
+ ;; MIME-TYPE->RDF-LANGUAGE.
+ (define (rdf-language->mime-type lang)
+   (let loop ((l mime-lang-mappings))
+     (cond ((null? l)
+            #f)
+           ((string=? lang (cdar l))
+            (caar l))
+           (else
+            (loop (cdr l))))))
+
+
  ;; Given a Java STREAM, this reads in the RDF within it, and returns
- ;; the resulting model.
- (define (rdf-ingest-from-stream stream)
+ ;; the resulting model.  MIME-TYPE may be #f, in which case we use the
+ ;; default language for the Model read function.  If it is given, it
+ ;; must be one of the mime-types which is handled by MIME-TYPE->RDF-LANGUAGE.
+ ;; 
+ ;; It is not clear what the read method does if the language parameter
+ ;; is not one of these three, but just to be sure, we check the language, and
+ ;; return #f if it is not valid.
+ (define (rdf-ingest-from-stream stream mime-type)
    (define-generic-java-method
      read)
-   (let ((model (new-empty-model)))
-     (read model
-           stream
-           (->jstring ""))))     ;base -- let the serial'n handle this
+   (define-java-class <java.lang.string>)
+   (let ((language                      ;set to valid language (jstring or null)
+          (if mime-type                 ;...or #f
+                       (let ((l (mime-type->rdf-language mime-type)))
+                         (and l (->jstring l)))
+                       (java-null <java.lang.string>))))
+     (and language
+          (let ((model (new-empty-model)))
+            (read model
+                  stream
+                  (->jstring "")        ;base -- let the serial'n handle this
+                  (or language ))))))
+
+ ;; Return the object S, which should be either a Java or Scheme string,
+ ;; as a Scheme string.
+ (define (as-scheme-string s)
+   (cond ((string? s)
+          s)
+         ((java-object? s)              ;should be a jstring
+          (->string s))
+         (else
+          #f)))
 
 )
