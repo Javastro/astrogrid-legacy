72a92aa7809cf2ec01841d9b93d0a407d7f680f3
Norman Gray <norman.gray@glasgow.ac.uk>
2006-02-26 18:02:14 +0000

    GET and PUT interface now fully (?) implemented.

--- experiments/norman/quaestor/src/main/scm/quaestor.scm
+++ experiments/norman/quaestor/src/main/scm/quaestor.scm
@@ -10,12 +10,17 @@
 (import utils)
 (require-library 'quaestor/knowledgebase)
 (import knowledgebase)
-;(require-library 'quaestor/jena)
-;(import jena)
+(require-library 'quaestor/jena)
+(import jena)
 
 (require-library 'sisc/libs/srfi/srfi-1)
 (import* srfi-1
-         remove)
+         remove
+         filter)
+(require-library 'sisc/libs/srfi/srfi-13)
+(import* srfi-13
+         string-prefix?
+         string-downcase)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -65,48 +70,109 @@
     set-status
     set-content-type
     get-writer
+    get-headers
     println)
-  (define (no-can-do code msg)
-    (set-http-response response code)
-    (response-page "Quaestor: not found" `((p ,msg))))
+  (define-java-classes
+    <java.lang.string>)
+
+  (define msgs
+    (let ((msglist '()))
+      (lambda msg
+        (if (null? msg)
+            (apply string-append (reverse msglist))
+            (set! msglist (cons msg msglist))))))
+
+  ;; Examine any Accept headers in the request.  Return #f if there were
+  ;; Accept headers and we can't satisfy them;
+  ;; return (mime-string . rdf-language) if we can satisfy a request;
+  ;; return (mime-string . "RDF/XML") if there were no Accept headers.
+  (define (find-ok-language rq)
+    (let ((lang-mime-list
+           (map ->string
+                (enumeration->list (get-headers rq (->jstring "accept"))))))
+      (msgs (format #f "lang-mime-list=~s~%" lang-mime-list))
+      (if (null? lang-mime-list)
+          (cons (rdf-language->mime-type "RDF/XML")
+                "RDF/XML")              ;explicit default language
+          (let loop ((ml lang-mime-list))
+            (if (null? ml)
+                #f                      ;ooops
+                (let ((lang-string (mime-type->rdf-language (car ml))))
+                  (msgs (format #f "mime-type->rdf-language: ~s -> ~s"
+                                (car ml) lang-string))
+                  (if lang-string
+                      (cons (car ml)
+                            lang-string)
+                      (loop (cdr ml)))))))))
+
   (cond ((= (length path-info-list) 1)
          (let ((kb-name (car path-info-list)))
-           (let ((kb (get-kb kb-name)))
-             (if kb                     ;normal case
-                 (begin (set-http-response response '|SC_OK|)
-                        (cond ((or (not query-string)
-                                   (string=? query-string "model"))
-                               (write (kb 'get-model)
-                                      (get-output-stream response)))
-                              ((string=? query-string "metadata")
-                               (set-content-type response
-                                                 (->jstring "text/plain"))
-                               (println (get-writer response)
-                                        (or (kb 'get-metadata-as-jstring)
-                                            (->jstring ""))))
-                              (else
-                               (no-can-do '|SC_BAD_REQUEST|
-                                          (format #f "bad query: ~a"
-                                                  query-string)))))
-                 (no-can-do '|SC_NOT_FOUND|
-                            (format #f "No such knowledgebase: ~a" kb-name))))))
+           (let ((kb (get-kb kb-name))
+                 (mime-and-lang (find-ok-language request))
+                 (query (string->symbol (or query-string "model"))))
+             (cond ((and kb
+                         (eq? query 'model)
+                         mime-and-lang) ;normal case
+                    (set-http-response response '|SC_OK|)
+                    (set-content-type response
+                                      (->jstring (car mime-and-lang)))
+                    (write (kb 'get-model)
+                           (get-output-stream response)
+                           (->jstring (cdr mime-and-lang)))
+                    #t)
+
+                   ((and kb
+                         (eq? query 'model))
+                    (no-can-do response
+                               '|SC_NOT_ACCEPTABLE|
+                               "Cannot generate requested content-type:~%~a"
+                               (msgs)))
+
+                   ((and kb
+                         (eq? query 'metadata))
+                    (set-http-response response '|SC_OK|)
+                    (set-content-type response ;currently wrong: md is text
+                                      (->jstring (car mime-and-lang)))
+                    (println (get-writer response)
+                             (or (kb 'get-metadata-as-jstring)
+                                 (->jstring "")))
+                    #t)
+
+                   (kb
+                    (no-can-do response
+                               '|SC_BAD_REQUEST|
+                               "bad query: ~a" query-string))
+
+                   (else
+                    (no-can-do response
+                               '|SC_NOT_FOUND|
+                               "No such knowledgebase: ~a" kb-name))))))
+
         ((= (length path-info-list) 2)
-         (let ((kb-name (car path-info-list))
-               (submodel-name (cadr path-info-list)))
-           (let ((kb (get-kb kb-name)))
-             (if kb
-                 (let ((submodel (kb 'get-model submodel-name)))
-                   (if submodel
-                       (begin (set-http-response response '|SC_OK|)
-                              (write submodel
-                                     (get-output-stream response))
-                              #t)
-                       (no-can-do '|SC_NOT_FOUND|
-                                  (format #f "No such submodel ~a/~a"
-                                          kb-name submodel-name))))
-                 (no-can-do '|SC_NOT_FOUND|
-                            (format #f "No such knowledgebase ~a"
-                                    kb-name))))))
+         (no-can-do response
+                    '|SC_NOT_IMPLEMENTED|
+                    "Retrieval of submodel is disabled pending refactoring")
+;;          (let ((kb-name (car path-info-list))
+;;                (submodel-name (cadr path-info-list)))
+;;            (let ((kb (get-kb kb-name)))
+;;              (if kb
+;;                  (let ((submodel (kb 'get-model submodel-name))
+;;                        (lang-pair (find-ok-language request)))
+;;                    (cond ((and submodel lang-pair)
+;;                           (set-http-response response '|SC_OK|)
+;;                           (write submodel
+;;                                  (get-output-stream response)
+;;                                  (->jstring (cdr lang-pair)))
+;;                           #t)XXX
+;;                        (no-can-do response
+;;                                   '|SC_NOT_FOUND|
+;;                                   (format #f "No such submodel ~a/~a"
+;;                                           kb-name submodel-name))))
+;;                  (no-can-do response
+;;                             '|SC_NOT_FOUND|
+;;                             (format #f "No such knowledgebase ~a"
+;;                                     kb-name)))))
+         )
         (else
          #f)))
 
@@ -123,44 +189,26 @@
 ;; are two elements in the path).  It is an error to create a
 ;; knowledgebase where one of that name exists already.
 (define (put request response)
-  (define-generic-java-method
-    get-reader)
+
   (with-failure-continuation
        (make-fc request response '|SC_INTERNAL_SERVER_ERROR|)
     (lambda ()
-      (define (no-can-do response-code msg)
-        (set-http-response response response-code)
-        (response-page "Quaestor: no can do" `((p ,msg))))
+      (define-generic-java-methods
+        get-reader
+        get-input-stream)
       (let ((path-list (request->path-list request)))
-        (cond ((= (length path-list) 1) ;create new knowledgebase
-              ;; should this check that there's no content?  How?
-               (let ((kb-name (car path-list)))
-                 (if (get-kb kb-name)           ;already exists
-                     (no-can-do '|SC_FORBIDDEN| ;correct? or SC_CONFLICT?
-                                (format #f "Knowledgebase ~a already exists"
-                                        kb-name))
-                     (let ((kb (new-kb kb-name))) ;normal case
-                       (kb 'set-metadata (reader->jstring (get-reader request)))
-                       (set-http-response response '|SC_NO_CONTENT|)
-                       #t))))
+        (cond ((= (length path-list) 1)
+               (manage-knowledgebase (car path-list)
+                                     (get-reader request)
+                                     (request->query-string request)
+                                     response))
 
               ((= (length path-list) 2)
-               ;; Upload a RDF/XML submodel
-               (let ((kb-name (car path-list))
-                     (submodel-name (cadr path-list)))
-                 (let ((kb (get-kb kb-name)))
-                   (define-generic-java-method
-                     get-input-stream)
-                   (if kb
-                       (begin (kb 'add-submodel  ;normal case
-                                  submodel-name
-                                  (rdf-ingest-from-stream
-                                   (get-input-stream request)))
-                              (set-http-response response '|SC_NO_CONTENT|)
-                              #t)
-                       (no-can-do '|SC_BAD_REQUEST|
-                                  (format #f "No such knowledgebase ~a"
-                                          kb-name))))))
+               (update-submodel (car path-list)
+                                (cadr path-list)
+                                (request->content-header-strings request)
+                                (get-input-stream request)
+                                response))
 
               (else                     ;ooops
                (let ()
@@ -171,6 +219,93 @@
                   `((p "The request path "
                        (code ,(->string (get-path-info request)))
                        " has the wrong number of elements (1 or 2)"))))))))))
+
+;; Create a new KB, or manage an existing one.  The knowledgebase is
+;; called kb-name (a symbol), and the content of the request is read
+;; from the given reader.
+(define (manage-knowledgebase kb-name reader query response)
+  (let ((kb (get-kb kb-name)))
+    (cond ((and kb
+                query
+                (string=? query "metadata")) ;update metadata
+           (kb 'set-metadata
+               (reader->jstring reader))
+           (set-http-response response '|SC_NO_CONTENT|)
+           #t)
+
+          ((and kb query)  ;unrecognised query
+           (no-can-do response '|SC_BAD_REQUEST|
+                      "Unrecognised query ~a?~a"
+                      kb-name query))
+
+          (kb              ;already exists
+           (no-can-do response '|SC_FORBIDDEN| ;correct? or SC_CONFLICT?
+                      "Knowledgebase ~a already exists"
+                      kb-name))
+
+          (query           ;no knowledgebase, but there is a query
+           (no-can-do response '|SC_BAD_REQUEST|
+                      "Knowledgebase ~a does not exist, so query ~a is not allowed"
+                      kb-name query))
+
+          (else            ;normal case
+           (let ((nkb (new-kb kb-name))) ;normal case
+             (nkb 'set-metadata
+                  (reader->jstring reader))
+             (set-http-response response '|SC_NO_CONTENT|)
+             #t)))))
+
+;; Given a knowledgebase called KB-NAME, upload a RDF/XML submodel called
+;; KB-NAME which is available from the given STREAM.  The CONTENT-HEADERS
+;; are an alist (symbol . string), where the symbol is 'type, 'length, and
+;; so on.
+(define (update-submodel kb-name submodel-name content-headers stream response)
+  ;; Given an alist ALIST of (key . value) pairs, and a list
+  ;; RECOGNISED of recognised symbols, return true if each of the alist
+  ;; keys, which are all symbols, is a member of the list RECOGNISED.
+  (define (check-cars alist recognised)
+    (if (null? alist)
+        #t
+        (if (memq (caar alist) recognised)
+            (check-cars (cdr alist) recognised)
+            #f)))
+
+  (let ((kb (get-kb kb-name))
+        (ok-headers '(type length)))
+    (cond ((not (check-cars content-headers ok-headers))
+           (no-can-do response '|SC_NOT_IMPLEMENTED|
+                      "PUT found content headers (~a), can only handle (~a)"
+                      (map (lambda (h) (string-append "content-" (car h)))
+                           content-headers)
+                      (map (lambda (h) (string-append "content-" h))
+                           ok-headers)))
+
+          (kb                         ;normal case
+           (let* ((rdf-mime (and (assq 'type content-headers)
+                                 (cdr (assq 'type content-headers))))
+                  (submodel (rdf-ingest-from-stream stream rdf-mime)))
+             (if submodel
+                 (begin (kb 'add-submodel  ;normal case
+                            submodel-name
+                            submodel)
+                        (set-http-response response '|SC_NO_CONTENT|)
+                        #t)
+                 (no-can-do response '|SC_BAD_REQUEST|
+                            "Bad RDF MIME type! ~a" mime))))
+
+          (else
+           (no-can-do response '|SC_BAD_REQUEST|
+                      (format #f "No such knowledgebase ~a"
+                              kb-name))))))
+
+;; Given a RESPONSE, set the response status to the given RESPONSE-CODE,
+;; and produce a status page using the given format and arguments.
+(define (no-can-do response response-code fmt . args)
+  (let ((msg (apply format `(#f ,fmt ,@args))))
+    (set-http-response response response-code)
+    (response-page "Quaestor: no can do" `((p ,msg)))))
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -210,7 +345,8 @@
 ;;                        (->string (decode n s (->jstring "UTF-8"))))
 ;;                      (->list (split path-string (->jstring "/"))))))))
 
-;; Extract the query-string from the request, returning it as a (scheme) string
+;; Extract the query-string from the request, returning it as a (scheme)
+;; string, or #f if there is no query.
 (define (request->query-string request)
   (define-generic-java-method
     get-query-string)
@@ -218,6 +354,34 @@
     (if (java-null? qs)
         #f
         (->string qs))))
+
+;; Extract the content-* headers from the REQUEST, and return them as an alist:
+;; (name . value), where 'name' is the header name with 'content-' removed,
+;; and 'value' is the value as a Scheme string.
+(define (request->content-header-strings request)
+  (filter (lambda (x) x)
+          (map (lambda (p)
+                 (let ((h (car p)))
+                   (and (string-prefix? "content-" h)
+                        (cons (string->symbol (substring h 8 (string-length h)))
+                              (cdr p)))))
+               (request->header-alist request))))
+
+;; Return the set of request headers as an alist, each element of which is
+;; of the form (header-string . value-string) (both scheme strings)
+(define request->header-alist
+  (let ((alist #f))
+    (lambda (request)
+      (define-generic-java-methods
+        get-header-names
+        get-header)
+      (or alist
+          (set! alist
+                (map (lambda (header-jname)
+                       (cons (->string header-jname)
+                             (->string (get-header request header-jname))))
+                     (enumeration->list (get-header-names request)))))
+      alist)))
 
 ;; Given a READER, return a Java string containing the contents of the stream.
 (define (reader->jstring reader)
@@ -262,14 +426,19 @@
   (lambda (error-record cont)
     (set-http-response response status)
 
-    (set-content-type response (->jstring "text/html"))
-    (response-page "Internal server error"
-                   `((p (strong "Error: " ,(error-message error-record)))
-                     (h2 "Stack trace:")
-                     (pre ,(with-output-to-string
-                             (lambda ()
-                               (print-stack-trace cont))))
-                     ,@(tabulate-request-information request)))
+;;     (set-content-type response (->jstring "text/html"))
+;;     (response-page "Internal server error"
+;;                    `((p (strong "Error: " ,(error-message error-record)))
+;;                      (h2 "Stack trace:")
+;;                      (pre ,(with-output-to-string
+;;                              (lambda ()
+;;                                (print-stack-trace cont))))
+;;                      ,@(tabulate-request-information request)))
+    (set-content-type response (->jstring "text/plain"))
+    (format #f "Internal server error~%~%Error: ~a~%~%Stack trace:~%~a~%"
+            (error-message error-record)
+            (with-output-to-string
+              (lambda () (print-stack-trace cont))))
 
 ;;     (set-content-type response (->jstring "text/plain"))
 ;;     (let ((w (get-writer response)))
