e3eb539d1fe19228d2f0aca4e31473a4566e088d
Norman Gray <norman.gray@glasgow.ac.uk>
2006-02-27 14:12:05 +0000

    Rework so that the main model is merged lazily and on demand (avoids   problems about removing models before updating submodels Add get-model-tbox/abox

--- experiments/norman/quaestor/src/main/scm/quaestor/knowledgebase.scm
+++ experiments/norman/quaestor/src/main/scm/quaestor/knowledgebase.scm
@@ -4,6 +4,10 @@
 
 (require-library 'quaestor/jena)
 
+(require-library 'sisc/libs/srfi/srfi-1)
+(import* srfi-1
+         filter)
+
 (module knowledgebase
   (kb-new
    kb-get
@@ -57,7 +61,7 @@
                     _model-list))
         kb)))
 
-  ;; Returns a list of symbols naming available knowledgebases
+  ;; Returns a list of available knowledgebases.
   (define (kb-get-names)
     (map car _model-list))
 
@@ -81,27 +85,28 @@
   ;; Add a new submodel to the model.  Returns the original or an updated
   ;; submodel list, or #f on any errors (there's nothing which triggers #f
   ;; at present, but it's documented to do this just in case)
-  (define (add-submodel model
+  (define (add-submodel ;model
                         submodel-list
                         new-submodel-name
-                        new-submodel-model)
-    (define-generic-java-method
-      add)
+                        new-submodel-model
+                        tbox?)
+;;     (define-generic-java-method
+;;       add)
     (if (not (and (symbol? new-submodel-name)
                   (is-java-type?
                    new-submodel-model
                    '|com.hp.hpl.jena.rdf.model.Model|)))
         (error 'make-kb "Bad call (add ~s ~s)"
                new-submodel-name new-submodel-model))
-    ;; The following should be a bit more sophisticated:
-    ;; we should remove the previous submodel before adding
-    ;; the new one.  Can we detect any errors?
-    (add model new-submodel-model)
-    (let ((sm-pair (assq new-submodel-name submodel-list)))
-      (if sm-pair
-          (begin (set-cdr! sm-pair submodel)        ;already exists
+;;     ;; The following should be a bit more sophisticated:
+;;     ;; we should remove the previous submodel before adding
+;;     ;; the new one.  Can we detect any errors?
+;;     (add model new-submodel-model)
+    (let ((sm-list (assq new-submodel-name submodel-list)))
+      (if sm-list
+          (begin (set-cdr! (cdr sm-list) submodel)        ;already exists
                  submodel-list)
-          (cons (cons new-submodel-name new-submodel-model) ;new submodel
+          (cons `(,new-submodel-name ,tbox? . ,new-submodel-model) ;new
                 submodel-list))))
   
 
@@ -109,11 +114,13 @@
   ;; The model and submodels may be retrieved, and new ones added, by
   ;; calling the procedure with a command as the second argument.
   ;;
-  ;;    (kb 'add-submodel SUBMODEL-NAME SUBMODEL)
-  ;;        Add or update a submodel with the given name.
+  ;;    (kb 'add-abox/tbox SUBMODEL-NAME SUBMODEL)
+  ;;        Add or update a abox/tbox with the given name.
   ;;        SUBMODEL-NAME may be a string or a symbol.
   ;;    (kb 'get-model)
   ;;        Return the model, or #f if none exists.
+  ;;    (kb 'get-model-abox/tbox)
+  ;;        Return the merged abox/tbox submodels, or #f if there are none.
   ;;    (kb 'get-metadata-as-string)
   ;;    (kb 'get-metadata-as-jstring)
   ;;        Return the model's metadata, if any, as a Scheme or Java string
@@ -121,40 +128,58 @@
   ;;        Return the named submodel, or #f if none exists.
   ;;    (kb 'set-metadata INFO)
   ;;        Set the metadata to the arbitrary string INFO.
+  ;;    (kb 'info)
+  ;;        Return alist with info
   (define (make-kb kb-name)
-    (let ((model (new-empty-model)) ;overall model for the KB
-          (submodels '())     ;a list of (name . submodel) pairs
+    (let (;(model (new-empty-model)) ;overall model for the KB
+          (submodels '())     ;a list of (name tbox? . submodel) pseudo-lists
           (metadata #f))
 
       (lambda (cmd . args)
         (case cmd
-          ((add-submodel)
-           ;; (kb 'add-submodel SUBMODEL-NAME SUBMODEL)
+          ((add-abox add-tbox)
+           ;; (kb 'add-abox/tbox SUBMODEL-NAME SUBMODEL)
            (if (not (= (length args) 2))
                (error 'make-kb
-                      "Bad call to add-submodel: wrong number of args in ~s"
+                      "Bad call to add-abox/tbox: wrong number of args in ~s"
                       args))
            (set! submodels
-                 (add-submodel model
+                 (add-submodel ;model
                                submodels
                                (as-symbol (car args))
-                               (cadr args))))
+                               (cadr args)
+                               (eq? cmd 'add-tbox))))
 
           ((get-model)
            ;; (kb 'get-model [SUBMODEL-NAME])
            ;; Return model or #f
            (case (length args)
              ((0)
-              model)
+              ;model
+              (rdf-merge-models (map cddr submodels))
+              )
              ((1)
               (let ((sm (assq (as-symbol (car args))
                               submodels)))
-                (and sm (cdr sm))))
+                (and sm (cddr sm))))
              (else
-              (if (not (= (length args) 1))
-                  (error 'make-kb
-                         "Bad call to get: wrong number of args in ~a"
-                         args)))))
+              (error 'make-kb
+                     "Bad call to get: wrong number of args in ~a"
+                     args))))
+
+          ((get-model-tbox get-model-abox)
+           ;; (kb 'get-model-tbox/abox)
+           ;; return merged models or #f if none
+           (or (null? args)
+               (error 'make-kb
+                      "Bad call to get-model-tbox/abox: wrong no args ~s" args))
+           (let ((models (filter (if (eq? cmd 'get-model-tbox)
+                                     (lambda (x) (cadr x))
+                                     (lambda (x) (not (cadr x))))
+                                 submodels)))
+             (if (null? models)
+                 #f
+                 (rdf-merge-models (map cddr models)))))
 
           ((get-metadata-as-string)
            (cond ((is-java-type? metadata '|java.lang.String|)
@@ -180,6 +205,12 @@
                       "bad call to set-metadata: wrong number of args in ~s"
                       args))
            (set! metadata (car args)))
+
+          ((info)
+           (list (cons 'submodels (map (lambda (sm)
+                                         `((name . ,(car sm))
+                                           (tbox . ,(cadr sm))))
+                                       submodels))))
 
           (else
            (error 'make-kb "impossible command for knowledgebase: ~s" cmd))))))
