95dd3daf0d823c852386dc1e3ea8455735cc55eb
Norman Gray <norman.gray@glasgow.ac.uk>
2006-03-12 19:57:53 +0000

    Give exported procedures sisc-xml: prefix Improve error handling in xml->sexp/input-source Add sisc-xml:skip-whitespace? parameterizable procedure xml->sexp/input-source now handles namespaces

--- experiments/norman/quaestor/src/test/scm/util/sisc-xml.scm
+++ experiments/norman/quaestor/src/test/scm/util/sisc-xml.scm
@@ -1,23 +1,44 @@
-;; Test cases for the util/siscsax.scm package
+;; Test cases for the util/sisc-xml.scm package
+
+(require-library 'sisc/libs/srfi/srfi-39) ;parameter objects: form PARAMETERIZE
 
 (require-library 'util/sisc-xml)
 (import sisc-xml)
 
 (expect-failure xml->sexp-1
-        (xml->sexp/string ""))
+        (sisc-xml:xml->sexp/string ""))
 
-(expect xml->sexp-2
+(expect xml->sexp-basic
         '(*TOP* (doc (p "hello") (p)))
-        (xml->sexp/string "<doc><p>hello</p><p/></doc>"))
+        (sisc-xml:xml->sexp/string "<doc><p>hello</p><p/></doc>"))
 
-(expect xml->sexp-3
+(expect xml->sexp-basic-with-spaces
         '(*TOP* (doc (p "hello  ") (p)))
-        (xml->sexp/string "<doc>  <p>hello  </p>  <p>   </p> </doc>"))
+        (sisc-xml:xml->sexp/string "<doc>  <p>hello  </p>  <p>   </p> </doc>"))
 
-(expect xml->sexp-4
+(expect xml->sexp-basic-plus-whitespace
+        '(*TOP* (doc "  " (p "hello  ") "  " (p "   ") " "))
+        (parameterize ((sisc-xml:skip-whitespace? #f))
+          (sisc-xml:xml->sexp/string
+           "<doc>  <p>hello  </p>  <p>   </p> </doc>")))
+
+;; The order of attributes is preserved by xml->sexp, though it's not
+;; thus documented.
+(expect xml->sexp-attributes
         '(*TOP* (doc (p (@ (class "simple"))
                         "content1")
-                     (p (@ (att1  "value1") ; order of these two not significant
-                           (class "complicated")))
+                     (p (@ (class "complicated")
+                           (att1  "value1")))
                      (p "content2")))
-        (xml->sexp/string "<doc><p class='simple'  >content1</p>  <p class='complicated' att1=\"value1\"/> <p>content2</p></doc>"))
+        (sisc-xml:xml->sexp/string "<doc><p class='simple'  >content1</p>  <p class='complicated' att1=\"value1\"/> <p>content2</p></doc>"))
+
+(expect xml->sexp-namespacing
+        '(*TOP* (doc (p (@ ((urn:example . a1) "v1")
+                           (a2 "v2"))
+                        "c1")
+                     ((urn:example . q)
+                      "c2")
+                     ((urn:example2 . r)
+                      "c3")))
+        (sisc-xml:xml->sexp/string
+         "<doc xmlns:x='urn:example'><p x:a1='v1' a2='v2'>c1</p><x:q>c2</x:q><r xmlns='urn:example2'>c3</r></doc>"))
