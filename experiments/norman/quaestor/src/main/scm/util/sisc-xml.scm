0f18ac91c8a26f974000923b241667d3bb5ea0a2
Norman Gray <norman.gray@glasgow.ac.uk>
2006-03-12 19:57:15 +0000

    Give exported procedures sisc-xml: prefix Improve error handling in xml->sexp/input-source Add sisc-xml:skip-whitespace? parameterizable procedure

--- experiments/norman/quaestor/src/main/scm/util/sisc-xml.scm
+++ experiments/norman/quaestor/src/main/scm/util/sisc-xml.scm
@@ -1,28 +1,60 @@
 ;; Utility module -- SAX->sexp using the Java XML parser
+;;
+;; Exports three procedures.
+;;
+;; sisc-xml:xml->sexp/file FILENAME
+;; Given a file name, parse it and return a SSAX-style sexp representing it. 
+;;
+;; sisc-xml:xml->sexp/string STRING
+;; Parse the XML in the given string.
+;;
+;; sisc-xml:skip-whitespace? BOOLEAN
+;; Set the behaviour of the conversion to the sexp.  If the boolean is set
+;; true, then all-whitespace content is skipped (so that
+;; "<doc> <p>content  </p> </doc>" would be parsed as (doc (p "content  ")),
+;; but if it is false, it is all included verbatim, so that the above would
+;; parse to (doc " " (p "content  ") " ").  This is a SRFI-39 parameter, 
+;; so may be set inside a PARAMETERIZE special form.
 
 (import s2j)
 
+(require-library 'sisc/libs/srfi/srfi-39) ;parameter objects
+
 (module sisc-xml
-(xml->sexp/file
- xml->sexp/string)
+(sisc-xml:xml->sexp/file
+ sisc-xml:xml->sexp/string
+ sisc-xml:skip-whitespace?)
+
+(import srfi-39)
 
 (define-java-classes
   <org.xml.sax.content-handler>)
+
+;; Procedure NULL-METHOD does nothing
 (define (null-method name) #f)
+
+;; Create a ssax-accumulator.  This returns a function which is carried about
+;; by the SAX ContentHandler below.
 (define (new-ssax-accumulator)
-  (let ((stack '((*TOP*))))
+  (let ((stack '((*TOP*)))
+        (currently-parsing? #f))
     (lambda (op . args)                    ;op a symbol, args list of strings
       (cond ((eq? op 'start-element)
-             (let ((el (car args))
-                   (attlist (get-sax-atts-list (cadr args))))
-               (if attlist
-                   (set! stack
-                         (cons `((@ ,@attlist)
-                                 ,(string->symbol el))
-                               stack))
-                   (set! stack
-                         (cons (list (string->symbol el))
-                               stack)))))
+             (let ((ns (car args))         ;args: ns-or-false
+                   (el (cadr args))        ;element name
+                   (attlist (get-sax-atts-list (caddr args)))) ;attlist
+               (let ((elx (if ns
+                              (cons (string->symbol ns)
+                                    (string->symbol el))
+                              (string->symbol el))))
+                 (if attlist
+                     (set! stack
+                           (cons `((@ ,@attlist)
+                                   ,elx)
+                                 stack))
+                     (set! stack
+                           (cons (list elx)
+                                 stack))))))
             ((eq? op 'end-element)
              (set! stack `((,(reverse (car stack)) . ,(cadr stack))
                            . ,(cddr stack))))
@@ -32,8 +64,14 @@
             ((eq? op 'processing-instruction)
              (set-car! stack
                        `((*PI* ,(car args) ,(cadr args)) . ,(car stack))))
+            ((eq? op 'start-document)
+             (set! currently-parsing? #t))
+            ((eq? op 'end-document)
+             (set! currently-parsing? #f))
             ((eq? op '*TOP*)
-             (reverse (car stack)))
+             (if currently-parsing?
+                 (error 'new-ssax-accumulator "Can't retrieve *TOP* mid-parse")
+                 (reverse (car stack))))
             (else
              (error 'new-ssax-accumulator "Unknown op: ~a" op))))))
 
@@ -45,20 +83,26 @@
   (define-generic-java-methods
     get-length
     get-local-name
-    get-value)
+    get-value
+    (get-uri |getURI|))
   (let ((natts (->number (get-length sax-atts))))
     (if (= natts 0)
         #f
         (let loop ((al '())
                    (n 0))
           (if (>= n natts)
-              al
-              (let ((name (->string (get-local-name sax-atts (->jint n))))
-                    (value (->string (get-value sax-atts (->jint n)))))
-                (loop (cons (list (string->symbol name)
-                                  value)
-                            al)
-                      (+ n 1))))))))
+              (reverse al)
+              (let ((nj (->jint n)))
+                (let ((ns (->string (get-uri sax-atts nj)))
+                      (name (string->symbol
+                             (->string (get-local-name sax-atts nj))))
+                      (value (->string (get-value sax-atts nj))))
+                  (loop (cons (list (if (> (string-length ns) 0)
+                                        (cons (string->symbol ns) name)
+                                        name)
+                                    value)
+                              al)
+                        (+ n 1)))))))))
 
                                         ;Given a java string, return scheme #t
                                         ;if the string matches all-whitespace
@@ -77,24 +121,23 @@
                                         ;ssax-accumulator.  If SKIP-WHITESPACE?
                                         ;is true, then omit any all-whitespace
                                         ;strings.
-(define-java-proxy (make-content-handler-proxy ssax-accumulator
-                                               skip-whitespace?)
+(define-java-proxy (make-content-handler-proxy ssax-accumulator)
   (<org.xml.sax.content-handler>)
   (define (characters h ch start length)
     (define-java-class <java.lang.string>)
     (let ((js (java-new <java.lang.string> ch start length)))
-      (if (and skip-whitespace?
+      (if (and (sisc-xml:skip-whitespace?)
                (string-all-whitespace? js))
           (null-method 'characters)
           (ssax-accumulator 'characters (->string js)))))
   (define (end-document h)
-    (null-method 'end-document))
+    (ssax-accumulator 'end-document))
   (define (end-element h namespace-uri local-name q-name)
     (ssax-accumulator 'end-element))
   (define (end-prefix-mapping h prefix)
     (null-method 'end-prefix-mapping))
   (define (ignorable-whitespace h ch start length)
-    (if skip-whitespace?
+    (if (sisc-xml:skip-whitespace?)
         (null-method 'ignorable-whitespace)
         (ssax-accumulator 'characters 
                           (->string
@@ -107,13 +150,18 @@
   (define (skipped-entity h name)
     (null-method 'skipped-entity))
   (define (start-document h)
-    (null-method 'start-document))
+    (ssax-accumulator 'start-document))
   (define (start-element h namespace-uri local-name q-name atts)
-    (ssax-accumulator 'start-element (->string local-name) atts))
+    (let ((ns (->string namespace-uri)))
+      (ssax-accumulator 'start-element
+                        (and (> (string-length ns) 0)
+                             ns)
+                        (->string local-name)
+                        atts)))
   (define (start-prefix-mapping h prefix uri)
     (null-method 'start-prefix-mapping)))
 
-(define (xml->sexp/file filename)
+(define (sisc-xml:xml->sexp/file filename)
   (define-java-classes
     <org.xml.sax.input-source>
     <java.io.file-reader>)
@@ -121,7 +169,7 @@
                                     (java-new <java.io.file-reader>
                                               (->jstring filename)))))
 
-(define (xml->sexp/string string)
+(define (sisc-xml:xml->sexp/string string)
   (define-java-classes
     <org.xml.sax.input-source>
     <java.io.string-reader>)
@@ -129,28 +177,37 @@
                                     (java-new <java.io.string-reader>
                                               (->jstring string)))))
 
-(define (xml->sexp/input-source source)
+;; Procedure: XML->SEXP/INPUT-SOURCE
+;; Read XML from the given SAX InputSource, returning a SExp.
+(define (xml->sexp/input-source input-source)
   (define-generic-java-method parse)
   (let* ((sa (new-ssax-accumulator))
          (xml-reader (get-xml-reader-with-proxy
-                      (make-content-handler-proxy sa #t))))
-    ;; Complicated call to (parse xml-reader source), to unpack and rethrow any
-    ;; java.lang.reflect.UndeclaredThrowableException.
-    ;; This error unpacking isn't quite right, but it does seem to excavate
-    ;; the underlying error message somehow...
+                      (make-content-handler-proxy sa))))
+    ;; Complicated call to (parse xml-reader input-source), to unpack and
+    ;; rethrow any errors.  I think there are circumstances where the
+    ;; proxy can throw java.lang.reflect.UndeclaredThrowableException, which
+    ;; needs special handling, but I can't reproduce this right now, nor
+    ;; find the details in the SISC docs.
     (with/fc (lambda (m e)
                (define-generic-java-methods
-                 get-cause
-                 get-undeclared-throwable
-                 get-message)
-               (let ((cause (get-cause (error-message m))))
-                 (error 'xml->sexp/input-source "reader error: ~s/~s/~s"
-                        cause
-                        (get-undeclared-throwable (error-message m))
-                        (if (java-null? cause)
-                            "null cause"
-                            (get-message cause)))))
-       (lambda () (parse xml-reader source)))
+                 get-message
+                 get-system-id
+                 get-line-number
+                 get-column-number)
+               (let ((sax-exception (error-message m)))
+                 (let ((sysid   (get-system-id sax-exception))
+                       (line-no (->number (get-line-number sax-exception)))
+                       (col-no  (->number (get-column-number sax-exception))))
+                   (error 'xml->sexp/input-source
+                          "parse error:~a:~a:~a: ~a"
+                          (if (java-null? sysid)
+                              "?"
+                              (->string (get-system-id sax-exception)))
+                          (if (< line-no 0) "?" line-no)
+                          (if (< col-no 0)  "?" col-no)
+                          (->string (get-message sax-exception))))))
+       (lambda () (parse xml-reader input-source)))
     (sa '*TOP*)))
 
 (define (get-xml-reader-with-proxy content-handler-proxy)
@@ -175,5 +232,14 @@
                  (->jstring "http://xml.org/sax/features/namespace-prefixes")
                  (->jboolean #f))
     reader))
+
+;; Parameter procedure: SISC-XML:SKIP-WHITESPACE?
+;; This is a SRFI-39 parameter.  It is a procedure which returns
+;; a boolean value indicating whether all-whitespace content read within
+;; the input XML is skipped (if true) or included verbatim (if #f).  Since
+;; it is a parameter, it may be manipulated using the SRFI-39 PARAMETERIZE
+;; special form.
+(define sisc-xml:skip-whitespace?
+  (make-parameter #t))
 
 )
