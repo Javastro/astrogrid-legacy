6d1592d7c5a14de7c653d58eabe5f514796e80a3
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-03-13 18:29:08 +0000

    temporarily adjusted to avoid out-of-memory error caused by huge swathe of vizier records.

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/scope/ConeProtocol.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/scope/ConeProtocol.java
@@ -1,4 +1,4 @@
-/*$Id: ConeProtocol.java,v 1.2 2006/03/13 14:55:09 KevinBenson Exp $
+/*$Id: ConeProtocol.java,v 1.3 2006/03/13 18:29:08 nw Exp $
  * Created on 27-Jan-2006
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -15,7 +15,12 @@ import org.astrogrid.acr.astrogrid.ResourceInformation;
 import org.astrogrid.acr.nvo.Cone;
 import org.astrogrid.desktop.modules.ui.UIComponent;
 
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * @author Noel Winstanley nw@jb.man.ac.uk 27-Jan-2006
@@ -38,7 +43,22 @@ public class ConeProtocol extends DalProtocol {
      * @see org.astrogrid.desktop.modules.ui.scope.DalProtocol#listServices()
      */
     public ResourceInformation[] listServices() throws Exception{
+        /* @todo quick hack to get round memory limitations of current registyr delegate & information parsers.
         return reg.adqlSearchRI(cone.getRegistryQuery());
+        shold e able to get all this data in one go when I've written a stax client to the registry
+        */
+        ResourceInformation[] cones = reg.adqlSearchRI("Select * from Registry where @xsi:type like '%ConeSearch'");
+        List results = new ArrayList(Arrays.asList(cones));
+        // nnnhg. this is so hacky. I think I'd better take a shower after this.
+        /*
+        char[] c = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        for (int i = 0;i < c.length;i++) { 
+            ResourceInformation[] vizs = reg.adqlSearchRI("Select * from Registry where @xsi:type like '%TabularSkyService' and vr:identifier like 'ivo://CDS/%' and vs:table/vs:column/vs:ucd = 'POS_EQ_RA_MAIN' " +
+                    " and vr:title like '" + c[i] + "%' ");
+            results.add(vizs);
+        }
+*/
+        return (ResourceInformation[])results.toArray(new ResourceInformation[results.size()]);
     }
 
     /**
@@ -53,6 +73,9 @@ public class ConeProtocol extends DalProtocol {
 
 /* 
 $Log: ConeProtocol.java,v $
+Revision 1.3  2006/03/13 18:29:08  nw
+temporarily adjusted to avoid out-of-memory error caused by huge swathe of vizier records.
+
 Revision 1.2  2006/03/13 14:55:09  KevinBenson
 New first draft of helioscope and the stap spec protocol
 
