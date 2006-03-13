e3c7214533938adfba4b789eb6849ddc43beb7e3
Norman Gray <norman.gray@glasgow.ac.uk>
2006-03-13 23:59:56 +0000

    Initial version

---
+++ experiments/norman/quaestor/src/test/scm/quaestor/knowledgebase.scm
@@ -0,0 +1,63 @@
+;; test cases for the knowledgebase module
+
+(require-library 'quaestor/knowledgebase)
+(import knowledgebase)
+
+(require-library 'quaestor/jena)
+(import* jena rdf:new-empty-model)
+
+(expect kb-create-1
+        #t
+        (not (not (kb:new "try1"))))
+(expect kb-create-2
+        #t
+        (not (not (kb:new "try2"))))
+(expect-failure kb-create-duplicate     ;can't create duplicates
+                (kb:new "try1"))
+
+(expect kb-names
+        '(try2 try1)                    ;order isn't significant
+        (kb:get-names))
+
+(expect kb-get-1
+        #t
+        (not (not (kb:get "try1"))))
+(expect kb-get-2
+        #t
+        (not (not (kb:get "try2"))))
+(expect kb-get-3
+        #f
+        (kb:get "try3"))
+
+(kb:discard "try2")
+(expect kb-reget-1
+        #t
+        (not (not (kb:get "try1"))))
+(expect kb-reget-2
+        #f
+        (kb:get "try2"))
+
+(define m (kb:get "try1"))
+(expect kb-retrieve-empty
+        #f
+        (m 'get-model))
+(expect kb-has-empty
+        #f
+        (m 'has-model))
+(m 'add-abox 's1 (rdf:new-empty-model))
+(expect kb-has-non-empty
+        #t
+        (not (not (m 'has-model))))
+(expect kb-retrieve-non-empty
+        #t
+        (not (not ((kb:get "try1") 'get-model))))
+
+(expect kb-has-model-string
+        #t
+        (not (not (m 'has-model "s1"))))
+(expect kb-has-model-symbol
+        #t
+        (not (not (m 'has-model 's1))))
+(expect kb-has-no-submodel
+        #f
+        (m 'has-model 's2))
