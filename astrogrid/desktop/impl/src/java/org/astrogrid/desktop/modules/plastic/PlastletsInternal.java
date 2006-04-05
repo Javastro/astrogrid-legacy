33821b49c059d753f0545276f52af963a8f61819
John Taylor <John.Taylor@astrogrid.org>
2006-04-05 23:21:35 +0000

    plastlets

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/plastic/PlastletsInternal.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.plastic;
+
+/**
+ * @author jdt
+ *
+ */
+public interface PlastletsInternal {
+	/**
+	 * Enable/Disable ALL plastlets.
+	 * @param enabled
+	 */
+	public void setAllEnabled(boolean enabled);
+}
