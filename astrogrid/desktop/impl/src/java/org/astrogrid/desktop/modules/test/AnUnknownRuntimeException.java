f1f264d62956177d5fbe6c3ec1b64683ad2492f7
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-06-15 09:57:39 +0000

    added testing module

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/test/AnUnknownRuntimeException.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.test;
+
+/** simple exception - just used for testing.
+ * @author Noel Winstanley
+ * @since Jun 15, 20061:36:33 AM
+ */
+public class AnUnknownRuntimeException extends RuntimeException {
+
+	public AnUnknownRuntimeException() {
+		super();
+	}
+
+	public AnUnknownRuntimeException(String arg0) {
+		super(arg0);
+	}
+
+}
---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/test/TransportTestImpl.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.test;
+
+import org.astrogrid.acr.NotFoundException;
+import org.astrogrid.acr.test.TransportTest;
+
+/** implementation of transport test.
+ * @author Noel Winstanley
+ * @since Jun 15, 20061:34:13 AM
+ */
+public class TransportTestImpl implements TransportTest {
+
+	public byte[] echoByteArray(byte[] arg0) {
+		return arg0;
+	}
+
+	public void throwCheckedException() throws NotFoundException {
+		throw new NotFoundException("from throwCheckedException");
+	}
+
+	public void throwUncheckedException() {
+		throw new NullPointerException("from throwUncheckedException");
+	}
+
+	public void throwUncheckedExceptionOfUnknownType() {
+		throw new AnUnknownRuntimeException("from throwUncheckedExceptionOfUnknownType");
+	}
+
+}
