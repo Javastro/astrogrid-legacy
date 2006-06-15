1a9221e00ac8784d3e5dc878862e6cf0675c85bf
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-06-15 16:34:31 +0000

    support for 1st attempt at ui testing.

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/BuildInprocessWorkbench.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/BuildInprocessWorkbench.java
@@ -3,16 +3,27 @@
  */
 package org.astrogrid.desktop;
 
+import javax.swing.JFrame;
+
 import org.astrogrid.Workbench1;
+import org.astrogrid.desktop.modules.system.UIInternal;
+import org.uispec4j.UISpecAdapter;
+import org.uispec4j.Window;
 
 /** class that assebles and creates a new in-process Workbench.
  * 
  * used within test setup for unti tests.
+ * also provides a reference to the main window - so it can be used in ui testing.
  * @author Noel Winstanley
  * @since Jun 6, 20061:02:10 AM
  */
-public class BuildInprocessWorkbench extends BuildInprocessACR {
+public class BuildInprocessWorkbench extends BuildInprocessACR implements UISpecAdapter{
 protected void configureLauncher() {
 	Workbench1.configureLauncherAsWorkbench(launcher);
+}
+
+public Window getMainWindow() {
+	UIInternal ui = (UIInternal) getHivemindRegistry().getService(UIInternal.class);
+	return new Window((JFrame)ui.getComponent());
 }
 }
