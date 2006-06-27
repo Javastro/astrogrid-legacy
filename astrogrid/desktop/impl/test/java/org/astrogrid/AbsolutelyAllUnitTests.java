bdfa88077884eaf7bd62a5227573c840926f3d12
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-06-27 19:17:13 +0000

    added missing tests.

--- astrogrid/desktop/impl/test/java/org/astrogrid/AbsolutelyAllUnitTests.java
+++ astrogrid/desktop/impl/test/java/org/astrogrid/AbsolutelyAllUnitTests.java
@@ -30,6 +30,7 @@ public class AbsolutelyAllUnitTests {
 		suite.addTest(org.astrogrid.desktop.modules.ag.recorder.AllUnitTests.suite());
 		suite.addTest(org.astrogrid.desktop.modules.ag.AllUnitTests.suite());
 		suite.addTest(org.astrogrid.desktop.modules.plastic.AllUnitTests.suite());
+		suite.addTest(org.astrogrid.desktop.modules.ui.sendto.AllUnitTests.suite());
 		return suite;
 	}
 
