3fc0afb2f10414efc9d53bcd380ad87f9069a5f2
John Taylor <John.Taylor@astrogrid.org>
2006-08-02 01:13:13 +0000

    regenerated

--- astrogrid/desktop/impl/test/java/org/astrogrid/desktop/modules/plastic/AllUnitTests.java
+++ astrogrid/desktop/impl/test/java/org/astrogrid/desktop/modules/plastic/AllUnitTests.java
@@ -4,20 +4,20 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 public class AllUnitTests {
-/** tests the features of the plastic hub */
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for PLASTIC");
-		//$JUnit-BEGIN$
-		suite.addTestSuite(ApplicationStoreUnitTest.class);
-		suite.addTestSuite(RMIListenerUnitTest.class);
-		suite.addTestSuite(SysTrayUnitTest.class);
-		suite.addTestSuite(RegisterNoCallbackUnitTest.class);
-		suite.addTestSuite(PollingUnitTest.class);
-		suite.addTestSuite(AdvancedUnitTest.class);
-		suite.addTestSuite(PlasticClientProxyUnitTest.class);
-		suite.addTestSuite(BasicUnitTest.class);
-		//$JUnit-END$
-		return suite;
-	}
+    /** tests the features of the plastic hub */
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Tests for PLASTIC");
+        //$JUnit-BEGIN$
+        suite.addTestSuite(RMIListenerUnitTest.class);
+        suite.addTestSuite(SysTrayUnitTest.class);
+        suite.addTestSuite(RegisterNoCallbackUnitTest.class);
+        suite.addTestSuite(ApplicationStoreUnitTest.class);
+        suite.addTestSuite(PlasticClientProxyUnitTest.class);
+        suite.addTestSuite(BasicUnitTest.class);
+        suite.addTestSuite(PollingUnitTest.class);
+        suite.addTestSuite(AdvancedUnitTest.class);
+        //$JUnit-END$
+        return suite;
+    }
 
 }
