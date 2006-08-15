e3dceb82810b5763caecc76e5e9fd23e5d995af9
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-08-15 10:12:05 +0000

    added caching system.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/system/CacheFactory.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.system;
+
+import net.sf.ehcache.CacheManager;
+
+/** interface to a data cache.
+ * @author Noel Winstanley
+ * @since Aug 8, 20061:16:54 AM
+ */
+public interface CacheFactory {
+	
+	public CacheManager getManager() ;
+	
+	public void flush();
+	
+	public final static String RESOURCES_CACHE = "resources";
+	public final static String PW_CACHE = "pw";
+	public final static String DOCUMENTS_CACHE = "documents";
+	public final static String BULK_CACHE = "bulk";	
+
+}
---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/system/CacheFactoryImpl.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.system;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.DiskStoreConfiguration;
+
+import org.astrogrid.acr.builtin.ShutdownListener;
+
+/** implementation ofo the data cache - uses the ehCache libraries,
+ * @author Noel Winstanley
+ * @since Aug 8, 20061:17:09 AM
+ */
+public class CacheFactoryImpl implements ShutdownListener, CacheFactory {
+
+	private final CacheManager manager;
+	
+	public CacheFactoryImpl(String workingDir) {
+		Configuration conf = new Configuration();
+		DiskStoreConfiguration diskStore = new DiskStoreConfiguration();
+		diskStore.setPath(workingDir);
+		conf.addDiskStore(diskStore);
+		
+		// necessarty to create a defaults section.
+		CacheConfiguration defaults = new CacheConfiguration();
+		defaults.setName("cache_defaults");
+		defaults.setMaxElementsInMemory(10000);
+		defaults.setEternal(false);
+		defaults.setTimeToIdleSeconds(120);
+		defaults.setTimeToLiveSeconds(120);
+		defaults.setOverflowToDisk(true);
+		defaults.setDiskPersistent(false);
+		defaults.setDiskExpiryThreadIntervalSeconds(120);
+		defaults.setMemoryStoreEvictionPolicy("LRU");
+		conf.setDefaultCacheConfiguration(defaults);
+		
+		
+		// these cache's are mostly disk based - don't want to 
+		// increate the memory load too much 
+		// and even fetching from disk cache is much faster than 
+		// querying reg.
+		
+		// cache for individual resource documents.
+		// seems to get really really large. so will keep this a memory cache for now.
+		// most interesting things happen in bulk queries anyhow - and these seem to store quite efficiently.
+		
+		// will always hold the 200 most recently used resources.s
+		CacheConfiguration resources = new CacheConfiguration() {{
+			setName(RESOURCES_CACHE);
+			setMaxElementsInMemory(200); // 3-4 queries? will get flooded by astroscope in one go.
+			setOverflowToDisk(false);
+			setDiskPersistent(false);
+			setTimeToLiveSeconds(10 * 60); // 10 minutes	
+			setTimeToIdleSeconds(2 * 60); // 2 minutes
+			//setDiskExpiryThreadIntervalSeconds(2 * 60); // 2 minutes
+		}};
+		conf.addCache(resources);
+		
+		// cache for registry documents - resource docs, and xquery docs
+		CacheConfiguration documents = new CacheConfiguration() {{
+			setName(DOCUMENTS_CACHE); 
+			setMaxElementsInMemory(1); // only keep a few in memory.
+			setOverflowToDisk(true);
+			setDiskPersistent(true);
+			setTimeToLiveSeconds(3 * 24 * 60 * 60); // 3 days
+			setTimeToIdleSeconds(1 * 24 * 60 * 60); // 1 days		
+			setDiskExpiryThreadIntervalSeconds(10 * 60); // 10 minutes
+		}};
+		conf.addCache(documents);
+		
+		// cache for bulk searches, etc. of registry objects
+		// not so sure about this one really..
+		CacheConfiguration bulk = new CacheConfiguration() {{
+			setName(BULK_CACHE); 
+			setMaxElementsInMemory(1); // keep the smallest amount in memory.
+			setOverflowToDisk(true);
+			setDiskPersistent(true);
+			setTimeToLiveSeconds(3 * 24 * 60 * 60); // 3 days
+			setTimeToIdleSeconds(1 * 24 * 60 * 60); // 1 days		
+			setDiskExpiryThreadIntervalSeconds(10 * 60); // 10 minutes
+		}};
+		conf.addCache(bulk);
+		
+		//disk-cache of parameterized workfows.
+		CacheConfiguration pw = new CacheConfiguration() {{
+			setName(PW_CACHE);
+			setMaxElementsInMemory(1);
+			setOverflowToDisk(true);
+			setDiskPersistent(true);
+			setTimeToLiveSeconds(3 * 24 * 60 * 60); // 3 days
+			setTimeToIdleSeconds(1 * 24 * 60 * 60); // 1 days		
+			setDiskExpiryThreadIntervalSeconds(10 * 60); // 10 minutes		
+		}};
+		conf.addCache(pw);
+		
+		manager = new CacheManager(conf);
+	}
+	
+	
+	public CacheManager getManager() {
+		return manager;
+	}
+	
+	public void halting() {
+//		List keys = manager.getCache(RESOURCES_CACHE).getKeys();
+//		System.out.println(keys.size());
+//		Set s = new TreeSet(keys);
+//		System.out.println(s.size());
+//		for (Iterator i = s.iterator(); i.hasNext(); ) {
+//			System.out.println(i.next());
+//		}
+		manager.shutdown();
+	}
+
+	public String lastChance() {
+		return null;
+	}
+
+
+	public void flush() {
+		getManager().clearAll();
+	}
+
+}
