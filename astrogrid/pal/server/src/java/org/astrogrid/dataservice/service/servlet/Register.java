7cd4d11d6ed019e5f32c7423c2acc89e434b02ce
Kona Andrews <Kona.Andrews@astrogrid.org>
2006-10-05 13:58:47 +0000

    Rolling back registry-related changes.

--- astrogrid/pal/server/src/java/org/astrogrid/dataservice/service/servlet/Register.java
+++ astrogrid/pal/server/src/java/org/astrogrid/dataservice/service/servlet/Register.java
@@ -1,5 +1,5 @@
 /*
- * $Id: Register.java,v 1.4 2006/09/26 06:54:10 clq2 Exp $
+ * $Id: Register.java,v 1.5 2006/10/05 13:58:47 kea Exp $
  */
 
 package org.astrogrid.dataservice.service.servlet;
@@ -16,7 +16,7 @@ import org.astrogrid.cfg.ConfigFactory;
 import org.astrogrid.dataservice.metadata.VoDescriptionServer;
 import org.astrogrid.dataservice.service.ServletHelper;
 import org.astrogrid.registry.client.RegistryDelegateFactory;
-import org.astrogrid.registry.client.DelegateProperties;
+//import org.astrogrid.registry.client.DelegateProperties;
 import org.astrogrid.registry.client.query.RegistryService;
 import org.astrogrid.dataservice.metadata.v0_10.VoResourceSupport;
 
@@ -36,7 +36,8 @@ public class Register extends DefaultServlet {
       try {
          String regParam = request.getParameter("RegistryUrl");
          if ((regParam == null) || (regParam.trim().length()==0)) {
-            regParam =  ConfigFactory.getCommonConfig().getString(DelegateProperties.ADMIN_URL_PROPERTY);
+            //regParam =  ConfigFactory.getCommonConfig().getString(DelegateProperties.ADMIN_URL_PROPERTY);
+            regParam =  ConfigFactory.getCommonConfig().getString(RegistryDelegateFactory.ADMIN_URL_PROPERTY);
          }
          /*
          // KEA TOFIX COMPLETE THIS REGISTRY CHECK IN BRANCH FOLLOWING 
