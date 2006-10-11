bd648cdfbfd1324facda4489ceb1798120f32fa1
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2006-10-11 20:37:26 +0000

    Now maintains a complete console session rather than a one off run.

--- astrogrid/adql/test/java/org/astrogrid/adql/Interactive.java
+++ astrogrid/adql/test/java/org/astrogrid/adql/Interactive.java
@@ -12,7 +12,14 @@ import org.astrogrid.adql.v1_0.beans.* ;
 import org.apache.xmlbeans.*;
 /**
  * Interactive
- *
+ * 
+ * Conducts a console based conversation.
+ * 
+ * Enter a query in string form.
+ * The query will be compiled into adql/x or an error returned.
+ * 
+ * To end the session. Type in a ";" and press Enter.
+ * The query will be errored. Then type in another ";" to exit.
  *
  * @author Jeff Lusted jl99@star.le.ac.uk
  * Sep 18, 2006
@@ -24,11 +31,44 @@ public class Interactive {
 		 SelectDocument queryDoc = null ;
 
 			System.out.println("Reading from standard input...");
-			System.out.print("Enter an SQL-like expression :");			
-			queryDoc = compiler.exec();  
-			if( queryDoc != null )
-				System.out.println( queryDoc.toString() ) ;
-
+			while( true ) {
+				System.out.println("Enter an SQL-like expression :");	
+				
+				try {
+					queryDoc = compiler.exec();  
+					System.out.println( queryDoc.toString() ) ;				
+				}
+				catch( ParseException pex ) {
+					System.out.println( pex.getMessage() ) ;
+					if( exitRequested( compiler ) ) {					
+						System.out.println( "Goodbye!" ) ;
+						System.exit(0) ;
+					}
+				}
+				catch( Exception ex ) {
+					ex.printStackTrace() ;
+					if( exitRequested( compiler ) ) {					
+						System.out.println( "Goodbye!" ) ;
+						System.exit(0) ;
+					}	
+				}
+			}
+					
+	 }
+	 
+	 private static boolean exitRequested( AdqlStoX compiler ) {
+		 char ch;
+		 try {
+			 ch = (char)System.in.read() ;
+			 if( ch != ';' ) {
+				 compiler.ReInit( System.in ) ;
+				 return false ;
+			 }
+		 }
+		 catch( Exception ex ) {
+			 ;
+		 }
+		 return true ;		 
 	 }
 
 }
