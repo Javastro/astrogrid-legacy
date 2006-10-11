665fbe8f492382df52fd431c44d718effb21c5ea
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-10-11 10:36:49 +0000

    improved stilts embeddeing

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/util/TablesImpl.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/util/TablesImpl.java
@@ -3,13 +3,27 @@
  */
 package org.astrogrid.desktop.modules.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
 
+import org.astrogrid.acr.InvalidArgumentException;
+import org.astrogrid.acr.NotFoundException;
+import org.astrogrid.acr.SecurityException;
+import org.astrogrid.acr.ServiceException;
 import org.astrogrid.acr.util.Tables;
+import org.astrogrid.desktop.modules.ag.MyspaceInternal;
 
+import uk.ac.starlink.table.StarTable;
 import uk.ac.starlink.table.StarTableFactory;
 import uk.ac.starlink.table.StarTableOutput;
+import uk.ac.starlink.table.StarTableWriter;
 import uk.ac.starlink.table.StoragePolicy;
+import uk.ac.starlink.table.TableBuilder;
+import uk.ac.starlink.table.TableFormatException;
 
 /** Implementation of the tables utility.
  * @author Noel Winstanley
@@ -17,21 +31,108 @@ import uk.ac.starlink.table.StoragePolicy;
  */
 public class TablesImpl implements Tables {
 
-	public TablesImpl() {
+	public TablesImpl(MyspaceInternal vos) {
+		//@todo - candidate for factoring out?
+		this.builderFactory = new StarTableFactory(false);
 		// configure to be more memory-efficient.
-		StoragePolicy.setDefaultPolicy( StoragePolicy.PREFER_DISK ) ;
-		
+		this.builderFactory.setStoragePolicy(StoragePolicy.PREFER_DISK);
+		this.writerFactory = new StarTableOutput();
+		this.vos = vos;
 	}
 	
+	protected final StarTableFactory builderFactory;
+	protected final StarTableOutput writerFactory;
+	protected final MyspaceInternal vos;
 	
-	public void convert(String inLocation, String inFormat
-			, String outLocation, String outFormat)
-			throws IOException {
-		
-		 new StarTableOutput().writeStarTable( new StarTableFactory( false ) 
-         .makeStarTable( inLocation, 
-                         inFormat ), 
-          outLocation, outFormat ); 
+	public String convert(String input, String inFormat, String outFormat) throws InvalidArgumentException, ServiceException {
+		TableBuilder tableBuilder;
+		StarTableWriter tableWriter;
+		try {
+			tableBuilder = builderFactory.getTableBuilder(inFormat);
+		} catch (TableFormatException x) {
+			throw new InvalidArgumentException(inFormat);
+		}
+		try {
+			tableWriter = writerFactory.getHandler(outFormat);
+		} catch (TableFormatException x) {
+			throw new InvalidArgumentException(outFormat);
+		}
+		InputStream is = null;
+		ByteArrayOutputStream os = null;
+		try {
+			is = new ByteArrayInputStream(input.getBytes());
+			StarTable st = builderFactory.makeStarTable(is,tableBuilder);
+			os = new ByteArrayOutputStream();
+			writerFactory.writeStarTable(st,os,tableWriter);
+			os.close();
+			return os.toString();
+		} catch (IOException e) {
+			throw new ServiceException(e);
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					// nevermind
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch (IOException e) {
+					// nevermind
+				}
+			}
+		}
+	}
+	
+	public void convertFile(URI inLocation, String inFormat
+			, URI outLocation, String outFormat) throws InvalidArgumentException, ServiceException, NotFoundException, SecurityException  {
+		TableBuilder tableBuilder;
+		StarTableWriter tableWriter;
+		try {
+			tableBuilder = builderFactory.getTableBuilder(inFormat);
+		} catch (TableFormatException x) {
+			throw new InvalidArgumentException(inFormat);
+		}
+		try {
+			tableWriter = writerFactory.getHandler(outFormat);
+		} catch (TableFormatException x) {
+			throw new InvalidArgumentException(outFormat);
+		}
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = vos.getInputStream(inLocation);
+			StarTable st = builderFactory.makeStarTable(is,tableBuilder);
+			os = vos.getOutputStream(outLocation);
+			writerFactory.writeStarTable(st,os,tableWriter);
+		} catch (IOException e) {
+			throw new ServiceException(e);
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (IOException e) {
+					// nevermind
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+				} catch (IOException e) {
+					// nevermind
+				}
+			}
+		}		
+	}
+	
+	public String[] listOutputFormats() {
+		return (String[])writerFactory.getKnownFormats().toArray(new String[]{});
+	}
+	
+	public String[] listInputFormats() {
+		return (String[])builderFactory.getKnownFormats().toArray(new String[]{});
 	}
 
 }
