67ea0e6d4f9d777bcdabbbf4ec63524bf653d830
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2006-10-30 12:09:16 +0000

    skeleton voevent module.

---
+++ astrogrid/desktop/api/src/java/uk/ac/estar/voevent/Inject.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package uk.ac.estar.voevent;
+
+/**
+ * Inject VOEvent messages into the network
+ * 
+ * ADD USER DOCUMENTATION HERE - valid XHTML
+ * 
+ * @see (VOEVENT HOMEPAGE URL)
+ * @author Noel Winstanley (CHANGE ME)
+ * @since 2006.3.rc5
+ * 
+ * @service voevent.inject
+ * 
+ */
+public interface Inject {
+
+	
+	// add method signatures here..
+	
+}
