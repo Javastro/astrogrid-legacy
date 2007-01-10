027eefd3d32be351ea929c6fde6bb783e07898ad
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-01-10 14:56:00 +0000

    work-around for oddity in hivemind.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ag/HivemindBundlePreferences.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.ag;
+
+import org.astrogrid.filemanager.common.BundlePreferences;
+
+/** Subclass of bundlePreferences that accepts int-valued preferences.
+ * @author Noel Winstanley
+ * @since Jan 10, 20072:44:09 PM
+ */
+public class HivemindBundlePreferences extends BundlePreferences {
+
+	public void setMaxExtraNodesInt(int arg0) {
+		super.setMaxExtraNodes(Integer.valueOf(arg0));
+	}
+	
+	public void setPrefetchDepthInt(int arg0) {
+		super.setPrefetchDepth(Integer.valueOf(arg0));
+	}
+	
+}
