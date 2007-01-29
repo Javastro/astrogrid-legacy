42bd81bb3dfe5b7bcb482e7ba7b78297f9c9b900
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-01-29 10:40:23 +0000

    new addition.

---
+++ astrogrid/desktop/impl/test/java/org/astrogrid/desktop/modules/ui/SwingLibraryTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.ui;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.swing.UIDefaults;
+import javax.swing.UIManager;
+
+import junit.framework.TestCase;
+
+/** Test of various oddities of Swing - mostly for me to understand what thigns do.
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Jan 25, 20073:55:41 PM
+ */
+public class SwingLibraryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testUIManager() throws Exception {
+		UIDefaults defs = UIManager.getDefaults();
+		System.out.println(defs);
+	}
+}
