eab0554acbd24b673979d0e4b5f414cd10a0a5ed
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-02-05 18:45:19 +0000

    corrected launch script.

--- astrogrid/desktop/impl/build/launch.sh
+++ astrogrid/desktop/impl/build/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -
 args=-Djava.library.path="%{INSTALL_PATH}/lib"
 cp="%{INSTALL_PATH}/lib/%{jarname}:%{INSTALL_PATH}/lib/jdic_stub_Linux-%{jdicversion}.jar:%{INSTALL_PATH}/lib/jdic_stub_Solaris-%{jdicversion}.jar"
-"%{JAVA_HOME}/bin/java" -Xmx512m $args -cp $cp  %{mainclass}$*
+"%{JAVA_HOME}/bin/java" -Xmx512m $args -cp $cp  %{mainclass} $*
