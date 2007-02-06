7ad1bec6d1d90a768670a0189f8995ee16d438d7
gg78 <gg78>
2007-02-06 14:47:56 +0000

    added prompts to change variables

--- astrogrid/installscripts/install.sh
+++ astrogrid/installscripts/install.sh
@@ -35,7 +35,21 @@ export ASTROGRID_EMAIL=astrogrid.demo@metagrid.org
 export ASTROGRID_ADMIN="Astrogrid admin"
 export ASTROGRID_BASE=http://${ASTROGRID_HOST}:${ASTROGRID_PORT}
 
+
+for SETTING in JAVA_HOME ASTROGRID_HOME ASTROGRID_USER ASTROGRID_PASS ASTROGRID_HOST ASTROGRID_PORT ASTROGRID_AUTH ASTROGRID_EMAIL ASTROGRID_ADMIN ASTROGRID_BASE
+do
+echo "$SETTING setting: ${!SETTING} - return for OK or enter new value"
+read SETTING_NEW
+if [ ! $SETTING_NEW = "" ]
+then
+    export $SETTING=$SETTING_NEW
+else
+    echo "setting unchanged"
+fi
+done
+
 echo "Installing AstroGrid services"
+echo "  JAVA_HOME       : ${JAVA_HOME:?"undefined"}"
 echo "  ASTROGRID_HOME  : ${ASTROGRID_HOME:?"undefined"}"
 echo "  ASTROGRID_USER  : ${ASTROGRID_USER:?"undefined"}"
 echo "  ASTROGRID_PASS  : ${ASTROGRID_PASS:?"undefined"}"
@@ -48,7 +62,16 @@ echo "  ASTROGRID_EMAIL : ${ASTROGRID_EMAIL:?"undefined"}"
 echo "  ASTROGRID_ADMIN : ${ASTROGRID_ADMIN:?"undefined"}"
 echo "  ASTROGRID_BASE  : ${ASTROGRID_BASE:?"undefined"}"
 
-
+echo "Are these settings OK (y/n)?"
+read SETTINGSOK
+if [ $SETTINGSOK = "y" ]
+then
+    echo "OK, continuing..."
+else
+    echo "settings not OK - stopping"
+    exit 1
+fi    
+    
 #
 # Check system tools already installed
 . ${SCRIPT_PATH}/system.sh
