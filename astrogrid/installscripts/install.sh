2d79493c5306bd30134e51025fafce6c58396add
gg78 <gg78>
2007-02-06 18:38:33 +0000

    *** empty log message ***

--- astrogrid/installscripts/install.sh
+++ astrogrid/installscripts/install.sh
@@ -18,6 +18,9 @@ INSTALL_DATE=`date "+%Y%m%d%H%M%S"`
 INSTALL_USER=`whoami`
 
 #
+# Check if settings have been overridden
+
+#
 # Test settings.
 export JAVA_HOME=/usr/java/jdk1.5.0_08
 
@@ -28,15 +31,15 @@ export ASTROGRID_PASS=W6G117
 export ASTROGRID_HOST=`hostname -f`
 export ASTROGRID_PORT=8080
 
-export ASTROGRID_MAVEN=http://www.astrogrid.org/maven
+export ASTROGRID_MAVEN=http://www.astrogrid.org/maven/org.astrogrid
 
 export ASTROGRID_AUTH=org.metagrid.caltech-demo
 export ASTROGRID_EMAIL=astrogrid.demo@metagrid.org
 export ASTROGRID_ADMIN="Astrogrid admin"
-export ASTROGRID_BASE=http://${ASTROGRID_HOST}:${ASTROGRID_PORT}
+export ASTROGRID_INTERNAL=http://${ASTROGRID_HOST}:${ASTROGRID_PORT}
+export ASTROGRID_EXTERNAL=http://${ASTROGRID_HOST}:${ASTROGRID_PORT}
 
-
-for SETTING in JAVA_HOME ASTROGRID_HOME ASTROGRID_USER ASTROGRID_PASS ASTROGRID_HOST ASTROGRID_PORT ASTROGRID_AUTH ASTROGRID_EMAIL ASTROGRID_ADMIN ASTROGRID_BASE
+for SETTING in JAVA_HOME ASTROGRID_HOME ASTROGRID_USER ASTROGRID_PASS ASTROGRID_HOST ASTROGRID_PORT ASTROGRID_AUTH ASTROGRID_EMAIL ASTROGRID_ADMIN ASTROGRID_INTERNAL ASTROGRID_EXTERNAL
 do
 echo "$SETTING setting: ${!SETTING} - return for OK or enter new value"
 read SETTING_NEW
@@ -49,27 +52,28 @@ fi
 done
 
 echo "Installing AstroGrid services"
-echo "  JAVA_HOME       : ${JAVA_HOME:?"undefined"}"
-echo "  ASTROGRID_HOME  : ${ASTROGRID_HOME:?"undefined"}"
-echo "  ASTROGRID_USER  : ${ASTROGRID_USER:?"undefined"}"
-echo "  ASTROGRID_PASS  : ${ASTROGRID_PASS:?"undefined"}"
+echo "  JAVA_HOME           : ${JAVA_HOME:?"undefined"}"
+echo "  ASTROGRID_HOME      : ${ASTROGRID_HOME:?"undefined"}"
+echo "  ASTROGRID_USER      : ${ASTROGRID_USER:?"undefined"}"
+echo "  ASTROGRID_PASS      : ${ASTROGRID_PASS:?"undefined"}"
 
-echo "  ASTROGRID_HOST  : ${ASTROGRID_HOST:?"undefined"}"
-echo "  ASTROGRID_PORT  : ${ASTROGRID_PORT:?"undefined"}"
+echo "  ASTROGRID_HOST      : ${ASTROGRID_HOST:?"undefined"}"
+echo "  ASTROGRID_PORT      : ${ASTROGRID_PORT:?"undefined"}"
 
-echo "  ASTROGRID_AUTH  : ${ASTROGRID_AUTH:?"undefined"}"
-echo "  ASTROGRID_EMAIL : ${ASTROGRID_EMAIL:?"undefined"}"
-echo "  ASTROGRID_ADMIN : ${ASTROGRID_ADMIN:?"undefined"}"
-echo "  ASTROGRID_BASE  : ${ASTROGRID_BASE:?"undefined"}"
+echo "  ASTROGRID_AUTH      : ${ASTROGRID_AUTH:?"undefined"}"
+echo "  ASTROGRID_EMAIL     : ${ASTROGRID_EMAIL:?"undefined"}"
+echo "  ASTROGRID_ADMIN     : ${ASTROGRID_ADMIN:?"undefined"}"
+echo "  ASTROGRID_INTERNAL  : ${ASTROGRID_INTERNAL:?"undefined"}"
+echo "  ASTROGRID_EXTERNAL  : ${ASTROGRID_EXTERNAL:?"undefined"}"
 
 echo "Are these settings OK (y/n)?"
 read SETTINGSOK
-if [ $SETTINGSOK = "y" ]
+if [ ! $SETTINGSOK = "y" ]
 then
-    echo "OK, continuing..."
-else
     echo "settings not OK - stopping"
     exit 1
+else
+    echo "OK, continuing..."
 fi    
     
 #
@@ -79,14 +83,8 @@ fi
 # Install Tomcat.
 . ${SCRIPT_PATH}/tomcat.sh
 #
-# Install AstroGrid Registry.
+# Install AstroGrid registry.
 . ${SCRIPT_PATH}/registry.sh
 #
-# Install AstroGrid FileStore.
+# Install AstroGrid filestore.
 . ${SCRIPT_PATH}/filestore.sh
-#
-# Install AstroGrid FileManager.
-. ${SCRIPT_PATH}/filemanager.sh
-#
-# Install AstroGrid Community.
-. ${SCRIPT_PATH}/community.sh
