88b5abaa2db4f4239617d92f7bddf1724db4865f
Norman Gray <norman.gray@glasgow.ac.uk>
2007-02-08 11:12:59 +0000

    Adjust documentation Ensure that content type and content type parameters are non-null in   all cases

--- experiments/norman/quaestor/src/main/org/eurovotech/quaestor/helpers/HttpResult.java
+++ experiments/norman/quaestor/src/main/org/eurovotech/quaestor/helpers/HttpResult.java
@@ -6,7 +6,10 @@ import java.io.InputStreamReader;
 
 
 /**
- * Encapsulates the result of an HTTP transaction.
+ * Encapsulates the result of an HTTP transaction obtained from 
+ * a QuaestorConnection.
+ * @see org.eurovotech.quaestor.helpers.QuaestorConnection
+ * @see <a href="http://www.ietf.org/rfc/rfc2045.txt" >RFC 2045: (MIME) Part One: Format of Internet Message Bodies</a>
  */
 public class HttpResult {
     private int status;
@@ -19,17 +22,20 @@ public class HttpResult {
     static {
         parameters = java.util.regex.Pattern.compile("([^;]*)(; *(.*))?");
     }
-        
+
+    /**
+     * Protected constructor
+     */
     HttpResult(int status, String contentType, InputStream content) {
         this.status = status;
 
         if (contentType == null) {
-            this.responseContentType = null;
-            this.responseContentTypeParameters = null;
+            this.responseContentType = "";
+            this.responseContentTypeParameters = "";
         } else {
             java.util.regex.Matcher m = parameters.matcher(contentType);
             if (m.matches()) {
-                this.responseContentType = m.group(1);
+                this.responseContentType = m.group(1).toLowerCase();
                 this.responseContentTypeParameters = 
                         (m.group(3) == null ? "" : m.group(3));
             } else {
@@ -44,11 +50,28 @@ public class HttpResult {
         return status;
     }
 
-    /** Obtains the MIME type of the transaction response */
+    /**
+     * Obtains the MIME type of the transaction response.  This is
+     * guaranteed to be lower case.  Any content-type parameters are removed.
+     *
+     * @return the content type, or the empty string if no
+     * content-type is available
+     */
     public String getContentType() {
-        return (responseContentType == null ? "" : responseContentType);
+        return responseContentType;
     }
 
+    /**
+     * Obtains any parameters on the MIME content-type of the transaction
+     * response.  MIME content-types can have parameters following the media
+     * type, for example <code>Content-type: text/plain;
+     * charset="us-ascii"</code>, and this method returns any text
+     * after the semicolon in such a content-type header, with leading
+     * whitespace stripped.  See RFC 2045 for definitions.
+     *
+     * @return any content-type parameters as a string, or an empty
+     *   string if there are no parameters on the content-type
+     */
     public String getContentTypeParameters() {
         return responseContentTypeParameters;
     }
@@ -63,8 +86,8 @@ public class HttpResult {
     /** 
      * Obtains the content of the HTTP transaction response as a
      * string.  If there was no response stream for some reason,
-     * or if this method has already been called, then return null
-     * without error.
+     * then return null without error.  This method may be called
+     * multiple times, returning the same content each time.
      *
      * @throws java.io.IOException if there is a problem reading from the stream
      */
@@ -81,7 +104,7 @@ public class HttpResult {
                 = new BufferedReader
                 (new InputStreamReader(responseStream));
 
-        char[] buffer = new char[256];
+        char[] buffer = new char[4096];
         int nread;
         while ((nread = r.read(buffer, 0, buffer.length)) >= 0) {
             sb.append(buffer, 0, nread);
