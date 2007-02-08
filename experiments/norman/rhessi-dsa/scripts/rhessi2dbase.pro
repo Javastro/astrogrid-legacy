44caaa64a0267d969d7642914db68a5fb56412cf
Norman Gray <norman.gray@glasgow.ac.uk>
2007-02-08 14:37:44 +0000

    Initial versionrhessi-dsa/

---
+++ experiments/norman/rhessi-dsa/scripts/rhessi2dbase.pro
@@ -0,0 +1,170 @@
+; NAME:
+;       rhessi2dbase
+;
+; PURPOSE:
+;        To retrieve observational summary and quick look data and save
+;        in text format
+;
+; CALLING SEQUENCE:
+;       rhessi2dbase, time_interval
+;
+; INPUTS:
+;       time_interval - 2 element array, [start_time, end_time]
+;       outfn         - string, a filename to receive the output
+;
+; OUTPUTS:
+;        text file where colomns are:
+;        1) start time of 4 second time interval (time in ISO 8601 format)
+;        2) Corrected count rate in 3-6 keV range (integer)
+;        3)                        6-12 keV
+;        4)                        12-25 keV
+;        5)                        25-50 keV
+;        6)                        50-100 keV
+;        7)                        100-300 keV
+;        8)                        300-800 keV
+;        9)                        800-7000 keV
+;        10)                       7000-20000 keV
+;        11) URL to (not corrected) count rate picture (string)
+;        12) URL to corrected count rate picture       (string)
+;        13) Attenuator state (0= open,1=thin,3=thin+thick)
+;        14) Eclipse flag (1=night)
+;        15) Southern Atlantic Anomaly flag
+;        16) Particle flag
+;        17) Data gap flag
+;        18) Flare flag
+;        19) Front decimation weight
+;        20) Rear decimation weight
+;        21) Front segment decimation energy
+;        22) Front segment decimation chanel/128
+;
+;
+; KEYWORDS:
+;
+; EXAMPLE:
+;       IDL> rhessi2dbase, time_interval = ['20-Jul-2002 01:31:00','25-Jul-2002 00:00:00']
+;
+;
+; HISTORY:
+;        written, eduard@astro.gla.ac.uk, 10-July-2006
+;-
+
+pro rhessi2dbase, time_interval=time_interval, outfn=outfn
+;time_interval=['20-Jul-2002 01:31:00','25-Jul-2002 00:00:00']
+
+; First, set up an error handler which reports any error to a file
+; rhesse2dbase.err and then bombs out.  We exit with an error status,
+; but in fact this is pointless, because stupid sswidl doesn't pass
+; on the exit status.
+catch, error_status
+if error_status ne 0 then begin
+    get_lun, errmsg
+    openw, errmsg, 'rhessi2dbase.err'
+    printf, errmsg, 'error message: ', !error_state.msg
+    printf, errmsg, 'error code   : ', error_status
+    printf, errmsg, 'exiting...'
+    exit, status=1 ; not passed on to env through sswidl
+endif
+
+IF (N_elements(time_interval) EQ 0) THEN message, 'ERROR: time interval should be supplied'
+
+IF (anytim(time_interval(0)) LT anytim('12-Feb-2002 01:32:00')) THEN $
+print,'WARNING: start time set: 12-Feb-2002 01:32:00'
+
+o= hsi_obs_summary(obs_time_interval=time_interval)
+counts =o -> getdata(/corrected)
+
+; check that we've got some data, and fail if not
+counts_ok = size(counts,/type)
+if size(counts, /type) eq 8 then begin
+    thesize = size(counts)
+    print, 'retrieved data: ', thesize[1], ' elements'
+endif else begin
+    message, 'can''t find data in range'
+endelse
+
+
+RATE0= counts.countrate(0,*)
+RATE1= counts.countrate(1,*)
+RATE2= counts.countrate(2,*)
+RATE3= counts.countrate(3,*)
+RATE4= counts.countrate(4,*)
+RATE5= counts.countrate(5,*)
+RATE6= counts.countrate(6,*)
+RATE7= counts.countrate(7,*)
+RATE8= counts.countrate(8,*)
+
+time=o->getaxis(/ut)
+time_iso = anytim(time,/ccsds)
+
+;retrieving flags
+flags     = o -> getdata(class='flag')
+;help,flags.flags
+;info_flag = o -> get(/info,class='flag')
+;print, info_flag.flag_ids
+
+SAA          = flags.flags(0,*)
+Eclipse      = flags.flags(1,*)
+Flare        = flags.flags(2,*)
+Attenuator   = flags.flags(14,*)
+NON_solar    = flags.flags(16,*)
+Gap          = flags.flags(17,*)
+FDEnergy     = flags.flags(18,*)
+RDEnergy     = flags.flags(25,*)
+FDWeight     = flags.flags(19,*)
+Particles    = flags.flags(24,*)
+RDWeight     = flags.flags(29,*)
+BAD          = flags.flags(31,*)
+
+hsi_filedb_read, filedb,filedb_trange=time_interval
+;reading file database
+
+URL_rate      =strarr(n_elements(time))
+URL_corrected =strarr(n_elements(time))
+
+; Fill in the url_rate and url_corrected arrays with the URLs to the
+; appropriate quick-look images.  blockstart[] (blockend) is the array of
+; times for the start (end) of each block with the same URL.
+glasgow='http://www.astro.gla.ac.uk/rhessi/metadata/'
+blockstart=filedb.start_time
+blockend=filedb.end_time
+For i=0,N_elements(blockstart)-1 do begin
+    OK_range=where((time GE blockstart(i)) AND (time LE blockend(i)) AND (time GE time(0)))
+    start_t=ANYTIM(blockstart(i),/UTC_EXT)
+    Year =STRING(start_t.year,FORMAT='(I04)',/PRINT)
+    Month=STRING(start_t.month,FORMAT='(I02)',/PRINT)
+    Day  =STRING(start_t.month,FORMAT='(I02)',/PRINT)
+    date=YEAR+'/'+MONTH+'/'+DAY+'/'
+    if size(ok_range,/n_dimensions) gt 0 then begin
+        URL_rate(min(OK_range):max(OK_range))      =glasgow+date+'hsi_'+time2file(blockstart(i),/seconds)+'_rate.png'
+        URL_corrected(min(OK_range):max(OK_range)) =glasgow+date+'hsi_'+time2file(blockstart(i),/seconds)+'_corrected_rate.png'
+    endif
+end
+
+if keyword_set(outfn) then $
+    filename=outfn $
+else $
+    filename='out_'+time2file(time_interval(0))+'.txt'
+
+;; Following traces output -- pretty, but unnecessary
+;loginterval = n_elements(time)/10
+;logcounter = 0
+
+openw,1,filename
+out_format='(A19,9("	",I6),"	", A82,"	",A92,6("	",I01),4("	",I03),"	")'
+
+for i=0L, n_elements(time)-1 do begin
+    printf,1, time_iso(i), rate0(i) , rate1(i), rate2(i), rate3(i), rate4(i),$
+      rate5(i), rate6(i), rate7(i), rate8(i),URL_rate(i), URL_corrected(i),$
+      Attenuator(i),Eclipse(i),SAA(i),Particles(i),Gap(i),Flare(i),$
+      FDWeight(i), RDWeight(i), FDEnergy(i),RDEnergy(i),$
+      format=out_format
+
+    ;logcounter = logcounter+1
+    ;if logcounter eq loginterval then begin
+    ;    print, time_iso(i)
+    ;    logcounter = 0
+    ;endif
+END
+close,1
+
+end
---
+++ experiments/norman/rhessi-dsa/scripts/upload.sh
@@ -0,0 +1,322 @@
+#! /bin/sh -
+#
+# Usage: $progname [options]                    # perform incremental update
+#        $progname [options] --all              # new db, upload all data
+#        $progname [options] startdate enddate  # upload data in range
+# Options:
+#     --dry-run      : don't do anything, just show what would be done
+#     --inc='spec'   : use given increment, eg --inc="5 day"
+#
+# The first invocation is intended for the cron job, and will do all
+# the updates in the time interval between the last time this script
+# ran (based on the date of the timestamp file written at the end);
+# the second is intended to do a complete re-conversion and re-upload;
+# and the second is for maintainance use, to upload a specific period.
+# 
+# The rhessi2dbase.pro script turns out to be quadratic (!) in the length
+# of the interval given to it.  Uploading in increments of 5 days
+# seems to be a reasonable tradeoff point.
+#
+# `date` is rather flaky about parsing times (it seems to parse
+# 2007-01-01 and 2007-01-01T00:00:00 differently!), so be rather careful
+# about dates below, in general leaving times out of anything passed to `date`.
+
+progname=`basename $0`
+
+dsahome=/home/norman/rhessi-dsa
+
+# where do the scripts live?
+scriptdir=$dsahome/scripts
+
+# the stamp file used by update mode
+stampfile=$dsahome/last-run-stamp
+
+# where does the RHESSI software live
+rhessihome=/opt/rhessi
+
+# when doing an update, where's the root of the tree where we start
+# 'find'ing updates.
+#
+# FIXME: This is specific to 2007.  Once the upload is confirmed to be
+# running smoothly, adjust this to include $rhessihome perhaps, or
+# whatever the current year is.  Not ideal -- what's the best plan?
+searchbase=$rhessihome/2007
+
+# which machine is the database on
+mysql_host=bern
+
+# credentials for connection to the MySQL server
+mysql_credentials='-unorman -psitrepdentist'
+
+########################################
+#
+# Functions
+
+Usage() {
+    cat >&2 <<EOF
+Usage: $progname [options]                    # perform incremental update
+       $progname [options] --all              # new db, upload all data
+       $progname [options] startdate enddate  # upload data in range
+Options:
+    --dry-run      : don't do anything, just show what would be done
+    --inc='spec'   : use given increment, eg --inc="5 day"
+EOF
+    exit 1
+}
+
+process_dates() {
+    start=$1
+    finish=$2
+
+    echo "$progname: process_dates $start -- $finish"
+    rm -f rhessi2dbase.err
+
+    uploadfile=rhessi.upload-$start
+    rm -f $uploadfile
+
+    {
+      echo ".com $scriptdir/rhessi2dbase"
+      echo "rhessi2dbase,time_interval=['$start','$finish'],outfn='$uploadfile'"
+    } | $scriptdir/ssw-for-rhessi >ssw-$start.log 2>&1
+
+    if test -f rhessi2dbase.err; then
+        # send error message to (original) stdout
+        echo "$progname: rhessi2dbase failed in directory $PWD"
+        echo "$progname: rhessi2dbase failed in directory $PWD" >&3
+        cat rhessi2dbase.err >&3
+        exit 1                                     # JUMP OUT
+    elif ! test -f $uploadfile; then
+        echo "$progname: rhessi2dbase appears to have failed: no file $PWD/$uploadfile"
+        echo "$progname: rhessi2dbase appears to have failed: no file $PWD/$uploadfile" >&3
+        exit 1                                     # JUMP OUT
+    else
+        # all OK!
+        rm -f ssw-$start.log
+    fi
+
+    # Upload this $uploadfile in a separate process
+    # A call to 'exit' in this process won't abort the parent process,
+    # so leave behind a file upload.stop in that case.
+    (
+        # collapse spaces at the beginning and end of fields
+        # (delimited by tabs)
+        sed 's/ *	 */	/g' $uploadfile >$uploadfile-temp
+        mv $uploadfile-temp $uploadfile
+
+        echo "load data low_priority local infile '$PWD/$uploadfile' into table rhessi;" | \
+          mysql -h$mysql_host $mysql_credentials dsa >mysql-$start.log 2>&1
+
+        status=$?
+        if test $status = 0; then
+            echo "$progname: successfully uploaded $uploadfile"
+            echo "$progname: NOT removing $uploadfile"
+            #rm -f $uploadfile mysql-$start.log
+        else
+            echo "$progname: mysql upload failed for file $PWD/$uploadfile" >&3
+            {
+                echo "mysql upload failed for file $PWD/$uploadfile"
+                echo "... exit status=$status"
+                echo "Logfile:"
+                cat mysql-$start.log
+            } >upload.stop
+        fi
+    ) &
+    echo "$progname: launched uploader for file $uploadfile"
+}
+
+########################################
+#
+# Get going...
+
+########################################
+#
+# Setup
+
+dryrun=false
+doall=false
+daylist=false
+
+workdir=X # dummy
+
+# Current PID is the same as the process group ID for the processes
+# started by this script.  "kill -TERM -$pgid" is how to kill the lot at once,
+# including this one.
+pgid=$$
+
+# The following is a bit of whimsy, to log the memory usage
+# of the group of processes.  Leave it in, just in case it
+# becomes useful again.
+#(
+#    while :
+#    do
+#        echo -n "--- "
+#        date --iso-8601=seconds
+#        ps -o pid,pgid,stat,bsdtime,cputime,%cpu,%mem,rss,sz,vsz,maj_flt,min_flt,args
+#        sleep 5
+#    done
+#) >pslog-$pgid &
+#echo "$progname: Logging ps in $PWD/pslog-$pgid!"
+
+########################################
+#
+# Argument parsing
+
+while test $# -ge 1; do
+    if expr "x$1" : 'x-' >/dev/null; then
+        if test "x$1" = x--all; then
+            doall=:
+        elif test "x$1" = x--dry-run; then
+            dryrun=:
+        elif expr "x$1" : x--inc= >/dev/null; then
+            inc=`expr "x$1" : 'x--inc=\(.*\)'`
+        else
+            Usage
+        fi
+    else
+        break
+    fi
+    shift
+done
+
+# Make sure $inc is set
+if test -z "$inc"; then
+    inc='5 day'
+fi
+
+if $doall; then
+    # ignore any arguments
+    # start time before the start of the RHESSI mission on 2002-02-12
+    startdate=2002-02-12
+    # end date is today
+    enddate=`date --utc --iso-8601`
+elif test $# = 0; then
+#    startdate=`date --utc --iso-8601 -d"2 days ago"`
+#    # end time is midnight of today (ie, this morning)
+#    enddate=`date --utc --iso-8601`
+    if test -f $stampfile; then
+        set `find $searchbase -newer $stampfile -name \*.fits|sed 's,/[^/]*$,,'|uniq|sed s,$rhessihome'/\(....\)/\(..\)/\(..\),\1-\2-\3,'`
+        daylist=:
+    else
+        echo "No file $stampfile" >&2
+        exit 1
+    fi
+elif test $# = 2; then
+    startdate=$1
+    enddate=$2
+else
+    Usage
+fi
+
+########################################
+#
+# Finished parsing; start work
+
+if ! $dryrun; then
+    workdir=`mktemp -p /tmp -d rhessi-upload-XXXXXX` || {
+        echo "Failed to make temporary work directory"
+        exit 1
+    }
+    cd $workdir
+
+    echo "$progname: PGID $pgid"
+    echo "$progname: work directory $workdir"
+
+    # Save stdout to fd3
+    exec 3>&1
+    # Redirect stdout
+    exec >upload-rhessi.log
+
+    echo "$progname: work directory $workdir"
+fi
+
+if $doall; then
+    echo "$progname: recreating table"
+    if ! $dryrun; then
+        { 
+          echo "drop table if exists rhessi;"
+          cat $scriptdir/dsa-create.mysql
+        } | mysql -h$mysql_host $mysql_credentials dsa
+        echo "$progname: recreated table"
+    fi
+    echo "$progname: Uploading all data, in $inc chunks"
+elif $daylist; then
+    echo "$progname: uploading data for days $@"
+else
+    echo "$progname: Uploading data in $startdate -- $enddate, in $inc chunks"
+fi
+echo -n "$progname: starting at..."
+date
+
+if $daylist; then
+
+    if test $# = 0; then
+        echo "$progname: No updates in period"
+    fi
+    while test $# -gt 0; do
+        endperiod=`date --utc --iso-8601 -d"$1 1 day"`
+
+        # check if an upload failed, so that we should abort this script
+        if test -f upload.stop; then
+            echo "Upload failed" >&3
+            cat upload.stop >&3
+            exit 1                              # JUMP OUT
+        fi
+
+        # The business...
+        if $dryrun; then
+            echo "process date $1 --- $endperiod"
+        else
+            process_dates $1 $endperiod
+        fi
+        shift
+    done
+
+else
+
+    # the +%s time at which we should stop the loop
+    endseconds=`date --utc -d"$enddate" +%s`
+
+    # We'll go through the following loop only once in the $doall case
+    while test `date --utc -d$startdate +%s` -lt $endseconds
+    do
+        # check if an upload failed, so that we should abort this script
+        if test -f upload.stop; then
+            echo "Upload failed" >&3
+            cat upload.stop >&3
+            exit 1                              # JUMP OUT
+        fi
+
+        if test -z "$inc"; then
+            endperiod=$enddate
+        else
+            endperiod=`date --utc --iso-8601 -d"$startdate + $inc"`
+        fi
+
+        # The business...
+        if $dryrun; then
+            echo "process dates $startdate --- $endperiod"
+        else
+            process_dates $startdate $endperiod
+        fi
+
+        startdate=$endperiod
+    done
+
+fi
+
+echo -n "$progname: ...ending at "
+date
+
+#echo "$progname: Processes in PG $pgid, logged in file pslog-$pgid:"
+#ps j
+#kill -TERM -$pgid
+#ps j
+
+touch $stampfile
+
+# Temporary (?) logging output, mailed to crontab owner,
+# to prompt review and tidyup.  Automate this in future?
+echo "$progname cron job finished at `date`" >&3
+echo "Review and delete work directory `hostname`:$workdir" >&3
+
+exit 0
