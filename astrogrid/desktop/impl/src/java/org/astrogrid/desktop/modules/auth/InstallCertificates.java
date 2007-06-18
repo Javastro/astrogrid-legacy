ad5f0f122b0312d1a824f2555281a673666a0406
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-06-18 16:31:54 +0000

    reimplemented so it doesn't block startup process

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/auth/InstallCertificates.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/auth/InstallCertificates.java
@@ -3,6 +3,9 @@
  */
 package org.astrogrid.desktop.modules.auth;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,7 +15,12 @@ import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.apache.commons.lang.StringUtils;
+import org.astrogrid.acr.ServiceException;
 import org.astrogrid.desktop.modules.system.ui.UIContext;
 import org.astrogrid.desktop.modules.ui.BackgroundWorker;
 import org.astrogrid.io.Piper;
@@ -23,14 +31,19 @@ import org.astrogrid.io.Piper;
  * @since Sep 7, 200611:27:55 AM
  */
 public class InstallCertificates implements Runnable {
+	/**
+	 * Logger for this class
+	 */
+	private static final Log logger = LogFactory
+			.getLog(InstallCertificates.class);
 
 	private final File certDir;
-	private final List certList;
+	private final List certLists;
 	private final UIContext ui;
-	public InstallCertificates(final UIContext ui, final String certDir, final List certList) {
+	public InstallCertificates(final UIContext ui, final String certDir, final List certLists) {
 		super();
 		this.certDir = new File(certDir);
-		this.certList = certList;
+		this.certLists = certLists;
 		this.ui = ui;
 		
 	}
@@ -38,38 +51,73 @@ public class InstallCertificates implements Runnable {
 		(new BackgroundWorker(ui,"Checking for new certificates") {
 			protected Object construct()  {
 				if (! certDir.exists()) {
+					logger.info("Certificate directory does not exist - creating " + certDir);
 					certDir.mkdirs();
 				}
-				for (Iterator i = certList.iterator(); i.hasNext(); ) {
-					InputStream is = null;
-					OutputStream os = null;
+				XMLInputFactory fac = XMLInputFactory.newInstance();				
+				for (Iterator i = certLists.iterator(); i.hasNext(); ) {
+					// fetch and parse each of these certificate lists.
+					XMLStreamReader in = null;
+					String certList = (String)i.next();
+					logger.info("Reading certificate list from " + certList);
 					try {
-					URL url = new URL((String)i.next());
+						in = fac.createXMLStreamReader((new URL(certList)).openStream());
+						while (in.hasNext()) {
+							in.next();
+							if (in.isStartElement() && in.getLocalName().equals("item")) {
+								handleSingleCertificate(in.getElementText().trim());
+							}
+						}
+					} catch (XMLStreamException x) {
+						logger.warn("Failed to parse " + certList );
+					} catch (IOException x) {
+						logger.warn("Failed to parse " + certList );						
+					} finally {
+						try {
+							if (in != null) {
+								in.close();
+							}
+						} catch (XMLStreamException x) {
+						}
+					}					
+				}
+				return null;
+			}
+
+			// checks whether we've already downloaded a particular certificate.
+			// and fetches it if not.
+			private void handleSingleCertificate(final String certificateURL) {
+				InputStream is = null;
+				OutputStream os = null;
+				URL url = null;
+				try {
+					url = new URL(certificateURL);
 					String certName = StringUtils.substringAfterLast(url.toString(),"/");
 					File cert = new File(certDir,certName);
 					if (!cert.exists()) { // missing - download it.
+						logger.info("Downloading new certificate " + url);
 						os = new FileOutputStream(cert);
 						is = url.openStream();
 						Piper.pipe(is,os);
 					}
-					} catch (IOException e) {
-						if (is != null) {
-							try {
-								is.close();
-							} catch (IOException x) {
-								// oh-well.
-							}
+				} catch (IOException e) {
+					logger.warn("Failed to downlooad certificate " + url);
+				} finally {
+					if (is != null) {
+						try {
+							is.close();
+						} catch (IOException x) {
+							// oh-well.
 						}
-						if (os != null) {
-							try {
-								os.close();
-							} catch (IOException x) {
-								// never mind
-							}
+					}
+					if (os != null) {
+						try {
+							os.close();
+						} catch (IOException x) {
+							// never mind
 						}
 					}
 				}
-				return null;
 			}
 		}).start();
 	}
