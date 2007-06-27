0c0e2321cb5e3c17db2d996133d769cb0be5a6cb
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-06-27 11:09:40 +0000

    altered the splash screen.

--- astrogrid/desktop/impl/src/java/org/astrogrid/VOExplorer.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/VOExplorer.java
@@ -1,4 +1,4 @@
-/*$Id: VOExplorer.java,v 1.1 2007/03/09 15:34:24 nw Exp $
+/*$Id: VOExplorer.java,v 1.2 2007/06/27 11:09:40 nw Exp $
  * Created on 04-Jul-2005
  *
  * Copyright (C) AstroGrid. All rights reserved.
@@ -26,7 +26,7 @@ public class VOExplorer {
     
     public static void main(String[] args) {
     	//@todo provide a splash screen.
-        SplashWindow.splash(VOExplorer.class.getResource("voexplorer-splash.gif"));
+        SplashWindow.splash(VOExplorer.class.getResource("acr-splash.gif"));
         SplashWindow.invokeMain("org.astrogrid.VOExplorer1", args);
         SplashWindow.disposeSplash();
     }
