9284501fa43af58b7e685f17ac0d9aedebf3ac91
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-07-23 12:26:49 +0000

    moved adql interface here.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ivoa/AdqlInternal.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.ivoa;
+
+import org.astrogrid.acr.InvalidArgumentException;
+import org.astrogrid.acr.ivoa.Adql;
+
+/** extension of public adql interface.
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Jul 17, 20071:05:27 AM
+ */
+public interface AdqlInternal extends Adql {
+/**
+ * parse adql/s into adql/x (as a string)
+ * @param arg0 adqls
+ * @return equivalent adqlx.
+ * @throws InvalidArgumentException
+ */
+    public String s2xs(String arg0) throws InvalidArgumentException;
+}
