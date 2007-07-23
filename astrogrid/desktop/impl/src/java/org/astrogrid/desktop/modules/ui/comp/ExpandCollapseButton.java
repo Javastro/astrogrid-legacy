b80fb2fc2a3147b20e3b72e036d3bfb1bd6f09ab
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-07-23 11:51:17 +0000

    component that controls a collapsible frame

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/ExpandCollapseButton.java
@@ -0,0 +1,26 @@
+package org.astrogrid.desktop.modules.ui.comp;
+
+import javax.swing.Action;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+
+import org.astrogrid.desktop.icons.IconHelper;
+
+import com.l2fprod.common.swing.JCollapsiblePane;
+
+/** button that controls a jCollapsiblepane */
+public class ExpandCollapseButton extends JToggleButton {
+	/**
+	 * construct a new expand-collapse button
+	 * @param pane the pane to expand or collapse on button click.
+	 */
+	public ExpandCollapseButton(JCollapsiblePane pane) {
+		final Action toggleAction = pane.getActionMap().get(JCollapsiblePane.TOGGLE_ACTION);
+		toggleAction.putValue(JCollapsiblePane.EXPAND_ICON, IconHelper.loadIcon("expand22.png"));
+		toggleAction.putValue(JCollapsiblePane.COLLAPSE_ICON, IconHelper.loadIcon("contract22.png"));
+		this.setAction(toggleAction);		
+		setText("");
+		putClientProperty("is3DEnabled", Boolean.FALSE);
+		setBorderPainted(false);			
+	}
+}
