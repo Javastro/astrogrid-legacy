a8f359ad52049f9c77517e56b43143d7d1d94c0a
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-10-07 10:51:14 +0000

    new simple button for 'pinning' docs.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/PinnableLabel.java
@@ -0,0 +1,62 @@
+package org.astrogrid.desktop.modules.ui.comp;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JToggleButton;
+import javax.swing.SwingConstants;
+
+import org.astrogrid.desktop.icons.IconHelper;
+
+import com.l2fprod.common.swing.icons.EmptyIcon;
+
+
+/** a button that appears to be a label, and can be 'pinned' */
+public class PinnableLabel extends JToggleButton {
+
+    private static final Font fnt = new JLabel().getFont();
+    
+    public PinnableLabel(String text) {
+        super(text,UIConstants.PIN_ICON);
+        setFont(fnt);
+        setBorder(null);
+        setDisabledIcon(new EmptyIcon());
+        setSelectedIcon(UIConstants.PIN_ICON);
+        setRolloverEnabled(true);
+        setRolloverIcon(UIConstants.PIN_ICON);
+        setHorizontalAlignment(SwingConstants.LEFT);
+        setHorizontalTextPosition(SwingConstants.LEFT);
+        putClientProperty("is3DEnabled", Boolean.FALSE);
+        setFocusable(false);
+        setBorderPainted(false);              
+    }
+    
+    // if a param is disabled, make sure it's not still selectd.
+    public void setEnabled(boolean b) {
+        super.setEnabled(b);
+        if (! b && isSelected() ) {
+            setSelected(false);
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        javax.swing.JPanel p = new javax.swing.JPanel();
+        PinnableLabel l = new PinnableLabel("hi there");
+        p.add(l);
+        PinnableLabel l1 = new PinnableLabel("ho");
+        l1.setEnabled(false);
+        p.add(l1);
+        PinnableLabel l2 =new PinnableLabel("foo");
+        l2.setIcon(IconHelper.loadIcon("edit16.png"));
+        p.add(l2);
+        JFrame f = new javax.swing.JFrame();
+        f.getContentPane().add(p);
+        f.pack();
+        f.show();
+        
+    } 
+   
+    
+}
