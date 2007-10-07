ed88e2498ec3f096edde3b89f4ffc7e358d81954
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-10-07 10:50:21 +0000

    added ability to expand /collapse a vanilla JPane too.

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/ExpandCollapseButton.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/ExpandCollapseButton.java
@@ -1,25 +1,59 @@
 package org.astrogrid.desktop.modules.ui.comp;
 
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.Action;
+import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
 import org.astrogrid.desktop.icons.IconHelper;
 
 import com.l2fprod.common.swing.JCollapsiblePane;
 
-/** button that controls a jCollapsiblepane */
-public class ExpandCollapseButton extends JToggleButton {
+/** button that controls a jCollapsiblepane, or a normal pane. */
+public class ExpandCollapseButton extends JToggleButton implements ItemListener {
 	/**
+     * 
+     */
+    private static final String CONTRACT_ICON = "contract16.png";
+    /**
+     * 
+     */
+    private static final String EXPAND_ICON = "expand16.png";
+    private JPanel pane;
+
+    /**
 	 * construct a new expand-collapse button
 	 * @param pane the pane to expand or collapse on button click.
 	 */
 	public ExpandCollapseButton(JCollapsiblePane pane) {
+	    this();
 		final Action toggleAction = pane.getActionMap().get(JCollapsiblePane.TOGGLE_ACTION);
-		toggleAction.putValue(JCollapsiblePane.EXPAND_ICON, IconHelper.loadIcon("expand22.png"));
-		toggleAction.putValue(JCollapsiblePane.COLLAPSE_ICON, IconHelper.loadIcon("contract22.png"));
-		this.setAction(toggleAction);		
-		setText("");
-		putClientProperty("is3DEnabled", Boolean.FALSE);
-		setBorderPainted(false);			
+		toggleAction.putValue(JCollapsiblePane.EXPAND_ICON, IconHelper.loadIcon(EXPAND_ICON));
+		toggleAction.putValue(JCollapsiblePane.COLLAPSE_ICON, IconHelper.loadIcon(CONTRACT_ICON));
+		this.setAction(toggleAction);
+        setText("");		
 	}
+	
+	public ExpandCollapseButton(JPanel pane) {
+	    this.pane = pane;
+        setIcon(IconHelper.loadIcon(EXPAND_ICON));
+	    setSelectedIcon(IconHelper.loadIcon(CONTRACT_ICON));
+	    addItemListener(this);
+        setText("");	    
+	}
+	
+	private ExpandCollapseButton() {
+		putClientProperty("is3DEnabled", Boolean.FALSE);
+		//setBorderPainted(false);	
+		//setContentAreaFilled(false);
+		//setBorder(null);
+		
+	}
+
+	// used when working with a normal jPanel.
+    public void itemStateChanged(ItemEvent e) {
+            pane.setVisible(isSelected());
+    }
 }
