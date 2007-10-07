ea6cd97611126811f4add054ba178930e72cd961
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-10-07 11:08:02 +0000

    harness for interactivly exercising the resource chooser.

---
+++ astrogrid/desktop/impl/test/java/org/astrogrid/desktop/modules/dialogs/ResourceChooserUIExercise.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.dialogs;
+
+import java.net.URI;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.astrogrid.acr.dialogs.ResourceChooser;
+import org.astrogrid.desktop.ARTestSetup;
+import org.astrogrid.desktop.InARTestCase;
+import org.astrogrid.desktop.modules.cds.CoordinateSystemTest;
+
+/** bring up a file explorer dialogue for interactive testing.
+ * uses the Junit fixtures and stuff for bringing up the AR, but doesn't do any assertions itself.
+ * 
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Oct 1, 200710:01:06 AM
+ */
+public class ResourceChooserUIExercise extends InARTestCase {
+    private ResourceChooser chooser;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.chooser = (ResourceChooser) assertServiceExists(ResourceChooser.class,"dialogs.resourceChooser");
+    }
+    
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        chooser = null;
+    }
+    
+    public void testRunChooser() throws Exception {
+        URI res = chooser.fullChooseResource("exercise this dialogue",true,true,true);
+        System.out.println(res);
+    }
+    
+    public void testRunChooserAgain() throws Exception {
+        testRunChooser(); // verify it gets back to the starting position each time it's called.
+        
+    }
+    
+    public void testLocalFilesOnly() throws Exception {
+        URI res = chooser.fullChooseResource("local files only",false,true,false);
+        System.out.println(res);        
+        
+    }
+    
+    public void testRunFullChooserAgain() throws Exception {
+        testRunChooser();        
+    }
+
+    public void testURLsOnly() throws Exception {
+        URI res = chooser.fullChooseResource("urls only",false,false,true);
+        System.out.println(res);        
+        
+    }
+    
+    public void testRunFullChooserAgainAgain() throws Exception {
+        testRunChooser();        
+    }    
+    
+   public void testChooseFolder() throws Exception {
+       URI res = chooser.chooseFolder("choose a folder",true);
+       System.out.println(res);
+}
+
+   public void testRunFullChooserYetAgain() throws Exception {
+       testRunChooser();        
+   }
+   
+    public static Test suite() {
+        return new ARTestSetup(new TestSuite(ResourceChooserUIExercise.class));
+    }
+    
+}
