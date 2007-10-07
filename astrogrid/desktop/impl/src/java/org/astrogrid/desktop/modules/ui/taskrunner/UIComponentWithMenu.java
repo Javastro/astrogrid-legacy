4a666461ddd55a2959263f129f97a8a00aacb437
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-10-07 11:04:18 +0000

    new interface, used to pass a context menu to adql editor.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/taskrunner/UIComponentWithMenu.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.ui.taskrunner;
+
+import javax.swing.JMenu;
+
+import org.astrogrid.desktop.modules.ui.UIComponent;
+
+/** extension of the ui component interface with an additional method to 
+ * access a menubar menu to which context-sensitive actions can be added.
+ * 
+ * The client of this interface (i.e. whatever is adding things to the menu) is 
+ * responsible for changing their visibiliy as the context changes. one way to do this
+ * would be to add a componentListener or HierarchyListener for visibility events.
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Oct 6, 20072:53:55 PM
+ */
+public interface UIComponentWithMenu extends UIComponent {
+    /** menubar menu to add context sensitive actions to. Use assuming it to already have other
+     * menu entries.
+     * It's this client's responsibility to manage the visibility of any menu entries it
+     * adds to this menu
+     */
+    JMenu getContextMenu();
+}
