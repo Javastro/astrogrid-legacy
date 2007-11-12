0c75de4098f69eb06b1ad58ac191d8a19473964a
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2007-11-12 11:49:59 +0000

    debugged and added ability to have none-managed items in teh menu too.

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/EventListPopupMenuManager.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/comp/EventListPopupMenuManager.java
@@ -6,6 +6,7 @@ package org.astrogrid.desktop.modules.ui.comp;
 import java.awt.Component;
 import java.util.Iterator;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JPopupMenu;
 
@@ -23,10 +24,9 @@ public class EventListPopupMenuManager implements ListEventListener {
 	private final EventList el;
 	private final JPopupMenu popup;
 	private final boolean reverse;
-	private final JButton triggerButton;
-	public EventListPopupMenuManager(final EventList el,  final JPopupMenu popup, boolean reverse) {
-		this(el,null,popup,reverse);
-	}
+	private final AbstractButton triggerButton;
+    private final int startingSize;
+
 
 	
 	/**
@@ -35,18 +35,21 @@ public class EventListPopupMenuManager implements ListEventListener {
 	 * @param triggerButton  the button that triggers this menu (will handle
 	 * enabled / disabled as menu shrinks and grows) - may be null
 	 * @param popup the popup menu to display items in
-	 * @param reverse if true, display the items in reverse order.
+	 * @param reverse if true, display the items in reverse order. If false,
+	 * additional components may be added to the menu before and after the 
+	 * creation of the menu manager. If true, no other components should be added.
 	 */
-	public EventListPopupMenuManager(final EventList el, final JButton triggerButton, final JPopupMenu popup, boolean reverse) {
+	public EventListPopupMenuManager(final EventList el, final AbstractButton triggerButton, final JPopupMenu popup, boolean reverse) {
 		super();
 		this.el = el;
 		this.popup = popup;
 		this.reverse = reverse;
 		this.triggerButton = triggerButton;
+		startingSize = popup.getComponentCount(); 
 		if (triggerButton != null) {
-			triggerButton.setEnabled(el.size() > 0);
+			triggerButton.setEnabled(startingSize + el.size() > 0);
 		}
-		int ix = 0;
+		int ix = startingSize;
 		for (Iterator i = el.iterator(); i.hasNext(); ) {
 			Component o = (Component)i.next();
 			popup.insert(o,mbReverseInsert(ix++));
@@ -64,7 +67,7 @@ public class EventListPopupMenuManager implements ListEventListener {
 				case ListEvent.INSERT:
 					o = (Component)el.get(i);
 					popup.insert(o,mbReverseInsert(i));
-					if (triggerButton != null && el.size() ==1) { // first insert
+					if (triggerButton != null && popup.getComponentCount() ==1) { // first insert
 						triggerButton.setEnabled(true);
 					}
 					break;
@@ -75,7 +78,7 @@ public class EventListPopupMenuManager implements ListEventListener {
 					break;
 				case ListEvent.DELETE:
 					popup.remove(mbReverse(i));
-					if (triggerButton != null && el.size() ==0) {
+					if (triggerButton != null && popup.getComponentCount() ==0) {
 						triggerButton.setEnabled(false);
 					}
 					break;
@@ -89,7 +92,7 @@ public class EventListPopupMenuManager implements ListEventListener {
 	 */
 	private int mbReverse(int i) {
 		if (! reverse) {
-			return i;
+			return i + startingSize;
 		} else {
 			int sz = popup.getComponentCount();
 			return (sz-1)-i;
@@ -99,7 +102,7 @@ public class EventListPopupMenuManager implements ListEventListener {
 	/** as insert adds before an item, the calculation is slightly different */
 	private int mbReverseInsert(int i) {
 		if (! reverse) {
-			return i;
+			return i + startingSize;
 		} else {
 			int sz = popup.getComponentCount();
 			return sz -i;
