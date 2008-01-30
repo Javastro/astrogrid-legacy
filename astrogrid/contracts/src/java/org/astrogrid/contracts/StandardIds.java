87203af242d256f65845a8ca2c745cdc2c4933e6
Guy Rixon <gtr@ast.cam.ac.uk>
2008-01-30 14:04:24 +0000

    2008.0a2. StandardID values are added for registry, STAP and SSAP.

--- astrogrid/contracts/src/java/org/astrogrid/contracts/StandardIds.java
+++ astrogrid/contracts/src/java/org/astrogrid/contracts/StandardIds.java
@@ -17,33 +17,53 @@ public class StandardIds {
    * Cone search: ivo://ivoa.net/std/ConeSearch.
    * Taken from IVOA's XML schema for http://www.ivoa.net/xml/ConeSearch/v1.0.
    */
-  public static String CONE_SEARCH_1_0 = "ivo://ivoa.net/std/ConeSearch";
+  public final static String CONE_SEARCH_1_0 = "ivo://ivoa.net/std/ConeSearch";
   
   /**
    * Simple Image Access Protocol: ivo://ivoa.net/std/SIA.
    * Taken from IVOA's XML schema for http://www.ivoa.net/xml/SIA/v1.0.
    */
-  public static String SIAP_1_0 = "ivo://ivoa.net/std/SIA";
+  public final static String SIAP_1_0 = "ivo://ivoa.net/std/SIA";
+  
+  /**
+   * Simple Spectral Access Protocol: ivo://ivoa.net/std/SSA.
+   * Taken from IVOA's XML schema for http://www.ivoa.net/xml/SSA/v0.2
+   */
+  public final static String SSAP_1_0 = "ivo://ivoa.net/std/SSA";
   
   /**
    * Open Sky Node: ivo://ivoa.net/std/OpenSkyNode.
    * Taken from IVOA's XML schema for http://www.ivoa.net/xml/SkyNode/v0.2,
    */
-  public static String OPEN_SKY_NODE_0_2 = "ivo://ivoa.net/std/OpenSkyNode";
+  public final static String OPEN_SKY_NODE_0_2 = 
+      "ivo://ivoa.net/std/OpenSkyNode";
+  
+  /**
+   * IVOA registry: ivo://ivoa.net/std/Registry.
+   * Taken from IVOA's standard document for the registry interface.
+   */
+  public final static String REGISTRY_1_0 = "ivo://ivoa.net/std/Registry";
   
   /**
    * Common Execution Architecture: ivo://org.astrogrid/std/CEA/v1.0.
    * Chosen by AstroGrid during cycle 1 of AG3.
    * A capability for CEA v1.0 implies the CEC SOAP service.
    */
-  public static String CEA_1_0 = "ivo://org.astrogrid/std/CEA/v1.0";
+  public final static String CEA_1_0 = "ivo://org.astrogrid/std/CEA/v1.0";
+  
+  /**
+   * Simple Temporal Access Protocol: ivo://org.astrogrid/std/STAP/v1.0.
+   * Chosen by AstroGrid during cycle 1 of AG3.
+   */
+  public final static String STAP_1_0 = 
+      "ivo://org.astrogrid/std/STAP/v1.0";
   
   /**
    * PolicyManager: ivo://org.astrogrid/std/Community/v1.0#PolicyManager.
    * Chosen by AstroGrid during cycle 1 of AG3.
    * PolicyManager is the community's SOAP service for reporting home space.
    */
-  public static String POLICY_MANAGER_1_0 = 
+  public final static String POLICY_MANAGER_1_0 = 
       "ivo://org.astrogrid/std/Community/v1.0#PolicyManager";
   
   /**
@@ -51,7 +71,7 @@ public class StandardIds {
    * Chosen by AstroGrid during cycle 1 of AG3.
    * SecurityService is the community's SOAP interface for checking passwords.
    */
-  public static String SECURITY_SERVICE_1_0 =
+  public final static String SECURITY_SERVICE_1_0 =
       "ivo://org.astrogrid/std/Community/v1.0#SecurityService";
   
   /**
@@ -61,7 +81,7 @@ public class StandardIds {
    * IVOA may later issue a standardID for MyProxy; if so, their URI
    * should be used instead of this one.
    */
-  public static String MY_PROXY_2 = 
+  public final static String MY_PROXY_2 = 
       "ivo://org.astrogrid/std/Community/v1.0#MyProxy";
   
   /**
@@ -69,7 +89,7 @@ public class StandardIds {
    * Chosen by AstroGrid during cycle 1 of AG3.
    * This refers to AstroGrid's implementation of VOSI 0.3.
    */
-  public static String VOSI_AVAILABILITY_0_3 =
+  public final static String VOSI_AVAILABILITY_0_3 =
       "ivo://org.astrogrid/std/VOSI/v0.3#availability";
   
   /**
@@ -77,7 +97,7 @@ public class StandardIds {
    * Chosen by AstroGrid during cycle 1 of AG3.
    * This refers to AstroGrid's implementation of VOSI 0.3.
    */
-  public static String VOSI_CAPABILITIES_0_3 =
+  public final static String VOSI_CAPABILITIES_0_3 =
       "ivo://org.astrogrid/std/VOSI/v0.3#capabilities";
   
   /**
@@ -87,7 +107,7 @@ public class StandardIds {
    * The tables capability allows a client to download from a catalogue service
    * a description of the tables in that service.
    */
-  public static String VOSI_TABLES_0_3 =
+  public final static String VOSI_TABLES_0_3 =
       "ivo://org.astrogrid/std/VOSI/v0.3#tables";
   
   /**
@@ -97,7 +117,7 @@ public class StandardIds {
    * The application capability allows a client to download a description of
    * the CEA application hosted by the service.
    */
-  public static String VOSI_APPLICATION_0_3 =
+  public final static String VOSI_APPLICATION_0_3 =
       "ivo://org.astrogrid/std/VOSI/v0.3#ceaApplication";
   
 }
