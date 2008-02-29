defad66dae16f2053b6e643e0f6a0318f7cb87c5
gg78 <gg78>
2008-02-29 17:43:43 +0000

    non-root users can refresh the scripts checked out of cvs

---
+++ astrogrid/stresstest/setup/updatescripts.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+
+# refresh the scripts checked out of cvs by run
+# 
+
+
+#
+# Set the project directory.
+PROJECT_BASE=/var/local/projects
+ASTROGRID_BASE=${PROJECT_BASE}/astrogrid
+
+pushd ${ASTROGRID_BASE}
+   CVS_RSH=ssh
+   export CVS_RSH
+   cvs -z3 -d :ext:flexiscale@cvs.astrogrid.org:/devel update
+popd
+
+
