ce10181302e3f17728c31bfb613e20d43d541d54
Dave Morris <dmr@roe.ac.uk>
2008-03-01 02:30:25 +0000

    ....

--- astrogrid/stresstest/setup/launch.sh
+++ astrogrid/stresstest/setup/launch.sh
@@ -25,7 +25,7 @@ COMMAND2='~/python/dave/copymove.py --user dave --outer 200 --wait 10 --debug --
 COMMAND3='~/python/dave/copymove.py --user dave --outer 200 --wait 15 --debug --exit ; echo ; echo "Test done" ; read OK'
 COMMAND4='~/python/dave/copymove.py --user dave --outer 200 --wait 20 --debug --exit ; echo ; echo "Test done" ; read OK'
 
-gnome-terminal --geometry=80x20+0+50    --tab -e "ssh agtester-000@alpha $VOEXPLORER" --tab -e "ssh agtester-000@alpha $COMMAND1"
+gnome-terminal --geometry=80x20+000+050 --tab -e "ssh agtester-000@alpha $VOEXPLORER" --tab -e "ssh agtester-000@alpha $COMMAND1"
 gnome-terminal --geometry=80x20+200+150 --tab -e "ssh agtester-001@alpha $VOEXPLORER" --tab -e "ssh agtester-001@alpha $COMMAND2"
 gnome-terminal --geometry=80x20+400+250 --tab -e "ssh agtester-002@alpha $VOEXPLORER" --tab -e "ssh agtester-002@alpha $COMMAND3"
 gnome-terminal --geometry=80x20+600+350 --tab -e "ssh agtester-003@alpha $VOEXPLORER" --tab -e "ssh agtester-003@alpha $COMMAND4"
@@ -34,4 +34,6 @@ gnome-terminal --geometry=80x20+600+350 --tab -e "ssh agtester-003@alpha $VOEXPL
 # Useful to tidy up after a failed test.
 # gnome-terminal -e "ssh root@alpha"
 # for pid in `ps -e | grep java | awk '{ print $1 }'`; do echo $pid ; kill $pid; done
+
+
 
