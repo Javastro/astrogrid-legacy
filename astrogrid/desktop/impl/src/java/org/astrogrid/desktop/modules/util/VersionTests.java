7470e851006c85af7710139c138fde2a2058ff88
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2008-04-23 11:18:57 +0000

    added test for 1.5 java.

---
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/util/VersionTests.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.util;
+
+import org.apache.commons.lang.SystemUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/** Run through some tests to verify we've got the correct versions of things
+ * 
+ * 
+ * at the moment it's only the Java version.
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Apr 16, 20089:19:27 AM
+ */
+public class VersionTests extends TestSuite {
+    /**
+     * Logger for this class
+     */
+    private static final Log logger = LogFactory.getLog(VersionTests.class);
+    private static final float REQUIRED_JAVA_VERSION = 1.5f;
+    /**
+     * 
+     */
+    public VersionTests() {
+        addTest(new TestCase("Java version") {
+            @Override
+            protected void runTest() throws Throwable {
+                assertTrue("Java version is " + SystemUtils.JAVA_VERSION_FLOAT,
+                        SystemUtils.isJavaVersionAtLeast(REQUIRED_JAVA_VERSION));
+            }
+        });
+    }
+    
+    // for stand-alone development.
+    public static Test suite() {
+        return new VersionTests();
+    }
+}
