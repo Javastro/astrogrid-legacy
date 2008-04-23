86da65d22672c0cb9a0545a05ac31f9fdbcd0fe2
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2008-04-23 11:26:32 +0000

    will add tests here.

---
+++ astrogrid/desktop/impl/test/java/org/astrogrid/desktop/modules/background/AllBackgroundUnitTests.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.astrogrid.desktop.modules.background;
+
+/**
+ * @author Noel.Winstanley@manchester.ac.uk
+ * @since Apr 17, 20085:20:44 PM
+ * @TEST implement some!
+ */
+public class AllBackgroundUnitTests {
+
+}
