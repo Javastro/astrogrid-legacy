85131b8fd0f461b50f784cb4c6a6131d842ffbc7
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2008-04-23 10:48:24 +0000

    removed - unused.

--- astrogrid/desktop/impl/build/launch.bat
+++
@@ -1,3 +0,0 @@
-@echo off 
-start javaw -Xmx512m -Djava.library.path="$INSTALL_PATH\lib" -cp "$INSTALL_PATH\lib\$jarname;$INSTALL_PATH\lib\jdic_stub_Windows-$jdicversion.jar" $mainclass %1 %2 %3 %4 %5
-
