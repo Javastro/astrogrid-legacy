2fec5d997627c1e922097a42a77b8b9254cc3071
Paul Harrison <paul.harrison@manchester.ac.uk>
2008-05-17 16:17:49 +0000

    *** empty log message ***

---
+++ astrogrid/desktop/capi/core/arcontainers.cpp
@@ -0,0 +1,13 @@
+/*
+ * $Id: arcontainers.cpp,v 1.1 2008/05/17 16:17:49 pah Exp $
+ * 
+ * Created on 29 Oct 2007 by Paul Harrison (paul.harrison@manchester.ac.uk)
+ * Copyright 2007 Astrogrid. All rights reserved.
+ *
+ * This software is published under the terms of the Astrogrid 
+ * Software License, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+#include "arcontainers.h"
+
