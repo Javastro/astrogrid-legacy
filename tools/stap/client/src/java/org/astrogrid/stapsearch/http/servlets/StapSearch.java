2b7ecf0c74cc442b09e074cb3bc98b4b302db50e
Paul Harrison <paul.harrison@manchester.ac.uk>
2008-06-19 16:32:57 +0000

    changed so that any exceptions are not silently (to the client) ignored - have not safeguarded against any of them though

--- tools/stap/client/src/java/org/astrogrid/stapsearch/http/servlets/StapSearch.java
+++ tools/stap/client/src/java/org/astrogrid/stapsearch/http/servlets/StapSearch.java
@@ -18,7 +18,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id: StapSearch.java,v 1.1 2006/08/15 15:40:00 KevinBenson Exp $
+ *  $Id: StapSearch.java,v 1.2 2008/06/19 16:32:57 pah Exp $
  */
 
 package org.astrogrid.stapsearch.http.servlets;
@@ -34,6 +34,7 @@ import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.io.PrintWriter;
 import java.io.IOException;
@@ -43,87 +44,83 @@ import org.astrogrid.config.Config;
 /**
  * Helper servlet for initializing the log4j framework in a webcontainer.
  */
-public class StapSearch extends HttpServlet {    
-    
+public class StapSearch extends HttpServlet {
+
     /**
-     * conf - Config variable to access the configuration for the server normally
-     * jndi to a config file.
+     * conf - Config variable to access the configuration for the server
+     * normally jndi to a config file.
+     * 
      * @see org.astrogrid.config.Config
-     */   
+     */
     public static Config conf = null;
-    
+
     /**
      * Static to be used on the initiatian of this class for the config
-     */   
+     */
     static {
-       if(conf == null) {
-          conf = org.astrogrid.config.SimpleConfig.getSingleton();
-       }
-    }    
-    
-    
+	if (conf == null) {
+	    conf = org.astrogrid.config.SimpleConfig.getSingleton();
+	}
+    }
+
     /**
-     * Method: destroy
-     * Description: destroy method for the servlet conatiner to call when killing the servlet. Cleans up any
-     * timer information making sure they are cancelled.  Otherwise the timer class may not let the servlet container
-     * shutdown till ti runs.
-     *
+     * Method: destroy Description: destroy method for the servlet conatiner to
+     * call when killing the servlet. Cleans up any timer information making
+     * sure they are cancelled. Otherwise the timer class may not let the
+     * servlet container shutdown till ti runs.
+     * 
      */
     public void destroy() {
-        super.destroy();
+	super.destroy();
     }
-    
-    /**
-     *  Empty method.
-     *
-     * @param req HTTP Request object
-     * @param res HTTP Response object
-     */
-    public void doGet(HttpServletRequest req, HttpServletResponse res) {
-        
-        System.setProperty("http.agent","astrogrid-stap-" + req.getServerName());
-        res.setContentType("text/xml;charset=UTF-8");
-        SimpleDateFormat dateFormat = new SimpleDateFormat(
-        "yyyy-MM-dd'T'HH:mm:ss");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
 
-        try {
-            Date startTime = dateFormat.parse(req.getParameter("START"));
-            Date endTime = dateFormat.parse(req.getParameter("END"));
-            Calendar startTimeCal = Calendar.getInstance();
-            Calendar endTimeCal = Calendar.getInstance();
-            startTimeCal.setTime(startTime);
-            endTimeCal.setTime(endTime);
-            
-            String service = req.getParameter("service");
-            if(service == null || service.trim().length() == 0) {
-                System.out.println("Service name not part of the request exiting now, later default it");
-                return;
-            }
-            
-            String className = conf.getString("stapsearch.service." + service);
-            System.out.println("classname found = " + className );
-            
-            try {
-                PrintWriter output = res.getWriter();        
-                try {
-                    Class cl = Class.forName(className);
-                    IStapSearch ss = (IStapSearch)cl.newInstance();
-                    ss.execute(startTimeCal, endTimeCal, req.getParameterMap(), output);
-                }catch(ClassNotFoundException cfe) {
-                    output.print("Cannot Find class to implement service = " + service);
-                }catch(InstantiationException ie) {
-                    output.print("Cannot instantiate the service = " + service);
-                }catch(IllegalAccessException iae) {
-                    output.print("Illegal access to instantiate service = "  + service);
-                }
-                output.close();
-            }catch(IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }catch(java.text.ParseException pe){
-            pe.printStackTrace();
-        }
+    /**
+     * Empty method.
+     * 
+     * @param req
+     *                HTTP Request object
+     * @param res
+     *                HTTP Response object
+     * @throws throws
+     *                 ServletException, IOException
+     */
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+	    throws ServletException, IOException {
+
+	System.setProperty("http.agent", "astrogrid-stap-"
+		+ req.getServerName());
+	res.setContentType("text/xml;charset=UTF-8");
+	SimpleDateFormat dateFormat = new SimpleDateFormat(
+		"yyyy-MM-dd'T'HH:mm:ss");
+	dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+
+	try {
+	    Date startTime = dateFormat.parse(req.getParameter("START"));
+	    Date endTime = dateFormat.parse(req.getParameter("END"));
+	    Calendar startTimeCal = Calendar.getInstance();
+	    Calendar endTimeCal = Calendar.getInstance();
+	    startTimeCal.setTime(startTime);
+	    endTimeCal.setTime(endTime);
+
+	    String service = req.getParameter("service");
+	    if (service == null || service.trim().length() == 0) {
+	        System.out
+	    	    .println("Service name not part of the request exiting now, later default it");
+	        return;
+	    }
+
+	    String className = conf.getString("stapsearch.service." + service);
+	    System.out.println("classname found = " + className);
+
+	    PrintWriter output = res.getWriter();
+
+	    Class cl = Class.forName(className);
+	    IStapSearch ss = (IStapSearch) cl.newInstance();
+	    ss.execute(startTimeCal, endTimeCal, req.getParameterMap(), output);
+	    output.close();
+	} catch (Exception e) {
+	    throw new ServletException("problem with search", e);
+	}
+
     }
-    
 }
