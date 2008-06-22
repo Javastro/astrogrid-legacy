47c41bc2f08725695317feaa40c50762d99444ab
Norman Gray <norman.gray@glasgow.ac.uk>
2008-06-22 17:40:45 +0000

    Improve distribution script -- general productising. Release 0.4.7

--- experiments/norman/quaestor/src/etc/start-jetty.sh
+++ experiments/norman/quaestor/src/etc/start-jetty.sh
@@ -13,8 +13,7 @@ PATHTOHERE=`dirname $0`
 D=`cd $PATHTOHERE; pwd`
 cd $D
 
-#echo java -cp $D/quaestor-lib-@VERSION@.jar:$D/war/WEB-INF org.eurovotech.quaestor.SchemeJettyServer "$@"
-exec java -cp $D/quaestor-lib-@VERSION@.jar:$D/war/WEB-INF org.eurovotech.quaestor.SchemeJettyServer "$@"
+exec java -cp $D/quaestor-lib-@VERSION@.jar:$D/war/WEB-INF/classes org.eurovotech.quaestor.SchemeJettyServer "$@"
 
 # shouldn't get here
 echo "Exec failed!" >&2
