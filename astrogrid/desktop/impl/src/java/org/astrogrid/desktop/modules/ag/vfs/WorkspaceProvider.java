d9b3ae2637a5472a49305f1a3424e18b29e616fb
Guy Rixon <gtr@ast.cam.ac.uk>
2008-07-17 12:58:05 +0000

    New-style account names are converted to old-style home-space IVORNs to support the old MySpace client.

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ag/vfs/WorkspaceProvider.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ag/vfs/WorkspaceProvider.java
@@ -49,20 +49,48 @@ public class WorkspaceProvider extends AbstractFileProvider implements VfsFilePr
 	// at the moment, requests a login, and then rewrites to the users myspace home.
 	public FileObject findFile(FileObject baseFile, String uri,
 			FileSystemOptions fileSystemOptions) throws FileSystemException  {
-		UserInformation user = comm.getUserInformation();
-		String homespace = user.getId().toString();
-		try {
-			if (uri.equals("workspace:") || uri.equals("workspace://")) {
-				// uri parser chokes on these forms. if there's no path following
-				uri = "workspace:/";
-			}
-		URI u = new URI(uri);
-		String target = homespace + u.getSchemeSpecificPart();
-		//@todo add in alternate machinery for resolving to vospace where available.
-		return getContext().getFileSystemManager().resolveFile(target);
-		} catch (URISyntaxException e) {
-			throw new FileSystemException(e);
-		}
+          String homespace = getMySpaceHome(comm.getUserInformation());
+	  try {
+	    if (uri.equals("workspace:") || 
+                uri.equals("workspace://") ||
+                uri.equals("workspace:///")) {
+	      // uri parser chokes on these forms.
+	      uri = "workspace:/";
+	    }
+	    URI u = new URI(uri);
+            System.out.println("findFile(): " + uri);
+	    String target = homespace + u.getSchemeSpecificPart();
+	    //@todo add in alternate machinery for resolving to vospace where available.
+	    return getContext().getFileSystemManager().resolveFile(target);
+	  } catch (URISyntaxException e) {
+	    throw new FileSystemException(e);
+	  }
 	}
+        
+        /**
+         * Determines the homespace IVORN in MySpace. from a qualified user-name.
+         * The IVORN is in the old style, i.e. ivo://authority/user# where the
+         * authority part is taken from the IVORN for the community where the
+         * user is registered.
+         *
+         * @param user All the user information provided by the community.
+         * @return The IVORN for the homespace in string form.
+         * @throws FileSystemException If the community information is insufficient.
+         */
+        private String getMySpaceHome(UserInformation user) throws FileSystemException {
+          
+          // Extract the IVOA authority of the community in which 
+          // the user is registered.
+          String c1 = user.getCommunity();
+          if (c1 == null || !c1.startsWith("ivo://")) {
+            throw new FileSystemException("Community name " + c1 + " is invalid");
+          }
+          String c2 = c1.substring(6); // Strip ivo://
+          int slash = c2.indexOf('/');
+          String authority = (slash == -1)? c2 : c2.substring(0,slash);
+          
+          // Form the old-style IVORN for the home space.
+          return "ivo://" + authority + '/' + user.getName() + '#';
+        }
 
 }
