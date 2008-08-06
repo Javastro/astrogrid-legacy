4463b5f4716e55279fc3a71d3f66c12e6a61ec14
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2008-08-06 11:32:15 +0000

    fixed NPE

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/scope/ScopeServicesList.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/ui/scope/ScopeServicesList.java
@@ -3,8 +3,6 @@
  */
 package org.astrogrid.desktop.modules.ui.scope;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -12,23 +10,17 @@ import java.util.List;
 
 import javax.swing.table.TableColumn;
 
-import net.sf.ehcache.Ehcache;
-
-import org.apache.commons.collections.BidiMap;
-import org.apache.commons.collections.bidimap.DualHashBidiMap;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.astrogrid.acr.ivoa.resource.Resource;
 import org.astrogrid.acr.ivoa.resource.Service;
-import org.astrogrid.desktop.hivemind.IterableObjectBuilder;
 import org.astrogrid.desktop.modules.ivoa.RegistryInternal;
 import org.astrogrid.desktop.modules.system.CSH;
 import org.astrogrid.desktop.modules.system.pref.Preference;
 import org.astrogrid.desktop.modules.system.ui.ActivitiesManager;
 import org.astrogrid.desktop.modules.ui.AstroScopeLauncherImpl;
 import org.astrogrid.desktop.modules.ui.TypesafeObjectBuilder;
-import org.astrogrid.desktop.modules.ui.UIComponent;
 import org.astrogrid.desktop.modules.ui.comp.ExceptionFormatter;
 import org.astrogrid.desktop.modules.ui.comp.ModularColumn;
 import org.astrogrid.desktop.modules.ui.scope.QueryResults.QueryResult;
@@ -40,7 +32,6 @@ import org.astrogrid.desktop.modules.votech.AnnotationService;
 import org.astrogrid.desktop.modules.votech.VoMonInternal;
 
 import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.GlazedLists;
 
 /** specialized subtype of registrygooglepanel that displays summary
  * of queries retrievers.
@@ -61,9 +52,9 @@ public class ScopeServicesList extends RegistryGooglePanel
 	 * @param pref 
 	 * @param pref
 	 */
-	public ScopeServicesList(AstroScopeLauncherImpl parent,RegistryInternal reg
-	        ,TypesafeObjectBuilder uiBuilder, ActivitiesManager acts
-	        ,VoMonInternal vomon, final CapabilityIconFactory iconFac,AnnotationService annServer, Preference pref) {
+	public ScopeServicesList(final AstroScopeLauncherImpl parent,final RegistryInternal reg
+	        ,final TypesafeObjectBuilder uiBuilder, final ActivitiesManager acts
+	        ,final VoMonInternal vomon, final CapabilityIconFactory iconFac,final AnnotationService annServer, final Preference pref) {
 		super(parent,reg, uiBuilder
 		        ,createServicesListViews(parent,uiBuilder,acts)
 		        ,vomon, iconFac,annServer,pref);
@@ -72,7 +63,7 @@ public class ScopeServicesList extends RegistryGooglePanel
 		CSH.setHelpIDString(this,"scope.viz.services");
 	} 
 
-	protected static  ResourceViewer[] createServicesListViews(AstroScopeLauncherImpl parent, TypesafeObjectBuilder uiBuilder, ActivitiesManager acts) {
+	protected static  ResourceViewer[] createServicesListViews(final AstroScopeLauncherImpl parent, final TypesafeObjectBuilder uiBuilder, final ActivitiesManager acts) {
 	    return (ResourceViewer[])ArrayUtils.addAll(
                 new ResourceViewer[]{
                         uiBuilder.createResultsResourceView(parent,acts)
@@ -87,30 +78,32 @@ public class ScopeServicesList extends RegistryGooglePanel
  */
 	private final QueryResults queryResults = new QueryResults();
 	/** overridden - to show additional columns */
-	protected ResourceTableFomat createTableFormat() {
+	@Override
+    protected ResourceTableFomat createTableFormat() {
 	    return new ServicesListTableFormat(annServer,vomon,iconFac);
 	}
 
 	// query summarizer interface
-	public void clear() {
+	@Override
+    public void clear() {
 		super.clear();
 		queryResults.clear();
 
 	}
 	/** add a number of services to the tabular display */
-	public void addAll(Retriever[] retrievers) {
-	    VizModel model = astroscope.getVizModel(); // can only access this now, not in the construct, as it's not yet been initialized at this point.
+	public void addAll(final Retriever[] retrievers) {
+	    final VizModel model = astroscope.getVizModel(); // can only access this now, not in the construct, as it's not yet been initialized at this point.
 	    for (int i = 0; i < retrievers.length; i++) {
-            Retriever abstractRetriever = retrievers[i];
+            final Retriever abstractRetriever = retrievers[i];
 	        // eagerly create this, and hang onto it - else it tends to get GC'd and we
 	        // lost the results tree
 	        FileObject fo = null;
 	        try {
 	            fo = model.createResultsDirectory(abstractRetriever);
-	        } catch (FileSystemException e) {
+	        } catch (final FileSystemException e) {
 	            logger.warn("Inable to create results directory for " + abstractRetriever.getLabel());
 	        }
-	        QueryResult qr = new QueryResult(abstractRetriever,fo);
+	        final QueryResult qr = new QueryResult(abstractRetriever,fo);
 	        queryResults.addResult(qr);
 	    }
 	    items.getReadWriteLock().writeLock().lock();
@@ -123,30 +116,37 @@ public class ScopeServicesList extends RegistryGooglePanel
 	    }        
 	}
 
-	public void addQueryFailure(Retriever ri, Throwable t) {
-	    String message= ExceptionFormatter.formatException(t);
-	    if (astroscope.isTransientWarnings()) {
-	        parent.showTransientWarning("Unable to query " + ri.getLabel()
-	            ,message.length() < 200 ? message : "See services view for details");
+	public void addQueryFailure(final Retriever ri, final Throwable t) {
+	    final QueryResult result = queryResults.getResult(ri); // there may be no result if it's a hanger-on from a previous search.
+
+	    if (result != null) {
+	        final String message= ExceptionFormatter.formatException(t);
+	        if (astroscope.isTransientWarnings()) {
+	            parent.showTransientWarning("Unable to query " + ri.getLabel()
+	                    ,message.length() < 200 ? message : "See services view for details");
+	        }
+
+	        result.error = message;
+	        notifyServiceUpdated(ri);
 	    }
-	    queryResults.getResult(ri).error = message;
-	    notifyServiceUpdated(ri);
 	}
 
-    public void addQueryResult(Retriever ri, AstroscopeTableHandler handler) {
-        final QueryResult qr = queryResults.getResult(ri);
-        qr.count = new Integer(handler.getResultCount());
-        queryResults.associateNode(qr,handler.getServiceNode());
-        notifyServiceUpdated(ri);
+    public void addQueryResult(final Retriever ri, final AstroscopeTableHandler handler) {
+        final QueryResult qr = queryResults.getResult(ri);// there may be no result if it's a hanger-on from a previous search.
+        if (qr != null) {
+            qr.count = new Integer(handler.getResultCount());
+            queryResults.associateNode(qr,handler.getServiceNode());
+            notifyServiceUpdated(ri);
+        }
     }
     
-    private void notifyServiceUpdated(Retriever abstractRetriever) {        
+    private void notifyServiceUpdated(final Retriever abstractRetriever) {        
         // Creating a new RetrieverService here will not give an object which
         // is actually in the table alredy, but it will have the correct
         // equivalence relations (equals()/hashCode()).
-        Service ri = new RetrieverService(abstractRetriever);
+        final Service ri = new RetrieverService(abstractRetriever);
 
-        int ix = items.indexOf(ri);
+        final int ix = items.indexOf(ri);
         if (ix != -1) {
           items.set(ix,ri); // updates the table
         }
@@ -167,13 +167,13 @@ public class ScopeServicesList extends RegistryGooglePanel
 	/** extends the resource table format with two additional columns */
 	private class ServicesListTableFormat extends ResourceTableFomat{
 
-        public ServicesListTableFormat(AnnotationService annService,
-                VoMonInternal vomon, CapabilityIconFactory capBuilder) {
+        public ServicesListTableFormat(final AnnotationService annService,
+                final VoMonInternal vomon, final CapabilityIconFactory capBuilder) {
             super(annService, vomon, capBuilder);
-            List colList = new ArrayList(Arrays.asList(getColumns()));
+            final List colList = new ArrayList(Arrays.asList(getColumns()));
             colList.add(new Column(RESULTS_NAME,Integer.class,new Comparator() {
                 // has to handle integers, and the string 'failed'
-                public int compare(Object arg0, Object arg1) {
+                public int compare(final Object arg0, final Object arg1) {
                     if (arg0 instanceof String) {
                         if (arg1 instanceof String) {
                             return ((String)arg0).compareTo((String)arg1);
@@ -187,17 +187,20 @@ public class ScopeServicesList extends RegistryGooglePanel
                     }
                 }
             }){
-                public Object getColumnValue(Object baseObj) {
-                    Retriever r = ((RetrieverService)baseObj).getRetriever();
+                @Override
+                public Object getColumnValue(final Object baseObj) {
+                    final Retriever r = ((RetrieverService)baseObj).getRetriever();
                     return queryResults.getResult(r).getFormattedResultCount();
                 }
-                public void configureColumn(TableColumn tcol) {
+                @Override
+                public void configureColumn(final TableColumn tcol) {
                     tcol.setPreferredWidth(60);
                 }
             });
             colList.add(new StringColumn(SUBNAME_NAME) {
-                protected String getValue(Resource res) {
-                    Retriever r = ((RetrieverService)res).getRetriever();
+                @Override
+                protected String getValue(final Resource res) {
+                    final Retriever r = ((RetrieverService)res).getRetriever();
                     return r.getSubName();
                 }
             });
@@ -209,7 +212,8 @@ public class ScopeServicesList extends RegistryGooglePanel
         private final static String SUBNAME_NAME = "SubName";
 
 	    
-	    public String[] getDefaultColumns() {
+	    @Override
+        public String[] getDefaultColumns() {
 	        return new String[] {
 	                STATUS_NAME,RESULTS_NAME,LABEL_NAME,SUBNAME_NAME,CAPABILITY_NAME,
 	        };
