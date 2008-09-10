e776f68f0bc175bd62bcc263d8659ec3bf80829b
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2008-09-10 10:42:54 +0000

    Swift image search. Second rough draft.

--- astrogrid/swift/src/python/swift_image_search.py
+++ astrogrid/swift/src/python/swift_image_search.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-import string, sys
+import string, sys, os, time, random
+from astrogrid import acr
+from astrogrid import SiapSearch
+from astrogrid import MySpace
 
 #
-#
+# Global arguments passed in...
 #
 ra = 0
 dec = 0
@@ -58,15 +61,92 @@ def processIvornFile( filePath ):
 					sources.append( ivorns )
 					ivorns = []
 			else:
-				ivorns.append( l )
+				# We make the ivorn itself a first item in a list.
+				# Subsequently we will add the service and its thread of execution.
+				ivorns.append( [l] )
 	if len( ivorns ) > 0:
 		sources.append( ivorns )
 	return sources
 
-#
-# Mainline...
-#
+##################################################
+# Mainline...                                    #
+##################################################
+print 'Started at: ' + time.strftime('%T') 
+
+# Validate the input arguments passed to us...
 validateArgs()
 print ra, dec, radius, ifile, odir
+
+# Retrieve ivorns for the sources we wish to search...
 sources = processIvornFile( ifile )
-print sources
+
+# Log in to Astrogrid runtime...
+acr.login() 				
+print 'Logged in at ' + time.strftime('%T') 
+
+# Create the output directory locally and in VOSpace...
+m = MySpace()
+print( 'Output folder is called ' + odir )
+os.mkdir( odir )
+print( 'making myspace dir at ' + time.strftime('%T') )
+m.mkdir( '#' + odir )
+print( 'myspace dir made by ' + time.strftime('%T') )
+
+#siap = SiapSearch( 'ivo://archive.eso.org/dss' )
+#print siap.info['content']['description']
+
+# Trigger off the image searches on separate threads...
+# We have a list sources...
+sIndex = 0
+for s in sources:
+	# We have a list of ivorns for each source...
+	iIndex = 0
+	for i in s:
+#		print 'ivorn item: ####################################################################### '
+		print i[0] 
+		# Generate a service for each ivorn...
+		siap = SiapSearch( i[0] ) 
+		i.append( siap )
+		imageDir = '#' + odir + '/' + str(sIndex) + '_' + str(iIndex)
+		m.mkdir( imageDir )
+		res, thread = siap.execute( ra, dec, radius, saveDatasets = imageDir )
+		i.append( thread )
+#		print siap.info['content']['description']
+#		print time.strftime('%T')
+		iIndex = iIndex+1
+	sIndex = sIndex+1
+
+count = 0
+while count < 120 :
+	sourcesFinished = 0
+	for s in sources:
+		for i in s:
+      # If one thread in a source is finished, that's good enough...
+			if i[2].isAlive() == False:
+				sourcesFinished = sourcesFinished+1 
+				break
+	if sourcesFinished == len( sources ):
+		break 
+	print( 'Sleeping for 1 second' )
+	time.sleep(1)
+	count = count+1
+
+print( 'downloading results at ' + time.strftime('%T') )
+
+sIndex = 0
+for s in sources:
+	iIndex = 0
+	for i in s:
+		if i[2].isAlive() == False:
+			d = odir + '/' + str(sIndex) + '_' + str(iIndex)
+			list = m.ls( directory= '#' + d ) ;
+			os.mkdir( d )
+			for x in list:
+				print( 'Downloading ' + x[2] )
+				m.readfile( '#' + d + '/' + x[2], ofile= d  + '/' + x[2] )
+				print( 'Down finished at ' + time.strftime('%T') )
+			break
+		iIndex = iIndex+1
+	sIndex = sIndex+1
+
+print( 'Finished at '  + time.strftime('%T') ) 
