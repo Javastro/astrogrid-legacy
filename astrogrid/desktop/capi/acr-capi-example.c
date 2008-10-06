58d4b71776e45778616507cd6268c142e4c79e65
Paul Harrison <paul.harrison@manchester.ac.uk>
2008-10-06 14:00:05 +0000

    mior reformatting

--- astrogrid/desktop/capi/acr-capi-example.c
+++ astrogrid/desktop/capi/acr-capi-example.c
@@ -1,7 +1,7 @@
 /*
  * Example of calling the ACR from C.
  * @author Paul Harrison
- */ 
+ */
 
 #include <stdio.h>
 #include "arintf.h"
@@ -10,42 +10,42 @@
 
 int main(int argc, char* argv[]) {
     init_ar();
- 
+
     /* some examples.. */
     struct SesamePositionBean  result = cds_sesame_resolve("Crab");
-    
+
     double ra = result.ra;
     double dec = result.dec;
     double size = 0.1;
- 
+
     /* doing a cone search */
-    char * ivorn = "ivo://wfau.roe.ac.uk/twomass-dsa/wsa/twomass_psc/cone";
+    char * ivorn = "ivo://wfau.roe.ac.uk/twomass-dsa/wsa";
 //    ivorn ="ivo://irsa.ipac/2MASS-PSC";
     struct Resource_Base registry;
-    /* TODO what about if the resource structure is some subclass - unions..  */ 
+    /* TODO what about if the resource structure is some subclass - unions..  */
      registry = ivoa_registry_getResource(ivorn);
-    
+
     int type = registry._type;
     type++;
-    
+
     char * desc = registry.d.resource.content.description;
-    
-   
+
+
     JString query = ivoa_cone_constructQuery(ivorn, ra, dec, size);
     char * votable = ivoa_cone_executeVotable(query);
 
-   
+
     ivorn = "ivo://org.astrogrid/SExtractor";
     registry = ivoa_registry_getResource(ivorn);
-    printResource_Base(registry);    
-    
+    printResource_Base(registry);
+
     ListOfService_Base servers = astrogrid_applications_listServersProviding(ivorn);
     int nservices = servers.n;
     struct Service ceaserver = servers.list[0].d.service;
     printService(ceaserver);
     ivorn = "ivo://irsa.ipac/2MASS-ASKYW-AT";
     query = ivoa_siap_constructQuery(ivorn, ra, dec, size);
-    ListOfACRKeyValueMap siapresult = ivoa_siap_execute ( query) ; 
+    ListOfACRKeyValueMap siapresult = ivoa_siap_execute ( query) ;
     printf ("%d siap results\n first result\n", siapresult.n);
     int i;
     ACRKeyValueMap res = siapresult.list[0];
@@ -53,6 +53,6 @@ int main(int argc, char* argv[]) {
     for ( i = 0; i < res.n; i++) {
 	    printf("key=%s  val=%s \n",res.map[i].key, res.map[i].val);
 	}
-    
-    
+
+
 }
