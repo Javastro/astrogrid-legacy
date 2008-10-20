7adc15a97139e55ae36e5bcbe332be85dd3c1b6f
Paul Harrison <paul.harrison@manchester.ac.uk>
2008-10-20 10:36:06 +0000

    uws integration test

---
+++ astrogrid/applications/commandline/src/integration/org/astrogrid/applications/uws/UWSIntegrationTest.java
@@ -0,0 +1,43 @@
+/*
+ * $Id: UWSIntegrationTest.java,v 1.1 2008/10/20 10:36:06 pah Exp $
+ * 
+ * Created on 19 Sep 2008 by Paul Harrison (paul.harrison@manchester.ac.uk)
+ * Copyright 2008 Astrogrid. All rights reserved.
+ *
+ * This software is published under the terms of the Astrogrid 
+ * Software License, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+
+package org.astrogrid.applications.uws;
+
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ * IntegrationTest for UWS server.
+ * @author Paul Harrison (paul.harrison@manchester.ac.uk) 19 Sep 2008
+ * @version $Name:  $
+ * @since VOTech Stage 7
+ */
+public class UWSIntegrationTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
+
+
+/*
+ * $Log: UWSIntegrationTest.java,v $
+ * Revision 1.1  2008/10/20 10:36:06  pah
+ * uws integration test
+ *
+ */
