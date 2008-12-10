8bd148e2f27188b6f74230600ae48b160a16e480
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2008-12-10 16:06:01 +0000

    Complete - taskSession Manager - document

--- astrogrid/desktop/api/src/java/org/astrogrid/acr/builtin/SessionManager.java
+++ astrogrid/desktop/api/src/java/org/astrogrid/acr/builtin/SessionManager.java
@@ -9,20 +9,42 @@ import org.astrogrid.acr.Finder;
 import org.astrogrid.acr.InvalidArgumentException;
 import org.astrogrid.acr.NotApplicableException;
 import org.astrogrid.acr.SecurityException;
+import org.astrogrid.acr.system.Configuration;
 
-/** AR Service: manage user sessions.
+/** AR Service: Create and manage sessions.
+ * <p/>
  * 
- * There's one default user session - this is the one who's connection 
- * properties are described in {@code ~/.astrogrid-properties}  (for XMLRPC & HTTP) 
- * and to which RMI clients connect to automatically using the Finder.
+ * When using the Astro Runtime in a multi-user or server-side setting, it may be necessary to have more than one user 
+ * simultaneously interacting with the AR. Without some additional mechanism, all users would share the same authentication and program state
+ * - in particular, 2 users could not be simultaneously logged in as different identities.
+ * <p/>
+ * The {@code SessionManager} allows the programmer to create new <i>sessions</i>. Each user session contains the authentication
+ * and program state of a single user. More than one session may exist simultaneously within the AR, and the state within sessions
+ * are isolated from each other. By creating a new session for each user, multiple users with different identities can access a single
+ * AR. 
+ * </p>
+ * By default, all AR api calls are made within the <i>default session</i>.
+ * The  connection 
+ * properties for this session are described in {@code ~/.astrogrid-properties}  (for XMLRPC & HTTP) 
+ * and this is the session that RMI clients connect to automatically using the {@link Finder}.
+ * <p/>
+ * From the default session, a <i>new</i> user session can be created by calling {@link #createNewSession(long)}. A unique <i>session identifier</i>
+ * is returned. A session can be checked for availability using {@link #exists(String)} and disposed of using {@link #dispose(String)}.
+ * <p/>
+ * Once a session has been created, it must be <i>connected</i> to. The connection method depends on how the AR is being accessed:
+ * <dl>
+ * <dt>XMLRPC<dt>
+ * <dd>Pass the session identifier to {@link #findXmlRpcSession(String)}, which returns a URL that can then be connected to using an XMLRPC library</dd>
+ * <dt>HTTP</dt>
+ * <dd>Pass the session identifier to {@link #findHttpSession(String)}, which returns the base URL for performing HTTP function calls in this session</dd>
+ * <dt>RMI</dt>
+ * <dd>Pass the session identifier to {@link Finder#findSession(String)}, which returns an instance of {@link ACR} for this session</dd>
+ * </dl>
  * 
- * This SessionManager service allows a client connected to the default user session to 
- * create a new session - which will not share state such as authentication,
- * login information, etc with the default session. This is useful in a multi-user 
- * or server-side settings.
- * 
- * Once a session has been created, call the {@link #findHttpSession(String)}, {@link #findXmlRpcSession(String)}
- * or {@link Finder#findSession(String)} to access the information required to connect to that session.
+ * No matter how the session is accessed, it presents an unaltered AR API - it's just that
+ * user authentication and state is isolated from other sessions. Not all data is isolated
+ * - in particular cached information (such as registry entries), and configuration settings
+ * (accessed from {@link Configuration}) are shared between all sessions. 
  * 
  * {@stickyWarning This session manager does not make any strong guarantees about
  * security - the purpose of this feature is to enable multiple user identities within the same AR -
@@ -31,6 +53,28 @@ import org.astrogrid.acr.SecurityException;
  * and it may also be possible to determine how to connect to other sessions - hence
  * it is recommended that all clients connecting to the AR are mutually trusting.
  * }
+ * 
+ * {@example "Python XMLRPC Example"
+# connect to the AR (in the default session)
+from xmlrpc import Server
+from os.path import expanduser
+ar = Server(file(expanduser('~/.astrogrid-desktop')).next().strip() +'xmlrpc')
+sm = ar.builtin.sessionManager
+
+# create a new user session (with timeout of 10 mins)
+sessID = sm.createNewSession(10)
+
+#find xmlrpc endpoint for the new session
+sessURL = sm.findXmlRpcSession(sessID)
+
+#connect to the new session
+sessAR = Server(sessURL)
+
+#work within the neww session
+sessAR.astrogrid.community.login("username","password","community")
+...
+
+}
  * @service builtin.sessionManager
  * @author Noel.Winstanley@manchester.ac.uk
  */
@@ -38,7 +82,7 @@ public interface SessionManager {
 
 	/** create a new session.
 	 * 
-	 * @param inactivityTimeout number of minutes that this session must be inactive before it is disposed. 
+	 * @param inactivityTimeout number of minutes that this session can be inactive before it is disposed. 
 	 * @return a sessionId - an identifier for the newly created session.
 	 * @throws NotApplicableException if this AR does not support creating new sessions.
 	 * @throws SecurityException if the current user or client is not permitted to create new sessions.
@@ -51,11 +95,11 @@ public interface SessionManager {
 	/** dispose of a session.
 	 * 
 	 * @note Once disposed of, a session cannot be connected to.
-	 * @param sessionId a sessionId that currently exists.
+	 * @param sessionId a sessionId that currently exists. If this is the id of the default session, nothing happens.
 	 */
 	void dispose(String sessionId);
 
-	/** connect to an session using XML-RPC
+	/** Find the endpoint to connect to a session using XML-RPC
 	 * 
 	 * @param sessionId an existing sessionId
 	 * @return a full URL that an XML-RPC client should connect to to work with this session. No further manipulation
@@ -64,7 +108,7 @@ public interface SessionManager {
 	 */
 	URL findXmlRpcSession(String sessionId) throws InvalidArgumentException;
 	
-	/** connect to an session using Http
+	/** Find the endpoint to connect to a session using HTTP
 	 * 
 	 * @param sessionId an existing sessionId
 	 * @return a URL that should be used as  the <b>base</b> for HTTP invocations of the services in this session. 
