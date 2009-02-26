29ef670865d79a90d5785c0fbc38631d17085668
Paul Harrison <paul.harrison@manchester.ac.uk>
2009-02-26 11:17:41 +0000

    first attempt at a stress test

---
+++ astrogrid/applications/client/src/integration/org/astrogrid/applications/test/ExerciseUWS.java
@@ -0,0 +1,87 @@
+/*
+ * $Id: ExerciseUWS.java,v 1.1 2009/02/26 11:17:41 pah Exp $
+ * 
+ * Created on 23 Feb 2009 by Paul Harrison (paul.harrison@manchester.ac.uk)
+ * Copyright 2009 Astrogrid. All rights reserved.
+ *
+ * This software is published under the terms of the Astrogrid 
+ * Software License, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+
+package org.astrogrid.applications.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import net.ivoa.uws.JobSummary;
+
+import org.astrogrid.uws.client.UWS;
+import org.astrogrid.uws.client.UWSClient;
+import org.astrogrid.uws.client.UWSException;
+import org.astrogrid.uws.client.UWSJobCreationException;
+
+public class ExerciseUWS implements Runnable {
+
+    private URI uwsroot;
+    private UWS client;
+    private JobSummary job;
+    private static String tool;
+    
+    static {
+        StringBuffer toolbuf = new StringBuffer();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(ExerciseUWS.class.getClass().getResourceAsStream("/org/astrogrid/applications/test/testTool.xml")));
+        String line;
+        try {
+            while ((line= rd.readLine())!=null) {
+                toolbuf.append(line);
+            }
+            tool = toolbuf.toString();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+    public void run() {
+        try {
+            long time = System.currentTimeMillis();
+            job = client.createJob(tool);
+            long inittime = System.currentTimeMillis();
+            client.runJob(job.getJobId());
+            long runtime = System.currentTimeMillis();
+            
+            System.out.println("jobid:"+job.getJobId()+
+                    ":createtime: "+(inittime-time)+" : create instant: "+ (time) + ":runtime: "+(runtime - inittime));
+
+        } catch (UWSJobCreationException e) {
+            e.printStackTrace();
+        } catch (UWSException e) {
+            e.printStackTrace();
+        }
+    }
+    public ExerciseUWS()  {
+        try {
+            uwsroot = new URI("http://localhost:8888/astrogrid-cea-cec/uws/jobs");
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        client = new UWSClient(uwsroot);
+ 
+    }
+    
+}
+
+
+/*
+ * $Log: ExerciseUWS.java,v $
+ * Revision 1.1  2009/02/26 11:17:41  pah
+ * first attempt at a stress test
+ *
+ */
---
+++ astrogrid/applications/client/src/integration/org/astrogrid/applications/test/StressTest.java
@@ -0,0 +1,51 @@
+/*
+ * $Id: StressTest.java,v 1.1 2009/02/26 11:17:41 pah Exp $
+ * 
+ * Created on 20 Feb 2009 by Paul Harrison (paul.harrison@manchester.ac.uk)
+ * Copyright 2009 Astrogrid. All rights reserved.
+ *
+ * This software is published under the terms of the Astrogrid 
+ * Software License, a copy of which has been included 
+ * with this distribution in the LICENSE.txt file.  
+ *
+ */ 
+
+package org.astrogrid.applications.test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class StressTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+ 
+        int corepool = 100;
+        int maxpool = 100;
+        long keepalive = Long.MAX_VALUE ;
+        TimeUnit unit = TimeUnit.SECONDS;
+        BlockingQueue<Runnable> workqueue = new SynchronousQueue<Runnable>(true);
+        ExecutorService executor =  new ThreadPoolExecutor(corepool, maxpool,keepalive, unit, workqueue);//Executors.newCachedThreadPool();
+        
+        for (int i = 0; i < 100; i++) {
+            ExerciseUWS client = new ExerciseUWS();
+            executor.execute(client);
+        }
+        executor.shutdown();
+    }
+
+}
+
+
+/*
+ * $Log: StressTest.java,v $
+ * Revision 1.1  2009/02/26 11:17:41  pah
+ * first attempt at a stress test
+ *
+ */
