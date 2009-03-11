08760f373574c2886151c75f3cc74c65a9174e2e
Guy Rixon <gtr@ast.cam.ac.uk>
2009-03-11 12:06:15 +0000

    I extended it to run absolute-location.xsl. As a result, it now transcribes the schemata from source tree to site-publication tree.

--- astrogrid/contracts/src/java/org/astrogrid/contracts/SchemaIndexer.java
+++ astrogrid/contracts/src/java/org/astrogrid/contracts/SchemaIndexer.java
@@ -5,12 +5,8 @@ package org.astrogrid.contracts;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.FilenameFilter;
-import java.io.InputStream;
 import java.io.PrintWriter;
-import java.io.Writer;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
@@ -28,52 +24,53 @@ import javax.xml.transform.stream.StreamSource;
 public class SchemaIndexer {
 
   public static void main(String[] args) throws Exception {
-    File prefix = new File("src/schema");
-    SchemaIndexer i = new SchemaIndexer(new File("target/site/schema/index.html"));
+    File target = new File("target/site/schema");
+
+    SchemaIndexer i = new SchemaIndexer(target);
     i.beginDocument();
 
     i.beginSection("Common Execution Architecture and UWS");
-    i.searchTree(prefix, new File(prefix, "cea"));
-    i.endSection();
-
-    i.beginSection("VOSpace");
-    i.searchTree(prefix, new File(prefix, "vo-space"));
-    i.endSection();
-
-    i.beginSection("Resource-registry entries");
-    i.searchTree(prefix, new File(prefix, "vo-resource-types"));
-    i.endSection();
-
-    i.beginSection("Data formats");
-    i.searchTree(prefix, new File(prefix, "vo-formats"));
-    i.endSection();
-
-    i.beginSection("Query language");
-    i.searchTree(prefix, new File(prefix, "adql"));
+    i.searchTree(new File("src/schema/cea"));
     i.endSection();
 
     i.beginSection("Coordinate systems");
-    i.searchTree(prefix, new File(prefix, "stc"));
+    i.searchTree(new File("src/schema/stc"));
+    i.endSection();
+
+    i.beginSection("Data formats");
+    i.searchTree(new File("src/schema/vo-formats"));
     i.endSection();
 
     i.beginSection("DSA metadata");
-    i.searchTree(prefix, new File(prefix, "dsa"));
-    i.endSection();
-
-    i.beginSection("Registry service");
-    i.searchTree(prefix, new File(prefix, "registry"));
+    i.searchTree(new File("src/schema/dsa"));
     i.endSection();
 
     i.beginSection("JES");
-    i.searchTree(prefix, new File(prefix, "jes"));
+    i.searchTree(new File("src/schema/jes"));
     i.endSection();
 
     i.beginSection("OAI");
-    i.searchTree(prefix, new File(prefix, "oai"));
+    i.searchTree(new File("src/schema/oai"));
+    i.endSection();
+
+    i.beginSection("Registry service");
+    i.searchTree(new File("src/schema/registry"));
+    i.endSection();
+
+    i.beginSection("Resource-registry entries");
+    i.searchTree(new File("src/schema/vo-resource-types"));
+    i.endSection();
+
+    i.beginSection("Query language");
+    i.searchTree(new File("src/schema/adql"));
     i.endSection();
 
     i.beginSection("VOEvent");
-    i.searchTree(prefix, new File(prefix, "VOEvent"));
+    i.searchTree(new File("src/schema/VOEvent"));
+    i.endSection();
+
+    i.beginSection("VOSpace");
+    i.searchTree(new File("src/schema/vo-space"));
     i.endSection();
 
     i.endDocument();
@@ -81,9 +78,10 @@ public class SchemaIndexer {
 
   private PrintWriter out;
 
-  public SchemaIndexer(File f) throws Exception {
-    f.createNewFile();
-    out = new PrintWriter(f, "UTF-8");
+  public SchemaIndexer(File targetTree) throws Exception {
+    targetTree.mkdirs();
+    File index = new File(targetTree, "index.html");
+    out = new PrintWriter(index, "UTF-8");
   }
 
   public void beginDocument() throws Exception {
@@ -117,15 +115,28 @@ public class SchemaIndexer {
     out.println();
   }
 
+  /**
+   * Searches a directory tree, "breadth first". First, lists all .xsd and
+   * .wsdl files. Second, calls itself recursively on each sub-directory.
+   *
+   * @param startingDirectory The directry to search.
+   */
+  public void searchTree(File startingDirectory) throws Exception {
+    searchTree(null, startingDirectory);
+  }
 
   /**
    * Searches a directory tree, "breadth first". First, lists all .xsd and
    * .wsdl files. Second, calls itself recursively on each sub-directory.
    *
-   * @param prefix A prefix to be excluded from paths passed to XSLT.
+   * @param prefix A name prefix used when extracting namespaces.
    * @startingDirectory The directry to search.
    */
   public void searchTree(File prefix, File startingDirectory) throws Exception {
+    prefix = (prefix == null)? new File(startingDirectory.getName()) :
+                               new File(prefix, startingDirectory.getName());
+    System.out.print(prefix);
+    System.out.print(" => ");
     System.out.println(startingDirectory);
 
     File[] schemata = startingDirectory.listFiles(
@@ -138,7 +149,9 @@ public class SchemaIndexer {
 
     for (int i = 0; i < schemata.length; i++) {
       System.out.println(schemata[i].getAbsolutePath());
-      parseSchema(prefix, schemata[i]);
+      extractNamespace(prefix, schemata[i]);
+      transcribeSchema(prefix, schemata[i]);
+
     }
 
     File[] directories = startingDirectory.listFiles(
@@ -157,22 +170,37 @@ public class SchemaIndexer {
   /**
    * Parses a schema file (XSD or WSDL), extracting the target namespace.
    *
-   * @param prefix A prefix to be excluded from paths passed to XSLT.
+   * @param prefix A prefix to added to the links in the output.
    * @param schemaFile The file to transform.
    */
-  public void parseSchema(File prefix, File schemaFile) throws Exception {
+  public void extractNamespace(File prefix, File schemaFile) throws Exception {
     StreamSource schema = new StreamSource(schemaFile);
     StreamSource transform = new StreamSource(
       new FileInputStream("src/xsl/extract-namespace.xsl")
     );
     Transformer t = TransformerFactory.newInstance().newTransformer(transform);
     StreamResult result = new StreamResult(out);
-    int prefixLength = prefix.getAbsolutePath().length() + 1; // +1 => include a slash
-    String relativePath = schemaFile.getAbsolutePath().substring(prefixLength);
-    t.setParameter("filename", relativePath);
+    String href = prefix.getName() + "/" + schemaFile.getName();
+    t.setParameter("filename", href);
     t.transform(schema, result);
+  }
 
-
+  /**
+   * Copies the source schemata to the site-publishing tree, transforming
+   * them in transit to fix the location and schemalocation attributes.
+   */
+  public void transcribeSchema(File prefix, File schemaFile) throws Exception {
+    StreamSource schema = new StreamSource(schemaFile);
+    StreamSource transform = new StreamSource(
+      new FileInputStream("src/xsl/absolute-location.xsl")
+    );
+    Transformer t = TransformerFactory.newInstance().newTransformer(transform);
+    File outDir = new File("target/site/schema", prefix.toString());
+    outDir.mkdirs();
+    File outFile = new File(outDir, schemaFile.getName());
+    System.out.println("Transforming to " + outFile.toString());
+    StreamResult result = new StreamResult(outFile);
+    t.transform(schema, result);
   }
 
 }
