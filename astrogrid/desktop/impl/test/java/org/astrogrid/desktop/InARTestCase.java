9f92adffb058806daac3ca8f36fa1d10837bf109
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2009-03-24 13:22:50 +0000

    added some generic niceness.

--- astrogrid/desktop/impl/test/java/org/astrogrid/desktop/InARTestCase.java
+++ astrogrid/desktop/impl/test/java/org/astrogrid/desktop/InARTestCase.java
@@ -27,13 +27,13 @@ public class InARTestCase extends TestCase {
 		super.tearDown();
 	}
     protected ACR getACR() throws Exception {
-        ACRInternal acr = ARTestSetup.fixture.getACR();
+        final ACRInternal acr = ARTestSetup.fixture.getACR();
         assertNotNull(acr);
         return acr;
     }	
     
     protected Registry getHivemindRegistry() throws Exception {
-    	Registry hivemindRegistry2 = ARTestSetup.fixture.getHivemindRegistry();
+    	final Registry hivemindRegistry2 = ARTestSetup.fixture.getHivemindRegistry();
     	assertNotNull(hivemindRegistry2);
     	return hivemindRegistry2;
     }
@@ -44,20 +44,20 @@ public class InARTestCase extends TestCase {
      */
     protected String getUsername() {
     	assertTrue("No ag.test.username defined", System.getProperties().containsKey(ARTestSetup.AG_TEST_USERNAME));
-    	String s =  System.getProperty(ARTestSetup.AG_TEST_USERNAME,null);
+    	final String s =  System.getProperty(ARTestSetup.AG_TEST_USERNAME,null);
     	return s;
     }
     
     // access the test user's community.
     protected String getCommunity() {
     	assertTrue("No ag.test.community defined",System.getProperties().containsKey(ARTestSetup.AG_TEST_COMMUNITY));
-    	String s =  System.getProperty(ARTestSetup.AG_TEST_COMMUNITY);
+    	final String s =  System.getProperty(ARTestSetup.AG_TEST_COMMUNITY);
     	return s;
     }
 
     protected String getPassord() {
     	assertTrue("No ag.test.password defined",System.getProperties().containsKey(ARTestSetup.AG_TEST_PASSWORD));
-    	String s =  System.getProperty(ARTestSetup.AG_TEST_PASSWORD);
+    	final String s =  System.getProperty(ARTestSetup.AG_TEST_PASSWORD);
     	return s;
     }
 
@@ -71,26 +71,26 @@ public class InARTestCase extends TestCase {
 	 * @throws InvalidArgumentException
 	 * @throws NotFoundException
 	 */
-	public final   Object assertServiceExists(final Class clazz, final String name) throws Exception {
-		ACR reg = getACR();
-		Object r =  reg.getService(clazz);
+	public final   <T> T assertServiceExists(final Class<T> clazz, final String name) throws Exception {
+		final ACR reg = getACR();
+		final Object r =  reg.getService(clazz);
 		assertNotNull(r);
 		assertTrue(clazz.isInstance(r));
-		Object r1 =  reg.getService(name);
+		final Object r1 =  reg.getService(name);
 		assertNotNull(r1);
 		//assertSame(r,r1); won't be the same for the case of RMI. check the same interfaces are supported on both objects instead.
 		assertEquals(Arrays.asList(r.getClass().getInterfaces())
 		        ,Arrays.asList(r1.getClass().getInterfaces())
 		        );
-		return r;
+		return (T) r;
 	}
 	
 	/** test a specific underlying component exists */
-	public final  Object assertComponentExists(final Class clazz, final String name) throws Exception {
-		Registry hivemindRegistry = getHivemindRegistry();
-		Object r =  hivemindRegistry.getService(name, clazz);
+	public final <T>  T assertComponentExists(final Class<T> clazz, final String name) throws Exception {
+		final Registry hivemindRegistry = getHivemindRegistry();
+		final Object r =  hivemindRegistry.getService(name, clazz);
 		assertNotNull(r);
 		assertTrue(clazz.isInstance(r));
-		return r;
+		return (T) r;
 	}	
 }
