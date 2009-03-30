254cafa8b5e0d14187ca37d852a6949b6c39308a
Noel Winstanley <Noel.Winstanley@astrogrid.org>
2009-03-30 15:06:41 +0000

    Complete - taskimplement samp http://www.ivoa.net/cgi-bin/twiki/bin/view/IVOA/SampMTypes

--- astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/system/messaging/BiMessagingImpl.java
+++ astrogrid/desktop/impl/src/java/org/astrogrid/desktop/modules/system/messaging/BiMessagingImpl.java
@@ -6,21 +6,32 @@ package org.astrogrid.desktop.modules.system.messaging;
 import javax.swing.JMenu;
 import javax.swing.JSeparator;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.astrogrid.desktop.modules.system.SchedulerInternal.DelayedContinuation;
+import org.astrogrid.desktop.modules.system.pref.Preference;
+import org.astrogrid.samp.client.HubConnector;
+import org.astrogrid.samp.client.SampException;
+
+import uk.ac.starlink.plastic.PlasticUtils;
 
 /** Implementation of messaging that combines both plastic and samp.
  * @author Noel.Winstanley@manchester.ac.uk
  * @since Mar 25, 200910:29:07 AM
  */
 public class BiMessagingImpl extends PlasticOnlyMessagingImpl {
-
+    private static final Log logger = LogFactory.getLog(BiMessagingImpl.class);
     private final Samp samp;
+    private final Preference startSamp;
+    private final Preference startPlastic;
 
     /**
      * @param tupp
      */
-    public BiMessagingImpl(final Samp samp, final TupperwareInternal tupp) {
+    public BiMessagingImpl(final Preference startSamp, final Preference startPlastic,final Samp samp, final TupperwareInternal tupp) {
         super(tupp);
+        this.startSamp = startSamp;
+        this.startPlastic = startPlastic;
         this.samp = samp; 
         
     }
@@ -38,12 +49,27 @@ public class BiMessagingImpl extends PlasticOnlyMessagingImpl {
     
     @Override
     public DelayedContinuation execute() {
-        //@todo dirty hack - dunno if this is correct.
-        // think where to place this.
-        // should be - if not already connected? connect, if fail, start intrnal, connect again.
-   //     samp.connectAction().actionPerformed(new ActionEvent(this,0,(String)samp.connectAction().getValue(Action.ACTION_COMMAND_KEY))); // can't pass null in here.
-
-        return super.execute();
+        // try to connect.
+        final HubConnector connector = samp.getConnector();
+        try {
+            connector.getConnection();
+        } catch (final SampException x) {
+            logger.warn("SampException when attempting to connect to hub",x);
+        }
+        // set it to auto-connecto from now on.
+        connector.setAutoconnect(10);
+        if (!connector.isConnected() && startSamp.asBoolean()) {
+            samp.startInternalHubAction().actionPerformed(null);
+            // will auto-connect in a mo.
+        }
+        
+        if (startPlastic.asBoolean()&& ! PlasticUtils.isHubRunning()) {
+            // this action starts internal hub _and_ connects to it.
+                tupperware.startInternalHubAction().actionPerformed(null);            
+        } else if (PlasticUtils.isHubRunning()) {
+            tupperware.connectAction().actionPerformed(null);            
+        }
+        return null;
     }
 
 }
