767befb8919f2bea2b72e435f59fa764e16cad24
Paul Harrison <paul.harrison@manchester.ac.uk>
2009-06-20 14:21:25 +0000

    doc

---
+++ astrogrid/applications/client/src/java/org/astrogrid/uws/client/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes to form the basis of a generalised Universal Worker Service (UWS) client {@link http://www.ivoa.net/cgi-bin/twiki/bin/view/IVOA/AsynchronousHome}
+ */
+package org.astrogrid.uws.client;
