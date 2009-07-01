96410eb2383dddf46e276943e09d40c889b474ac
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2009-07-01 11:02:29 +0000

    Added download menus. Updated README in zip bundle

--- astrogrid/adql2/zipdoc/adqli.sh
+++ astrogrid/adql2/zipdoc/adqli.sh
@@ -3,16 +3,18 @@
 # Executes an interactive command line conversation with the ADQL base parser.
 #
 # Copy required jars to a suitable directory and place its path here...
-ADQL=/home/YOUR_HOME_HERE/adql-2008-1-1
+ADQL=/home/YOUR_HOME_HERE/adql-2009-1
 
 CLASSPATH2=.
-CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlparser-utils-2008.1.1.jar
-CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlparser-base-2008.1.1.jar
-CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlbeans-2008.1.1.jar
-CLASSPATH2=$CLASSPATH2:$ADQL/xbean-2.3.0.jar
-CLASSPATH2=$CLASSPATH2:$ADQL/jsr173_1.0_api-2.3.0.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlparser-utils-2009.1.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlparser-base-2009.1.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/astrogrid-adqlbeans-2009.1.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/xmlbeans-2.3.0.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/jsr173_1.0_api-1.0.jar
 CLASSPATH2=$CLASSPATH2:$ADQL/log4j-1.2.8.jar
 CLASSPATH2=$CLASSPATH2:$ADQL/commons-logging-1.0.4.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/xmlbeans-qname-2.3.0.jar
+CLASSPATH2=$CLASSPATH2:$ADQL/stax-api-1.0.1.jar
 CLASSPATH2=$CLASSPATH2:$ADQL/saxon-6.5.5.jar
 
 CLASSPATH=$CLASSPATH2:$CLASSPATH
