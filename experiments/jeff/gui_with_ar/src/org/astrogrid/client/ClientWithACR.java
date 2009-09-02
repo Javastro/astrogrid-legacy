6907a95201eb2be1c65a2aa1d5ce468a948ac8f5
Jeff Lusted <Jeff.Lusted@astrogrid.org>
2009-09-02 11:12:25 +0000

    Seems to be working.

--- experiments/jeff/gui_with_ar/src/org/astrogrid/client/ClientWithACR.java
+++ experiments/jeff/gui_with_ar/src/org/astrogrid/client/ClientWithACR.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,8 +35,6 @@ public class ClientWithACR {
 	
 	private static Log log = LogFactory.getLog( ClientWithACR.class ) ;
 	
-	private SimpleQueryBuilder sqb ;
-	
 	Finder f = new Finder();
 	ACR ar ; 
 	Applications apps ;
@@ -51,111 +50,147 @@ public class ClientWithACR {
 			" And a.psfMag_r>20.0 And a.psfMag_g>0 And a.mode=1 And a.probPSF=1" ;
 	
 	public static void main( String argv[] ) {		
-		ClientWithACR client = new ClientWithACR() ;		
-		client.execute() ;
-	}
-	
-	public ClientWithACR() {
-		sqb = new SimpleQueryBuilder() ;
-		
-		
-		
-	}
-	
-	public void execute() {
-		if( log.isTraceEnabled() ) log.trace( "enter: execute()" ) ;
+		final ClientWithACR client = new ClientWithACR() ;	
+		final SimpleQueryBuilder sqb = new SimpleQueryBuilder() ;
 		try {
-						
+			client.retrieveACR() ;
+			sqb.setACR( client.ar, client.apps, client.rpm, client.sd ) ;
+		}
+		catch( final ACRException ex ) {
+			sqb.setException( ex ) ;
+		}
+		finally {
+			sqb.setVisible( true ) ;
+		}
+	}
+	
+	public ClientWithACR() {}
+	
+	public void retrieveACR() throws ACRException {	
+		try {
+			//
+			// Retrieve the ACR...
+			// I think this needs to be done on the main thread,
+			// before the gui is started,
+			// given the property setting that is going on here.
+			// (But it might be worth experimentation).
 			System.setProperty("java.awt.headless","true");
 			ar = f.find() ;
-			System.setProperty("java.awt.headless","false");
-			
-			apps = (Applications) ar.getService(Applications.class) ;
-			rpm = (RemoteProcessManager) ar.getService( RemoteProcessManager.class ) ;
-			sd = (Shutdown) ar.getService( Shutdown.class ) ;
-			URI appURI = new URI( appIvorn ) ;
-			Document doc = apps.createTemplateDocument( appURI, "ADQL" ) ;
-			Tool tool = (Tool) Unmarshaller.unmarshal( Tool.class, doc ) ;
-			ParameterValue[] pvs = tool.getInput().getParameter() ;
-			pvs[0].setValue( adqlQuery ) ;
-			doc = doc.getImplementation().createDocument( null, null, null ) ;
-			Marshaller.marshal(tool,doc) ;
-//			XMLUtils.PrettyDocumentToStream(doc,System.out);
-			
-			URI jobURI = rpm.submitTo( doc, new URI(serverIvorn) ) ;
+
 			//
-			// Need to set up a listener here...
-			rpm.addRemoteProcessListener( jobURI, new JobListener() ) ;
-						
-			
-			
+			// I would suspect these other artifacts could be retrieved on
+			// a background thread in the gui...
+
+			//
+			// This is the service that will do the query...
+			apps = (Applications) ar.getService(Applications.class) ;
+			//
+			// This is a convenience object for managing the remote process...
+			rpm = (RemoteProcessManager) ar.getService( RemoteProcessManager.class ) ;
+			//
+			// This is to manage shutdown in a controlled fashion...
+			sd = (Shutdown) ar.getService( Shutdown.class ) ;			
 		}
-		catch( URISyntaxException  usx ) {
-			log.error( "", usx ) ;
-		}
-		catch( InvalidArgumentException iax ) {
-			log.error( "", iax ) ;
-		}
-		catch( NotFoundException nfx ) {
-			log.error( "", nfx ) ;
-		}
-		catch( ServiceException sx ) {
-			log.error( "", sx ) ;
-		}
-		catch( ACRException acrx ) {
-			log.error( "", acrx ) ;
-		}
-		catch( ValidationException vx ) {
-			log.error( "", vx ) ;
-		}
-		catch( MarshalException mx ) {
-			log.error( "", mx ) ;
+		finally {
+			System.setProperty("java.awt.headless","false");
 		}
 		
-		if( log.isTraceEnabled() ) log.trace( "exit : execute()" ) ;	
 	}
 	
-	class JobListener implements RemoteProcessListener {
-		
-		//
-		// Question. If this is made more sophisticated,
-		// will thread safety need to be taken into account,
-		// or is this guaranteed by the calling environment?
-		
-		public JobListener() {}
-
-		public void messageReceived( URI arg0, ExecutionMessage message ) {
-			log.info( "Message:\n" + message.getContent() ) ;
-		}
-
-		public void resultsReceived( URI arg0, Map resultsMap ) {
-			log.info( "Results:\n" + resultsMap.toString() ) ;			
-		}
-
-		public void statusChanged( URI jobURI, String status ) {
-
-			try {
-				if( status.equalsIgnoreCase( ExecutionInformation.ERROR ) 
-					||
-					status.equalsIgnoreCase( ExecutionInformation.UNKNOWN )				
-				) {
-					log.error( "Error status on ClientWithACR." + status ) ;
-					rpm.delete( jobURI ) ;
-					sd.reallyHalt() ;
-				}
-				else if ( status.equalsIgnoreCase( ExecutionInformation.COMPLETED ) ) {
-					log.info( "COMPLETED status on ClientWithACR." ) ;
-					log.info( "Results: " + rpm.getResults( jobURI) ) ;
-					rpm.delete( jobURI ) ;
-					sd.reallyHalt() ;
-				}
-			}
-			catch( Exception ex ) {
-				log.error( "", ex ) ;
-			} 
-			
-		}
-		
-	}
+//	public void execute() {
+//		if( log.isTraceEnabled() ) log.trace( "enter: execute()" ) ;
+//		try {
+//						
+//			System.setProperty("java.awt.headless","true");
+//			ar = f.find() ;
+//			System.setProperty("java.awt.headless","false");
+//			
+//			apps = (Applications) ar.getService(Applications.class) ;
+//			rpm = (RemoteProcessManager) ar.getService( RemoteProcessManager.class ) ;
+//			sd = (Shutdown) ar.getService( Shutdown.class ) ;
+//			URI appURI = new URI( appIvorn ) ;
+//			Document doc = apps.createTemplateDocument( appURI, "ADQL" ) ;
+//			Tool tool = (Tool) Unmarshaller.unmarshal( Tool.class, doc ) ;
+//			ParameterValue[] pvs = tool.getInput().getParameter() ;
+//			pvs[0].setValue( adqlQuery ) ;
+//			doc = doc.getImplementation().createDocument( null, null, null ) ;
+//			Marshaller.marshal(tool,doc) ;
+////			XMLUtils.PrettyDocumentToStream(doc,System.out);
+//			
+//			URI jobURI = rpm.submitTo( doc, new URI(serverIvorn) ) ;
+//			//
+//			// Need to set up a listener here...
+//			rpm.addRemoteProcessListener( jobURI, new JobListener() ) ;
+//						
+//			
+//			
+//		}
+//		catch( URISyntaxException  usx ) {
+//			log.error( "", usx ) ;
+//		}
+//		catch( InvalidArgumentException iax ) {
+//			log.error( "", iax ) ;
+//		}
+//		catch( NotFoundException nfx ) {
+//			log.error( "", nfx ) ;
+//		}
+//		catch( ServiceException sx ) {
+//			log.error( "", sx ) ;
+//		}
+//		catch( ACRException acrx ) {
+//			log.error( "", acrx ) ;
+//		}
+//		catch( ValidationException vx ) {
+//			log.error( "", vx ) ;
+//		}
+//		catch( MarshalException mx ) {
+//			log.error( "", mx ) ;
+//		}
+//		
+//		if( log.isTraceEnabled() ) log.trace( "exit : execute()" ) ;	
+//	}
+	
+//	class JobListener implements RemoteProcessListener {
+//		
+//		//
+//		// Question. If this is made more sophisticated,
+//		// will thread safety need to be taken into account,
+//		// or is this guaranteed by the calling environment?
+//		
+//		public JobListener() {}
+//
+//		public void messageReceived( URI arg0, ExecutionMessage message ) {
+//			log.info( "Message:\n" + message.getContent() ) ;
+//		}
+//
+//		public void resultsReceived( URI arg0, Map resultsMap ) {
+//			log.info( "Results:\n" + resultsMap.toString() ) ;			
+//		}
+//
+//		public void statusChanged( URI jobURI, String status ) {
+//
+//			try {
+//				if( status.equalsIgnoreCase( ExecutionInformation.ERROR ) 
+//					||
+//					status.equalsIgnoreCase( ExecutionInformation.UNKNOWN )				
+//				) {
+//					log.error( "Error status on ClientWithACR." + status ) ;
+//					rpm.delete( jobURI ) ;
+//					sd.reallyHalt() ;
+//				}
+//				else if ( status.equalsIgnoreCase( ExecutionInformation.COMPLETED ) ) {
+//					log.info( "COMPLETED status on ClientWithACR." ) ;
+//					log.info( "Results: " + rpm.getResults( jobURI) ) ;
+//					rpm.delete( jobURI ) ;
+//					sd.reallyHalt() ;
+//				}
+//			}
+//			catch( Exception ex ) {
+//				log.error( "", ex ) ;
+//			} 
+//			
+//		}
+//		
+//	}
 
 }
--- experiments/jeff/gui_with_ar/src/org/astrogrid/client/SimpleQueryBuilder.java
+++ experiments/jeff/gui_with_ar/src/org/astrogrid/client/SimpleQueryBuilder.java
@@ -7,6 +7,12 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -14,16 +20,76 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+
+import org.astrogrid.acr.ACRException;
+import org.astrogrid.acr.InvalidArgumentException;
+import org.astrogrid.acr.NotFoundException;
+import org.astrogrid.acr.ServiceException;
+import org.astrogrid.acr.astrogrid.Applications;
+import org.astrogrid.acr.astrogrid.ExecutionInformation;
+import org.astrogrid.acr.astrogrid.ExecutionMessage;
+import org.astrogrid.acr.astrogrid.RemoteProcessListener;
+import org.astrogrid.acr.astrogrid.RemoteProcessManager;
+import org.astrogrid.acr.builtin.ACR;
+import org.astrogrid.acr.builtin.Shutdown;
+import org.astrogrid.applications.beans.v1.parameters.ParameterValue;
+import org.astrogrid.workflow.beans.v1.Tool;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.w3c.dom.Document;
 
 public class SimpleQueryBuilder extends JFrame {
 
 	private static final long serialVersionUID = 7516420866606716212L;
 	
+	private ACR ar ; 
+	private Applications apps ;
+	private RemoteProcessManager rpm ;
+	private Shutdown sd ;
+	private ACRException acrException ;
+	
 	private JTextArea queryText ;
 	private JComboBox collectionCB ;
 	private JButton submitButton, resultsButton, resetButton ;
-
+	
+	String appIvorn = "ivo://wfau.roe.ac.uk/sdssdr5-dsa/dsa/ceaApplication" ;
+	String serverIvorn = "ivo://wfau.roe.ac.uk/sdssdr5-dsa/dsa" ;
+	String adqlQuery = 
+		    "Select Top 100 a.ra, a.dec, a.psfMag_g, a.psfMag_r " +
+		    "From PhotoTag as a " +
+			"Where a.ra>110 And a.ra<230 And a.dec>20 And a.dec<=22 " +
+			" And  (a.psfMag_g-a.psfMag_r <0.4 )  " +
+			" And a.psfMag_r>20.0 And a.psfMag_g>0 And a.mode=1 And a.probPSF=1" ;
+	
+	public synchronized void setACR( ACR ar, Applications apps, RemoteProcessManager rpm, Shutdown sd ) {
+		this.ar = ar ;
+		this.apps = apps ;
+		this.rpm = rpm ;
+		this.sd = sd ;
+		SwingUtilities.invokeLater( new Runnable() {
+			public void run(){
+				queryText.setText( adqlQuery  ) ;
+			}
+		} ) ;
+	}
+	
+	public synchronized void setException( final ACRException acrException ) {
+		this.acrException = acrException ;
+		SwingUtilities.invokeLater( new Runnable() {
+			public void run(){
+				queryText.setText( acrException.getLocalizedMessage() ) ;
+			}
+		} ) ;		
+	} 
+	
+	public synchronized ACRException getException() {
+		return this.acrException ;
+	}
+	
 	public SimpleQueryBuilder() {
 		super( "Simple Query Builder" ) ;
 		setSize( 600, 600 ) ;
@@ -82,9 +148,15 @@ public class SimpleQueryBuilder extends JFrame {
 		c.gridx = 5 ;
 		panel.add( resetButton, c ) ;
 		
-		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ) ;
-		setVisible( true ) ;
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ) ;	
 		
+		//
+		// This could probably be made more subtle...
+		addWindowListener( new WindowAdapter() {
+			public void windowClosing( WindowEvent event ) {
+				sd.reallyHalt() ;
+			}
+		} ) ;
 	}
 	
 	public static void main( String argv[] ) {
@@ -92,9 +164,36 @@ public class SimpleQueryBuilder extends JFrame {
 	}
 	
 	class SubmitActionListener implements ActionListener {
-
+		
 		public void actionPerformed( ActionEvent e ) {
-			queryText.setText( "Just submitted" ) ;	
+			String adqlString = queryText.getText() ;
+			queryText.setText( "Submitting without using background thread..." ) ;	
+			try {	
+				
+				URI appURI = new URI( appIvorn ) ;
+				Document doc = apps.createTemplateDocument( appURI, "ADQL" ) ;
+				Tool tool = (Tool) Unmarshaller.unmarshal( Tool.class, doc ) ;
+				ParameterValue[] pvs = tool.getInput().getParameter() ;
+				pvs[0].setValue( adqlString ) ;
+				doc = doc.getImplementation().createDocument( null, null, null ) ;
+				Marshaller.marshal(tool,doc) ;
+//				XMLUtils.PrettyDocumentToStream(doc,System.out);
+				
+				URI jobURI = rpm.submitTo( doc, new URI(serverIvorn) ) ;
+				//
+				// Need to set up a listener here...
+				rpm.addRemoteProcessListener( jobURI, new JobListener() ) ;
+				queryText.setText( "Submitted successfully!" ) ;							
+			}
+			catch( final Exception  usx ) {
+//				SwingUtilities.invokeLater( new Runnable() {
+//					Exception usx1 = usx ;
+//					public void run() {					
+//						queryText.setText( usx.getLocalizedMessage() ) ;			
+//					}
+//				}) ;	
+				queryText.setText( usx.getLocalizedMessage() ) ;	
+			}	
 			submitButton.setEnabled( false ) ;
 			resultsButton.setEnabled( true ) ;
 		}
@@ -115,6 +214,112 @@ public class SimpleQueryBuilder extends JFrame {
 			queryText.setText( "Type query here..." ) ;	
 			submitButton.setEnabled( true ) ;
 			resultsButton.setEnabled( false ) ;
+		}
+		
+	}
+
+	public synchronized ACR getAr() {
+		return ar;
+	}
+
+	public synchronized Applications getApps() {
+		return apps;
+	}
+
+	public synchronized RemoteProcessManager getRpm() {
+		return rpm;
+	}
+
+	public synchronized Shutdown getSd() {
+		return sd;
+	}
+	
+	class JobListener implements RemoteProcessListener {
+		
+		//
+		// Question. If this is made more sophisticated,
+		// will thread safety need to be taken into account,
+		// or is this guaranteed by the calling environment?
+		
+		public JobListener() {}
+
+		public void messageReceived( URI arg0, final ExecutionMessage message ) {
+//			queryText.setText( queryText.getText() + "\n" + message.getContent() ) ;
+			SwingUtilities.invokeLater( new Runnable() {
+				public void run() {
+					queryText.setText( queryText.getText() + "\n" + message.getContent() ) ;
+				}
+			}) ;
+		}
+
+		public void resultsReceived( URI arg0, final Map resultsMap ) {	
+//			Map resultsMap1 = resultsMap ;
+//			Iterator it = resultsMap.entrySet().iterator() ;
+//			if( it.hasNext() ) {
+//				queryText.setText( it.next().toString() ) ;
+//			}				
+			SwingUtilities.invokeLater( new Runnable() {
+				public void run() {
+					Map resultsMap1 = resultsMap ;
+					Iterator it = resultsMap.entrySet().iterator() ;
+					if( it.hasNext() ) {
+						queryText.setText( it.next().toString() ) ;
+					}				
+				}
+			}) ;		
+		}
+		
+		public void statusChanged( final URI jobURI, final String status ) {
+
+			try {
+				if( status.equalsIgnoreCase( ExecutionInformation.ERROR ) 
+					||
+					status.equalsIgnoreCase( ExecutionInformation.UNKNOWN )				
+				) {
+					SwingUtilities.invokeLater( new Runnable() {
+						public void run() {
+							String status1 = status ;
+							queryText.setText( status ) ;
+							try {
+								rpm.delete( jobURI ) ;
+							}
+							catch( Exception ex ) {
+								;
+							}						
+						}
+					}) ;
+//					queryText.setText( status ) ;
+					
+				}
+				else if ( status.equalsIgnoreCase( ExecutionInformation.COMPLETED ) ) {
+					SwingUtilities.invokeLater( new Runnable() {
+						public void run() {
+							try {
+								queryText.setText(  rpm.getResults( jobURI).toString() ) ;
+							}
+							catch( Exception ex ) {
+								queryText.setText(  ex.getLocalizedMessage() ) ;
+							}
+						}
+					}) ;
+//					try {
+//						queryText.setText(  rpm.getResults( jobURI).toString() ) ;
+//					}
+//					catch( Exception ex ) {
+//						queryText.setText(  ex.getLocalizedMessage() ) ;
+//					}
+				}
+			}
+			catch( final Exception ex ) {
+				SwingUtilities.invokeLater( new Runnable() {				
+					public void run() {
+						Exception ex1 = ex ;
+						queryText.setText( ex1.getLocalizedMessage() ) ;						
+					}
+				} ) ;
+//				queryText.setText( ex.getLocalizedMessage() ) ;	
+			} 
+			
 		}
 		
 	}
