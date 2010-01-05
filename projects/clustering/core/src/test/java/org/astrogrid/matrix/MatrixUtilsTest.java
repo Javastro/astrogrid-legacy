1a23fbb037e393ab255bce4b03c0dec409d2fbc4
Paul Harrison <paul.harrison@manchester.ac.uk>
2010-01-05 21:26:09 +0000

    more tests

--- projects/clustering/core/src/test/java/org/astrogrid/matrix/MatrixUtilsTest.java
+++ projects/clustering/core/src/test/java/org/astrogrid/matrix/MatrixUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * $Id: MatrixUtilsTest.java,v 1.2 2009/09/08 19:22:21 pah Exp $
+ * $Id: MatrixUtilsTest.java,v 1.3 2010/01/05 21:26:09 pah Exp $
  * 
  * Created on 5 Dec 2008 by Paul Harrison (paul.harrison@manchester.ac.uk)
  * Copyright 2008 Astrogrid. All rights reserved.
@@ -27,6 +27,7 @@ import org.astrogrid.cluster.cluster.CovarianceKind;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.astrogrid.matrix.MatrixUtils.*;
 
 public class MatrixUtilsTest extends AbstractMatrixTest{
     
@@ -85,12 +86,82 @@ public class MatrixUtilsTest extends AbstractMatrixTest{
         assertTrue("determinant error too much ", Math.abs(0.06218776 - result) < 1e-7);
         
     }
+    
+    @Test
+    public void testVprod(){
+        Vector a = new DenseVector(new double[]{1,2,3});
+        Vector b = new DenseVector(new double[]{1,2,3});
+        
+        Matrix result = vprod(a,b);
+        Matrix expected = new AGDenseMatrix(new double[][]{{1,2,3},{2,4,6},{3,6,9}});
+        assertResult(result, expected, "vprod function");
+      
+        
+    }
 
-}
+    @Test
+    public void testVectoradd(){
+        Vector a = new DenseVector(new double[]{1,2,3});
+        Vector b = new DenseVector(new double[]{1,2,3});
+        a.add(b);
+        Vector expected = new DenseVector(new double[]{2,4,6});
+        expected.add(-1.0, a);
+        double norm = expected.norm(Vector.Norm.Infinity);
+        if(Math.abs(norm)> 1e-8){
+            fail("sum function does not return correct value");
+        }
+   }
+    
+    @Test
+    public void testColumnDelete() {
+        Matrix result= new AGDenseMatrix(new double[][]{{1,2,3,4},{1,2,3,4},{1,2,3,4}});
+        result.delCol(2);
+        Matrix expected = new AGDenseMatrix(new double[][]{{1,2,4},{1,2,4},{1,2,4}});
+        assertResult(result, expected, "column delete function");
+    }
+    
+    @Test
+    public void testRowDelete() {
+        Matrix result= new AGDenseMatrix(new double[][]{{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}});
+        result.delRow(2);
+        Matrix expected = new AGDenseMatrix(new double[][]{{1,1,1,1},{2,2,2,2},{4,4,4,4}});
+        assertResult(result, expected, "column delete function");
+    }
+    
+    @Test 
+    public void testVecElemDelete() {
+        DenseVector v = new DenseVector(new double[]{1,2,3,4});
+        Vector result = delElement(v, 0);
+        Vector expected = new DenseVector(new double[]{2,3,4});
+        expected.add(-1.0, result);  
+        double norm = expected.norm(Vector.Norm.Infinity);
+        if(Math.abs(norm)> 1e-8){
+            fail("delete function does not return correct value");
+        }
+         result = delElement(v, 1);
+         expected = new DenseVector(new double[]{1,3,4});
+        expected.add(-1.0, result);  
+        norm = expected.norm(Vector.Norm.Infinity);
+        if(Math.abs(norm)> 1e-8){
+            fail("delete function does not return correct value");
+        }
+        result = delElement(v, 3);
+        expected = new DenseVector(new double[]{1,2,3});
+       expected.add(-1.0, result);  
+       norm = expected.norm(Vector.Norm.Infinity);
+       if(Math.abs(norm)> 1e-8){
+           fail("delete function does not return correct value");
+       }
+
+    }
+ }
 
 
 /*
  * $Log: MatrixUtilsTest.java,v $
+ * Revision 1.3  2010/01/05 21:26:09  pah
+ * more tests
+ *
  * Revision 1.2  2009/09/08 19:22:21  pah
  * added new determinant test to check against sign changes
  *
